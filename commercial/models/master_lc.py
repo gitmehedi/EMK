6c13aa145e1f356ea1bd5a505beb606a57a34a9a
Mehedi Hasan <md.mehedi.info@gmail.com>
2016-04-25 10:41:06 +0600

    [MOD] Master LC form correction

--- commercial/models/master_lc.py
+++ commercial/models/master_lc.py
@@ -9,49 +9,46 @@ class MasterLC(models.Model):
     
     _name = 'master.lc'
     
-    # Buyer Work Order fields
+    """ Buyer Work Order fields """
     name = fields.Char(string="Serial", size=30, readonly=True)
     mlc_code = fields.Char(string='Code')
     
     lc_no = fields.Char(string="LC No", size=30, required=True)
     
-    lc_open_date = fields.Date(string="LC Opening Date", required=True, default=date.today().strftime('%Y-%m-%d'))
-    lc_lien_date = fields.Date(string="LC Lien Date", required=True, default=date.today().strftime('%Y-%m-%d'))
-    lc_expiry_date = fields.Date(string="LC Expiry Date", required=True, default=date.today().strftime('%Y-%m-%d'))
+    lc_open_date = fields.Date(string="LC Opening Date", default=date.today().strftime('%Y-%m-%d'), required=True)
+    lc_lien_date = fields.Date(string="LC Lien Date", default=date.today().strftime('%Y-%m-%d'))
+    lc_expiry_date = fields.Date(string="LC Expiry Date", default=date.today().strftime('%Y-%m-%d'))
     
-    lc_value = fields.Float(string="LC Value", digits=(15, 2))
-    tolerance = fields.Float(string='Tolerance', digits=(15, 2), required=True)
-    tenor = fields.Integer(string='Tenor', required=True)
-    
+    lc_value = fields.Float(string="LC Value", digits=(15, 2), required=True)
+    tolerance = fields.Float(string='Tolerance (%)', digits=(15, 2))
     inco_term_place = fields.Char(string="Inco Term Place", size=30)
-    
     bb_lc = fields.Integer(string="BB LC Limit", default=False)
     
     remarks = fields.Text(string='Remarks')
     
-    # Relationship fields
+    """ Relationship fields """
     buyer_id = fields.Many2one('res.partner', string="Buyer", required=True,
                                domain=[('customer', '=', 'True')])
    
     lc_open_bank_id = fields.Many2one('res.bank', string="LC Opening Bank", required=True)
-    lc_lien_bank_id = fields.Many2one('res.bank', string="LC Lien Bank", required=True)
-    lc_advising_bank_id = fields.Many2one('res.bank', string="Advising Bank", required=True)
+    lc_lien_bank_id = fields.Many2one('res.bank', string="LC Lien Bank")
+    lc_advising_bank_id = fields.Many2one('res.bank', string="Advising Bank")
     receive_user_id = fields.Many2one('res.bank', string="Receive Through")
-    currency = fields.Many2one('res.currency', string="Currency", required=True)
+    currency_id = fields.Many2one('res.currency', string="Currency", required=True)
     
-    payment_term = fields.Selection([("10", "10 Days"), ("15", "15 Days"), ("30", "30 Days")], string='Payment Terms', required=True)
-    inco_term = fields.Many2one('commercial.term', string="Inco Term", required=True)
+    payment_term_id = fields.Many2one('account.payment.term', string="Payment Terms/Tenor",
+                                      readonly=True, states={'draft':[('readonly', False)]})
+    inco_term_id = fields.Many2one('stock.incoterms', string="Inco Term")
     
     state = fields.Selection([('draft', "Draft"), ('confirm', "Confirm")], default='draft')
     
-    # One2many relationships
+    """ One2many relationships """
     master_lc_details_ids = fields.One2many('master.lc.details', 'master_lc_id')
     
     allow_partial_shipment = fields.Boolean(string="Allow Partial Shipment")
     allow_trans_shipment = fields.Boolean(string="Allow Trans Shipment")
-    lc_close = fields.Boolean(string="LC Close")
-    
-    # All kinds of validation message
+
+    """ All kinds of validation message """
     @api.multi
     def _validate_data(self, value):
         msg , filterInt, filterNum, filterChar = {}, {}, {}, {}
@@ -70,7 +67,7 @@ class MasterLC(models.Model):
         return True
     
     
-    # All function which process data and operation
+    """ All function which process data and operation """
     
     @api.model
     def create(self, vals):
