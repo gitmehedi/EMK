3915c4d10a6c2874b68e633e447373a21e983d37
Mehedi Hasan <md.mehedi.info@gmail.com>
2016-04-26 13:02:00 +0600

    [BF] Fix mc_ids bug on 'BOM Consumption Form'

---
+++ commercial/wizard/master_lc_wizard.py
@@ -0,0 +1,27 @@
+from openerp import api, exceptions, fields, models
+from openerp.addons.helper import validator
+
+class MasterLCWizard(models.TransientModel):
+    """
+    Product Style Amendment Wizard
+    """
+    _name = "master.lc.wizard"
+
+    @api.multi
+    def amaendment_copy(self, context=None):
+        
+        obj = self.env['master.lc'].browse(context['active_id'])
+        res = obj.extend_copy(context)
+        
+        
+        return {
+            'view_type': 'form',
+            'view_mode': 'form',
+            'src_model': 'master.lc',
+            'res_model': 'master.lc',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'res_id': res.id
+        }
+       
+       
