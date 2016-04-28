948e32f8a517530c0065456d3dd66d5adfa2755c
Saimon <shajal8015@gmail.com>
2016-04-28 13:07:49 +0600

    update stock reservation merchandising

--- stock_reservation/__openerp__.py
+++ stock_reservation/__openerp__.py
@@ -16,6 +16,8 @@
 #
 #    You should have received a copy of the Genweb2 Ltd
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#--------------- Configuration Setup----------------------
 #    Create a Picking type (Type of Operation) where name will be "Stock Reservation"
 #    go to setting menu and click warehouse menu. 
 #    checked 
