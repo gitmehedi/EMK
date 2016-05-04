ffc3aceb2d2224712d9dc95f1d2012c93abd2ce5
mdmithuncse <md.mithuncse@yahoo.com>
2016-05-04 11:01:11 +0600

    Modify customer payments and journal search popup

--- account_check_deposit_extend/models/account_move_line_extend.py
+++ account_check_deposit_extend/models/account_move_line_extend.py
@@ -3,9 +3,15 @@ from openerp import models, fields, api
 class AccountMoveLineExtends(models.Model):
     _inherit = 'account.move.line'
     
-    #Database fields
-    cheque_received_date = fields.Date(string='Cheque Received Date', default=fields.Date.today(), help='Please enter cheque received date.')
-    cheque_number = fields.Char(string='Cheque Number', size=50,  help='Please enter cheque number.')
-    posting_date = fields.Date(string='Cheque Posting Date', default=fields.Date.today(), help='Please enter cheque posting date.')
-    issuing_bank = fields.Many2one('res.bank', ondelete='set null', string='Name of Bank') 
-    bank_branch_name = fields.Char(string='Branch Name', size=100, help='Please enter branch name.')
+    cheque_date = fields.Date(compute='get_computed_columns', store=False,string='Cheque Date')
+    cheque_number = fields.Char(compute='get_computed_columns', store=False,string='Cheque Number')
+    issuing_bank = fields.Char(compute='get_computed_columns', store=False,string='Name of Bank')
+    bank_branch_name = fields.Char(compute='get_computed_columns', store=False,string='Branch Name')
+    is_depositedtobank=fields.Boolean(string='Is Deposited To Bank?',default=False)
+    
+    def get_computed_columns(self):
+        res =self.env['account.voucher'].search([('move_id', '=', self.move_id.id)])
+        self.cheque_date=res[0].cheque_date
+        self.cheque_number=res[0].cheque_number
+        self.issuing_bank=res[0].issuing_bank
+        self.bank_branch_name=res[0].bank_branch_name
