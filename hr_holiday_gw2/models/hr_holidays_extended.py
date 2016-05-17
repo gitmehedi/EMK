9c73e27c6e0e516a937f9de44019c39be2d6650a
Matiar Rahman <matiar.rahman@gmail.com>
2016-05-17 10:56:22 +0600

    Bug Fix: A team lead can't submit his leave application.

--- hr_holiday_gw2/models/hr_holidays_extended.py
+++ hr_holiday_gw2/models/hr_holidays_extended.py
@@ -4,6 +4,12 @@ class HrHolidaysExtended(models.Model):
        _inherit = 'hr.holidays'
        
        officer_id = fields.Many2one('res.users', string='HR Officer', compute = '_get_officer_id', store=True)
+       state = fields.Selection([('draft', 'To Submit'), ('cancel', 'Cancelled'),('confirm', 'To Approve'), ('refuse', 'Refused'), ('validate1', 'Second Approval'), ('validate', 'Approved')],
+            'Status', readonly=True, copy=False, default='draft',
+            help='The status is set to \'To Submit\', when a holiday request is created.\
+            \nThe status is \'To Approve\', when holiday request is confirmed by user.\
+            \nThe status is \'Refused\', when holiday request is refused by manager.\
+            \nThe status is \'Approved\', when holiday request is approved by manager.')
        
        @api.depends('employee_id')
        def _get_officer_id(self):
@@ -11,5 +17,5 @@ class HrHolidaysExtended(models.Model):
            if(self.employee_id.parent_id):
                print "test......................................................................................................."
                self.officer_id = self.employee_id.parent_id.user_id.id
-           
-     
+        
+        
