87337e866d7cad02800cdd6515d655ea00d89696
Saimon <shajal8015@gmail.com>
2016-05-24 13:25:31 +0600

    Good Receive, QC Receive, QC Pass Menu done in inventory

--- stock_extend/models/inherited_stock_picking.py
+++ stock_extend/models/inherited_stock_picking.py
@@ -20,59 +20,11 @@ class InheritedStockPicking(models.Model):
 	qc_receive_flag = fields.Boolean(string='QC Receive', default=False)
 	qc_pass_flag = fields.Boolean(string='QC Pass', default=False)
 	
-'''
-	def create(self, cr, user, vals, context=None):
-	    context = context or {}
-	    print "_____pick______",self,"___vals_--",vals
-	    if ('name' not in vals) or (vals.get('name') in ('/', False)):
-	        ptype_id = vals.get('picking_type_id', context.get('default_picking_type_id', False))
-	        sequence_id = self.pool.get('stock.picking.type').browse(cr, user, ptype_id, context=context).sequence_id.id
-	        vals['name'] = self.pool.get('ir.sequence').next_by_id(cr, user, sequence_id, context=context)
-	    # As the on_change in one2many list is WIP, we will overwrite the locations on the stock moves here
-	    # As it is a create the format will be a list of (0, 0, dict)
-	    if vals.get('move_lines') and vals.get('location_id') and vals.get('location_dest_id'):
-	        for move in vals['move_lines']:
-	            if len(move) == 3:
-	                move[2]['location_id'] = vals['location_id']
-	                move[2]['location_dest_id'] = vals['location_dest_id']
-	    return super(InheritedStockPicking, self).create(cr, user, vals, context)
-	'''
 	   
 class InheritedStockMove(models.Model):
     _inherit = 'stock.move'
 	
 
-    def check_recompute_pack_op(self, cr, uid, ids, context=None):
-        pickings = list(set([x.picking_id for x in self.browse(cr, uid, ids, context=context) if x.picking_id]))
-        pickings_partial = []
-        pickings_write = []
-        pick_obj = self.pool['stock.picking']
-        for pick in pickings:
-            if pick.state in ('waiting', 'confirmed'): #In case of 'all at once' delivery method it should not prepare pack operations
-                continue
-            # Check if someone was treating the picking already
-            if not any([x.qty_done > 0 for x in pick.pack_operation_ids]):
-                pickings_partial.append(pick.id)
-            else:
-                pickings_write.append(pick.id)
-    # 		print '+++++++++++++++--___pick.id___-----',pick.id
-    # 		print '+++++++++++++++--___pick.state___-----',pick.state
-    # 		print '+++++++++++++++--___good_receive_flag___-----',pick.good_receive_flag
-    # 		print '+++++++++++++++--___pick.qc_receive_flag___-----',pick.qc_receive_flag
-    # 		print '+++++++++++++++--___pick.qc_pass_flag___-----',pick.qc_pass_flag
-    # 		if (pick.good_receive_flag == False) and (pick.qc_receive_flag == False) and (pick.state == "assigned" or pick.state == "partially_available"):
-    # 			pick_obj.write(cr, uid, pick.id, {'qc_receive_flag': True}, context=context)
-    # 		
-    # 		elif (pick.good_receive_flag == False) and (pick.qc_receive_flag == True) and (pick.qc_pass_flag == False) and (pick.qc_pass_flag == False)  and (pick.state == "assigned" or pick.state == "partially_available"):
-    # 				pick_obj.write(cr, uid, pick.id, {'qc_pass_flag': True}, context=context)
-    	
-    	if pickings_partial:
-    	    pick_obj.do_prepare_partial(cr, uid, pickings_partial, context=context)
-    	if pickings_write:
-    	    pick_obj.write(cr, uid, pickings_write, {'recompute_pack_op': True}, context=context)
-    
-        return True
-    
     @api.cr_uid_ids_context
     def _picking_assign(self, cr, uid, move_ids, context=None):
         """Try to assign the moves to an existing picking
@@ -108,7 +60,6 @@ class InheritPurchaseOrder(models.Model):
 	
 	@api.model
 	def _prepare_picking(self):
-		print '########____prepare_picking_____######'
 		if not self.group_id:
 		    self.group_id = self.group_id.create({
 		        'name': self.name,
