4b44a5ff0b15d476c340c036d35d069ce63f0a1d
RipanBasak <kumar.ripan@genweb2.com>
2016-05-30 17:48:38 +0600

    [hr_exit]  creation new module for hr exit management process

---
+++ hr_exit/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'HR Exit Mangement Process',
+    'author':  'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Human Resource',
+    'data': [
+
+             'security/ir_rule.xml'
+    ],
+    'depends': ['hr'],
+    'summary': 'HR Exit process management',
+    'category': 'HR Exit Management',
+    'summary': 'HR Exit process management ',
+    'description': 'HR holiday extension '
+}
+
+
+
