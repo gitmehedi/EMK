7c9206b2924a4d6241ebf99706ead08d1e7c1ad0
Md.Mithun <md.mithuncse@yahoo.com>
2016-06-06 18:36:03 +0600

    [hr_exit]Add employee exit search.

--- hr_exit/__openerp__.py
+++ hr_exit/__openerp__.py
@@ -10,6 +10,9 @@
              'views/checklist_item_view.xml',
              'views/configure_checklists_view.xml',
              'views/employee_exit_req_view.xml',
+             'wizard/employee_exit_search_popup.xml',
+             #'report/report_employee_exit.xml',
+             #'report/hr_exit_report.xml'
              #'security/emp_exit_security.xml'
     ],
     'depends': ['hr'],
