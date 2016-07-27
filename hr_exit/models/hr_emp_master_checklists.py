75fe8332238022a748d6e430ed79b634232f59f7
RipanBasak <kumar.ripan@genweb2.com>
2016-07-27 14:56:26 +0600

    [Hr_exit] remove unnecessay  comment from master_checklist file

--- hr_exit/models/hr_emp_master_checklists.py
+++ hr_exit/models/hr_emp_master_checklists.py
@@ -4,14 +4,8 @@ from openerp import models, fields, api, exceptions
 class ConfigureEmpChecklist(models.Model):
     _name = "hr.emp.master.checklists"
 
-
-    # employee_id = fields.Many2one('hr.employee', select=True, invisible=False,
-    #                                   default=lambda self: self.employee_gets())
     employee_id = fields.Many2one('hr.employee', select=True, invisible=False,  default=lambda self: self._employee_gets())
-    # emp_name = fields.Many2one('hr.employee', string='Employee Name', help='Please enter responsible user name.')
     _rec_name = 'employee_id'
-    # department = fields.Many2one('hr.department', ondelete='set null', string='Department', related='employee_id.department_id',
-    #                              help='Please enter responsible department name.')
     department_id = fields.Many2one('hr.department', string='Department', related='employee_id.department_id')
     state = fields.Selection([('draft', 'Draft'), ('done', 'Done'), ('send', 'Send')], readonly=True, copy=False,
                              default='draft')
@@ -46,38 +40,4 @@ class ConfigureEmpChecklist(models.Model):
         return 1
 
 
-        #
-        # _rec_name = 'employee_id'
-        # employee_id = fields.Many2one('hr.employee', select=True, invisible=False,
-        #                               default=lambda self: self._employee_gets())
-        # check_list_type_id = fields.Many2one('hr.exit.checklist.type', string="Checklist Types")
-        # check_list_item = fields.Many2one('hr.exit.checklist.item', string="Checklist Types")
-        #
-        # # Relational fields
-        # checklist_name = fields.Char(string='Name', size=100)
-        # checklists_id = fields.Many2one('hr.exit.configure.checklists', string="Checklist")
-        # status = fields.Boolean(string='Status', default= False)
-        # state = fields.Selection([('draft', 'To Submit'),('validate', 'Approved')])
-        # emp_name=fields.Many2one('hr.employee',string='Employee Name', help='Please enter responsible user name.')
-        #
-        # department=fields.Many2one('hr.department', ondelete='set null', string='Department', help='Please enter responsible department name.')
-        #
-        # #emp_checklist_ids = fields.One2many('hr.exit.configure.checklists.line','check_list_emp_id')
-        # check_list_line_ids = fields.One2many('hr.exit.configure.checklists.line','check_list_line_id', store=True)
-        #
-        #
-        # @api.multi
-        # def exit_submit(self):
-        #     return 1
-        #
-        #
-        @api.multi
-        def employee_gets(self):
-            # emp_id = context.get('default_employee_id', False)
-            # if emp_id:
-            #     return emp_id
-            ids = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)])
-            if ids:
-                return ids[0]
-            return False
 
