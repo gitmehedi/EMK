18208c2b913b75622ffe2aa8ca9792032dafe986
Saimon <shajal8015@gmail.com>
2016-07-27 19:35:25 +0600

    update map account Journal template

--- account_journal_template/models/map_journal_acc_journal_template.py
+++ account_journal_template/models/map_journal_acc_journal_template.py
@@ -8,3 +8,4 @@ class MapJournalAccJournalTemplate(models.Model):
 	journal_id = fields.Many2one('account.journal', required=True, string='Journal')
 	template_id = fields.Many2one('acc.journal.template', required=True, string='Journal Template')
 	active = fields.Boolean(default=True)
+	_rec_name= "id"
