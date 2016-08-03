536f4f4c476dd77bf97abcdfe38a488ff26670ba
Saimon <shajal8015@gmail.com>
2016-08-03 11:48:52 +0600

    update Account Journal Template

--- account_journal_template/models/acc_journal_template.py
+++ account_journal_template/models/acc_journal_template.py
@@ -1,4 +1,5 @@
-from openerp import api, fields, models
+from openerp import api, exceptions, fields, models
+from openerp.addons.helper import validator
 
 class AccJournalTemplate(models.Model):
 	_name = "acc.journal.template"
@@ -7,3 +8,47 @@ class AccJournalTemplate(models.Model):
 	name = fields.Char("Template", required=True)
 	active = fields.Boolean(default=True)
 	line_ids = fields.One2many('acc.journal.template.line', 'acc_journal_template_id', string='Template Line')
+
+	@api.multi
+	def check_duplicate(self, value):
+	    records = self.search_read([],['name'])
+	    check_val = {}
+	    field_val = ''
+	    field_name = ''
+	    if(value.get('name', False)):
+	        check_val['name'] = (value.get('name', False))
+	    for  value in check_val:
+	        field_val =  check_val[value]
+	        field_name =  value
+	    if field_name != "" and   field_val != "":
+	        check_duplicate = validator._check_duplicate_data(self, field_val, field_name, records, validator.msg)
+	        validator.generate_validation_msg(check_duplicate, "")
+	    return True
+	
+	
+	_sql_constraints = [
+	    ('_check_name_uniq', 'unique(name)', "Name already exists!"),
+	]
+	
+	@api.model
+	def create(self, vals):
+	    self.check_duplicate(vals)
+	    name_value = vals.get('name', False)
+	    if name_value:
+	        vals['name'] = name_value.strip()
+	    return super(AccJournalTemplate, self).create(vals)
+	
+	@api.multi
+	def write(self, vals):
+	    self.check_duplicate(vals)
+	    name_value = vals.get('name', False)
+	    if name_value:
+	        vals['name'] = name_value.strip()
+	    return super(AccJournalTemplate, self).write(vals)
+	
+	@api.multi
+	def copy(self, vals):
+		name_vale=('Copy' or '')+' '+(self.name or '')
+		if self.name:
+			vals['name'] = name_vale
+		return super(AccJournalTemplate, self).copy(vals)
--- account_journal_template/models/map_journal_acc_journal_template.py
+++ account_journal_template/models/map_journal_acc_journal_template.py
@@ -1,4 +1,5 @@
-from openerp import api, fields, models
+from openerp import api, exceptions, fields, models
+from openerp.exceptions import ValidationError
 
 class MapJournalAccJournalTemplate(models.Model):
 	_name = "map.journal.acc.journal.template"
@@ -8,4 +9,9 @@ class MapJournalAccJournalTemplate(models.Model):
 	journal_id = fields.Many2one('account.journal', required=True, string='Journal')
 	template_id = fields.Many2one('acc.journal.template', required=True, string='Journal Template')
 	active = fields.Boolean(default=True)
+	dup_flag = fields.Boolean(default=False)
 	_rec_name= "id"
+
+	_sql_constraints = [
+        ('charges_key_unique', 'unique (journal_id, template_id)', 'This record is already exists.')
+    ]
