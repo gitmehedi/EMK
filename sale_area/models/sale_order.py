736f940c53ebb34724e122cb2967168f199197ac
root <matiar.rahman@gmail.com>
2016-08-16 15:32:53 +0600

    added a new module named sale-area

---
+++ sale_area/models/sale_order.py
@@ -0,0 +1,25 @@
+from openerp.osv import osv, fields
+
+class sale_order(osv.osv):
+    '''
+    Sale Order
+    '''
+    _inherit = 'sale.order'    
+
+    _columns = {
+        'area_id': fields.many2one('sale.area','Sales Area', select=True),
+    }
+    
+    def _get_default_sale_area(self, cr, uid, ids, context=None):
+        user_pool = self.pool.get('res.users')
+        user = user_pool.browse(cr, uid, uid, context=context)
+        if user and user.area_id:
+            return user.area_id.id
+        else:
+            return False
+    
+    _defaults = {
+        'area_id': _get_default_sale_area,
+    }
+            
+sale_order()
