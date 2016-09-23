3a1a8d5b055acba2e928e3d7c3f242cdc81a670a
root <matiar.rahman@gmail.com>
2016-09-23 21:00:31 +0600

    remove unused modules

--- cg_sales/__init__.py
+++
@@ -1,4 +0,0 @@
-import models
-import wizard
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- cg_sales/models/purchase_order.py
+++
@@ -1,7 +0,0 @@
-from openerp.osv import osv
-from openerp import api, fields, models
-
-class purchaseorderline(models.Model):
-    _inherit = 'purchase.order.line'
-    
-    remark = fields.Char( string='Remarks')
