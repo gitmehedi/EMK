dfaee7e8aa4346d190054c29ed69842772b3b70f
Mehedi Hasan <md.mehedi.info@gmail.com>
2016-11-02 18:51:30 +0600

    [RF] Refactor code of 'Goods Receive Matrix'.

--- purchase_order_pebbles/models/goods_receive_matrix_line.py
+++ purchase_order_pebbles/models/goods_receive_matrix_line.py
@@ -1,20 +1,15 @@
 from openerp import api, exceptions, fields, models
-from datetime import date
-from openerp.exceptions import Warning
 
 class GoodsReceiveMatrixLine(models.Model):
     _name = 'goods.receive.matrix.line'
 
-    product_id = fields.Many2one('product.product', 'Product')
-    product_qty = fields.Float(digits=(5, 2), string='Qty', size=20)
-    matrix_id = fields.Many2one('goods.receive.matrix', 'Product Matrix')
-#     color_attribute_id = fields.Many2one('product.attribute', string='Color Attribute')
-#     color_value_id = fields.Many2one('product.attribute.value', string='Color')
-#     size_attribute_id = fields.Many2one('product.attribute', string='Size Attribute')
-#     size_value_id = fields.Many2one('product.attribute.value', string='Size')
-    product_uom_id = fields.Integer(string='UOM', size=40)
-    
-    size_variant_id=fields.Many2one(comodel_name='product.custom.variant')
-    other_variant_id=fields.Many2one(comodel_name='product.custom.variant')
+    """ Required and Optional Fields """
 
-    
+    product_uom_id = fields.Integer(string='UOM', size=40)
+    product_qty = fields.Float(digits=(5, 2), string='Qty', size=20)
+
+    """ Relational Fields """
+    product_id = fields.Many2one('product.product', string="Product")
+    matrix_id = fields.Many2one('goods.receive.matrix', string="Product Matrix")
+    size_variant_id=fields.Many2one('product.attribute.value')
+    color_variant_id=fields.Many2one('product.attribute.value')
--- stock_distribution_matrix/models/inventory_distribution_to_shop.py
+++ stock_distribution_matrix/models/inventory_distribution_to_shop.py
@@ -1,51 +1,56 @@
 from openerp import api, fields, models
 from lxml import etree
+
+
 class inventory_distribution_to_shop(models.Model):
     _name = 'stock.distribution.to.shop'
-    _rec_name = 'product_tmp_id' 
-    
+    _rec_name = 'product_tmp_id'
+
     @api.model
     def _Filter_DefaultWareHouse(self):
         return [('id', '=', self.env.ref('stock.warehouse0').id)]
 
-    
     def _Default_WareHouse_Id(self):
         res = self.env['stock.warehouse'].search([('id', '=', self.env.ref('stock.warehouse0').id)])
         return res and res[0] or False
- 
-    stock_distribution_lines_ids = fields.One2many('stock.distribution.to.shop.line', 'stock_distributions_id',states={'confirm':[('readonly', True)]})
-    
-    product_tmp_id = fields.Many2one('product.template',string='Product Template')
+
+    # models fields
+    product_tmp_id = fields.Many2one('product.template', string='Product Template')
     received_qty = fields.Float(string='Received Qty')
-    distribute_qty = fields.Float(string='Distribute Qty',readonly=True)
-    remain_qty = fields.Float(string='Remain Qty',compute='_get_remain_quntity',store=True)
-    
-#     warehoue_id = fields.Many2one('stock.warehouse',string='Warehouse',domain=_Filter_DefaultWareHouse,default=_Default_WareHouse_Id)
-    warehoue_id = fields.Many2one('stock.warehouse',string='Warehouse',default=_Default_WareHouse_Id)
-    
-    name=fields.Char('Distribution Ref#',readonly=True)
+    distribute_qty = fields.Float(string='Distribute Qty', readonly=True)
+    remain_qty = fields.Float(string='Remain Qty', compute='_get_remain_quntity', store=True)
+    name = fields.Char('Distribution Ref#', readonly=True)
     receive_date = fields.Date(string='Received Date', default=fields.Date.today, required=True, readonly=True)
     notification_date = fields.Date(string='Notification Date', default=fields.Date.today, required=True, readonly=True)
-    
-    
+
+    # relational fields
+    stock_distribution_lines_ids = fields.One2many('stock.distribution.to.shop.line', 'stock_distributions_id',
+                                                   states={'confirm': [('readonly', True)]})
+    warehoue_id = fields.Many2one('stock.warehouse', string='Warehouse', default=_Default_WareHouse_Id)
+
     state = fields.Selection([
-               ('draft','Waiting'),
-               ('generate','Generated'),
-               ('confirm','Confirm'),
-               ('transfer','Transfer'),
-           ], string='Status', index=True, readonly=True, default='draft',copy=False,states={'draft':[('readonly', False)]})
+        ('draft', 'Waiting'),
+        ('generate', 'Generated'),
+        ('confirm', 'Confirm'),
+        ('transfer', 'Transfer'),
+    ], string='Status', index=True, readonly=True, default='draft', copy=False, states={'draft': [('readonly', False)]})
+
+
     
 
 
     @api.one
     def action_generate_distribution_matrix(self):
-        self.state = 'generate'   
         """
         Generate Matrix Use Case
-        -------------------------------------------- 
-            1. if User select Warehouse
-            2. Before not Generate (matrix generate once a time) 
+        --------------------------------------------
+        1. if User select Warehouse
+        2. Before not Generate (matrix generate once a time)
+
         """
+
+        self.state = 'generate'
+
         if self.warehoue_id and len(self.stock_distribution_lines_ids)==0:
             vals = []
             stock_quant_obj=self.pool['stock.quant'].read_group(self.env.cr,self.env.uid,
@@ -54,10 +59,6 @@ class inventory_distribution_to_shop(models.Model):
                                        groupby=[('product_id')]
                                        )
 
-            print vals
-            print "--------------- Start ------------------"
-            print stock_quant_obj
-            print "--------------- End ------------------"
             for product in stock_quant_obj:
                 product_id=product['product_id'][0]
                 on_hand_qty=product['qty']
@@ -73,7 +74,7 @@ class inventory_distribution_to_shop(models.Model):
                         distribute_line['pos_shop_id'] =pos_shop.id
                         distribute_line['distribute_qty'] =0
                         vals.append(distribute_line)
-            print vals
+
             for distribute_line in vals:
                     self.stock_distribution_lines_ids.create(distribute_line)
                      
@@ -91,14 +92,13 @@ class inventory_distribution_to_shop(models.Model):
 
     @api.multi
     def write(self, vals):
-        print ""
-        res=super(inventory_distribution_to_shop, self).write(vals)
-        
-        return res
+        return super(inventory_distribution_to_shop, self).write(vals)
+
     
     @api.one
     def action_confirm(self):
-        self.state = 'confirm'   
+        self.state = 'confirm'
+
     @api.one
     def action_transfer(self):
         self.state = 'transfer'
@@ -110,7 +110,7 @@ class inventory_distribution_to_shop(models.Model):
         res=self.env['stock.picking'].create(stock_picking_hrd)
         for distribute_line in self.stock_distribution_lines_ids:
             if distribute_line.distribute_qty>0:
-                print distribute_line.distribute_qty
+
                 stock_move_order={}
                 stock_move_order['picking_id']=res.id
                 stock_move_order['product_id']=distribute_line.product_id.id
@@ -120,9 +120,9 @@ class inventory_distribution_to_shop(models.Model):
                 stock_move_order['location_id']=distribute_line.source_location_id.id
                 stock_move_order['location_dest_id']=distribute_line.target_location_id.id
                 move_id = self.env['stock.move'].create(stock_move_order)
-                
+
                 move_new_obj.action_done(self.env.cr, self.env.uid, [move_id.id])
-        
+
         
             
         
