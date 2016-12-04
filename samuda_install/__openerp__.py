c419e50007b278d662edabc60a62c5aca2a678b0
Mehedi Hasan <md.mehedi.info@gmail.com>
2016-12-04 23:40:07 +0600

    [ADD] Add new module named 'Samuda Install'

---
+++ samuda_install/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Samuda ERP Installation',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Samuda ERP Installation',
+    'version':'1.0',
+    'data': ['',
+    ],
+    'depends': [],
+    'category': 'Installation Module',
+    'summary': 'Install Samuda ERP',
+    'description': 'Install Samuda ERP Module',
+    'installable': True,
+    'application': True,
+}
