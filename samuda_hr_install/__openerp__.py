353d8885e76215db19f3c15ca746945a313634bc
gitmehedi <md.mehedi.info@gmail.com>
2016-12-15 12:59:38 +0600

    [RENAME] Rename samuda installation module name 'samuda-install' to 'samuda_hr_install'

--- samuda_hr_install/__openerp__.py
+++ samuda_hr_install/__openerp__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Samuda ERP Installation',
+    'name': 'Samuda ERP HR Installation',
     'author': 'Genweb2 Limited',
     'website': 'www.genweb2.com',
-    'category': 'Samuda ERP Installation',
+    'category': 'Samuda ERP HR Installation',
     'version':'1.0',
-    'data': [
-    ],
+    'data': [],
     'depends': [
         'hr_public_holidays',
         'hr_holidays_compute_days',
@@ -15,8 +14,8 @@
         'hr_earned_leave',
         ],
     'category': 'Installation Module',
-    'summary': 'Install Samuda ERP',
-    'description': 'Install Samuda ERP Module',
+    'summary': 'Install HR Module of Samuda ERP',
+    'description': 'Install HR Module of Samuda ERP',
     'installable': True,
     'application': True,
 }
