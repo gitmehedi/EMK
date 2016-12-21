953bde04762b8716935e35dc8a3e42ee6f2edd75
gitmehedi <md.mehedi.info@gmail.com>
2016-12-21 14:24:32 +0600

    [ADD] Add record rules in 'HR Public Holidays' module

--- hr_public_holidays/__openerp__.py
+++ hr_public_holidays/__openerp__.py
@@ -15,6 +15,7 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+        'security/ir_rule.xml',
         'wizard/hr_calendar_clone_wizard_view.xml',
         'views/hr_public_holidays_view.xml',
         'views/hr_holidays_calendar_view.xml',
