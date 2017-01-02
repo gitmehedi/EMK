0d847b5eb4fca273cbf2675bc38610b723815565
Mehedi Hasan <md.mehedi.info@gmail.com>
2017-01-02 20:43:30 +0600

    [MOD] Change on product unique module

--- product_unique/product.py
+++ product_unique/product.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
+
 from openerp import models, api, _
 from openerp.exceptions import Warning
 
 
-class product_product(models.Model):
-    _inherit = "product.product"
+class product_template(models.Model):
+    _inherit = 'product.template'
 
     @api.one
-    @api.constrains('company_id','active')
+    @api.constrains('name')
     def check_unique_company_and_default_code(self):
-        print "-----------------", self.company_id, self.active
-        if self.active and self.company_id:
-            filters = [('company_id', '=', self.company_id.id),
-                       ('active', '=', True)]
+         
+        if self.name and self.company_id:
+            filters = [['company_id', '=', self.company_id.id],
+                       ['name', '=ilike', self.name]]
             prod_ids = self.search(filters)
             if len(prod_ids) > 1:
                 raise Warning(_('There can not be two active products with same company.'))
