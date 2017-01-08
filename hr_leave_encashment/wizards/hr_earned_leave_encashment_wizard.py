9f8a78f44281fa456502120752079c7fcea4a4bd
gitmehedi <md.mehedi.info@gmail.com>
2017-01-08 18:00:22 +0600

    [ADD] Add wizard options for encashment data processing

--- hr_leave_encashment/wizards/hr_earned_leave_encashment_wizard.py
+++ hr_leave_encashment/wizards/hr_earned_leave_encashment_wizard.py
@@ -2,26 +2,27 @@ from openerp import models, fields
 from openerp import api
 
 
+
 class HrEarnedLeaveEncashmentWizard(models.TransientModel):
     _name = 'hr.earned.leave.encashment.wizard'
     
     
-    employee_id = fields.Many2one('hr.employee', string="Employee")
-#     serial_number = fields.Integer(size=100, string='Serial Number', required='True')
+    employee_ids = fields.Many2many('hr.employee', 'hr_employee_group_rel', 'payslip_id', 'employee_id', 'Employees')
+    
 #     name = fields.Integer(size=100, string='Employee ID', required='True')
 #     dept_name = fields.Char(size=100, string='Dept Name', required='True')
 #     pending_leave = fields.Integer(size=100, string='Pending Leave', required='True')
 #     allowed_leave = fields.Integer(size=100, string='Allowed Leave', required='True')
 #     is_leave_encashed = fields.Boolean(size=100, string='Do you want to Carry Forward the leave? ', required='True')
     
-#     
-#     @api.multi
-#     def generate_wizard_table(self):
-#         vals = {}
-#         chd_obj = self.env['hr.employee']
-#         #data = chd_obj.search([('id','=',self.year_id.id)])
-#         
-#         vals['id'] =  employee_id       
-#         chd_obj.create(vals)
-#         
-#         return True
+     
+    @api.multi
+    def compute_sheet(self):
+        vals = {}
+        chd_obj = self.env['hr.employee']
+        #data = chd_obj.search([('id','=',self.year_id.id)])
+         
+        vals['id'] =  employee_id       
+        chd_obj.create(vals)
+         
+        return True
