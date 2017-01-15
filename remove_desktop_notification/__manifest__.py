3205b1f230dbcd5e655808bb07611d1423284425
hasan.mehedi <hasanmehedi@hasan.mehdi>
2017-01-15 12:32:44 +0600

    [ADD] Add remove_desktop_notification module

---
+++ remove_desktop_notification/__manifest__.py
@@ -0,0 +1,14 @@
+{   
+    'name': 'Remove Desktop Notification',
+    'author':  'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Remove Desktop',
+    'data': [
+        # 'security/ir.model.access.csv',
+        'remote_desktop.xml',
+        ],
+    'depends': [],
+    'description': '''''',
+    'installable': True,
+    'version': '0.1',
+}
