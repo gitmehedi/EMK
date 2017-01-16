5705c6e778d974a1ecfdafd72f6111806e5edf80
root <matiar.rahman@gmail.com>
2017-01-16 21:38:53 +0600

    ..

---
+++ gbs_base_package/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Base Package",
+
+    'summary': """
+        This module will install base required addons.""",
+
+    'description': """
+        This module will install base required addons.
+    """,
+
+    'author': "Genweb2 Limited",
+    'website': "http://www.genweb2.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+                'web_export_view',
+                'disable_odoo_online',
+                'web_hide_db_poweredby_link',
+                'hide_desktop_notification_link'],
+
+    # always loaded
+    'data': [
+    ],
+}
---
+++ gbs_hr_package/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HR Package",
+
+    'summary': """
+        This module will install required addons in HR domain.""",
+
+    'description': """
+        This module will install required addons in HR domain.
+    """,
+
+    'author': "Genweb2 Limited",
+    'website': "http://www.genweb2.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['hr',
+                'hr_holidays'],
+
+    # always loaded
+    'data': [
+    ],
+}
