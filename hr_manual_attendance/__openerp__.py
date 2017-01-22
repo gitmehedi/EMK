8f522c0e84108c859f5c0748aa0ec6dff688ee92
rabbi <rabbi998@gmail.com>
2017-01-22 17:41:28 +0600

    [ADD] Introduced HR Manual Attendance module with basic form entry done

---
+++ hr_manual_attendance/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'HR Manual Attendance Request',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'HR Attendance',
+    'version':'1.0',
+    'data': [
+       'views/hr_manual_attendance_view.xml',
+ #      'views/inherited_hr_holidays_status_data.xml',
+    ],
+    
+    'description': 
+    """This module enables employee to request manual attendance""",        
+    'installable': True,
+    'application': True,
+}
