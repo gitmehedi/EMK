ca5ed02686d06cc88a156cd4b1d0fbee50b09592
rabbi <rabbi998@gmail.com>
2017-01-23 17:46:27 +0600

    [MOD] inherited mail_thread and added corresponding changes on view for that. Also dept name added with view improving done

--- hr_manual_attendance/models/hr_manual_attendance.py
+++ hr_manual_attendance/models/hr_manual_attendance.py
@@ -9,11 +9,11 @@ class HrManualAttendance(models.Model):
     _inherit = ['mail.thread']
 
     #Fields of Model    
-    name = fields.Char(size = 30)
+    name = fields.Char()
     reason = fields.Text(string='Reason')
     is_it_official = fields.Boolean(string='Is it official', default=False)
-    state = fields.Char(string='State', size = 30, default = 'draft')        
-    department_name = fields.Char(string='Department Name', size = 30)    
+    state = fields.Char(string='State', default = 'draft')        
+    #department_name = fields.Char(string='Department')    
     check_in_time_full_day = fields.Date(string = 'Check In time for full day')
     check_out_time_full_day = fields.Date(string = 'Check out time for full day')    
     check_in_time_sign_in = fields.Date(string = 'Check In time for Sign In')
@@ -24,8 +24,9 @@ class HrManualAttendance(models.Model):
         ('sign_out', 'Sign Out')
         ], string = 'Sign Type')
     
-    employee_id = fields.Many2one('hr.employee', string="Employee Name", required = True)
-
+    employee_id = fields.Many2one('hr.employee', string="Employee", required = True)
+    department_id = fields.Many2one('hr.department', related='employee_id.department_id', 
+                                    string='Department', store=True)
     
     
     
