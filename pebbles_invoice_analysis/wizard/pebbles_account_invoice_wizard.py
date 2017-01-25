ce129e7b038d858b6a3b9b63fdd30ab248e154db
gitmehedi <git.mehedi@gmail.com>
2017-01-25 10:22:21 +0600

    [R&D] R&D on pebbles wizards

--- pebbles_invoice_analysis/wizard/pebbles_account_invoice_wizard.py
+++ pebbles_invoice_analysis/wizard/pebbles_account_invoice_wizard.py
@@ -1,25 +1,5 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-import time
 
 from openerp import api, exceptions, fields, models
 
@@ -36,18 +16,20 @@ class PebblesAccountInvoiceWizard(models.TransientModel):
 
     @api.multi
     def invoice_create(self):
-
         context = {
             'product_id': self.product_id.id,
             'partner_id': self.partner_id.id,
             'category_id': self.category_id.id
         }
-        print "----------------context----------", context
+
+        # win_obj = self.env['ir.actions.act_window']
+        # return win_obj.for_xml_id('pebbles_invoice_analysis', 'pebbles_view_account_invoice_report_graph', context)
+
         return {
-            'view_type': 'graph',
+            'view_type': 'pivot',
             'view_mode': 'graph',
             'res_model': 'pebbles.account.invoice.report',
-            'view_id': False,
+            'view_id': 'pebbles_view_account_invoice_report_graph',
             'type': 'ir.actions.act_window',
 
             'context': context,
