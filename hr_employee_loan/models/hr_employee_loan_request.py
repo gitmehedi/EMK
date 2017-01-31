a3c17ec03032ece7a2917601349d2a0d1bbc1701
rita <you@example.com>
2017-01-31 17:03:55 +0600

    [ADD] Create loan request module, list view, form view, associate loan policy and loan proofs with loan request

--- hr_employee_loan/models/hr_employee_loan_request.py
+++ hr_employee_loan/models/hr_employee_loan_request.py
@@ -4,10 +4,21 @@ class HrEmployeeLoanRequest(models.Model):
     _name = 'hr.employee.loan.request'    
 
     name = fields.Char(size=100, string='Name', required='True')
-    code = fields.Char(size=100, string='Code', required='True')
-    loan_type = fields.Char(size=100, string='Loan Type', required='True')
-    policy_type = fields.Char(size=100, string='Policy Type', required='True')
-    employee_name = fields.Char(size=100, string='Employee Name', required='True')
-   
+    code_ids = fields.Char(size=100, string='Code', required='True')
     
-             
+    """ All relations fields """
+    employee_loan_proofs_ids = fields.Many2many('hr.employee.loan.proof', string = 'Proofs') 
+    employee_loan_policies_ids = fields.Many2many('hr.employee.loan.policy', string = 'Policies')
+    
+    """ All input fields """
+    loan_type_ids = fields.Selection([
+        ('home_loan', 'Home Loan'),
+        ], string = 'Loan Type',required='True')
+    applied_date_ids = fields.Char(size=100, string='Applied Date', required='True')
+    approved_date_ids = fields.Char(size=100, string='Approved Date', required='True')
+    disbursement_date_ids = fields.Char(size=100, string='Disbursement Date', required='True')
+    department_ids = fields.Selection([
+        ('management', 'Management')], string = 'Department',required='True')
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id)
+    user_id = fields.Selection([
+        ('admin', 'Administrator'),('employee', 'Employee')], string = 'User',required='True')
