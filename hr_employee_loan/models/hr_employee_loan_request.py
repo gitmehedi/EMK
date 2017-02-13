9f22ece42a4b7958b1a7c6c245eac25a50ca6f18
rita <you@example.com>
2017-02-13 18:20:29 +0600

    [MOD]Update Datetime function with state workflow

--- hr_employee_loan/models/hr_employee_loan_request.py
+++ hr_employee_loan/models/hr_employee_loan_request.py
@@ -1,5 +1,7 @@
 from openerp import models, fields
+import datetime
 from openerp import api
+
 
 class HrEmployeeLoanRequest(models.Model):
     _name = 'hr.employee.loan.request'
@@ -7,40 +9,47 @@ class HrEmployeeLoanRequest(models.Model):
 
     name = fields.Char(size=100, string='Loan Name', default="New")
     emp_code = fields.Char(string='Code')
-    duration = fields.Char(size=100, string='Duration(Months)')
-    principal_amount = fields.Float(string='Principal Amount')
-    notes = fields.Text(string='Notes', size=500, help='Please enter notes.')
-    Req_rate = fields.Float(size=100, string='Rate')
-    is_interest_payble = fields.Boolean(string='Is Interest Payable', required='True')
+    duration = fields.Char(size=100, string='Duration(Months)',required=True,
+                states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
+    principal_amount = fields.Float(string='Principal Amount',required=True,
+                        states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
+    notes = fields.Text(string='Notes', size=500, help='Please enter notes.',
+                        states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
+    Req_rate = fields.Float(size=100, string='Rate',required=True)
+    is_interest_payble = fields.Boolean(string='Is Interest Payable', required='True',
+                        states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
+    applied_date = fields.Datetime('Applied Date', readonly=True, copy=False,
+        states={'draft': [('invisible', True)], 'applied': [('readonly', True)], 'approved':[('invisible', True)]})
+    
+    approved_date = fields.Datetime('Approved Date', readonly=True, copy=False,
+        states={'draft': [('invisible', True)], 'applied': [('invisible', True)], 'approved':[('readonly', True)]})
+    
+  
+    disbursement_date = fields.Datetime('Disbursement Date', readonly=True, copy=False,
+        states={'draft': [('invisible', True)], 'applied': [('invisible', True)], 'approved':[('readonly', True)]})
     
     """ All relations fields """
     def _default_employee(self):
         return self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
 
     employee_id = fields.Many2one('hr.employee', string="Employee", default=_default_employee,
-                                  required=True, ondelete='cascade', index=True)
+                                  required=True, ondelete='cascade', index=True,
+                                  states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
     department_id = fields.Many2one('hr.department', string="Department", related="employee_id.department_id")
-    employee_loan_proofs_ids = fields.Many2many('hr.employee.loan.proof', string='Proofs')
-    employee_loan_policies_ids = fields.Many2many('hr.employee.loan.policy', string='Policies')
+    employee_loan_proofs_ids = fields.Many2many('hr.employee.loan.proof', string='Proofs',
+                                states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
+    employee_loan_policies_ids = fields.Many2many('hr.employee.loan.policy', string='Policies',
+                                    states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id)
     user_id = fields.Many2one('res.users', string='User')
-    loan_type_id = fields.Many2one('hr.employee.loan.types', string='Loan Type', required=True)
+    loan_type_id = fields.Many2one('hr.employee.loan.types', string='Loan Type', required=True,
+                                   states={'draft': [('invisible', False)], 'applied': [('readonly', True)], 'approved':[('readonly', True)]})
 
     """ All Selection fields """
-    
-    applied_date_id = fields.Datetime('Applied Date', readonly=True, index=True, copy=False,
-        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]})
-    
-    approved_date_id = fields.Datetime('Approved Date', readonly=True, copy=False,
-        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]})
-    
-  
-    disbursement_date_id = fields.Datetime('Disbursement Date', readonly=True, copy=False,
-        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]})
          
     interst_mode_id = fields.Selection([
         ('flat', 'Flat'),
-        ], string = 'Interest Mode')
+        ], string = 'Interest Mode',)
     
 
     state = fields.Selection([
@@ -48,22 +57,6 @@ class HrEmployeeLoanRequest(models.Model):
         ('applied', "Applied"),
         ('approved', "Approved"),
     ], default='draft')
-
-
-    """ Major functionality """
-
-    '''@api.model
-    def create(self, vals):
-        vals['name'] = self.env['ir.sequence'].get('emp_code')
-        
-        return super(HrEmployeeLoanRequest, self).create(vals)'''
-
-#     @api.multi
-#     def write(self, vals):
-#         vals['name'] = self.env['ir.sequence'].get('emp_code')
-#         
-#         return super(HrEmployeeLoanRequest, self).write(vals)
-
     
     """All function which process data and operation"""
     
@@ -83,9 +76,13 @@ class HrEmployeeLoanRequest(models.Model):
     def action_confirm(self):
         for loan in self:
             loan.state = 'applied'
+            loan.applied_date = datetime.datetime.now()
             loan.name = self.env['ir.sequence'].get('emp_code')
             
     @api.multi
     def action_done(self):
-        self.state = 'approved'
-
+        for loan in self:
+            self.state = 'approved'
+            loan.approved_date = datetime.datetime.now()
+            loan.disbursement_date = datetime.datetime.now()
+           
