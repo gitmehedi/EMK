0e5c9a1d76123fbc65c300d16d746ba6e1646714
rabbi <rabbi998@gmail.com>
2017-02-14 12:51:07 +0600

    [ADD] New module structure added for attendance and OT summary

---
+++ hr_attendance_and_ot_summary/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'HR Attendance and Over Time (OT) Summary',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'HR Attendance',
+    'version':'1.0',
+    'data': [
+       
+    ],
+    
+    'depends': [
+        'hr_attendance',
+        'hr',
+        'hr_holidays',
+    ],    
+    
+    'description': 
+    """This module will show attendance and over time summary at a glance of employees""",        
+    'installable': True,
+    'application': True,
+}
