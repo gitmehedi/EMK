c7bb3fb23983ec78c29da88eebb28fb4ba782833
rita <you@example.com>
2017-02-27 11:25:39 +0600

    [ADD] Create a employee module, view, onchange m2o employee_id and save oncahange readonly field and check duplicate data

---
+++ hr_employee_mobile_bills/models/hr_mobile_bill_line.py
@@ -0,0 +1,35 @@
+from openerp import models, fields
+from openerp import api
+
+class HrLeaveEncashmentLine(models.Model):    
+    _name = 'hr.mobile.bill.line'
+    _description = 'HR mobile bill line'    
+
+    
+    bill_amount = fields.Float(string="Amount", required=True)
+    emp_mobile_phone = fields.Char('Mobile Number', required=True)
+    
+  
+    """ Relational Fields """
+    
+    parent_id = fields.Many2one(comodel_name='hr.mobile.bill')
+    employee_id = fields.Many2one('hr.employee', string="Employee", store=True,
+                                  compute='onchange_emp_mobile_phone')
+    
+    
+    _sql_constraints = [
+        ('unique_mobile_number', 'unique(parent_id, emp_mobile_phone)',
+         'Mobile Number must be unique per bill!'),
+    ]
+    
+    
+    """All function which process data and operation"""
+    @api.depends('emp_mobile_phone')
+    @api.onchange('emp_mobile_phone')
+    def onchange_emp_mobile_phone(self):
+        for recode in self:
+            if recode.emp_mobile_phone:
+                emp_pool = self.env['hr.employee'].search([('mobile_phone','=',recode.emp_mobile_phone)], limit=1)
+                if emp_pool:
+                    recode.employee_id = emp_pool.id
+                
