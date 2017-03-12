e015401a9eb137ade65d9692a419e3215f265e10
rabbi <rabbi998@gmail.com>
2017-03-12 18:39:47 +0600

    [ADD] Encashment and carry forward module's access rights added

--- hr_leave_carry_forward/wizards/hr_leave_carry_forward_wizard.py
+++ hr_leave_carry_forward/wizards/hr_leave_carry_forward_wizard.py
@@ -46,6 +46,7 @@ class HrLeaveCarryForwardWizard(models.TransientModel):
                 line_ids  = line_obj.search([('employee_id', '=', val.id)])                  
                 holiday_status_obj = self.env['hr.holidays.status'].search([('leave_carry_forward',
                                                                              '=',True)])
+               
                 for hso in holiday_status_obj:
                     for l_id in line_ids:
                         vals1['holiday_status_id'] = hso.id  #leave type
