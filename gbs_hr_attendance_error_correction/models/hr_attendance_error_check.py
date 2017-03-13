03e8cb30b05f54f1c187430d6f5b63911ae378f8
mdmithuncse <md.mithuncse@yahoo.com>
2017-03-13 13:42:57 +0600

    Create HR attendance error check module.

---
+++ gbs_hr_attendance_error_correction/models/hr_attendance_error_check.py
@@ -0,0 +1,20 @@
+from openerp import api, fields, models
+
+class AttendanceCheckError(models.Model):
+    _inherit = 'hr.attendance'
+
+    _description = 'HR attendance error check'
+
+    has_error = fields.Boolean(default=False, string='has_error', compute='onchange_attendance_data', store=True)
+
+    @api.depends('check_in', 'check_out')
+    def onchange_attendance_data(self):
+        for att in self:
+            if att.check_in is None:
+                att.has_error = True
+            elif att.check_out is None:
+                att.has_error = True
+            elif att.check_out < att.check_in:
+                att.has_error = True
+            else:
+                att.has_error = False
