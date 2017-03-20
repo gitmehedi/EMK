1019d2698dfb2c1963c8b833454f16158ec64747
mdmithuncse <md.mithuncse@yahoo.com>
2017-03-20 18:10:06 +0600

    [Add] attendance error correction view

--- gbs_hr_attendance_error_correction/models/hr_attendance_error_correct.py
+++ gbs_hr_attendance_error_correction/models/hr_attendance_error_correct.py
@@ -14,3 +14,8 @@ class AttendanceErrorCorrect(models.Model):
     employee_name = fields.Many2one("hr.employee", string="Employee", required=False)
     from_date = fields.Datetime(string='From Date', required=True)
     to_date = fields.Datetime(string='To Date', required=True)
+    attendance_line_ids = fields.One2many('hr.attendance', 'employee_id', string='Employee Attendances')
+
+    """invoice_line_ids = fields.One2many('account.invoice.line', 'invoice_id', string='Invoice Lines',
+                                       oldname='invoice_line',
+                                       readonly=True, states={'draft': [('readonly', False)]}, copy=True)"""
