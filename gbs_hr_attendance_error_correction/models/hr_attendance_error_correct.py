a22e8812cec2cd4c03f86fd72a010ea4404fac44
mdmithuncse <md.mithuncse@yahoo.com>
2017-04-05 12:43:09 +0600

    Modify the tree view

--- gbs_hr_attendance_error_correction/models/hr_attendance_error_correct.py
+++ gbs_hr_attendance_error_correction/models/hr_attendance_error_correct.py
@@ -12,41 +12,35 @@ class AttendanceErrorCorrect(models.Model):
 
     department_name = fields.Many2one("hr.department", string="Department", required=False)
     employee_name = fields.Many2one("hr.employee", string="Employee", required=False)
-    from_date = fields.Datetime(string='From Date', required=True)
-    to_date = fields.Datetime(string='To Date', required=True)
+    from_date = fields.Date(string='From Date', required=True)
+    to_date = fields.Date(string='To Date', required=True)
     attendance_line_ids = fields.One2many('hr.attendance', 'employee_id', domain=[('has_error', '=', True)])
 
     @api.constrains('department_name', 'employee_name', 'from_date', 'to_date')
     def generate_filtered_data(self):
-        if self.type == 'department_type':
-            # departments = self.env['hr.department'].search([('id', '=', self.department_name.id)])
 
+        if self.type == 'department_type':
             employees = self.env['hr.employee'].search([('department_id', '=', self.department_name.id)])
 
             for emp in employees:
-                attendances = self.env['hr.attendance'].search([('has_error', '=', True), ('employee_id', '=', emp.id), ('check_in', '>=', self.from_date), ('check_out', '<=', self.to_date)])
+                attendances = self.env['hr.attendance'].search([('has_error', '=', True), ('employee_id', '=', emp.id),
+                                                                ('create_date', '>=', self.from_date), ('create_date', '<=', self.to_date)])
                 for att in attendances:
-                    print "=====Attendance List=====", att.id
+                    print "=====Departmentwise Attendances", att.id
 
         elif self.type == 'employee_type':
-            # employees = self.env['hr.employee'].search([('id', '=', self.employee_name.id)])
-
-            attendances = self.env['hr.attendance'].search([('has_error', '=', True), ('employee_id', '=', self.employee_name.id), ('check_in', '>=', self.from_date), ('check_out', '<=', self.to_date)])
-
+            attendances = self.env['hr.attendance'].search([('has_error', '=', True), ('employee_id', '=', self.employee_name.id),
+                                                            ('create_date', '>=', self.from_date), ('create_date', '<=', self.to_date)])
             for att in attendances:
-                print "=====Attendance List=====", att.id
-
-
-        '''for data in datas:
-            print "=Employee Name=", data.employee_id.id
-            print "=Employee Check In=", data.check_in
-            print "=Employee Check Out=", data.check_out'''
+                print "=====Employeewise Attendances", att.id
 
         '''return {
-            'view_type': 'tree',
-            'view_mode': 'tree',
-            'src_model': 'hr.attendance',
-            'res_model': 'hr.attendance',
-            'view_id': False,
+            'name': 'Error Data',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
             'type': 'ir.actions.act_window',
+            'res_model': 'hr.attendance',
+            'target': 'current',
+            'domain': domain,
+            'nodestroy': False
         }'''
