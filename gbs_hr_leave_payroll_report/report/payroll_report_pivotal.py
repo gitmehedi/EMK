4452b29dbbfa4ff1ce342960340a143b8cf360a5
rabbi <rabbi998@gmail.com>
2017-04-12 13:50:07 +0600

    [add] Payslip report header and body full dynamic now

--- gbs_hr_leave_payroll_report/report/payroll_report_pivotal.py
+++ gbs_hr_leave_payroll_report/report/payroll_report_pivotal.py
@@ -1,4 +1,7 @@
 from openerp import api, exceptions, fields, models
+import operator
+
+    
     
 class PayrollReportPivotal(models.AbstractModel):
     _name = 'report.gbs_hr_leave_payroll_report.report_absence_view_qweb'
@@ -8,14 +11,21 @@ class PayrollReportPivotal(models.AbstractModel):
         payslip_run_pool = self.env['hr.payslip.run']
         docs = payslip_run_pool.browse(docids[0])
         
-        code_seq_dict = []
-        
+        rule_list = []
         for slip in docs.slip_ids:
             for line in slip.line_ids:
-                if {line.code:line.sequence} not in code_seq_dict:
-                    code_seq_dict.append({line.code:line.sequence})
-                
-                    code_seq_dict.sort()
+                rule = {}
+                rule['name'] = line.name
+                rule['code'] = line.code
+                rule['seq'] = line.sequence
+                if rule not in rule_list:
+                    rule_list.append(rule)
+        
+        #rule_list = sorted(rule_list, key=lambda rule: rule[0])        
+        #print rule_list
+        
+        #a = rule_list.sort(key=operator.itemgetter(1))
+        #print '------------------', a
             
         payslips = []
         
@@ -30,18 +40,18 @@ class PayrollReportPivotal(models.AbstractModel):
                 payslip['doj'] = emp.initial_employment_date                  
             
             for line in slip.line_ids:
-                for rule in code_seq_dict:
-                    if line.code == rule.keys()[0]:
+                for rule in rule_list:
+                    if line.code == rule['code']:
                         payslip[line.code] = line.amount
                         
             payslips.append(payslip)
             
-        print "=======================", payslip        
+        #print "=======================", payslip        
         
         docargs = {
             'doc_ids': self.ids,
             'doc_model': 'hr.payslip.run',
-            #'data': data['form'],
             'docs': payslips,
+            'rules': rule_list,
         }
         return self.env['report'].render('gbs_hr_leave_payroll_report.report_absence_view_qweb', docargs)
