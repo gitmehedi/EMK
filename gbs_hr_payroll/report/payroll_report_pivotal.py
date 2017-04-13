52b93f952837ba649cd6f10732427c6ac37b57f4
rabbi <rabbi998@gmail.com>
2017-04-13 21:47:59 +0600

    [add] department wise employee added on payslip reports

--- gbs_hr_payroll/report/payroll_report_pivotal.py
+++ gbs_hr_payroll/report/payroll_report_pivotal.py
@@ -16,35 +16,46 @@ class PayrollReportPivotal(models.AbstractModel):
                 rule['name'] = line.name
                 rule['seq'] = line.sequence
                 rule['code'] = line.code
-               
+                # line.employee_id.department_id.name
+
                 if rule not in rule_list:
                     rule_list.append(rule)
                     
         sorted(rule.iteritems(), key=operator.itemgetter(1))
             
+        dept = self.env['hr.department'].search([])
+        dpt_payslips = dict((d.name,[]) for d in dept)
+
         payslips = []
+
+        department_name = ''
         
         for slip in docs.slip_ids:
             
             payslip = {}
-         
             payslip['emp_name'] = line.employee_id.name
+
             
             for emp in line.employee_id:            
                 payslip['designation'] = emp.job_id.name
-                payslip['doj'] = emp.initial_employment_date                  
-            
+                payslip['doj'] = emp.initial_employment_date
+
             for line in slip.line_ids:
                 for rule in rule_list:
                     if line.code == rule['code']:
+                        department_name = line.employee_id.department_id.name
                         payslip[line.code] = line.amount
+
+            if department_name in dpt_payslips:
+                dpt_payslips[department_name].append(payslip)
                         
-            payslips.append(payslip)
+            # payslips.append(dpt_payslips)
+            print "----", dpt_payslips
         
         docargs = {
             'doc_ids': self.ids,
             'doc_model': 'hr.payslip.run',
-            'docs': payslips,
+            'docs': dpt_payslips,
             'rules': rule_list,
         }
         return self.env['report'].render('gbs_hr_payroll.report_individual_payslip', docargs)
