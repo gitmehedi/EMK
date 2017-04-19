001068e5dc92295651d01105cc44e1d8d641d220
rabbi <rabbi998@gmail.com>
2017-04-19 15:15:04 +0600

    [add] One layer approval process added with three states e.g. draft/confirm/approve

--- hr_rostering/models/hr_shift_alter.py
+++ hr_rostering/models/hr_shift_alter.py
@@ -1,5 +1,4 @@
 from openerp import api,fields,models
-from openerp.exceptions import ValidationError,Warning
 from datetime import date
 import datetime
 
@@ -9,15 +8,40 @@ class HRShiftAlter(models.Model):
     _rec_name = 'employee_id'
     
     employee_id = fields.Many2one('hr.employee', string="Employee", required = True)
+    department_id = fields.Many2one('hr.department', related='employee_id.department_id', string='Department', store=True)
     alter_date = fields.Date(string = 'Alter Date')
     alter_time_start = fields.Datetime(string = 'Alter Time Start')
     alter_time_end = fields.Datetime(string = 'Alter Time End')
     ot_time_start = fields.Datetime(string = 'Over Time Start')
     ot_time_end = fields.Datetime(string = 'Over Time End')
     ot_included = fields.Boolean(string='Is OT included')
+    manager_id = fields.Many2one('hr.employee', string='Final Approval', readonly=True, copy=False)
 
+    state = fields.Selection([
+        ('draft', "Draft"),
+        ('confirmed', "Confirmed"),
+        ('approved', "Approved"), 
+        ('refuse', 'Refused'),
+    ], default = 'draft')
     
+    @api.multi
+    def action_approve(self):
+        manager = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)    
+        self.write({'state': 'approved', 'manager_id': manager.id})
     
+    @api.multi    
+    def action_confirm(self):
+        self.state = 'confirmed' 
+    
+    @api.multi
+    def action_refuse(self):
+        manager = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)    
+        self.write({'state': 'refuse', 'manager_id': manager.id})
+                        
+    @api.multi
+    def action_draft(self):
+        self.state = 'draft'
+              
     
 
 
