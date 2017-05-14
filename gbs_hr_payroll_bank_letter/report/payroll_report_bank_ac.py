c91039eb97e5f060eb05ee580524e7e04a2837e3
rabbi <rabbi998@gmail.com>
2017-05-14 18:46:18 +0600

    Bank Letter view added. Only amount to text and mother bank a/c adding remaining

--- gbs_hr_payroll_bank_letter/report/payroll_report_bank_ac.py
+++ gbs_hr_payroll_bank_letter/report/payroll_report_bank_ac.py
@@ -12,6 +12,9 @@ class PayrollReportBankAc(models.AbstractModel):
         slip_ids = slip_pool.search([('payslip_run_id','=',data['active_id']),
                                      ('employee_id.bank_account_id.bank_id','=',data['bank_id'])])
 
+        company_id = fields.Many2one('res.company', string='Company', ondelete='cascade',
+                                     default=lambda self: self.env.user.company_id)
+
         rule_list = []
         for slip in slip_pool.browse(slip_ids.ids):
             for line in slip.line_ids:
@@ -26,6 +29,7 @@ class PayrollReportBankAc(models.AbstractModel):
         rule_list = sorted(rule_list, key=lambda k: k['seq'])
 
         dpt_payslips_list = []
+        total_sum = []
         sn = 1
         dpt_payslips = {}
         dpt_payslips['val'] = []
@@ -34,34 +38,49 @@ class PayrollReportBankAc(models.AbstractModel):
         for slip in slip_pool.browse(slip_ids.ids):
             payslip = {}
             payslip['emp_name'] = slip.employee_id.name
-            payslip['designation'] = slip.employee_id.job_id.name
             payslip['acc_no'] = slip.employee_id.bank_account_id.acc_number
             payslip['emp_seq'] = slip.employee_id.employee_sequence
 
             for rule in rule_list:
                 payslip['NET'] = 0
                 for line in slip.line_ids:
-                    if line.code == "NET":
-                        payslip['NET'] = math.ceil(line.total)
+                    if line.code == 'NET':
+                        total_amt = line.total
+                        payslip['NET'] = math.ceil(total_amt)
+                        total_sum.append(total_amt)
+
                         break;
 
             dpt_payslips['val'].append(payslip)
                     
-            emp_sort_list  = dpt_payslips['val']
-            emp_sort_list = sorted(emp_sort_list, key=lambda k: k['emp_seq'])
+        emp_sort_list  = dpt_payslips['val']
+        emp_sort_list = sorted(emp_sort_list, key=lambda k: k['emp_seq'])
             
-            for ps in emp_sort_list:
-                ps['sn'] = sn
-                sn += 1
+        for ps in emp_sort_list:
+            ps['sn'] = sn
+            sn += 1
             
         dpt_payslips['val'] = emp_sort_list
         dpt_payslips_list.append(dpt_payslips)
+        all_total = sum(total_sum)
 
         docargs = {
             'doc_ids': self.ids,
             'doc_model': 'hr.payslip.run',
             'docs': dpt_payslips_list,
             'bank_name': data['bank_name'],
+            'bank_street1':data['bank_street1'],
+            'bank_street1': data['bank_street2'],
+            'bank_city': data['bank_city'],
+            'bank_zip': data['bank_zip'],
+            'bank_country': data['bank_country'],
+            'payslip_report_name': data['payslip_report_name'],
+            'cur_year': data['cur_year'],
+            'cur_month': data['cur_month'],
+            'cur_day': data['cur_day'],
+            'total_net': all_total,
+            'docs_len': len(rule_list) + 4,
+            'company': self.env['res.company']._company_default_get('gbs_hr_payroll_bank_letter').name
         }
         
         return self.env['report'].render('gbs_hr_payroll_bank_letter.report_individual_payslip1', docargs)
