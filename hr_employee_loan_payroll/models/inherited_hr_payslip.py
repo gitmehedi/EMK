0b6aa339a3fc81d6a9d58c24e7b7c06f1444a7a4
gitmehedi <git.mehedi@gmail.com>
2017-05-17 18:53:51 +0600

    [REFACTOR] Add various files for mobile bill, loan, attendance and meal bills

---
+++ hr_employee_loan_payroll/models/inherited_hr_payslip.py
@@ -0,0 +1,40 @@
+from odoo import api, fields, models, tools, _
+
+
+class InheritedHrMobilePayslip(models.Model):
+    """
+    Inherit HR Payslip models and add onchange functionality on 
+    employee_id
+    """
+    _inherit = "hr.payslip"
+
+    @api.onchange('employee_id', 'date_from', 'date_to')
+    def onchange_employee(self):
+
+        if self.employee_id:
+            self.input_line_ids = 0
+            super(InheritedHrMobilePayslip, self).onchange_employee()
+
+            """
+            Incorporate other payroll data
+            """
+            other_line_ids = self.input_line_ids
+            loan_data = self.env['hr.employee.loan.line'].search([('employee_id', '=', self.employee_id.id),
+                                                                  ('schedule_date', '>=', self.date_from),
+                                                                  ('schedule_date', '<=', self.date_to),
+                                                                  ('state', '=', 'pending')], limit=1)
+
+            """
+            Meal Bills
+            """
+            if loan_data and self.contract_id.id:
+                other_line_ids += other_line_ids.new({
+                    'name': 'Current Loan',
+                    'code': "LOAN",
+                    'amount': loan_data.installment,
+                    'contract_id': self.contract_id.id,
+                })
+            self.input_line_ids = other_line_ids
+
+
+
