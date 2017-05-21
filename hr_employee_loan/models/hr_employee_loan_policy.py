6fc4aabd5307db18dd3c5a6598a2819f64060f7c
ritachowdhury <ritaislam6789@gmail.com>
2017-05-21 14:54:20 +0600

    [MOD] Change Security access for loan module

--- hr_employee_loan/models/hr_employee_loan_policy.py
+++ hr_employee_loan/models/hr_employee_loan_policy.py
@@ -6,21 +6,27 @@ class HrEmployeeLoanPolicy(models.Model):
     name = fields.Char(size=100, string='Name', required='True')
     code = fields.Char(size=100, string='Code', required='True')
     value = fields.Float(size=100, string='Value', required='True')
+
     
     """ All relations fields """
     employee_ids = fields.Many2many('hr.employee', string='''Employee's''')
     employee_tag_ids = fields.Many2many('hr.employee.category', string='Employee Catagories')
-    
+    company_id = fields.Many2one('res.company', string='Company',ondelete='cascade', default=lambda self: self.env.user.company_id)
+
+
+    """ All Selection fields """
     policy_type_id = fields.Selection([
         ('flat', 'Max Loan Amount'),
         ('incremental', 'Gap Between Two Loans'),
         ('period', 'Qualifying Period'),
         ], string = 'Policy Type',required='True')
-    
-    company_id = fields.Many2one('res.company', string='Company',ondelete='cascade', default=lambda self: self.env.user.company_id)
+
     basis_id = fields.Selection([
         ('flat', 'Fix Amount')], string = 'Basis',required='True')
 
+
+
+    """All function which process data and operation"""
     @api.constrains('name')
     def _check_unique_constraint(self):
         if self.name:
