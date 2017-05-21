c8ca280de2590d355a8cc2ed5cdf530efae40609
ritachowdhury <ritaislam6789@gmail.com>
2017-05-21 10:59:12 +0600

    [Solved]'principal amount ,Number of installmentand duration is taking wrong entry like minus value,it should not be'has solved for loan module

--- hr_employee_loan/models/hr_employee_loan.py
+++ hr_employee_loan/models/hr_employee_loan.py
@@ -123,3 +123,15 @@ class HrEmployeeLoanRequest(models.Model):
             name = self.search(filters)
             if len(name) > 1:
                 raise Warning('[Unique Error] Name must be unique!')
+
+
+    def _check_qty(self, cr, uid, ids, context=None):
+        for order in self.browse(cr, uid, ids, context=context):
+            if order.duration < 0 or order.principal_amount :
+                return False
+
+        return True
+
+    _constraints = [
+        (_check_qty, ' principal_amount or duration cannot be negative !', ['duration']),
+    ]
