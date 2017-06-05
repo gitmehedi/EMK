d8b11d0593d5dc3dda5bbfb89900a75e09472e89
rabbi <rabbi998@gmail.com>
2017-06-05 11:45:46 +0600

    [ADD] Attendance module structure added

---
+++ gbs_hr_attendance_report/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'GBS HR Attendance Report',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'attendance',
+    'version': '1.0',
+    'depends': [
+        'hr_attendance',
+    ],
+    'data': [
+
+    ],
+
+    'summary': 'Generates check in and check out related report of employee(s)',
+    'description': 'Generates check in and check out related report of employee(s)',
+    'installable': True,
+    'application': True,
+}
