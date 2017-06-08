c15c95608287678a37a5b8b1215b6570ba6a6043
bappy <mahfuz.bappy@gmail.com>
2017-06-08 13:59:37 +0600

    Update model constante

--- hr_manual_attendance/models/hr_attendance_inherit.py
+++ hr_manual_attendance/models/hr_attendance_inherit.py
@@ -6,4 +6,4 @@ class HrAttendanceInherit(models.Model):
     _inherit = ['hr.attendance']
     
     manual_attendance_request =  fields.Boolean(string='Is it from manual attendance request', default=False)    
-    check_in = fields.Datetime(string="Check In", default=fields.Datetime.now)
+    check_in = fields.Datetime(string="Check In", required=False)
