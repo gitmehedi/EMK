8dd82ddac66ccddebd28dc30cc170bdaf3427597
gitmehedi <git.mehedi@gmail.com>
2017-06-08 14:14:31 +0600

    [FIX] Fix taka and paisa format in res.currency

--- amount_to_word_bd/models/res_currency.py
+++ amount_to_word_bd/models/res_currency.py
@@ -6,47 +6,19 @@ class Currency(models.Model):
 
     @api.model
     def amount_to_word(self, number):
-        def handel_upto_99(number):
-            predef = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight",
-                      9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen",
-                      16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty",
-                      40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety", 100: "Hundred",
-                      100000: "Lac", 10000000: "Crore", 1000000000: "billion"}
-
-            if number in predef.keys():
-                return predef[number]
-            else:
-                return predef[(number / 10) * 10] + ' ' + predef[number % 10]
-
-        def return_bigdigit(number, devideby):
-            predef = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight",
-                      9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen",
-                      16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty",
-                      40: "Forty",
-                      50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety", 100: "Hundred",
-                      1000: "Thousand",
-                      100000: "Lac", 10000000: "Crore", 1000000000: "Billion"}
-
-            if devideby in predef.keys():
-                return predef[number / devideby] + " " + predef[devideby]
-            else:
-                devideby /= 10
-                return handel_upto_99(number / devideby) + " " + predef[devideby]
-
         dev = {100: "Hundred", 1000: "Thousand", 100000: "Lac", 10000000: "Crore", 1000000000: "Billion"}
-
+        result = ""
         # Split amount for decimal value
         list = str(number).split('.')
         start_word = int(list[0])
-        end_word = int(list[1])
+        end_word = list[1]
 
         # Amount to word for integer portion
         if start_word is 0:
-            return "Zero"
+            result = ''
         if start_word < 100:
-            result = handel_upto_99(start_word)
+            result = self.handel_upto_99(start_word)
         else:
-            result = ""
             while start_word >= 100:
                 devideby = 1
                 length = len(str(start_word))
@@ -55,25 +27,61 @@ class Currency(models.Model):
 
                 if start_word % devideby == 0:
                     if devideby in dev:
-                        return handel_upto_99(start_word / devideby) + " " + dev[devideby]
+                        res = self.handel_upto_99(start_word / devideby) + " " + dev[devideby]
                     else:
-                        return handel_upto_99(start_word / (devideby / 10)) + " " + dev[devideby / 10]
+                        res = self.handel_upto_99(start_word / (devideby / 10)) + " " + dev[devideby / 10]
+                else:
+                    res = self.return_bigdigit(start_word, devideby)
 
-                res = return_bigdigit(start_word, devideby)
                 result = result + ' ' + res
 
                 if devideby not in dev:
                     start_word = start_word - ((devideby / 10) * (start_word / (devideby / 10)))
-
-                start_word = start_word - devideby * (start_word / devideby)
+                else:
+                    start_word = start_word - devideby * (start_word / devideby)
 
             if start_word < 100:
-                result = result + ' ' + handel_upto_99(start_word)
+                res = self.handel_upto_99(start_word)
+                result = result + ' ' + (res if res != 'Zero' else '')
 
-        # Amount to word for decimal portion
-        if end_word is 0:
-            result_paisa = "Zero"
-        if end_word < 100:
-            result_paisa = handel_upto_99(end_word)
+        if int(end_word) > 0:
+            end_word = int(end_word) if len(end_word) > 1 else int(end_word) * 10
+            paisa = self.handel_upto_99(end_word)
+            if start_word > 0:
+                result = result + ' Taka and ' + paisa + ' Paisa'
+            else:
+                result = paisa + ' Paisa'
+        else:
+            result = result + ' Taka'
 
-        return result + ' and '+result_paisa+' Paisa'
+        return result
+
+    def handel_upto_99(self, number):
+        predef = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight",
+                  9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen",
+                  16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty",
+                  40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety", 100: "Hundred",
+                  100000: "Lac", 10000000: "Crore", 1000000000: "billion"}
+
+        if number in predef.keys():
+            return predef[number]
+        else:
+            return predef[(number / 10) * 10] + ' ' + predef[number % 10]
+
+    def return_bigdigit(self, number, devideby):
+        predef = { 0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight",
+            9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen",
+            17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty",
+            60: "Sixty",
+            70: "Seventy",
+            80: "Eighty",
+            90: "Ninety",
+            100: "Hundred",
+            1000: "Thousand",
+            100000: "Lac", 10000000: "Crore", 1000000000: "Billion"}
+
+        if devideby in predef.keys():
+            return predef[number / devideby] + " " + predef[devideby]
+        else:
+            devideby /= 10
+            return self.handel_upto_99(number / devideby) + " " + predef[devideby]
