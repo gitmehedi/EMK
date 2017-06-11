ce4fc3a4dd121f97ccba16abc8367373d7558562
ritachowdhury <ritaislam6789@gmail.com>
2017-06-11 16:06:19 +0600

    [MOD]Modify daily_attendance_report

--- gbs_hr_attendance_report/report/daily_attendance_report.py
+++ gbs_hr_attendance_report/report/daily_attendance_report.py
@@ -1,37 +1,15 @@
+from openerp import api, fields, models
 
-# from odoo import api, models
-#
-#
-# class ReportSalesperson(models.AbstractModel):
-#     _name = 'report.gbs_hr_attendance_report.report_daily_attendance'
-#
-#     @api.model
-#     def render_html(self, docids, data=None):
-#         hr_att_pool = self.env['hr.attendance'].search([])
-#         docargs = {
-#             'docs': hr_att_pool,
-#         }
-#         return self.env['report'].render('gbs_hr_attendance_report.report_daily_attendance', docargs)
 
-import time
-from odoo import api, models
-from dateutil.parser import parse
-from odoo.exceptions import UserError
-
-class DailyAttendance(models.AbstractModel):
-    _name = 'report.gbs_hr_attendance_report.report_daily_attendance'
+class GbsHrAttendanceDurationCalc(models.AbstractModel):
+    _name = 'report.gbs_hr_attendance_report.report_hr_daily_att'
 
     @api.model
     def render_html(self, docids, data=None):
-        self.model = self.env.context.get('active_model')
-        docs = self.env[self.model].browse(self.env.context.get('active_id'))
-        records = []
+        hr_att_pool = self.env['hr.attendance'].search([])
+
         docargs = {
-            'doc_ids': self.ids,
-            'doc_model': self.model,
-            'docs': docs,
-            'time': time,
-            'orders':records
+            'docs': hr_att_pool,
         }
 
-        return self.env['report'].render('gbs_hr_attendance_report.report_daily_attendance', docargs)
+        return self.env['report'].render('gbs_hr_attendance_report.report_hr_daily_att', docargs)
