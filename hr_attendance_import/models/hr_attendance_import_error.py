10766b4d50e86b941d65493718b2135c7f568af8
bappy <mahfuz.bappy@gmail.com>
2017-06-13 16:06:31 +0600

    Implement Pull attendance error

--- hr_attendance_import/models/hr_attendance_import_error.py
+++ hr_attendance_import/models/hr_attendance_import_error.py
@@ -7,6 +7,7 @@ class HrAttendanceImportError(models.Model):
     check_in = fields.Char(string='Check In', required=False)
     check_out = fields.Char(string='Check Out', required=False)
     attempt_to_success = fields.Integer(string='Try', default=0)
+    attendance_server_id = fields.Integer(string='Server Id', required=False)
     import_id = fields.Many2one('hr.attendance.import', 'id', ondelete='cascade')
     
     
