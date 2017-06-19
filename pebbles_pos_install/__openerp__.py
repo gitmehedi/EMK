7ab392399493ee451c51b920138f4533d20a007b
gitmehedi <git.mehedi@gmail.com>
2017-06-19 09:43:33 +0600

    [ADD] Add new modules for pebbles pos installation

---
+++ pebbles_pos_install/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Pebbles POS  Module',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Pebbles Installation',
+    'version': '1.0.0',
+    'data': [],
+    'depends': ['point_of_sale',
+                'pos_loyalty',
+                # 'pebbles_pos_ticket',
+                'pos_gift_ticket',
+                'pos_notes',
+                # 'product_barcode_qweb',
+                ],
+    'category': 'Installation Module',
+    'summary': 'Install Pebbles Module',
+    'description': 'Install Pebbles Module',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
