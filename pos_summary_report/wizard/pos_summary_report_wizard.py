67fa537c5f3d09fd3f2b63c701520c9621ab3f22
gitmehedi <git.mehedi@gmail.com>
2017-06-20 16:18:27 +0600

    [ADD] Add on pos_summary_report

--- pos_summary_report/wizard/pos_summary_report_wizard.py
+++ pos_summary_report/wizard/pos_summary_report_wizard.py
@@ -2,13 +2,17 @@ from openerp import api, exceptions, fields, models
 
 
 class PosSummaryReportWizard(models.TransientModel):
-	_name = 'pos.summary.report.wizard'
+    _name = 'pos.summary.report.wizard'
 
-	start_date = fields.Date('Start Date', default=fields.Date.today(), required=True)
-	end_date = fields.Date('End Date', default=fields.Date.today())
-	point_of_sale_id = fields.Many2one('pos.config', string='Point Of Sale', required=True)
+    start_date = fields.Date('Start Date', default=fields.Date.today(), required=True)
+    end_date = fields.Date('End Date', default=fields.Date.today())
+    point_of_sale_id = fields.Many2one('pos.config', string='Point Of Sale', required=True)
 
-	@api.multi	
-	def pos_order_report(self,data):
-		return self.env['report'].get_action(self, 'report_pos_order_summary.report_pos_summary_qweb', data=data)
-		
+    @api.multi
+    def pos_order_report(self):
+
+        data = {}
+        data['start_date'] = self.start_date
+        data['end_date'] = self.end_date
+        data['point_of_sale_id'] = self.point_of_sale_id.id
+        return self.env['report'].get_action(self, 'pos_summary_report.report_pos_summary_qweb', data=data)
