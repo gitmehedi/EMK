2fc05bd31b476a9eae7f583ad5a01903f8c27e3b
gitmehedi <git.mehedi@gmail.com>
2017-06-21 16:40:24 +0600

    [MOD] Modify summary reports

--- pos_summary_report/report/pos_order_report.py
+++ pos_summary_report/report/pos_order_report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 from openerp import api, models
-import datetime
+from datetime import datetime
 
 
 class DailyCreditSettlementReport(models.AbstractModel):
@@ -26,16 +26,39 @@ class DailyCreditSettlementReport(models.AbstractModel):
         """
         params = (pos_config, start_date, end_date)
         self.env.cr.execute(query, params)
-        res =  self.env.cr.dictfetchall()
+        res = self.env.cr.dictfetchall()
         return res
 
     @api.multi
     def render_html(self, data=None):
+        lines = []
         report_obj = self.env['report']
         report = report_obj._get_report_from_name('stock_summary_report.report_stock_summary_qweb')
+        cash_counter = self.env['pos.config'].search([('id', '=', data['point_of_sale_id'])])
+        pos_order = self.env['pos.order'].search([('location_id', '=', cash_counter.stock_location_id.id)], order="date_order asc")
 
-        lines = self._generate_lines(data['start_date'], data['end_date'], data['point_of_sale_id'])
-
+        for record in pos_order:
+            rec = {}
+            sales_value = sum([r.price_subtotal for r in record.lines])
+            discount = sum([r.qty * r.price_unit - r.price_subtotal for r in record.lines])
+            net_sales = sum([r.price_subtotal_incl for r in record.lines])
+            rec['date_order'] = self.format_date(record.date_order)
+            rec['pos_reference'] = record.pos_reference
+            rec['sales_value'] = self.decimal(sales_value)
+            rec['amount_tax'] = self.decimal(net_sales - sales_value)
+            rec['discount'] = 0 if discount == 0 else '- {0}'.format(self.decimal(discount))
+            rec['net_sales'] = self.decimal(net_sales)
+            rec['cash'] = self.decimal(record.amount_total)
+            rec['credit_card'] = self.decimal(record.amount_total)
+            rec['total'] = self.decimal(record.amount_total)
+            lines.append(rec)
+        # self.env['pos.config'].search([])[0].operating_unit_id.partner_id.contact_address
+        address = {
+            'name': self.env.user.company_id.name,
+            'contact1': self.env.user.default_operating_unit_id.partner_id.street,
+            'contact2': self.env.user.default_operating_unit_id.partner_id.street,
+            'phone': self.env.user.default_operating_unit_id.partner_id.phone
+        }
         docargs = {
             'doc_ids': self._ids,
             'doc_model': report.model,
@@ -44,5 +67,12 @@ class DailyCreditSettlementReport(models.AbstractModel):
             'end_date': data['end_date'],
             'category_id': data['point_of_sale_id'],
             'lines': lines,
+            'address': address,
         }
         return report_obj.render('pos_summary_report.report_pos_summary_qweb', docargs)
+
+    def format_date(self, date):
+        return datetime.strptime(date[:10], '%Y-%m-%d').strftime('%d-%m-%Y')
+
+    def decimal(self, val):
+        return "{0:.2f}".format(round(val, 2))
--- pos_summary_report/wizard/pos_summary_report_wizard.py
+++ pos_summary_report/wizard/pos_summary_report_wizard.py
@@ -4,9 +4,14 @@ from openerp import api, exceptions, fields, models
 class PosSummaryReportWizard(models.TransientModel):
     _name = 'pos.summary.report.wizard'
 
+    @api.model
+    def _default_operating_unit(self):
+        return self.env.user.default_operating_unit_id.id
+
     start_date = fields.Date('Start Date', default=fields.Date.today(), required=True)
     end_date = fields.Date('End Date', default=fields.Date.today())
-    point_of_sale_id = fields.Many2one('pos.config', string='Point Of Sale', required=True)
+    point_of_sale_id = fields.Many2one('pos.config', string='Point Of Sale',
+                                       default=_default_operating_unit, required=True)
 
     @api.multi
     def pos_order_report(self):
