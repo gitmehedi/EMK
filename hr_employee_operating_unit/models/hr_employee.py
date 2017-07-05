e33241a2df87f1fe164e5f1442cf95bdef2a8361
rabbi <rabbi998@gmail.com>
2017-07-05 15:13:18 +0600

    Operating unit dropdown filter added, now OP unit is after company ID

--- hr_employee_operating_unit/models/hr_employee.py
+++ hr_employee_operating_unit/models/hr_employee.py
@@ -3,8 +3,10 @@ from openerp import fields, models
 class HrEmployee(models.Model):
 
     _inherit = 'hr.employee'
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id)
 
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
                                         default=lambda self:
                                         self.env['res.users'].
-                                        operating_unit_default_get(self._uid))
+                                        operating_unit_default_get(self._uid),
+                                      )
