371f63475211f0aee59aa64bf6a2b376b25eb023
gitmehedi <git.mehedi@gmail.com>
2017-07-06 11:36:21 +0600

    [FIX] Fix bug on purchase for imported data

--- purchase_order_pebbles/models/inherited_purchase_order_line.py
+++ purchase_order_pebbles/models/inherited_purchase_order_line.py
@@ -6,6 +6,18 @@ class InheritedPurchaseOrderLine(models.Model):
 
 	product_id = fields.Many2one('product.template', 'Product', domain=[('type','!=','service')])
 	receive_qty = fields.Float(string='Receive Quantity',default="0.0")
+
+	def onchange_product_id(self, cr, uid, ids, pricelist_id, product_id, qty, uom_id,
+							partner_id, date_order=False, fiscal_position_id=False, date_planned=False,
+							name=False, price_unit=False, state='draft', context=None):
+
+		if product_id:
+			prod_pool = self.pool.get('product.product')
+			product_id = prod_pool.search(cr, uid, [('product_tmpl_id','=',product_id)])[0]
+
+
+			return super(InheritedPurchaseOrderLine, self).onchange_product_id(cr, uid, ids,pricelist_id, product_id, qty, uom_id,
+							partner_id, date_order, fiscal_position_id, date_planned,name, price_unit, state, context)
 		
 	def name_get(self, cr, uid, ids, context):
 		res = []
