1877cd15b08bb142f6edf01128360f906f504d58
rabbi <rabbi998@gmail.com>
2017-07-13 21:32:54 +0600

    Short leave access rule added. Sequence added for leave days correction

--- gbs_hr_leave_duration_correction/__manifest__.py
+++ gbs_hr_leave_duration_correction/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'HR Leave Days Duration Correction',
     'version': '1.0',
     'category': 'leave',
+    'sequence': 30,
     'author': 'Genweb2 Limited',
     'website': 'www.genweb2.com',
     'summary': "This modules removes datetime from date_from and date_to fields and adds only date",
