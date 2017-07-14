ef02d8d6831647beccf4027a8eeb9f0ea52d0040
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-07-14 15:56:18 +0600

    Shifting Batch validation done,some small issue resolved

--- gbs_hr_attendance_grace_time/models/hr_attendance_grace_time.py
+++ gbs_hr_attendance_grace_time/models/hr_attendance_grace_time.py
@@ -20,9 +20,14 @@ class HrAttendanceGraceTime(models.Model):
     operating_unit_id = fields.Many2one('operating.unit', string='Select Operating Unit',
                                         required='True',
                                         )
-    check_current_date = fields.Boolean(compute='_compute_date', string='Date Check')
+    check_current_date = fields.Boolean(compute='_compute_date',string='Date Check',readonly=True)
 
 
+    @api.multi
+    @api.depends('effective_from_date')
+    def _compute_date(self):
+        if self.effective_from_date==date.today():
+            self.check_current_date=1
 
     @api.onchange('company_id')
     def onchange_company_id(self):
@@ -52,7 +57,6 @@ class HrAttendanceGraceTime(models.Model):
         query = """select MAX(effective_from_date) from hr_attendance_grace_time where company_id=%s and operating_unit_id=%s"""
         self._cr.execute(query, tuple([self.company_id.id,self.operating_unit_id.id]))
         get_previous_row_effective_from_date = self._cr.fetchone()
-        print get_previous_row_effective_from_date[0]
         # get_previous_row_effective_from_date = get_previous_row_value[0]
         if get_previous_row_effective_from_date[0] > self.effective_from_date:
             raise ValidationError(_("Present effective date can not less then previous effective date!!"))
--- hr_rostering/wizard/hr_shift_employee_batch_wizard.py
+++ hr_rostering/wizard/hr_shift_employee_batch_wizard.py
@@ -1,33 +1,42 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
+from datetime import date
 
 class HrShiftBatchEmployees(models.TransientModel):
     _name = 'hr.batch.employees'
     _description = 'Create Batch for all selected employees'
 
-    employee_ids = fields.Many2many('hr.employee', 'hr_shift_employee_group_rel', 'shift_id', 'employee_id', 'Employees')
-
+    effective_from = fields.Date(string='Effective Start Date', required=True,default=date.today())
+    effective_end = fields.Date(string='Effective End Date', required=True)
+    employee_ids = fields.Many2many('hr.employee', 'hr_shift_employee_group_rel',
+                                    'shift_id', 'employee_id', string='Employees')
+    shift_id = fields.Many2one("resource.calendar", string="Shift Name", required=True,
+                               domain="[('state', '=','approved' )]")
     @api.multi
     def generate_record(self):
         pool_shift_emp = self.env['hr.shifting.history']
         [data] = self.read()
         active_id = self.env.context.get('active_id')
-        if active_id:
-            [run_data] = self.env['hr.shift.employee.batch'].browse(active_id).read(['effective_from', 'effective_end', 'shift_id'])
-        effective_from = run_data.get('effective_from')
-        effective_end = run_data.get('effective_end')
-        shift_id=run_data.get('shift_id')
+        # if active_id:
+        #     [run_data] = self.env['hr.shift.employee.batch'].browse(active_id).read(['effective_from', 'effective_end', 'shift_id'])
+        # effective_from = run_data.get('effective_from')
+        # effective_end = run_data.get('effective_end')
+
+        effective_from = self.effective_from
+        effective_end = self.effective_end
+        shift_id=self.shift_id
         if not data['employee_ids']:
-            raise UserError(_("You must select employee(s) to generate payslip(s)."))
-        print shift_id[0]
+            raise UserError(_("You must select employee(s) to generate this process."))
         for employee in self.env['hr.employee'].browse(data['employee_ids']):
             # history_data = self.env['hr.shifting.history'].onchange_employee_id(shift_id,effective_from, effective_end, employee.id, contract_id=False)
             res = {
                 'employee_id': employee.id,
-                'shift_id': shift_id[0],
+                'shift_id': shift_id.id,
                 'effective_from': effective_from,
                 'effective_end': effective_end,
                 'shift_batch_id': active_id,
             }
             pool_shift_emp += self.env['hr.shifting.history'].create(res)
+        query = """ UPDATE hr_shift_employee_batch SET effective_from = %s,effective_end=%s,shift_id=%s WHERE id = %s"""
+        self._cr.execute(query, tuple([self.effective_from,self.effective_end,self.shift_id.id ,active_id]))
         return {'type': 'ir.actions.act_window_close'}
