6fa6b3eef32c9da3709f10d20bd9bcb4dea38619
gitmehedi <git.mehedi@gmail.com>
2017-07-17 18:31:40 +0600

    [MOD] Add new module reprint_pos_report for printing modules

---
+++ reprint_pos_report/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Reprint PoS Sale Ticket",
+    'author': 'Genweb2 Limited',
+    'website': "http://www.genweb2.com",
+    'category': 'Point of Sale',
+    'version': '0.1',
+    'summary': """
+        Reprint point of sale report from backend 
+    """,
+    'description': """
+        Reprint point of sale report from backend
+    """,
+    'depends': ['point_of_sale',
+                ],
+    'data': [
+        'report/reprint_sale_ticket.xml',
+        'report/reprint_sale_ticket_view.xml',
+        'views/inherit_pos_order_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
