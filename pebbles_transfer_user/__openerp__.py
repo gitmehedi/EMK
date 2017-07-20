1685344fa25d0071bd75ba2d2278a622d4756d62
gitmehedi <git.mehedi@gmail.com>
2017-07-20 19:58:43 +0600

    [ADD] Add new security modules pebbles_transfer_user

---
+++ pebbles_transfer_user/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Pebbles Transfer Users',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Pebbles Module',
+    'depends': ['base'],
+    'data': [
+        'security/security.xml',
+    ],
+    'summary': 'Pebbles Transfer Users',
+    'category': 'Permission',
+    'summary': 'Pebbles Transfer Users',
+    'description': 'Pebbles Transfer Users',
+    'installable': True,
+    'application': True,
+}
