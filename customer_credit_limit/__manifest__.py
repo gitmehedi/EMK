febb837480659b5ee055b270ec45c53501dd90c6
ritachowdhury <ritaislam6789@gmail.com>
2017-07-27 13:47:06 +0600

    [ADD]new module Customer credit limi

---
+++ customer_credit_limit/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Customer Credit Limited',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Sale',
+    'version': '1.0',
+    'depends': [
+        'hr',
+        'base',
+    ],
+    'data': [
+        'views/limit_view.xml',
+        'views/partner_inherit_view.xml',
+    ],
+    'summary': '',
+    'description':
+        "",
+    'installable': True,
+    'application': True,
+}
