9ef607ac209f9532381cc737c934caf5a61cb256
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-07-27 21:58:24 +0600

    Security Rule change in Manual Attendance and Alter Rostering

--- hr_rostering/models/hr_shift_alter.py
+++ hr_rostering/models/hr_shift_alter.py
@@ -7,31 +7,6 @@ class HRShiftAlter(models.Model):
     _name = 'hr.shift.alter'
     _rec_name = 'employee_id'
 
-
-    # user access funtion
-    def _employee_check_hr_manager(self):
-        user = self.env.user.browse(self.env.uid)
-        if user.has_group('hr.group_hr_manager'):
-            return True
-        else:
-            return False
-
-    def _employee_check_user(self):
-        user = self.env.user.browse(self.env.uid)
-        if user.has_group('base.group_user'):
-            return True
-        else:
-            return False
-
-
-    def _employee_check_dept_manager(self):
-        user = self.env.user.browse(self.env.uid)
-        if user.has_group('gbs_base_package.group_dept_manager'):
-            return True
-        else:
-            return False
-
-
     def _default_employee(self):
         return self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
 
@@ -44,14 +19,11 @@ class HRShiftAlter(models.Model):
     duty_end = fields.Datetime(string='Duty End')
     is_included_ot = fields.Boolean(string='Is OT')
 
+    my_menu_check = fields.Boolean(string='Check',readonly=True)
+
     ot_start = fields.Datetime(string='OT Start')
     ot_end = fields.Datetime(string='OT End')
     grace_time = fields.Float(string='Grace Time', default='1.5')
-
-    # user access fields
-    user_access_hr_manager = fields.Boolean(string='User Access Hr Manager',default=_employee_check_hr_manager)
-    user_access_dept_manager = fields.Boolean(string='User Access Department Manager',default=_employee_check_dept_manager)
-    user_access = fields.Boolean(string='User Access Normal',default=_employee_check_user)
 
     manager_id = fields.Many2one('hr.employee', string='Final Approval', readonly=True, copy=False)
 
@@ -61,13 +33,6 @@ class HRShiftAlter(models.Model):
         ('approved', "Approved"), 
         ('refuse', 'Refused'),
     ], default = 'draft')
-
-    # @api.onchange('user_access_dept_manager')
-    # def onchange_user_access_dept_manager(self):
-    #     if self.user_access_hr_manager!=True and self.user_access_dept_manager:
-    #         return {'domain': {'employee_id': [('department_id.manager_id.user_id','=',self.env.user.id)]}}
-    #     else:
-    #         return {'domain': {'employee_id': [('company_id', '=', self.env.user.company_id.id )]}}
 
     @api.multi
     def action_approve(self):
