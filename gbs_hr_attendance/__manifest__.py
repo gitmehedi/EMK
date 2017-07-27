957db71780dd00aa7205c901af5cc75af3416773
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-07-27 12:12:53 +0600

    Inherited module HR Attendance created

---
+++ gbs_hr_attendance/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'GBS HR Attendance',
+    'version': '1.0.0',
+    "author": "Genweb2 Limited",
+    "website": "http://www.genweb2.com",
+    'category': 'Human Resources',
+    
+    'depends': ['hr',
+                'hr_Attendance',
+                ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+}
