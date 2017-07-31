73101be7cef531d6348151bf8ab2b3682a264db8
rabbi <rabbi998@gmail.com>
2017-07-31 19:38:36 +0600

    Some bugs fixed, menu added user Sales>Products

--- gbs_sales_price_change/models/product_sale_price_history_line.py
+++ gbs_sales_price_change/models/product_sale_price_history_line.py
@@ -7,7 +7,6 @@ class ProductSalePriceHistiryLine(models.Model):
     _rec_name = 'product_id'
 
     product_id = fields.Many2one('product.product', string="Product", required=True, domain=[('sale_ok', '=', True)],readonly=True)
-    list_price = fields.Float(string='Old Price', related='product_id.list_price', readonly=True)
+    list_price = fields.Float(string='Old Price', readonly=True)
     new_price = fields.Float(string='New Price', required=True,readonly=True)
     approve_price_date = fields.Date(string='Approved Date',readonly=True)
-
