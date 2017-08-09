37da8a8cd29a10419a6a55c1891c5ff5817c0dbb
gitmehedi <git.mehedi@gmail.com>
2017-08-09 15:29:19 +0600

    [ADD] POS REF: Add point of sale referrence no in search option

--- pos_loyalty/__openerp__.py
+++ pos_loyalty/__openerp__.py
@@ -2,19 +2,21 @@
 
 {
     'name': 'POS Loyalty',
-    'author':  'Genweb2 Limited',
+    'author': 'Genweb2 Limited',
     'website': 'www.genweb2.com',
     'category': 'Garments & Apparels',
     'data': [
-            'security/ir.model.access.csv',
-            'views/loyalty_rules_view.xml',
-            'views/pos_loyality_assets.xml',
-            'views/assets.xml',
-            'views/inherited_account_view.xml',
-            'views/inherited_point_of_sale_view.xml',
-            'views/inherited_pos_session_view.xml',
-            'reports/inherited_pos_session_summary_report.xml',
-             ],
+        'security/ir.model.access.csv',
+        'views/loyalty_rules_view.xml',
+        'views/pos_loyality_assets.xml',
+        'views/assets.xml',
+        'views/inherited_account_view.xml',
+        'views/inherited_point_of_sale_view.xml',
+        'views/inherited_pos_session_view.xml',
+        'views/inherited_pos_order_view.xml',
+        'reports/inherited_pos_session_summary_report.xml',
+
+    ],
     'summary': 'POS Loyalty for point calculation and redemption',
     'depends': ['point_of_sale'],
     'description': 'Test Description',
@@ -22,20 +24,17 @@
     'summary': 'POS module extension',
     'description': """POS Loyalty for point calculation and redemption""",
     'depends': ["point_of_sale"],
-  
+
     'qweb': [
         'static/src/xml/pos_enhanced_template.xml',
     ],
-    'css': [ 
-           'static/src/js/pos_loyalty_custom.css',   
-           ],
-    'js': [ 
-           'static/src/js/pos_loyalty_custom.js',
-           ],
+    'css': [
+        'static/src/js/pos_loyalty_custom.css',
+    ],
+    'js': [
+        'static/src/js/pos_loyalty_custom.js',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
 }
-
-
-
