4378133d63c0454f2a7ad84f1eab98f052581cb1
ritachowdhury <ritaislam6789@gmail.com>
2017-08-14 11:18:08 +0600

    [ADD]Create a new bash_vat_bd for two field add into customer profile in Sales Module

---
+++ base_vat_bd/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Customers',
+    'version': '1.0',
+    'author': 'Genweb2 Limited',
+    'website': 'http://www.genweb2.com',
+    'category': 'Sales',
+    'depends': [
+        'sale',
+        'sales_team',
+    ],
+    'data': [
+        'views/inherited_res_partner_view.xml',
+        ],
+    'description': 'Customer TEX And VAT Registration Details',
+    'installable': True,
+    'application': True,
+}
