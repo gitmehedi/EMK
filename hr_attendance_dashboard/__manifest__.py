306345cdaa9cf954baad1ad32b4ec2da288b2a98
root <matiar.rahman@gmail.com>
2017-08-21 19:20:40 +0600

    add description

--- hr_attendance_dashboard/__manifest__.py
+++ hr_attendance_dashboard/__manifest__.py
@@ -1,11 +1,13 @@
 {
-    'name': 'Hr Attendance Dashboard',
-    'author':  'Genweb2',
+    'name': 'Employee Attendance Dashboard',
+    'description': '''Employee Attendance Dashboard''',
+    'author':  'Genweb2 Limited',
+    'version': '0.1',
     'website': 'www.genweb2.com',
     'data': [
              'hr_attendance_dashboard_view.xml',
              ],
     'depends': ['hr','hr_employee_operating_unit'],
-    'installable': True,
-    'version': '0.1',
+    'application': False,
+
 }
