6a3d8af3ba243659de78102cb40ea974a2e9b3eb
ritachowdhury <ritaislam6789@gmail.com>
2017-08-25 16:49:58 +0600

    [MOD]Modify delivery order

--- delivery_order/models/delivery_order.py
+++ delivery_order/models/delivery_order.py
@@ -1,4 +1,6 @@
 from openerp import api, fields, models, exceptions, _
+from openerp.exceptions import UserError, ValidationError
+import time,datetime
 
 class SaleDeliveryOrder(models.Model):
     _name = 'delivery.order'
@@ -7,20 +9,22 @@ class SaleDeliveryOrder(models.Model):
     def _current_employee(self):
         return self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
 
-    name = fields.Char(size=100, string="Name", required=True)
-    so_date = fields.Date('Sales Order Date',readonly= True)
-    approve_date = fields.Date('Approve Date')
-    deli_address = fields.Char('Delivery Address')
-    approve_by = fields.Many2one('hr.employee', string="Approved By", default=_current_employee, readonly= True)
-
-
-
+    name = fields.Char(size=100, string="Name", required=True ,readonly=True,
+                             states={'draft': [('readonly', False)]})
+    so_date = fields.Date('Sales Order Date', readonly=True,
+                             states={'draft': [('readonly', False)]})
+    deli_address = fields.Char('Delivery Address', readonly=True,
+                             states={'draft': [('readonly', False)]})
     """ All relations fields """
 
-    sale_order_id = fields.Many2one('sale.order',string='Order Lines')
-    parent_id = fields.Many2one('res.partner', 'Customer', ondelete='cascade', readonly=True)
-    payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms', readonly=True)
-    warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse', readonly=True)
+    sale_order_id = fields.Many2one('sale.order',string='Order Lines', required=True, readonly=True,
+                             states={'draft': [('readonly', False)]})
+    parent_id = fields.Many2one('res.partner', 'Customer', ondelete='cascade', readonly=True,
+                             states={'draft': [('readonly', False)]})
+    payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms', readonly=True,
+                             states={'draft': [('readonly', False)]})
+    warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse', readonly=True,
+                             states={'draft': [('readonly', False)]})
     line_ids = fields.One2many('delivery.order.line', 'parent_id', string="Products", readonly=True,
                                states={'draft': [('readonly', False)]})
     cash_ids = fields.One2many('cash.payment.line', 'pay_cash_id', string="Payment Terms:Cash", readonly=True,
@@ -32,31 +36,71 @@ class SaleDeliveryOrder(models.Model):
                                  states={'draft': [('readonly', False)]})
     lc_ids = fields.One2many('lc.payment.line', 'pay_lc_id', string="Payment Terms:Cash", readonly=True,
                              states={'draft': [('readonly', False)]})
-    """ All Selection fields """
-    state = fields.Selection([
-        ('draft', "Draft"),
-        ('applied', "Applied"),
-        ('approved', "Approved"),
-    ], default='draft')
 
+    requested_by = fields.Many2one('hr.employee', string="Requested By", default=_current_employee, readonly=True)
+
+    approver1_id = fields.Many2one('hr.employee', string="Approved By",
+                                   readonly=True)
+    approver2_id = fields.Many2one('hr.employee', string="Confirmed By",
+                                   readonly=True)
+
+    requested_date = fields.Date(string="Requested Date", default=datetime.date.today(), readonly=True)
+    approved_date = fields.Date('Approved Date',
+                                states={'draft': [('invisible', True)],
+                                        'validate': [('invisible', True)],
+                                        'close': [('invisible', False), ('readonly', True)],
+                                        'approve': [('invisible', False), ('readonly', True)]})
+    confirmed_date = fields.Date(string="Confirmed Date", _defaults=lambda *a: time.strftime('%Y-%m-%d'), readonly=True)
+
+
+    """ State fields for containing various states """
     so_type = fields.Selection([
-        ('credit_sales', 'Credit Sales'),
-        ('lc_sales', 'LC Sales'),
-    ], string='Sale Order Type',readonly= True)
+        ('cash', 'Cash'),
+        ('credit_sales', 'Credit'),
+        ('lc_sales', 'L/C'),
+    ], string='Sale Order Type', readonly=True, states={'draft': [('readonly', False)]})
 
-    """All function which process data and operation"""
+    """ State fields for containing various states """
+    state = fields.Selection([
+        ('draft', "To Submit"),
+        ('validate', "To Approve"),
+        ('approve', "Second Approval"),
+        ('close', "Approved")
+    ], default='draft')
+    """ All functions """
 
     @api.multi
+    def unlink(self):
+        for order in self:
+            if order.state != 'draft':
+                raise UserError(_('You can not delete this.'))
+            order.line_ids.unlink()
+        return super(SaleDeliveryOrder, self).unlink()
+
+    @api.one
     def action_draft(self):
         self.state = 'draft'
+        self.line_ids.write({'state':'draft'})
 
-    @api.multi
-    def action_confirm(self):
-        self.state = 'applied'
+    @api.one
+    def action_approve(self):
+        self.state = 'approve'
+        self.line_ids.write({'state':'approve'})
+        self.approver2_id = self.env.user.employee_ids.id
+        return self.write({'state': 'approve', 'approved_date': time.strftime('%Y-%m-%d %H:%M:%S')})
 
-    @api.multi
-    def action_done(self):
-        self.state = 'approved'
+    @api.one
+    def action_validate(self):
+        self.state = 'validate'
+        self.line_ids.write({'state':'validate'})
+
+    @api.one
+    def action_close(self):
+        self.state = 'close'
+        self.line_ids.write({'state':'close'})
+        self.approver1_id = self.env.user.employee_ids.id
+        return self.write({'state': 'close', 'confirmed_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+
 
     @api.onchange('sale_order_id')
     def onchange_sale_order_id(self):
@@ -68,3 +112,5 @@ class SaleDeliveryOrder(models.Model):
                 self.warehouse_id = sale_order_obj.warehouse_id.id
                 self.so_type = sale_order_obj.credit_sales_or_lc
                 self.so_date = sale_order_obj.confirmation_date
+
+
