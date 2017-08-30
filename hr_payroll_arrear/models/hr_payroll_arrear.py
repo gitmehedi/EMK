9f6eed757fea97161526ed2dd545fba977b8cfb0
ritachowdhury <ritaislam6789@gmail.com>
2017-08-30 18:47:32 +0600

    [ADD]Create new module hr_payroll_arrear module

--- hr_payroll_arrear/models/hr_payroll_arrear.py
+++ hr_payroll_arrear/models/hr_payroll_arrear.py
@@ -1,2 +1,52 @@
 from openerp import api, fields, models, _
 from openerp.exceptions import UserError, ValidationError
+
+
+class HrEmployeeArrear(models.Model):
+    _name = 'hr.payroll.arrear'
+    _order = 'name desc'
+
+    name = fields.Char(size=100, string="Description", required=True, readonly=True,
+                       states={'draft': [('readonly', False)]})
+    company_id = fields.Many2one('res.company', string='Company', index=True,
+                                 default=lambda self: self.env.user.company_id)
+
+    """ All relations fields """
+    line_ids = fields.One2many('hr.payroll.arrear.line', 'parent_id', string="Arrear Details", readonly=True,
+                               states={'draft': [('readonly', False)]})
+
+    """ All Selection fields """
+    state = fields.Selection([
+        ('draft', "Draft"),
+        ('applied', "Applied"),
+        ('approved', "Approved"),
+    ], default='draft')
+
+    """All function which process data and operation"""
+
+    @api.multi
+    def action_draft(self):
+        self.state = 'draft'
+        self.line_ids.write({'state': 'draft'})
+
+    @api.multi
+    def action_confirm(self):
+        self.state = 'applied'
+        self.line_ids.write({'state': 'applied'})
+
+    @api.multi
+    def action_done(self):
+        self.state = 'approved'
+        self.line_ids.write({'state': 'approved'})
+
+    @api.multi
+    def unlink(self):
+        for bill in self:
+            if bill.state != 'draft':
+                raise UserError(_('You can not delete this.'))
+            bill.line_ids.unlink()
+        return super(HrEmployeeArrear, self).unlink()
+
+
+
+
