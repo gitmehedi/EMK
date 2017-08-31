faa78fea8edeaa059c6f230111c68a8cde8c7e0e
gitmehedi <git.mehedi@gmail.com>
2017-08-31 16:42:42 +0600

    [DEV] Development some feature which is not complete properly yet.

--- stock_distribution_matrix/models/__init__.py
+++ stock_distribution_matrix/models/__init__.py
@@ -3,3 +3,4 @@ import inventory_distribution_to_shop
 import inventory_distribution_to_shop_line
 import stock_distribution_to_shop_notification
 import inherited_stock_inventory
+import purchase_order_quantity
