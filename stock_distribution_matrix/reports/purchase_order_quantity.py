f63e78562da0c0be5aa07a863c6ade295611bf67
gitmehedi <git.mehedi@gmail.com>
2017-09-11 19:01:35 +0600

    [IMP] Stock Distribution: Implement stock distribution for quantity.

--- stock_distribution_matrix/reports/purchase_order_quantity.py
+++ stock_distribution_matrix/reports/purchase_order_quantity.py
@@ -9,58 +9,36 @@ class StockRequisitionTransfer(models.AbstractModel):
         report_obj = self.env['report']
         report = report_obj._get_report_from_name('stock_distribution_matrix.purchase_order')
         records = self.env['purchase.order'].search([('id', '=', self.id)])
-        # data = {
-        #     "256L": {
-        #         'size': ['S', 'M', 'L', 'Multi'],
-        #         'color': {
-        #             'YELLOW': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #             'PIECE': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #             'CREAM': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #             'DEEP OLIVE': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #         }
-        #     },
-        #     "8000": {
-        #         'size': ['S', 'M', 'L', 'XL'],
-        #         'color': {
-        #             'YELLOW': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #             'PIECE': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #             'CREAM': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #             'DEEP OLIVE': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-        #         }
-        #     }
-        # }
-        color = 1
-        size = 2
-        # records.order_quantity_ids[4].product_id.attribute_value_ids[1].attribute_id
-        value = list(set([record.product_id.product_tmpl_id.name for record in records.order_quantity_ids]))
 
+        color, size = 1, 2
+
+        value = list(set([record.product_id.product_tmpl_id.name for record in records.order_quantity_ids]))
         colors = set([val.name for qty in records.order_quantity_ids for val in qty.product_id.attribute_value_ids if
                       val.attribute_id.id == color])
         sizes = set([val.name for qty in records.order_quantity_ids for val in qty.product_id.attribute_value_ids if
                      val.attribute_id.id == size])
-        data = {d: {'color': {c:[] for c in colors}} for d in value}
+
+        data = {d: {'color': {c: {s: {} for s in sizes} for c in colors}} for d in value}
+
         if records:
             for record in records.order_quantity_ids:
+                sz = None
+                cl = None
                 for clr in record.product_id.attribute_value_ids:
+                    if clr.name in sizes:
+                        sz = clr.name
                     if clr.name in colors:
-                        data[record.product_id.product_tmpl_id.name]['color'][clr.name].append((record.product_id.default_code, record.quantity))
-                # rec = {
-                #     'YELLOW': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-                #     'PIECE': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-                #     'CREAM': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-                #     'DEEP OLIVE': [(2, 2000), (3, 2000), (4, 2000), (5, 2000)],
-                # }
-                #         rec[]
-                #         rec = {
-                #             'name': record.product_id.display_name,
-                #             'size': record.product_id.default_code,
-                #             'quantity': record.quantity,
-                #         }
+                        cl = clr.name
 
+                    if sz and cl:
+                        data[record.product_id.product_tmpl_id.name]['color'][cl][sz][
+                            'code'] = record.product_id.default_code
+                        data[record.product_id.product_tmpl_id.name]['color'][cl][sz]['qty'] = record.quantity
 
         docargs = {
             'doc_ids': self._ids,
             'data': data,
+            'sizes': sizes,
             'doc_model': report.model,
             'docs': self,
         }
