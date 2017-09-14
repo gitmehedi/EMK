0f8100bf9d2dd313d27e0ad5deee22507ec4963b
gitmehedi <git.mehedi@gmail.com>
2017-09-14 16:10:37 +0600

    [ADD] Stock Distribution Matrix: Add report for stock distribution report which is transfer from warehouse to various shop.

--- stock_distribution_matrix/reports/stock_distribution_to_shop.py
+++ stock_distribution_matrix/reports/stock_distribution_to_shop.py
@@ -10,30 +10,34 @@ class StockRequisitionTransfer(models.AbstractModel):
         report = report_obj._get_report_from_name('stock_distribution_matrix.stock_distribution_to_shop')
 
         records = self.env['stock.distribution.to.shop'].search([('id', '=', self.id)])
-        data, count = [], 0
+        data, count = {}, 0
+
+        configObj = self.env['pos.config'].search([('state', '=', 'active'), ('active_shop', '=', True)])
+        config = {conf.id: conf.name for conf in configObj}
+
+        for val in records.product_tmp_id.product_variant_ids:
+            data[val.id] = {
+                'name': val.display_name,
+                'value': {key: {'name': val, 'value': None} for key, val in config.iteritems()}
+            }
 
         if records:
             header = {}
             header['product_name'] = records.product_tmp_id.name
             header['warehouse'] = records.warehoue_id.name
-
+            header['name'] = records.name
+        #
         for record in records.stock_distribution_lines_ids:
-            rec = {}
-            count = count + 1
-            rec['sn'] = count
-            rec['barcode'] = record.product_id.default_code
-            rec['name'] = record.product_id.name
-            rec['quantity'] = record.quantity
-            rec['receive_quantity'] = record.receive_quantity
-            data.append(rec)
-
-        print data
+            data[record.product_id.id]['value'][record.pos_shop_id.id]['value'] = int(record.distribute_qty)
 
         docargs = {
             'doc_ids': self._ids,
             'data': data,
-            'record': header_info,
+            'record': header,
+            'config': config,
             'doc_model': report.model,
             'docs': self,
+
         }
-        return report_obj.render('stock_requisition_transfer.stock_requisition', docargs)
+
+        return report_obj.render('stock_distribution_matrix.stock_distribution_to_shop', docargs)
