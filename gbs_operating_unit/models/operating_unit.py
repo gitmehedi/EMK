1869679ca86a3467f92b0fa0d8192c0fcf3d9cfb
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-09-18 13:48:36 +0600

    [IMP]Order by Sequence,implemented in Attendance Dashboard

--- gbs_operating_unit/models/operating_unit.py
+++ gbs_operating_unit/models/operating_unit.py
@@ -3,5 +3,6 @@ from odoo.exceptions import UserError, AccessError, ValidationError
 
 class GbsOperatingUnit(models.Model):
     _inherit = 'operating.unit'
+    _order = 'operating_unit_sequence'
 
     operating_unit_sequence = fields.Integer(string="Sequence")
