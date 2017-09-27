e3f6e4784ead8d00eed81e8dc3d3bc5298d6454d
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-09-27 20:26:13 +0600

    [IMP]Appointment Letter template created,print option not done

--- gbs_hr_recruitment/models/hr_applicant_inherit.py
+++ gbs_hr_recruitment/models/hr_applicant_inherit.py
@@ -15,7 +15,7 @@ class HrApplicantInherit(models.Model):
         ('approved', 'Approved'),
         ('declined', 'Declined'),
         ('reset', 'Reset To Draft'),
-    ], string='Status', default='draft')
+    ], string='Status', default='draft',track_visibility='onchange')
 
     @api.multi
     def generate_appointment_letter(self):
@@ -23,6 +23,33 @@ class HrApplicantInherit(models.Model):
 
         data['applicant_id'] = self.id
         return self.env['report'].get_action(self, 'gbs_hr_recruitment.report_app_letter', data=data)
+
+    # @api.multi
+    # def generate_appointment_letter(self):
+    #
+    #     ctx = dict()
+    #     ctx.update({
+    #         'default_use_template': True,
+    #          'default_composition_mode': 'comment',
+    #          'default_template_id': 17,
+    #          'mark_so_as_sent': True,
+    #          'custom_layout': 'sale.mail_template_data_notification_email_sale_order',
+    #          'default_model': 'sale.order',
+    #          'default_res_id': 1
+    #     })
+    #
+    #     return {
+    #         'type': 'ir.actions.act_window',
+    #         'view_type': 'form',
+    #         'view_mode': 'form',
+    #         'res_model': 'mail.compose.message',
+    #         'views': [(compose_form_id, 'form')],
+    #         'view_id': compose_form_id,
+    #         'target': 'new',
+    #         'context': ctx,
+    #     }
+    #     return self.env['report'].render('gbs_hr_recruitment.report_app_letter1', ctx)
+
 
     ####################################################
     # ORM Overrides methods
