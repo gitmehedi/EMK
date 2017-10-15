f3bcfa0cf6d749137c26578769f094e004c8dd5e
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-10-15 20:54:15 +0600

    [ADD]Extend module added for website hr recruitment controller,new module added for application form

---
+++ website_hr_recruitment_controller/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'Website Hr Recruitment Controller',
+    'version' : '1.0',
+    'author' : 'Genweb2 Limited',
+    'summary': 'Custom Controller for website hr recruitment .',
+    'description': """""",
+    'website' : 'http://www.genweb2.com',
+    'depends' : [
+        'website_hr_recruitment',
+    ],
+    'data' : [
+    ],
+    'auto_install': False,
+    'application': False,
+    'installable': True,
+}
