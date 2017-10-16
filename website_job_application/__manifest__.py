8a7378f60b78623ee8e80305c8ac7a3e3726766e
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-10-16 13:11:04 +0600

    [IMP]Submit bug fix in website job application

--- website_job_application/__manifest__.py
+++ website_job_application/__manifest__.py
@@ -4,10 +4,12 @@
     'summary': 'Extend Job Application Module',
     'author': 'Md. Mehedi Hasan',
     'website': 'https://github.com/gitmehedi',
-    'depends': ['web','hr_recruitment'],
+    'depends': ['web','hr_recruitment','website_hr_recruitment'],
     'data': [
         'views/inherited_hr_applicant_views.xml',
         'views/inherited_website_hr_recuitment_templates.xml',
+        'report/applicant_form_report.xml',
+        'report/applicant_form_report_template.xml',
     ],
     # 'qweb': [
     #     'static/src/xml/template.xml'
