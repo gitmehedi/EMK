7455ced691296bff6bca738caaea6362af69a274
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-10-17 16:55:01 +0600

    [ADD]District module added

--- bmd_recruitment_rule/models/bmd_inherit_job_position.py
+++ bmd_recruitment_rule/models/bmd_inherit_job_position.py
@@ -5,11 +5,5 @@ class BMDInheritedHrJob(models.Model):
     _inherit = 'hr.job'
 
     job_publish_date = fields.Date(string='Published Date')
+    required_education = fields.Many2one('hr.recruitment.degree',string = 'Educational Qualification')
     authorize_district = fields.Many2many('job.district', 'job_id', 'job_district_id', 'trans_id')
-
-
-
-class BMDJobDistrict(models.Model):
-    _name = 'job.district'
-
-    name = fields.Char(string='District Name')
