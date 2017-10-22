38fdb3c0afe5d085f0f8d9bed6f36a33c4d01842
mrbappy <mahfuz.bappy@gmail.com>
2017-10-22 18:37:25 +0600

    Update

--- hr_attendance_settings/models/hr_attendance_config_settings.py
+++ hr_attendance_settings/models/hr_attendance_config_settings.py
@@ -3,10 +3,9 @@ from odoo import api,fields, models
 class HRAttendanceConfigSettings(models.TransientModel):
     _name = 'hr.attendance.config.settings'
 
-    late_salary_deduction_rule = fields.Char(size=2,string = 'Allow rule, if any employee late in 3 days his/her salary will deduct.')
+    late_salary_deduction_rule = fields.Char(size=2)
 
-    @api.model
+    @api.one
     def execute(self):
-        return
-
-
+        vals = {'late_salary_deduction_rule': self.late_salary_deduction_rule}
+        return self.create(vals)
