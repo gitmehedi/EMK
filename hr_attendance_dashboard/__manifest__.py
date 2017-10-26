3d2ff7fb773ac59d018dc300f5751e2ba086738e
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-10-26 17:05:39 +0600

    [IMP]HR Attendance Dashboard,Access given to CXO and Head of plant

--- hr_attendance_dashboard/__manifest__.py
+++ hr_attendance_dashboard/__manifest__.py
@@ -4,15 +4,17 @@
     'category': 'Dashboard',
     'version': '0.1',
     'website': 'www.genweb2.com',
-    'data': [
-        'views/hr_attendance_dashboard_view.xml',
-        'views/inherited_att_employee_list_view.xml',
-        'security/ir.model.access.csv',
-    ],
     'depends': ['hr',
                 'gbs_operating_unit',
                 'hr_employee_operating_unit',
-                'gbs_hr_attendance_error_correction'],
+                'gbs_hr_attendance_error_correction',
+                'gbs_application_group'],
+    'data': [
+        'views/hr_attendance_dashboard_view.xml',
+        'views/inherited_att_employee_list_view.xml',
+        # 'security/ir.model.access.csv',
+    ],
+
     'description': '''This module help user to see attendance records by the dashboard''',
     'application': False,
     'installable': True,
