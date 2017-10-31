b00795f19d1a62828825ad08818810a629320229
ritachowdhury <ritaislam6789@gmail.com>
2017-10-31 14:55:07 +0600

    [IMP]'Product Price History Button: show tree view varient's group wise by defult, desc order' task is completed

--- product_sales_pricelist/models/product_sale_price_history_line.py
+++ product_sales_pricelist/models/product_sale_price_history_line.py
@@ -5,12 +5,13 @@ class ProductSalePriceHistiryLine(models.Model):
     _name = 'product.sale.history.line'
     _description = "Sale Price History"
     _rec_name = 'product_id'
+    _order = "effective_price_date desc"
 
     product_id = fields.Many2one('product.product', string="Product", required=True, domain=[('sale_ok', '=', True)],readonly=True)
     list_price = fields.Float(string='Old Price', readonly=True)
     new_price = fields.Float(string='New Price', required=True,readonly=True)
     approve_price_date = fields.Date(string='Approved Date',readonly=True)
+    effective_price_date = fields.Date(string='Effective Date',readonly=True)
     currency_id = fields.Many2one('res.currency', string="Currency", readonly=True)
-
     product_package_mode = fields.Many2one('product.packaging.mode', string= 'Packaging Mode', readonly=True)
     uom_id = fields.Many2one('product.uom', string="UoM", readonly=True)
