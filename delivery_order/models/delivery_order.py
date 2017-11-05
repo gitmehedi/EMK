7604aaca7ab898726aed29954addefb20c83c027
ritachowdhury <ritaislam6789@gmail.com>
2017-11-05 15:04:36 +0600

    [FIX] 'cheque line add from account.payment model to delivery.order'task is complited

--- delivery_order/models/delivery_order.py
+++ delivery_order/models/delivery_order.py
@@ -269,7 +269,8 @@ class DeliveryOrder(models.Model):
     def set_cheque_info_automatically(self, account_payment_pool):
         vals = []
         for payments in account_payment_pool:
-            if payments.journal_id.id != 12:## Changeit! 12 == cash
+            print payments.journal_id.type
+            if payments.journal_id.type != 'cash':
                 if not payments.is_this_payment_checked:
                     vals.append((0, 0, {'account_payment_id': payments.id,
                                         'amount': payments.amount,
