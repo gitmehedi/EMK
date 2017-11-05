56cfda975765b8fb21b0c0b5217c6a1500d091e4
mrbappy <mahfuz.bappy@gmail.com>
2017-11-05 11:40:34 +0600

    Update

--- gbs_lc_creation/models/letter_of_credit.py
+++ gbs_lc_creation/models/letter_of_credit.py
@@ -29,19 +29,18 @@ class LetterOfCredit(models.Model):
         help="Local: Local LC.\n"
              "Foreign: Foreign LC.", default="foreign")
 
-    first_party = fields.Many2one('res.company', string='Candidate')
-    first_party_bank = fields.Many2one('res.bank', string='Bank')
+    first_party = fields.Many2one('res.company', string='Candidate', required=True)
+    first_party_bank = fields.Many2one('res.bank', string='Bank', required=True)
 
-    second_party = fields.Many2one('res.partner', domain=[('customer', '=', True)], string='Candidate')
-    second_party_bank = fields.Text(string='Bank')
+    second_party = fields.Many2one('res.partner', domain=[('customer', '=', True)], string='Candidate', required=True)
+    second_party_bank = fields.Text(string='Bank', required=True)
 
-    require_lien = fields.Boolean(string='Require Lien', required=True, readonly=True, default=False,
-                                  states={'confirm': [('readonly', False)]})
-    lien_bank = fields.Text(string='LC Lien Bank', states={'confirm': [('readonly', False)]})
-    lien_date = fields.Date('LC Lien Date', states={'confirm': [('readonly', False)]})
+    require_lien = fields.Boolean(string='Require Lien', readonly=True, default=False)
+    lien_bank = fields.Text(string='LC Lien Bank')
+    lien_date = fields.Date('LC Lien Date')
 
-    lc_value = fields.Integer(string='LC Value', required=True, readonly=True, states={'confirm': [('readonly', False)]})
-    currency_id = fields.Many2one('res.currency', string='Currency', readonly=True, required=True, states={'confirm': [('readonly', False)]})
+    lc_value = fields.Integer(string='LC Value', readonly=True, states={'confirm': [('readonly', False)]}, required=True)
+    currency_id = fields.Many2one('res.currency', string='Currency', required=True)
 
     insurance_company_name = fields.Char(string='Insurance Company Name')
     insurance_number = fields.Char(string='Insurance Number')
@@ -51,22 +50,22 @@ class LetterOfCredit(models.Model):
     discharge_port = fields.Char(string='Discharge Port')
     cnf_agent = fields.Char(string='C&F Agent')
 
-    issue_date = fields.Date('Issue Date', readonly=True,required=True, states={'confirm': [('readonly', False)]})
-    expiry_date = fields.Date('Expiry Date', readonly=True,required=True, states={'confirm': [('readonly', False)]})
-    shipment_date = fields.Date('Shipment Date', readonly=True, states={'confirm': [('readonly', False)]})
+    issue_date = fields.Date('Issue Date', required=True)
+    expiry_date = fields.Date('Expiry Date', required=True)
+    shipment_date = fields.Date('Shipment Date', required=True)
 
-    master_lc_number = fields.Char(string='Master LC Number', readonly=True, states={'confirm': [('readonly', False)]})
+    master_lc_number = fields.Char(string='Master LC Number')
     hs_code = fields.Char(string='HS Code')
-    tolerance = fields.Float(string='Tolerance (%)', required=True, readonly=True, states={'confirm': [('readonly', False)]})## 10% plus and minus value
-    payment_terms = fields.Char(string='Payment Terms',readonly=True, states={'confirm': [('readonly', False)]})
-    period_of_presentation = fields.Float(string='Period of Presentation', readonly=True, states={'confirm': [('readonly', False)]})
-    ship_mode = fields.Char(string='Ship Mode', readonly=True, states={'confirm': [('readonly', False)]})
-    inco_terms = fields.Many2one('stock.incoterms',string='Inco Terms', readonly=True, states={'confirm': [('readonly', False)]})
-    partial_shipment = fields.Boolean(string='Allow Partial Shipment', readonly=True, states={'confirm': [('readonly', False)]})
-    trans_shipment =  fields.Boolean(string='Allow Trans. Shipment', readonly=True, states={'confirm': [('readonly', False)]})
-    lc_mode = fields.Char(string='LC Mode', readonly=True, states={'confirm': [('readonly', False)]})
-    terms_condition = fields.Text(string='Terms of Condition', readonly=True, states={'confirm': [('readonly', False)]})
-    remarks = fields.Text(string='Remarks', readonly=True, states={'confirm': [('readonly', False)]})
+    tolerance = fields.Float(string='Tolerance (%)')## 10% plus and minus value
+    payment_terms = fields.Char(string='Payment Terms')
+    period_of_presentation = fields.Float(string='Period of Presentation')
+    ship_mode = fields.Char(string='Ship Mode')
+    inco_terms = fields.Many2one('stock.incoterms',string='Inco Terms')
+    partial_shipment = fields.Boolean(string='Allow Partial Shipment')
+    trans_shipment =  fields.Boolean(string='Allow Trans. Shipment')
+    lc_mode = fields.Char(string='LC Mode')
+    terms_condition = fields.Text(string='Terms of Condition')
+    remarks = fields.Text(string='Remarks')
 
     attachment_ids = fields.One2many('commercial.attachment', 'lc_id', string='Attachment')
 
@@ -81,12 +80,6 @@ class LetterOfCredit(models.Model):
     _sql_constraints = [
         ('name_uniq', 'unique(name)', 'This Number is already in use')
     ]
-
-    # @api.model
-    # def create(self, vals):
-    #     seq = self.env['ir.sequence'].next_by_code('letter.credit') or '/'
-    #     vals['name'] = seq
-    #     return super(LetterOfCredit, self).create(vals)
 
     @api.multi
     def action_confirm(self):
