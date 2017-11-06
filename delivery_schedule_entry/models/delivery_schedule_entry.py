d8ecdd511b392e0e7b56bad471ae76f9e1fa2988
ritachowdhury <ritaislam6789@gmail.com>
2017-11-06 18:48:58 +0600

    [ADD]' add new feature send by mail for delivery schedule

--- delivery_schedule_entry/models/delivery_schedule_entry.py
+++ delivery_schedule_entry/models/delivery_schedule_entry.py
@@ -56,37 +56,36 @@ class DeliveryScheduleEntry(models.Model):
 
 
 
-    # @api.multi
-    # def generate_schedule_letter(self):
-    #
-    #     self.ensure_one()
-    #     ir_model_data = self.env['ir.model.data']
-    #     try:
-    #         template_id = \
-    #         ir_model_data.get_object_reference('delivery_schedule_entry', 'template_schedule_letter')[1]
-    #     except ValueError:
-    #         template_id = False
-    #     try:
-    #         compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
-    #     except ValueError:
-    #         compose_form_id = False
-    #     ctx = dict()
-    #     ctx.update({
-    #         'default_model': 'delivery.schedule.entry',
-    #         'default_res_id': self.ids[0],
-    #         'default_use_template': bool(template_id),
-    #         'default_template_id': template_id,
-    #         'default_composition_mode': 'comment',
-    #         'mark_so_as_sent': True,
-    #         'custom_layout': "delivery_schedule_entry.template_schedule_letter"
-    #     })
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'view_type': 'form',
-    #         'view_mode': 'form',
-    #         'res_model': 'mail.compose.message',
-    #         'views': [(compose_form_id, 'form')],
-    #         'view_id': compose_form_id,
-    #         'target': 'new',
-    #         'context': ctx,
-    #     }
+    @api.multi
+    def generate_schedule_letter(self):
+
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('delivery_schedule_entry', 'template_schedule_letter')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = dict()
+        ctx.update({
+            'default_model': 'delivery.schedule.entry',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'custom_layout': "delivery_schedule_entry.template_schedule_letter"
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
