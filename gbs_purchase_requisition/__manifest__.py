757818e3b9c6c97e68dd2989a031e921f5349cae
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-11-06 21:02:47 +0600

    [IMP]Purchase Requisition:Access right added,Unlink From Stock Indent,Multiple Indent selection option added

--- gbs_purchase_requisition/__manifest__.py
+++ gbs_purchase_requisition/__manifest__.py
@@ -4,13 +4,16 @@
     'website': 'www.genweb2.com',
     'category': 'Purchase Requisition',
     'version':'10.0.1',
-    'data': [
-        'views/gbs_purchase_requisition_view.xml',
-    ],
     'depends': [
         'purchase_requisition',
-    ],    
-    
+        'stock_indent',
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/gbs_purchase_requisition_view.xml',
+    ],
+
     'description': 
     "This module are compatible for PR",
     'installable': True,
