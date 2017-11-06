9205a751ac47c41ebd00458cb8e60e8b0ec21e21
ritachowdhury <ritaislam6789@gmail.com>
2017-11-06 18:36:56 +0600

    [FIX]'delivery_schedule_entry security issue' is resolved

--- delivery_schedule_entry/__manifest__.py
+++ delivery_schedule_entry/__manifest__.py
@@ -5,9 +5,8 @@
     'category': 'Sales',
     'version':'10.1.1.1',
     'depends': [
-        # 'sale',
-        # 'product_sales_pricelist',
-        # 'account',
+        'sale',
+        'product_sales_pricelist',
         'gbs_application_group',
         'sale_order_type',
     ],
@@ -15,7 +14,7 @@
     'data': [
         'security/ir.model.access.csv',
         #'security/ir_rule.xml',
-        'data/mail_template_data.xml',
+        #'data/mail_template_data.xml',
         'reports/delivery_se_report.xml',
         'reports/delivery_se_report_templates.xml',
         'views/delivery_schedule_entry_view.xml',
