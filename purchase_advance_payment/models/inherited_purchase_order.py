fdf9d4616ba8b42331967c872daf718f900a6a1f
gitmehedi <git.mehedi@gmail.com>
2017-11-09 18:48:01 +0600

    [ADD] Add purchase advance payment functionaltity for pebbles.

---
+++ purchase_advance_payment/models/inherited_purchase_order.py
@@ -0,0 +1,116 @@
+from openerp.osv import osv
+from openerp import models, fields
+from openerp.tools.translate import _
+
+
+class InheritedPuchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    def manual_invoice(self, cr, uid, ids, context=None):
+        """ create invoices for the given sales orders (ids), and open the form
+            view of one of the newly created invoices
+        """
+        purchse_obj = self.pool.get('purchase.order')
+        inv_line_obj = self.pool.get('account.invoice.line')
+        ir_property_obj = self.pool.get('ir.property')
+        fiscal_obj = self.pool.get('account.fiscal.position')
+        mod_obj = self.pool.get('ir.model.data')
+
+        # create invoices through the sales orders' workflow
+        inv_ids0 = set(inv.id for purchase in self.browse(cr, uid, ids, context) for inv in purchase.invoice_ids)
+        # self.signal_workflow(cr, uid, ids, 'manual_invoice')
+        for purchase in purchse_obj.browse(cr, uid, ids, context=context):
+            val = inv_line_obj.product_id_change(cr, uid, [], False,
+                                                 False, partner_id=purchase.partner_id.id,
+                                                 fposition_id=purchase.fiscal_position.id,
+                                                 company_id=purchase.company_id.id)
+            res = val['value']
+
+            # determine and check income account
+
+            prop = ir_property_obj.get(cr, uid,
+                                       'property_account_expense_categ', 'product.category', context=context)
+            prop_id = prop and prop.id or False
+            account_id = fiscal_obj.map_account(cr, uid, purchase.fiscal_position or False, prop_id)
+            if not account_id:
+                raise osv.except_osv(_('Configuration Error!'),
+                                     _('There is no expense account defined as global property.'))
+            res['account_id'] = account_id
+            if not res.get('account_id'):
+                raise osv.except_osv(_('Configuration Error!'),
+                                     _('There is no expense account defined for this product: "%s" (id:%d).'))
+
+            inv_values = {
+                'name': purchase.name,
+                'origin': purchase.name,
+                'type': 'in_invoice',
+                'reference': False,
+                'account_id': purchase.partner_id.property_account_payable.id,
+                'partner_id': purchase.partner_id.id,
+                'currency_id': purchase.pricelist_id.currency_id.id,
+                'comment': purchase.notes,
+                'payment_term': purchase.payment_term_id.id,
+                'fiscal_position': purchase.fiscal_position.id or purchase.partner_id.property_account_position.id,
+            }
+            inv_obj = self.pool.get('account.invoice')
+            inv_line_obj = self.pool.get('account.invoice.line')
+            context['type'] = 'in_invoice'
+            inv_id = inv_obj.create(cr, uid, inv_values, context=context)
+            inv_line_values = []
+
+            for line in purchase.order_line:
+                values = {
+                    'name': line.product_id.name,
+                    'origin': purchase.name,
+                    'account_id': res['account_id'],
+                    'price_unit': line.price_unit,
+                    'quantity': line.product_qty or 1.0,
+                    'discount': False,
+                    'uos_id': res.get('uos_id', False),
+                    # 'product_id': line.product_id.id,
+                    'invoice_line_tax_id': res.get('invoice_line_tax_id'),
+                    'invoice_id': inv_id,
+                }
+                inv_line_obj.create(cr, uid, values, context=context)
+
+            for invoice in purchase.invoice_ids:
+                invoice_values = {
+                    'name': invoice.invoice_line.name,
+                    'origin': invoice.invoice_line.name,
+                    'account_id': res['account_id'],
+                    'price_unit': -invoice.invoice_line.price_unit,
+                    'quantity': invoice.invoice_line.quantity or 1.0,
+                    'discount': False,
+                    'uos_id': res.get('uos_id', False),
+                    # 'product_id': invoice.invoice_line.product_id.id,
+                    'invoice_line_tax_id': res.get('invoice_line_tax_id'),
+                    'invoice_id': inv_id
+                }
+                inv_line_obj.create(cr, uid, invoice_values, context=context)
+
+                # purchase_obj = self.pool.get('purchase.order')
+                # context['type'] = 'in_invoice'
+
+                # inv_obj.button_reset_taxes(cr, uid, [inv_id], context=context)
+                # add the invoice to the sales order's invoices
+                purchse_obj.write(cr, uid, purchase.id, {'invoice_ids': [(4, inv_id)]}, context=context)
+
+        # inv_ids1 = set(inv.id for purchase in self.browse(cr, uid, ids, context) for inv in purchase.invoice_ids)
+        # determine newly created invoices
+        # new_inv_ids = list(inv_ids1 - inv_ids0)
+
+        res = mod_obj.get_object_reference(cr, uid, 'account', 'invoice_form')
+        res_id = res and res[1] or False,
+
+        return {
+            'name': _('Supplier Invoices'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': [res_id],
+            'res_model': 'account.invoice',
+            'context': "{'type':'in_invoice'}",
+            'type': 'ir.actions.act_window',
+            'nodestroy': True,
+            'target': 'current',
+            'res_id': inv_id or False,
+        }
--- purchase_advance_payment/wizard/purchase_make_invoice_advance.py
+++ purchase_advance_payment/wizard/purchase_make_invoice_advance.py
@@ -61,34 +61,34 @@ class PurchaseAdvancePaymentInv(osv.osv_memory):
     def _prepare_advance_invoice_vals(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-        sale_obj = self.pool.get('sale.order')
+        purchse_obj = self.pool.get('purchase.order')
         ir_property_obj = self.pool.get('ir.property')
         fiscal_obj = self.pool.get('account.fiscal.position')
         inv_line_obj = self.pool.get('account.invoice.line')
         wizard = self.browse(cr, uid, ids[0], context)
-        sale_ids = context.get('active_ids', [])
+        purchase_ids = context.get('active_ids', [])
 
         result = []
-        for sale in sale_obj.browse(cr, uid, sale_ids, context=context):
+        for purchase in purchse_obj.browse(cr, uid, purchase_ids, context=context):
             val = inv_line_obj.product_id_change(cr, uid, [], wizard.product_id.id,
-                                                 False, partner_id=sale.partner_id.id,
-                                                 fposition_id=sale.fiscal_position.id,
-                                                 company_id=sale.company_id.id)
+                                                 False, partner_id=purchase.partner_id.id,
+                                                 fposition_id=purchase.fiscal_position.id,
+                                                 company_id=purchase.company_id.id)
             res = val['value']
 
             # determine and check income account
             if not wizard.product_id.id:
                 prop = ir_property_obj.get(cr, uid,
-                                           'property_account_income_categ', 'product.category', context=context)
+                                           'property_account_expense_categ', 'product.category', context=context)
                 prop_id = prop and prop.id or False
-                account_id = fiscal_obj.map_account(cr, uid, sale.fiscal_position or False, prop_id)
+                account_id = fiscal_obj.map_account(cr, uid, purchase.fiscal_position or False, prop_id)
                 if not account_id:
                     raise osv.except_osv(_('Configuration Error!'),
-                                         _('There is no income account defined as global property.'))
+                                         _('There is no expense account defined as global property.'))
                 res['account_id'] = account_id
             if not res.get('account_id'):
                 raise osv.except_osv(_('Configuration Error!'),
-                                     _('There is no income account defined for this product: "%s" (id:%d).') % \
+                                     _('There is no expense account defined for this product: "%s" (id:%d).') % \
                                      (wizard.product_id.name, wizard.product_id.id,))
 
             # determine invoice amount
@@ -96,22 +96,22 @@ class PurchaseAdvancePaymentInv(osv.osv_memory):
                 raise osv.except_osv(_('Incorrect Data'),
                                      _('The value of Advance Amount must be positive.'))
             if wizard.advance_payment_method == 'percentage':
-                inv_amount = sale.amount_untaxed * wizard.amount / 100
+                inv_amount = purchase.amount_untaxed * wizard.amount / 100
                 if not res.get('name'):
                     res['name'] = self._translate_advance(cr, uid, percentage=True,
-                                                          context=dict(context, lang=sale.partner_id.lang)) % (
+                                                          context=dict(context, lang=purchase.partner_id.lang)) % (
                                       wizard.amount)
             else:
                 inv_amount = wizard.amount
                 if not res.get('name'):
                     # TODO: should find a way to call formatLang() from rml_parse
-                    symbol = sale.pricelist_id.currency_id.symbol
-                    if sale.pricelist_id.currency_id.position == 'after':
+                    symbol = purchase.pricelist_id.currency_id.symbol
+                    if purchase.pricelist_id.currency_id.position == 'after':
                         symbol_order = (inv_amount, symbol)
                     else:
                         symbol_order = (symbol, inv_amount)
                     res['name'] = self._translate_advance(cr, uid, context=dict(context,
-                                                                                lang=sale.partner_id.lang)) % symbol_order
+                                                                                lang=purchase.partner_id.lang)) % symbol_order
 
             # determine taxes
             if res.get('invoice_line_tax_id'):
@@ -122,7 +122,7 @@ class PurchaseAdvancePaymentInv(osv.osv_memory):
             # create the invoice
             inv_line_values = {
                 'name': res.get('name'),
-                'origin': sale.name,
+                'origin': purchase.name,
                 'account_id': res['account_id'],
                 'price_unit': inv_amount,
                 'quantity': wizard.qty or 1.0,
@@ -130,43 +130,45 @@ class PurchaseAdvancePaymentInv(osv.osv_memory):
                 'uos_id': res.get('uos_id', False),
                 'product_id': wizard.product_id.id,
                 'invoice_line_tax_id': res.get('invoice_line_tax_id'),
-                'account_analytic_id': sale.project_id.id or False,
+                # 'account_analytic_id': sale.project_id.id or False,
             }
             inv_values = {
-                'name': sale.client_order_ref or sale.name,
-                'origin': sale.name,
-                'type': 'out_invoice',
+                # 'name': sale.client_order_ref or sale.name,
+                'name': purchase.name,
+                'origin': purchase.name,
+                'type': 'in_invoice',
                 'reference': False,
-                'account_id': sale.partner_id.property_account_receivable.id,
-                'partner_id': sale.partner_invoice_id.id,
+                'account_id': purchase.partner_id.property_account_payable.id,
+                'partner_id': purchase.partner_id.id,
                 'invoice_line': [(0, 0, inv_line_values)],
-                'currency_id': sale.pricelist_id.currency_id.id,
-                'comment': sale.note,
-                'payment_term': sale.payment_term.id,
-                'fiscal_position': sale.fiscal_position.id or sale.partner_id.property_account_position.id,
-                'section_id': sale.section_id.id,
+                'currency_id': purchase.pricelist_id.currency_id.id,
+                'comment': purchase.notes,
+                'payment_term': purchase.payment_term_id.id,
+                'fiscal_position': purchase.fiscal_position.id or purchase.partner_id.property_account_position.id,
+                # 'section_id': sale.section_id.id,
             }
-            result.append((sale.id, inv_values))
+            result.append((purchase.id, inv_values))
         return result
 
-    def _create_invoices(self, cr, uid, inv_values, sale_id, context=None):
+    def _create_invoices(self, cr, uid, inv_values, purchase_id, context=None):
         inv_obj = self.pool.get('account.invoice')
-        sale_obj = self.pool.get('sale.order')
+        purchase_obj = self.pool.get('purchase.order')
+        context['type'] = 'in_invoice'
         inv_id = inv_obj.create(cr, uid, inv_values, context=context)
         inv_obj.button_reset_taxes(cr, uid, [inv_id], context=context)
         # add the invoice to the sales order's invoices
-        sale_obj.write(cr, uid, sale_id, {'invoice_ids': [(4, inv_id)]}, context=context)
+        purchase_obj.write(cr, uid, purchase_id, {'invoice_ids': [(4, inv_id)]}, context=context)
         return inv_id
 
     def create_invoices(self, cr, uid, ids, context=None):
         """ create invoices for the active sales orders """
-        sale_obj = self.pool.get('sale.order')
+        purchase_obj = self.pool.get('purchase.order')
         act_window = self.pool.get('ir.actions.act_window')
         wizard = self.browse(cr, uid, ids[0], context)
-        sale_ids = context.get('active_ids', [])
+        purchase_ids = context.get('active_ids', [])
         if wizard.advance_payment_method == 'all':
             # create the final invoices of the active sales orders
-            res = sale_obj.manual_invoice(cr, uid, sale_ids, context)
+            res = purchase_obj.manual_invoice(cr, uid, purchase_ids, context)
             if context.get('open_invoices', False):
                 return res
             return {'type': 'ir.actions.act_window_close'}
@@ -176,7 +178,7 @@ class PurchaseAdvancePaymentInv(osv.osv_memory):
             res = act_window.for_xml_id(cr, uid, 'sale', 'action_order_line_tree2', context)
             res['context'] = {
                 'search_default_uninvoiced': 1,
-                'search_default_order_id': sale_ids and sale_ids[0] or False,
+                'search_default_order_id': purchase_ids and purchase_ids[0] or False,
             }
             return res
         assert wizard.advance_payment_method in ('fixed', 'percentage')
