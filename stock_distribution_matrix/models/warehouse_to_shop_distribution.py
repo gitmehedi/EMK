335e82f8077eaab887596206d7462d86ea82d4af
gitmehedi <git.mehedi@gmail.com>
2017-11-16 20:33:49 +0600

    [IMP] Add functionality with warehouse to shop distribution.

--- stock_distribution_matrix/models/warehouse_to_shop_distribution.py
+++ stock_distribution_matrix/models/warehouse_to_shop_distribution.py
@@ -14,12 +14,8 @@ class WarehouseToShopDistribution(models.Model):
     """ default data functionality"""
 
     @api.model
-    def _Filter_DefaultWareHouse(self):
+    def default_warehouse(self):
         return [('id', '=', self.env.ref('stock.warehouse0').id)]
-
-    @api.model
-    def _default_operating_unit(self):
-        return self.env.user.default_operating_unit_id
 
     name = fields.Char('Distribution', readonly=True)
     distribute_date = fields.Date(string='Distribute Date', default=fields.Date.today, required=True, readonly=True,
@@ -32,9 +28,12 @@ class WarehouseToShopDistribution(models.Model):
     stock_distribution_lines_ids = fields.One2many('warehouse.to.shop.distribution.line', 'warehouse_distribution_id',
                                                    states={'confirm': [('readonly', True)]},
                                                    string='Product Distribution')
-    warehoue_id = fields.Many2one('operating.unit', string='Warehouse', required=True, readonly=True,
-                                  default=_default_operating_unit,
-                                  states={'draft': [('readonly', False)]})
+    warehoue_id = fields.Many2one('operating.unit', string='Warehouse')
+
+    warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse', required=True, readonly=True,
+                                   default=default_warehouse,
+                                   states={'draft': [('readonly', False)]})
+
     shop_id = fields.Many2one('operating.unit', string='Shop Name', required=True, readonly=True,
                               states={'draft': [('readonly', False)]})
 
@@ -122,12 +121,12 @@ class WarehouseToShopDistribution(models.Model):
 
         line_obj = self.env['stock.distribution.to.shop.line']
         move_new_obj = self.env['stock.move']
-        warehouse = self.env['pos.config'].search(
-            [('operating_unit_id', '=', self.warehoue_id.id), ('active_shop', '=', True)])
 
-        transit_location = self.env['stock.location'].search([('name', 'ilike', 'Inter Company Transit')])
+        warehouse_loc = self.env['stock.location'].search(
+            [('operating_unit_id', '=', self.warehouse_id.operating_unit_id.id)], limit=1)
+        transit_loc = self.env['stock.location'].search([('name', 'ilike', 'Inter Company Transit')])
 
-        picking = self.get_picking(src_loc=warehouse.stock_location_id, dest_loc=transit_location)
+        picking = self.get_picking(src_loc=warehouse_loc, dest_loc=transit_loc)
 
         for record in self.stock_distribution_lines_ids:
             if record.transfer_qty > 0:
@@ -137,8 +136,8 @@ class WarehouseToShopDistribution(models.Model):
                 rec['product_uom'] = record.product_id.uom_id.id
                 rec['name'] = record.product_id.name
                 rec['product_uom_qty'] = record.transfer_qty
-                rec['location_id'] = warehouse.stock_location_id.id
-                rec['location_dest_id'] = transit_location.id
+                rec['location_id'] = warehouse_loc.id
+                rec['location_dest_id'] = transit_loc.id
                 rec['procure_method'] = "make_to_stock"
                 rec['state'] = 'draft'
                 move_insert = move_new_obj.create(rec)
@@ -155,11 +154,11 @@ class WarehouseToShopDistribution(models.Model):
             raise ValidationError(_('Please Create a Picking Type, Otherwise your operation will not continue.'))
 
         move_new_obj = self.env['stock.move']
-        shop = self.env['pos.config'].search(
-            [('operating_unit_id', '=', self.shop_id.id), ('active_shop', '=', True)])
-        transit_location = self.env['stock.location'].search([('name', 'ilike', 'Inter Company Transit')])
 
-        picking = self.get_picking(src_loc=transit_location, dest_loc=shop.stock_location_id)
+        transit_location = self.env['stock.location'].search([('name', 'ilike', 'Inter Company Transit')])
+        dest_loc = self.env['stock.location'].search([('operating_unit_id', '=', self.shop_id.id)])
+
+        picking = self.get_picking(src_loc=transit_location, dest_loc=dest_loc)
 
         for record in self.stock_distribution_lines_ids:
             if record.transfer_qty > 0:
@@ -170,7 +169,7 @@ class WarehouseToShopDistribution(models.Model):
                 rec['name'] = record.product_id.name
                 rec['product_uom_qty'] = record.transfer_qty
                 rec['location_id'] = transit_location.id
-                rec['location_dest_id'] = shop.stock_location_id.id
+                rec['location_dest_id'] = dest_loc.id
                 rec['procure_method'] = "make_to_stock"
                 rec['state'] = 'draft'
                 move_insert = move_new_obj.create(rec)
