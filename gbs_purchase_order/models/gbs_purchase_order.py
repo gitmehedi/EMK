f4b014c1baa7ba9084eec2a655954d1ca0b84f18
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-11-16 18:09:19 +0600

    [IMP]Bug Fix

--- gbs_purchase_order/models/gbs_purchase_order.py
+++ gbs_purchase_order/models/gbs_purchase_order.py
@@ -130,7 +130,6 @@ class PurchaseOrder(models.Model):
             # if po.requisition_id.type_id.exclusive == 'exclusive':
             others_po = po.requisition_id.mapped('purchase_ids').filtered(lambda r: r.id != po.id)
             others_po.button_cancel()
-            po.requisition_id.action_done()
 
             for element in po.order_line:
                 if element.product_id == po.requisition_id.procurement_id.product_id:
