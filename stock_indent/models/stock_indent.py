b3228ac5517a99f25622e054dc628f546673e4c6
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-12-04 16:30:49 +0600

    [IMP]BUg Fix

--- stock_indent/models/stock_indent.py
+++ stock_indent/models/stock_indent.py
@@ -89,7 +89,7 @@ class IndentIndent(models.Model):
                                  states={'draft': [('readonly', False)], 'cancel': [('readonly', True)]},
                                  help="It specifies goods to be deliver partially or all at once")
 
-    check_pr_issued = fields.Boolean('Check PR Issued', default=True)
+    check_pr_issued = fields.Boolean(compute = '_compute_pr_issued',string = 'Check PR Issued')
 
     product_id = fields.Many2one(
         'product.product', 'Products',
@@ -108,6 +108,17 @@ class IndentIndent(models.Model):
     ####################################################
     # Business methods
     ####################################################
+
+    @api.multi
+    def _compute_pr_issued(self):
+        for indent in self:
+            pool_pr_obj = self.env['purchase.requisition'].search([('indent_ids', '=', indent.id)])
+            for i in pool_pr_obj:
+                for pr_line_id in i.line_ids:
+                    if pr_line_id.product_ordered_qty < indent.product_lines.product_uom_qty:
+                        indent.check_pr_issued = True
+                    else:
+                        indent.check_pr_issued = False
 
     @api.model
     def _default_stock_location(self):
@@ -384,7 +395,6 @@ class IndentIndent(models.Model):
         }
         query = """ INSERT INTO pr_indent_rel (pr_id,indent_id)VALUES (%s, %s) """
         self._cr.execute(query, tuple([purchase_req[0].id,self.id]))
-        self.check_pr_issued = False
         return result
 
     @api.multi
