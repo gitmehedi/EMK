7b646c7607e791d267dc4fc4bf7988669b37795b
ritachowdhury <ritaislam6789@gmail.com>
2017-12-28 17:13:03 +0600

    [ADD] Add report named 'Leave Summary' in main menu 'Leave' which will print all employees leave depending on department, operating_unit and leave_year.

--- gbs_hr_leave_report/report/leave_summary_report.py
+++ gbs_hr_leave_report/report/leave_summary_report.py
@@ -28,15 +28,68 @@ class HrLeaveSummaryReport(models.AbstractModel):
             header[1] = 'Name'
             header[2] = 'Designation'
             header[3] = 'Department'
-            header_data = self.env['hr.holidays.status'].search([])
+            header_data = self.env['hr.holidays.status'].search([], order='id ASC')
             for val in header_data:
                 header[len(header)] = {val.name: {'Avail': 0, 'Balance': 0}}
 
+        lists = self.get_data(data, header_data)
+
         docargs = {
-            'form_date': data['from_date'],
-            'to_date': data['to_date'],
-            'holiday_objs': holiday_objs,
+            'data': data,
+            'holiday_objs': lists,
             'operating_unit': data['operating_unit_id'],
             'header': header
         }
         return report_obj.render('gbs_hr_leave_report.report_emp_leave_summary', docargs)
+
+    @api.model
+    def get_data(self, data, header):
+        self._cr.execute('''
+            SELECT he.id, 
+                   he.name_related AS name, 
+                   hj.name         AS designation, 
+                   hd.name         AS department 
+            FROM   hr_employee he 
+                   LEFT JOIN hr_job hj 
+                          ON ( hj.id = he.job_id ) 
+                   LEFT JOIN hr_department hd 
+                          ON ( hd.id = he.department_id )
+            WHERE  he.department_id=%s 
+        ''' % (data['department_id']))
+
+        leaves = {val[0]: {
+            'name': val[1],
+            'designation': val[2],
+            'department': val[3],
+            'leave': {v.id: {
+                'avail': 0,
+                'balance': 0
+            } for v in header}} for val in self._cr.fetchall()}
+
+        sql = '''
+                SELECT he.id,
+                       hhl.type, 
+                       hhls.id                    AS holiday_type, 
+                       Sum(hhl.number_of_days_temp) AS temp_days,
+                       Sum(hhl.number_of_days) AS days 
+                FROM   hr_holidays hhl 
+                       LEFT JOIN hr_holidays_status hhls 
+                              ON ( hhls.id = hhl.holiday_status_id ) 
+                       LEFT JOIN hr_employee he 
+                              ON ( he.id = hhl.employee_id )
+                WHERE  he.department_id=%s
+                GROUP  BY he.name_related, 
+                          he.id, 
+                          hhls.id,
+                          hhl.type 
+                ''' % (data['department_id'])
+
+        self._cr.execute(sql)
+        for record in self._cr.fetchall():
+            rec = leaves[record[0]]['leave'][record[2]]
+
+            rec['avail'] = rec['avail'] + record[4]
+            if record[1] == 'remove':
+                rec['balance'] = rec['balance'] + abs(record[4])
+
+        return leaves
--- gbs_hr_leave_report/wizard/hr_holidays_summary_department.py
+++ gbs_hr_leave_report/wizard/hr_holidays_summary_department.py
@@ -7,22 +7,23 @@ class HRLeaveSummary(models.TransientModel):
     _name = 'hr.leave.summary.wizard'
     _description = 'HR Leaves Summary Report'
 
-    from_date = fields.Date(string='Date From', required=True,
-                            default=lambda *a: time.strftime('%Y-%m-01'))
-    to_date = fields.Date(string='Date To', required=True,
-                          default=lambda *a: time.strftime('%Y-%m-01'))
+    year_id = fields.Many2one('hr.leave.fiscal.year', string='Leave Year',required=True)
+    department_id = fields.Many2one('hr.department', string='Department')
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit', required=True,
                                         default=lambda self: self.env.user.default_operating_unit_id)
 
     @api.multi
     def process_report(self):
         data = {}
-        data['from_date'] = self.from_date
-        data['to_date'] = self.to_date
-        # data['operating_unit_id'] = self.operating_unit_id.name
+        data['department_id'] = self.department_id.id
+        data['department_name'] = self.department_id.name
+        data['year_id'] = self.year_id.id
+        data['year_name'] = self.year_id.name
+        data['operating_unit_id'] = self.operating_unit_id.id
+
         if self.operating_unit_id.name == 'All':
-            data['operating_unit_id'] = False
+            data['operating_unit_name'] = False
         else:
-            data['operating_unit_id'] = self.operating_unit_id.name
+            data['operating_unit_name'] = self.operating_unit_id.name
 
         return self.env['report'].get_action(self, 'gbs_hr_leave_report.report_emp_leave_summary', data=data)
