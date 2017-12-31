9364adb8301fd0e9f9734acccf1bcffd1bc09d67
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-12-31 13:41:48 +0600

    [ADD] Change custom_report module for odoo-10 and change on leave_summary_report module.

--- gbs_hr_leave_report/__manifest__.py
+++ gbs_hr_leave_report/__manifest__.py
@@ -1,27 +1,28 @@
 {
     'name': 'HR Leave Reports',
     'author': 'Genweb2 Limited',
-    'website': 'www.genweb2.com',    
-    'version':'1.0',
-    'data': [       
-       'wizard/hr_holidays_summary_department_views.xml',
-       'report/report_paperformat.xml',
-       'report/gbs_hr_leave_report.xml',
-       'report/gbs_hr_leave_report_templates.xml',
-       'report/leave_summary_report.xml',
+    'website': 'www.genweb2.com',
+    'version': '1.0',
+    'data': [
+        'wizard/hr_holidays_summary_department_views.xml',
+        'report/report_paperformat.xml',
+        'report/gbs_hr_leave_report.xml',
+        'report/gbs_hr_leave_report_templates.xml',
+        'report/leave_summary_report.xml',
     ],
     'depends': [
-       'hr_holidays',
-       'hr',
-       'gbs_hr_calendar',
-       'gbs_operating_unit',
-       'hr_employee_operating_unit',
-       'base',
-       'report',
-    ],    
-    
-    'description': """This module enables HR Manager to generate leave related reports in PDF format""",        
-  
+        'hr_holidays',
+        'hr',
+        'gbs_hr_calendar',
+        'gbs_operating_unit',
+        'hr_employee_operating_unit',
+        'base',
+        'report',
+        'custom_report',
+    ],
+
+    'description': """This module enables HR Manager to generate leave related reports in PDF format""",
+
     'installable': True,
     'application': True,
 }
