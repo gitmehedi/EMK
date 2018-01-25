28a66cbfcb0b0ce50caf1c8c948f5d0477cd7a83
ritachowdhury <ritaislam6789@gmail.com>
2018-01-25 20:48:37 +0600

    [ADD] Create a new module hr other allowance payroll

---
+++ hr_other_allowance_payroll/models/inherited_hr_payslip.py
@@ -0,0 +1,51 @@
+from odoo import api, fields, models, tools, _
+
+class InheritHRPayslipInput(models.Model):
+    _inherit = 'hr.payslip.input'
+
+    ref = fields.Char('Reference')
+
+    _inherit = "hr.payslip"
+
+    @api.multi
+    def action_payslip_done(self):
+        res = super(InheritHRPayslipInput, self).action_payslip_done()
+
+        other_ids = []
+        for input in self.input_line_ids:
+            if input.code == 'OAS':
+                other_ids.append(int(input.ref))
+
+        other_line_pool = self.env['hr.other.allowance.line']
+        other_data = other_line_pool.browse(other_ids)
+        other_data.write({'state':'adjusted'})
+
+        return res
+
+    @api.onchange('employee_id', 'date_from', 'date_to')
+    def onchange_employee(self):
+
+        if self.employee_id:
+            self.input_line_ids = 0
+            super(InheritHRPayslipInput, self).onchange_employee()
+
+            """
+            Incorporate other payroll data
+            """
+            other_line_ids = self.input_line_ids
+            other_datas = self.env['hr.other.allowance.line'].search([('employee_id', '=', self.employee_id.id),
+                                                              ('state','=','approved')])
+
+            """
+            Other Allowance Bills
+            """
+            for other_data in other_datas:
+                other_line_ids += other_line_ids.new({
+                    'name': 'Other Allowance',
+                    'code': "OAS",
+                    'amount': other_data.other_allowance_amount,
+                    'contract_id': self.contract_id.id,
+                    'ref': other_data.id,
+                })
+
+            self.input_line_ids = other_line_ids
