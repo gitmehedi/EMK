3e5a8860c8b7bd3456265983824e2be1ce74b58d
rabbi <rabbi998@gmail.com>
2018-01-28 15:02:00 +0600

    Cheque Received model, view pushed. Menu added under Accounting tab

---
+++ gbs_accounting_post_dated_cheque_received/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Post Dated Cheque Received',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Accounting',
+    'version':'10.1.1.1',
+    'depends': [
+    ],
+
+    'data': [
+        'views/cheque_received_view.xml',
+    ],
+
+    'summary': 'Post Dated Cheque Received',
+    'installable': True,
+    'application': False,
+}
