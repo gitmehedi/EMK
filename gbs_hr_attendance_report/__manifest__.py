2c7ff44938e0a2ae77d729c6a94bfebecd8e24f3
ritachowdhury <ritaislam6789@gmail.com>
2018-02-15 12:13:21 +0600

    [ADD]add custom layout in report

--- gbs_hr_attendance_report/__manifest__.py
+++ gbs_hr_attendance_report/__manifest__.py
@@ -13,6 +13,7 @@
         'hr_employee_operating_unit',
         'gbs_hr_attendance_utility',
         'gbs_hr_device_config',
+        'custom_report',
     ],
     'data': [
         'security/ir.model.access.csv',
