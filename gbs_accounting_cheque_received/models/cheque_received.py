bbcd2d89a1a63553ea67f6afd103fb131efea6e7
rabbi <rabbi998@gmail.com>
2018-02-22 19:48:35 +0600

    Journal entry dpne

--- gbs_accounting_cheque_received/models/cheque_received.py
+++ gbs_accounting_cheque_received/models/cheque_received.py
@@ -69,43 +69,51 @@ class ChequeReceived(models.Model):
     @api.multi
     def action_honoured(self):
         for cash_rcv in self:
-            # acc_move_line_pool = cash_rcv.env['account.move.line']
-            # account_move = cash_rcv.env['account.move']
-            #
-            # move_vals = {}
-            # move_vals['date'] = cash_rcv.date_on_cheque
-            # move_vals['company_id'] = cash_rcv.company_id.id
-            # move_vals['journal_id'] = cash_rcv.company_id.bank_journal_ids.id
-            # move_vals['partner_id'] = cash_rcv.partner_id.id
-            # move_vals['state'] = 'posted'
-            # move_vals['amount'] = cash_rcv.cheque_amount
-            # move = account_move.create(move_vals)
-            #
-            # move_line_vals = {}
-            # move_line_vals['partner_id'] = cash_rcv.partner_id.id
-            # move_line_vals['name'] = "rabbi Customer Payment"
-            # move_line_vals['debit'] = cash_rcv.cheque_amount
-            # move_line_vals['credit'] = -cash_rcv.cheque_amount
-            # move_line_vals['move_id'] = move.id
-            # move_line_vals['date_maturity'] = cash_rcv.date_on_cheque
-            # move_line_vals['currency_id'] = 3
-            # move_line_vals['account_id'] = cash_rcv.company_id.bank_journal_ids.default_debit_account_id.id
-            # res = acc_move_line_pool.create(move_line_vals)
-            #
-            # move_line_vals2 = {}
-            # move_line_vals2['partner_id'] = cash_rcv.partner_id.id
-            # move_line_vals2['name'] = "rabbi CUST.IN/2018/0014"
-            # move_line_vals2['debit'] = 0
-            # move_line_vals2['credit'] = -cash_rcv.cheque_amount
-            # move_line_vals2['move_id'] = move.id
-            # move_line_vals2['date_maturity'] = cash_rcv.date_on_cheque
-            # move_line_vals2['currency_id'] = 3
-            # move_line_vals2['account_id'] = cash_rcv.partner_id.property_account_receivable_id.id
+            acc_move_line_pool = cash_rcv.env['account.move.line']
+            account_move = cash_rcv.env['account.move']
 
+            move_vals = {}
+            move_vals['date'] = cash_rcv.date_on_cheque
+            move_vals['company_id'] = cash_rcv.company_id.id
+            move_vals['journal_id'] = cash_rcv.company_id.bank_journal_ids.id
+            move_vals['partner_id'] = cash_rcv.partner_id.id
+            move_vals['state'] = 'posted'
+            move_vals['amount'] = cash_rcv.cheque_amount
+            #move_vals['name'] = seq
+            move = account_move.create(move_vals)
+
+            move_line_vals = {}
+            move_line_vals['partner_id'] = cash_rcv.partner_id.id
+            move_line_vals['name'] = "Customer Payment"
+            move_line_vals['debit'] = 0
+            move_line_vals['credit'] = move.amount
+            move_line_vals['move_id'] = move.id
+            move_line_vals['date_maturity'] = cash_rcv.date_on_cheque
+            move_line_vals['journal_id'] = move.journal_id.id
+            move_line_vals['state'] = 'posted'
+            move_line_vals['account_id'] = cash_rcv.company_id.bank_journal_ids.default_debit_account_id.id
+            res = acc_move_line_pool.create(move_line_vals)
+
+            move_line_vals2 = {}
+            move_line_vals2['partner_id'] = cash_rcv.partner_id.id
+            move_line_vals2['name'] = cash_rcv.name
+            move_line_vals2['debit'] = move.amount
+            move_line_vals2['credit'] = 0
+            move_line_vals2['move_id'] = move.id
+            move_line_vals2['date_maturity'] = cash_rcv.date_on_cheque
+            move_line_vals2['journal_id'] = move.journal_id.id
+            move_line_vals2['account_id'] = cash_rcv.partner_id.property_account_receivable_id.id
+            move_line_vals2['state'] = 'posted'
+
+            res2 = acc_move_line_pool.create(move_line_vals2)
+
+            # Update Customer's Credit Limit & Receilable Amount
             cash_rcv.updateCustomersCreditLimit()
             cash_rcv.updateCustomersReceivableAmount()
 
             cash_rcv.state = 'honoured'
+
+
 
     @api.model
     def create(self, vals):
