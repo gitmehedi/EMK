427d8ac2c6380c99c7a909716734fc8a832f25d8
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2018-02-28 20:09:56 +0600

    [IMP]C and F Updated

--- com_shipment/wizard/__init__.py
+++ com_shipment/wizard/__init__.py
@@ -3,7 +3,6 @@ import on_board_wizard
 import gate_in_wizard
 import eta_wizard
 import doc_receive_wizard
-import cnf_quotation_wizard
 import cnf_approve_quotation_wizard
 import cnf_clear_wizard
 import send_to_cnf_wizard
--- gbs_purchase_quotation_cnf/models/purchase_quotation_cnf.py
+++ gbs_purchase_quotation_cnf/models/purchase_quotation_cnf.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api,_
-from odoo.exceptions import UserError
 
 class PurchaseCNFQuotation(models.Model):
     _name = 'purchase.order'
@@ -11,12 +10,15 @@ class PurchaseCNFQuotation(models.Model):
                                   ondelete='cascade',default=lambda self: self.env.context.get('shipment_id') or False)
     lc_id = fields.Many2one('letter.credit',string='LC Name',ondelete = 'cascade',related = 'shipment_id.lc_id')
 
+    partner_id = fields.Many2one('res.partner', string='C&amp;F Vendor', required=True, track_visibility='always',
+                                 default=lambda self: self.env.context.get('partner_id') or False)
 
     @api.multi
     def button_approve(self):
         for cnf in self:
             if cnf.cnf_quotation:
-                self.shipment_id.write({'state': 'approve_cnf_quotation'})
+                if self.state != 'draft':
+                    self.shipment_id.write({'state': 'approve_cnf_quotation'})
                 super(PurchaseCNFQuotation, self).button_approve()
             else:
                 super(PurchaseCNFQuotation, self).button_approve()
