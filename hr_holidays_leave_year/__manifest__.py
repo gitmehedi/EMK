ba71940d0982dc05dfd8436933375d547cad20a2
Mehedi Hasan <git.mehedi@gmail.com>
2018-03-13 18:43:14 +0600

    [MOD] Modify manifest

--- hr_holidays_leave_year/__manifest__.py
+++ hr_holidays_leave_year/__manifest__.py
@@ -4,9 +4,8 @@
     "author": "Genweb2 Limited",
     "website": "http://www.genweb2.com",
     'category': 'Human Resources',
-    
-    'summary': 'Computes the actual leave days '
-               'considering rest days and public holidays',
+    'summary': """Computes the actual leave days 
+               considering rest days and public holidays""",
     'depends': ['hr',  
                 'hr_holidays', 
                 'hr_holidays_multi_levels_approval',
