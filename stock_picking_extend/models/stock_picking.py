8014ffb00476b59e5884b913f37d98116a0a49ea
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2018-03-28 19:21:42 +0600

    [ADD]Stock Picking Inherited module added,Stock Picking LC module added

--- stock_picking_extend/models/stock_picking.py
+++ stock_picking_extend/models/stock_picking.py
@@ -5,29 +5,59 @@ from odoo import models, fields, api
 class Picking(models.Model):
     _inherit = "stock.picking"
 
-    transfer_type = fields.Selection([
-        ('inventory', 'Inventory'),
-        ('sale', 'Sale')],
-        readonly=True)
-    gate_pass = fields.Char(
-        'Gate Pass', index=True,
-        states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
-        help="Reference of the Gate Pass")
-    state = fields.Selection([
-        ('draft', 'Draft'), ('cancel', 'Cancelled'),
-        ('waiting', 'Waiting Another Operation'),
-        ('confirmed', 'Waiting Availability'),
-        ('partially_available', 'Partially Available'),
-        ('assigned', 'Available'),
-        ('quality_control', 'Quality Control'),
-        ('done', 'Done')],
-        string='Status', compute='_compute_state',
-        copy=False, index=True, readonly=True, store=True, track_visibility='onchange',
-        help=" * Draft: not confirmed yet and will not be scheduled until confirmed\n"
-             " * Waiting Another Operation: waiting for another move to proceed before it becomes automatically available (e.g. in Make-To-Order flows)\n"
-             " * Waiting Availability: still waiting for the availability of products\n"
-             " * Partially Available: some products are available and reserved\n"
-             " * Ready to Transfer: products reserved, simply waiting for confirmation.\n"
-             " * Transferred: has been processed, can't be modified or cancelled anymore\n"
-             " * Quality Control: If product is qc passed then transfer done otherwise transfer stoped and product can be return\n"
-             " * Cancelled: has been cancelled, can't be confirmed anymore")
+    receive_type = fields.Selection([
+        ('loan', 'Loan'),
+        ('other', 'Other')],
+        readonly=True,states={'draft': [('readonly', False)]})
+
+    # gate_pass = fields.Char(
+    #     'Gate Pass', index=True,
+    #     states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
+    #     help="Reference of the Gate Pass")
+    # qc_need = fields.Boolean(
+    #     'QC Need',default=False,help="Decision field that QC need or not")
+    # check_do_new_transfer = fields.Boolean(
+    #     'Check', default=False, compute="_compute_do_new_transfer")
+    # check_qc_need = fields.Boolean(
+    #     'Check', default=False, compute="_compute_qc_need")
+    # state = fields.Selection([
+    #     ('draft', 'Draft'), ('cancel', 'Cancelled'),
+    #     ('waiting', 'Waiting Another Operation'),
+    #     ('confirmed', 'Waiting Availability'),
+    #     ('partially_available', 'Partially Available'),
+    #     ('assigned', 'Available'),
+    #     ('quality_control', 'Quality Control'),
+    #     ('done', 'Done')],
+    #     string='Status', compute='_compute_state',
+    #     copy=False, index=True, readonly=True, store=True, track_visibility='onchange',
+    #     help=" * Draft: not confirmed yet and will not be scheduled until confirmed\n"
+    #          " * Waiting Another Operation: waiting for another move to proceed before it becomes automatically available (e.g. in Make-To-Order flows)\n"
+    #          " * Waiting Availability: still waiting for the availability of products\n"
+    #          " * Partially Available: some products are available and reserved\n"
+    #          " * Ready to Transfer: products reserved, simply waiting for confirmation.\n"
+    #          " * Transferred: has been processed, can't be modified or cancelled anymore\n"
+    #          " * Quality Control: If product is qc passed then transfer done otherwise transfer stoped and product can be return\n"
+    #          " * Cancelled: has been cancelled, can't be confirmed anymore")
+    #
+    # @api.multi
+    # @api.depends('qc_need')
+    # def _compute_do_new_transfer(self):
+    #     for rec in self:
+    #         if rec.qc_need == False:
+    #             if rec.state in ['draft','partially_available','assigned']:
+    #                 rec.check_do_new_transfer = True
+    #         if rec.state == 'quality_control':
+    #             rec.check_do_new_transfer = True
+    #
+    # @api.multi
+    # @api.depends('qc_need')
+    # def _compute_qc_need(self):
+    #     for rec in self:
+    #         if rec.qc_need == True:
+    #             if rec.state in ['assigned']:
+    #                 rec.check_qc_need = True
+    #
+    # @api.one
+    # def button_qc(self):
+    #     self.state = 'quality_control'
+
---
+++ stock_picking_lc/models/stock_picking.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import time
+
+from odoo import models, fields, api
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+class Picking(models.Model):
+    _inherit = "stock.picking"
+
+    receive_type = fields.Selection([
+        ('lc', 'LC'),
+        ('loan', 'Loan'),
+        ('other', 'Other')],
+        readonly=True, states={'draft': [('readonly', False)]})
+
+    shipment_id = fields.Many2one(
+        'purchase.shipment',
+        string='Shipment Number',
+        readonly=True,states={'draft': [('readonly', False)]})
+
+    @api.onchange('shipment_id')
+    def _onchange_shipment_id(self):
+        if not self.shipment_id:
+            return
+
+        product_lines = []
+        for ship_pro_line in self.shipment_id.shipment_product_lines:
+
+            date_planned = time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+            product_lines.append((0, 0, {
+                'name': self.name,
+                'origin': self.name,
+                'location_id': self.location_id.id,
+                'location_dest_id': self.location_dest_id.id,
+                'picking_id': self.id or False,
+                'picking_type_id': self.picking_type_id.id,
+                'date': date_planned,
+                'date_expected': date_planned,
+
+                'product_id': ship_pro_line.product_id.id,
+                'product_uom': ship_pro_line.product_uom.id,
+                'product_uom_qty': ship_pro_line.product_qty,
+                'state' : self.state,
+            }))
+
+        self.move_lines = product_lines
