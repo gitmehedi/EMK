ca0d3195dd4f7a86be04e99f078fd3907be1b2cb
rabbi <rabbi998@gmail.com>
2018-04-04 17:55:50 +0600

    Full structure of report done with some of the real data

--- delivery_qty_reports/wizards/daily_delivery_report_wizard.py
+++ delivery_qty_reports/wizards/daily_delivery_report_wizard.py
@@ -1,11 +1,13 @@
 from odoo import api, fields, models
 import datetime
 
+
 class GenerateDailyDeliveryReport(models.Model):
     _name = 'daily.delivery.report.wizard'
 
-    report_of_day = fields.Date(string="Report of day", required=True,)
+    report_of_day = fields.Datetime(string="Report of day", required=True, )
 
     @api.multi
     def process_delivery_report(self):
-        print 'Button clicked!!'
+        return self.env['report'].get_action(self, 'delivery_qty_reports.report_daily_delivery_products',
+                                           data={'report_of_day': self.report_of_day})
