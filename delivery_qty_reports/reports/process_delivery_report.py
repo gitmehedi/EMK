800d0261abdd161f9946cb5c465ab2f8d3774016
rabbi <rabbi998@gmail.com>
2018-04-05 15:25:51 +0600

    Time removed from DO date

--- delivery_qty_reports/reports/process_delivery_report.py
+++ delivery_qty_reports/reports/process_delivery_report.py
@@ -3,40 +3,42 @@ import time
 
 from datetime import date,datetime
 
+
+
 class ProcessDeliveryReport(models.AbstractModel):
     _name = 'report.delivery_qty_reports.report_daily_delivery_products'
 
     @api.model
     def render_html(self, docids, data=None):
 
-        stock_picking_pool = self.env['stock.picking'].search([('min_date', '<', data['report_of_day'])])
         do_list = []
 
         issued_do_sum_list = []
         delivery_qty_sum_list = []
         undelivery_qty_sum_list = []
 
+        stock_picking_pool = self.env['stock.picking'].search([('min_date', '<', data['report_of_day'])])
+
         for stocks in stock_picking_pool:
+
+            DO_date = datetime.strptime(stocks.min_date, "%Y-%m-%d %H:%M:%S").date()
+            DO_Qty = stocks.pack_operation_product_ids.product_qty
+
             data = {}
             data['partner_id'] = stocks.partner_id.name
-            data['do_date'] = stocks.min_date
+            data['do_date'] = DO_date
             data['do_no'] = stocks.name
-            data['do_qty'] = stocks.pack_operation_product_ids.product_qty
+            data['do_qty'] = DO_Qty
             data['un_delivered_qty'] = stocks.pack_operation_product_ids.product_qty - stocks.pack_operation_product_ids.qty_done
             data['delivered_qty'] = stocks.pack_operation_product_ids.qty_done
             data['product'] = stocks.pack_operation_product_ids.product_id.name
 
             ## Cross match with DO Date and today's date to get Issued D.O property
-            if datetime.strptime(stocks.min_date, "%Y-%m-%d %H:%M:%S").date() == date.today():
-                data['issued_do_today'] = stocks.pack_operation_product_ids.product_qty
+            if DO_date == date.today():
+                data['issued_do_today'] = DO_Qty
                 issued_do_sum_list.append(data['issued_do_today'])
             else:
                 data['issued_do_today'] = ''
-
-            #data['product_category']
-            #
-            # data['un_delivered_qty']
-
 
             sale_order_pool = self.env['sale.order'].search([('name', '=', stocks.origin)])
             if sale_order_pool:
