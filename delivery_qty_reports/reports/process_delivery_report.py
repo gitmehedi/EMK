aca5eb2cfb39ec0c99407adc0851651404174bb0
rabbi <rabbi998@gmail.com>
2018-04-05 14:09:58 +0600

    Daily delivery report done

--- delivery_qty_reports/reports/process_delivery_report.py
+++ delivery_qty_reports/reports/process_delivery_report.py
@@ -1,5 +1,7 @@
 from odoo import api, fields, models
+import time
 
+from datetime import date,datetime
 
 class ProcessDeliveryReport(models.AbstractModel):
     _name = 'report.delivery_qty_reports.report_daily_delivery_products'
@@ -8,8 +10,6 @@ class ProcessDeliveryReport(models.AbstractModel):
     def render_html(self, docids, data=None):
 
         stock_picking_pool = self.env['stock.picking'].search([('min_date', '<', data['report_of_day'])])
-        # sale_order_pool = self.env['sale.order'].search([('group_id','=',self.group)])
-
         do_list = []
 
         for stocks in stock_picking_pool:
@@ -17,15 +17,32 @@ class ProcessDeliveryReport(models.AbstractModel):
             data['partner_id'] = stocks.partner_id.name
             data['do_date'] = stocks.min_date
             data['do_no'] = stocks.name
-            # data['uom'] =
-            # data['do_qty']
-            # data['undelivered_qty']
-            # data['issued_do_today']
-            # data['delivered_qty']
+            data['do_qty'] = stocks.pack_operation_product_ids.product_qty
+            data['un_delivered_qty'] = stocks.pack_operation_product_ids.product_qty - stocks.pack_operation_product_ids.qty_done
+            data['delivered_qty'] = stocks.pack_operation_product_ids.qty_done
+            data['product'] = stocks.pack_operation_product_ids.product_id.name
+
+            ## Cross match with DO Date and today's date to get Issued D.O property
+            if datetime.strptime(stocks.min_date, "%Y-%m-%d %H:%M:%S").date() == date.today():
+                data['issued_do_today'] = stocks.pack_operation_product_ids.product_qty
+            else:
+                data['issued_do_today'] = ''
+
+            #data['product_category']
+            #
             # data['un_delivered_qty']
-            # data['product_category']
+
+
+            sale_order_pool = self.env['sale.order'].search([('name', '=', stocks.origin)])
+            if sale_order_pool:
+                for s in sale_order_pool:
+                    data['uom'] = s.order_line.product_uom.name
+            else:
+                data['uom'] = ''
+
             do_list.append(data)
 
-        docargs = { 'do_list': do_list }
+
+        docargs = {'do_list': do_list}
 
         return self.env['report'].render('delivery_qty_reports.report_daily_delivery_products', docargs)
