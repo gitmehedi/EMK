398837f063d299641f09d32da6ea673b0a8159b6
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2018-04-09 10:51:21 +0600

    [IMP]Bug Fix For Default Order Process

--- stock_operating_unit_extend/models/stock.py
+++ stock_operating_unit_extend/models/stock.py
@@ -21,3 +21,12 @@ class Stock(models.Model):
                         raise ValidationError(_('The chosen operating unit is not in the child locations of its'))
         res = super(Stock, self).write(values)
         return res
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.multi
+    @api.constrains('operating_unit_id', 'location_id', 'picking_id',
+                    'operating_unit_dest_id', 'location_dest_id')
+    def _check_stock_move_operating_unit(self):
+        pass
