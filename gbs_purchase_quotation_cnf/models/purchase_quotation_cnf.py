b7ecb47f2c5c30e0cbf05efc136b78601f750f91
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2018-04-13 18:07:41 +0600

    [IMP]C and F bug fix

--- gbs_purchase_quotation_cnf/models/purchase_quotation_cnf.py
+++ gbs_purchase_quotation_cnf/models/purchase_quotation_cnf.py
@@ -18,6 +18,9 @@ class PurchaseCNFQuotation(models.Model):
     def create(self, vals):
         if vals.get('cnf_quotation'):
             vals['name'] = self.env['ir.sequence'].next_by_code_new('cnf.quotation', datetime.today()) or '/'
+            shipment_pool = self.env['purchase.shipment']
+            shipment_obj = shipment_pool.search([('id', '=', vals['shipment_id'])])
+            shipment_obj.write({'state': 'approve_cnf_quotation'})
         return super(PurchaseCNFQuotation, self).create(vals)
 
     @api.multi
