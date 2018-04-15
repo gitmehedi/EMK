9c7cd33649323e6491717dd353d7edd70da2f163
ritachowdhury <ritaislam6789@gmail.com>
2018-04-15 14:31:53 +0600

    [FIX] stock_custom_summary_report

--- stock_custom_summary_report/wizard/print_report.py
+++ stock_custom_summary_report/wizard/print_report.py
@@ -9,22 +9,22 @@ class StockInventoryWizard(models.TransientModel):
 
     date_from = fields.Date("Date from", required=True)
     date_to = fields.Date("Date to", required=True)
-    shop_id = fields.Many2one('operating.unit', string='Unit Name', required=True)
+    operating_unit_id = fields.Many2one('operating.unit', string='Unit Name', required=True)
     category_id = fields.Many2one('product.category', string='Category', required=False)
     report_type_ids = fields.Many2many('report.type.selection', string="Report Type")
 
 
     @api.multi
     def report_print(self):
-        location = self.env['stock.location'].search([('operating_unit_id', '=', self.shop_id.id)])
+        location = self.env['stock.location'].search([('operating_unit_id', '=', self.operating_unit_id.id),('name', '=', 'Stock')])
         report_type = [val.code for val in self.env['report.type.selection'].search([])]
         selected_type = [val.code for val in self.report_type_ids]
 
         data = {}
         data['date_from'] = self.date_from
         data['date_to'] = self.date_to
-        data['shop_id'] = location.id
-        data['shop_name'] = self.shop_id.name
+        data['operating_unit_id'] = location.id
+        data['shop_name'] = self.operating_unit_id.name
         data['category_id'] = self.category_id.id
         data['report_type'] = selected_type if len(selected_type) > 0 else report_type
 
