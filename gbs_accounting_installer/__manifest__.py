173d6953168cca86fc57a7239e592eafc1e7d550
root <matiar.rahman@gmail.com>
2018-04-15 19:40:48 +0600

    add dependant addons

--- gbs_accounting_installer/__manifest__.py
+++ gbs_accounting_installer/__manifest__.py
@@ -6,8 +6,12 @@
     'category': 'Accounting',
     'depends': ['account_accountant',
                 'account_parent',
+                'account_type_menu',
                 'account_operating_unit',
-                'account_fiscal_year'],
+                'account_fiscal_year',
+                'account_move_fiscal_year',
+                'buff_partner_bank_branch',
+                'account_invoice_supplier_ref_unique'],
 
     'data': [ ],
     'description': 'Install all modules which is related to Accounting',
