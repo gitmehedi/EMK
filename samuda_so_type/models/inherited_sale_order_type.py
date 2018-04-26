63e99b9a0b650eebaea5d861e8f96791de8d05d1
rabbi <rabbi998@gmail.com>
2018-04-26 21:24:28 +0600

    Sales Order Type added on Invoice Analysis report

--- samuda_so_type/models/inherited_sale_order_type.py
+++ samuda_so_type/models/inherited_sale_order_type.py
@@ -27,7 +27,7 @@ class InheritedSaleOrderType(models.Model):
 class InheritSaleReport(models.Model):
     _inherit = 'sale.report'
 
-    type_id = fields.Many2one('sale.order.type', string='Sale Order Type')
+    type_id = fields.Many2one('sale.order.type', string='Sale Order Type',readonly=True)
 
 
     def _select(self):
@@ -85,3 +85,74 @@ class InheritSaleReport(models.Model):
                     partner.commercial_partner_id
         """
         return group_by_str
+
+
+
+###################################################
+# Add Sales Order Type on Invoice Analysis Report
+###################################################
+
+class InheritedAccountInvoiceReport(models.Model):
+    _inherit = 'account.invoice.report'
+
+
+    #product_template = fields.Many2one('product.template', string='Product Template', readonly=True)
+    sale_type_id = fields.Many2one('sale.order.type', string='Sale Order Type', readonly=True)
+
+
+    def _select(self):
+        select_str = """
+               SELECT sub.id, sub.date, sub.product_id, sub.partner_id, sub.country_id, sub.account_analytic_id,
+                   sub.payment_term_id, sub.uom_name, sub.currency_id, sub.journal_id,
+                   sub.fiscal_position_id, sub.user_id, sub.company_id, sub.nbr, sub.type, sub.state,
+                   sub.weight, sub.volume, sub.sale_type_id,
+                   sub.categ_id, sub.date_due, sub.account_id, sub.account_line_id, sub.partner_bank_id,
+                   sub.product_qty, sub.price_total as price_total, sub.price_average as price_average,
+                   COALESCE(cr.rate, 1) as currency_rate, sub.residual as residual, sub.commercial_partner_id as commercial_partner_id
+           """
+        return select_str
+
+    def _sub_select(self):
+        select_str = """
+                   SELECT ail.id AS id,
+                       ai.date_invoice AS date,
+                       ail.product_id, ai.partner_id, ai.payment_term_id, ail.account_analytic_id,
+                       u2.name AS uom_name,
+                       ai.currency_id, ai.journal_id, ai.fiscal_position_id, ai.user_id, ai.company_id,
+                       1 AS nbr, ai.sale_type_id AS sale_type_id,
+                       ai.type, ai.state, pt.categ_id, ai.date_due, ai.account_id, ail.account_id AS account_line_id,
+                       ai.partner_bank_id,
+                       SUM ((invoice_type.sign * ail.quantity) / u.factor * u2.factor) AS product_qty,
+                       SUM(ail.price_subtotal_signed * invoice_type.sign) AS price_total,
+                       SUM(ABS(ail.price_subtotal_signed)) / CASE
+                               WHEN SUM(ail.quantity / u.factor * u2.factor) <> 0::numeric
+                                  THEN SUM(ail.quantity / u.factor * u2.factor)
+                                  ELSE 1::numeric
+                               END AS price_average,
+                       ai.residual_company_signed / (SELECT count(*) FROM account_invoice_line l where invoice_id = ai.id) *
+                       count(*) * invoice_type.sign AS residual,
+                       ai.commercial_partner_id as commercial_partner_id,
+                       partner.country_id,
+                       SUM(pr.weight * (invoice_type.sign*ail.quantity) / u.factor * u2.factor) AS weight,
+                       SUM(pr.volume * (invoice_type.sign*ail.quantity) / u.factor * u2.factor) AS volume
+           """
+        return select_str
+
+
+    def _group_by(self):
+        group_by_str = """
+                   GROUP BY ail.id, ail.product_id, ail.account_analytic_id, ai.date_invoice, ai.id,
+                       ai.sale_type_id, ai.partner_id, ai.payment_term_id, u2.name, u2.id, ai.currency_id, ai.journal_id,
+                       ai.fiscal_position_id, ai.user_id, ai.company_id, ai.type, invoice_type.sign, ai.state, pt.categ_id,
+                       ai.date_due, ai.account_id, ail.account_id, ai.partner_bank_id, ai.residual_company_signed,
+                       ai.amount_total_company_signed, ai.commercial_partner_id, partner.country_id
+           """
+        return group_by_str
+
+
+
+
+
+
+
+
