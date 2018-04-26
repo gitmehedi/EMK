b789e005b6c0dee326476a0c183085b54a46346f
rabbi <rabbi998@gmail.com>
2018-04-26 20:54:50 +0600

    Sale Order Type added on Sales Analysis report

--- samuda_so_type/models/inherited_sale_order_type.py
+++ samuda_so_type/models/inherited_sale_order_type.py
@@ -18,3 +18,70 @@ class InheritedSaleOrderType(models.Model):
         name = self.env['sale.order.type'].search([('name', '=', self.name), ('operating_unit','=',self.operating_unit.id)])
         if len(name) > 1:
             raise ValidationError('Sale Order Type for this Name & Operating Unit already exists')
+
+
+
+#################################################
+# Add Sales Order Type to Sales Analysis Report
+################################################
+class InheritSaleReport(models.Model):
+    _inherit = 'sale.report'
+
+    type_id = fields.Many2one('sale.order.type', string='Sale Order Type')
+
+
+    def _select(self):
+        select_str = """
+            WITH currency_rate as (%s)
+             SELECT min(l.id) as id,
+                    l.product_id as product_id,
+                    t.uom_id as product_uom,
+                    sum(l.product_uom_qty / u.factor * u2.factor) as product_uom_qty,
+                    sum(l.qty_delivered / u.factor * u2.factor) as qty_delivered,
+                    sum(l.qty_invoiced / u.factor * u2.factor) as qty_invoiced,
+                    sum(l.qty_to_invoice / u.factor * u2.factor) as qty_to_invoice,
+                    sum(l.price_total / COALESCE(cr.rate, 1.0)) as price_total,
+                    sum(l.price_subtotal / COALESCE(cr.rate, 1.0)) as price_subtotal,
+                    count(*) as nbr,
+                    s.name as name,
+                    s.date_order as date,
+                    s.type_id AS type_id, 
+                    s.state as state,
+                    s.partner_id as partner_id,
+                    s.user_id as user_id,
+                    s.company_id as company_id,
+                    extract(epoch from avg(date_trunc('day',s.date_order)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
+                    t.categ_id as categ_id,
+                    s.pricelist_id as pricelist_id,
+                    s.project_id as analytic_account_id,
+                    s.team_id as team_id,
+                    p.product_tmpl_id,
+                    partner.country_id as country_id,
+                    partner.commercial_partner_id as commercial_partner_id,
+                    sum(p.weight * l.product_uom_qty / u.factor * u2.factor) as weight,
+                    sum(p.volume * l.product_uom_qty / u.factor * u2.factor) as volume
+        """ % self.env['res.currency']._select_companies_rates()
+        return select_str
+
+
+    def _group_by(self):
+        group_by_str = """
+            GROUP BY l.product_id,
+                    l.order_id,
+                    t.uom_id,
+                    t.categ_id,
+                    s.name,
+                    s.date_order,
+                    s.partner_id,
+                    s.user_id,
+                    s.state,
+                    s.company_id,
+                    s.pricelist_id,
+                    s.project_id,
+                    s.team_id,
+                    s.type_id,
+                    p.product_tmpl_id,
+                    partner.country_id,
+                    partner.commercial_partner_id
+        """
+        return group_by_str
