5dccede11911944dd2c967b97e56be422735bd20
mrbappy <mahfuz.bappy@gmail.com>
2018-05-03 19:02:58 +0600

    Implement lc sales

--- lc_sales_product/models/lc_product.py
+++ lc_sales_product/models/lc_product.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models,_
 class LCSO(models.Model):
     _inherit = "letter.credit"
 
-    so_ids = fields.Many2many('sale.order', 'so_lc_rel', 'lc_id', 'so_id', string='Sale Order')
+    so_ids = fields.Many2many('sale.order', 'so_lc_rel', 'lc_no', 'so_id', string='Sale Order')
     product_lines = fields.One2many('lc.product.line', 'lc_id', string='Product(s)')
 
     @api.onchange('so_ids')
@@ -14,10 +14,9 @@ class LCSO(models.Model):
             for obj in so_id.order_line:
                 vals.append((0, 0, {'product_id': obj.product_id,
                                     'name': obj.name,
-                                    'product_qty': obj.product_qty,
+                                    'product_qty': obj.product_uom_qty,
                                     'price_unit': obj.price_unit,
                                     'currency_id': obj.currency_id,
-                                    'date_planned': obj.date_planned,
                                     'product_uom':obj.product_uom
                                     }))
         self.product_lines = vals
