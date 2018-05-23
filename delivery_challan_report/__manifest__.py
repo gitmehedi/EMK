908df72aff1955c63e7f622da9b5550990bb3c31
rabbi <rabbi998@gmail.com>
2018-05-23 11:46:16 +0600

    Delivery Challan report done

--- delivery_challan_report/__manifest__.py
+++ delivery_challan_report/__manifest__.py
@@ -6,12 +6,11 @@
     'version':'1.1.1',
     'depends': [
         'stock',
-        'custom_report',
-
     ],
 
     'data': [
         'reports/delivery_challan_potro_report_view.xml',
+        'reports/delivery_challan_report_view.xml',
     ],
 
     'summary': 'Reports printing for Delivery Challan and Challan Potro',
