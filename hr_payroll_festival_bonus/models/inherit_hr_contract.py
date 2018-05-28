59886ac8589008c98ca63fceee24aadae1faf2f1
ritachowdhury <ritaislam6789@gmail.com>
2018-05-28 12:30:28 +0600

    modify

--- hr_payroll_festival_bonus/models/inherit_hr_contract.py
+++ hr_payroll_festival_bonus/models/inherit_hr_contract.py
@@ -1,18 +1,19 @@
 from odoo import api, fields, models
-import datetime
-#from datetime import datetime
+from datetime import datetime
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
+
 
 class InheritHrContract(models.Model):
     _inherit = 'hr.contract'
 
-    bonus_application = fields.Boolean('Bonus Application',compute='_compute_bonus_applicable')
+    bonus_applicable = fields.Boolean('Bonus Applicable',compute='_compute_bonus_applicable')
 
     @api.one
     def _compute_bonus_applicable(self):
-        self.bonus_application = False
-        #if datetime.strptime(self.trial_date_end, '%Y-%m-%d') > datetime.date.today():
-        # current_date = datetime.date.today()
-        # if self.trial_date_end > current_date:
-        #     self.bonus_application = True
+        self.bonus_applicable = False
+        current_date = datetime.now().date()
+        if self.trial_date_end > current_date.strftime(
+                DEFAULT_SERVER_DATE_FORMAT):
+            self.bonus_applicable = True
 
 
