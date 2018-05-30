c4e09ed4ca4bc69b44099194077a6f58da7d1555
ritachowdhry <ritachowdhry696@gmail.com>
2018-05-30 12:22:16 +0600

    Implement LC Sale reports

---
+++ hr_attendance_and_ot_summary/static/src/js/scrollbar.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function () {
+    jQuery('.scrollbar-external').scrollbar({
+        "autoScrollSize": false,
+        "scrollx": $('.external-scroll_x'),
+        "scrolly": $('.external-scroll_y')
+    });
+});
