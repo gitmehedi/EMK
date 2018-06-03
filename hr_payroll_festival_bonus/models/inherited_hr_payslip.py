aff728672c8468c321243d322e481efdfa932116
ritachowdhury <ritaislam6789@gmail.com>
2018-06-03 15:51:12 +0600

    [IMP] Hr payroll festival bonus with trial period

--- hr_payroll_festival_bonus/models/inherited_hr_payslip.py
+++ hr_payroll_festival_bonus/models/inherited_hr_payslip.py
@@ -6,9 +6,12 @@ class InheritHRPayslipsBatches(models.Model):
     type = fields.Selection([("0", "Regular Salary"),
                              ("1", "Festival Bonus"),
                              ("2", "Salary With Bonus")], "Type", required=True)
+    festival_date = fields.Date('Festival Date')
 
 
 class InheritHRPayslip(models.Model):
     _inherit = 'hr.payslip'
 
     type = fields.Selection(related='payslip_run_id.type',store=True)
+
+    festival_date = fields.Date('Festival Date',related='payslip_run_id.festival_date',store=True)
