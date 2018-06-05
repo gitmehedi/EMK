50b1a4f93ead34e37bf5ae178169773db9d42c96
mrbappy <mahfuz.bappy@gmail.com>
2018-06-05 12:55:08 +0600

    Update

--- gbs_lc_sales_access/__manifest__.py
+++ gbs_lc_sales_access/__manifest__.py
@@ -27,7 +27,7 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'security/ir_rule.xml',
         'views/menu_items.xml',
     ],
