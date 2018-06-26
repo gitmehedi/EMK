f55e41327542c9ec108decbddcac9658317e3abb
ritachowdhury <ritaislam6789@gmail.com>
2018-06-26 19:54:33 +0600

    [ADD] hr_exit

---
+++ hr_exit/models/configure_checklists_line.py
@@ -0,0 +1,13 @@
+from odoo import models, fields, api, exceptions
+
+class ConfigureChecklistsLine(models.Model):
+    _name = "hr.exit.configure.checklists.line"
+
+    check_list_type_id = fields.Many2one('hr.exit.checklist.type', string="Checklist Types")
+    status_line_id = fields.Many2one('hr.checklist.status')
+    checklist_item_id = fields.Many2one('hr.exit.checklist.item', string='Checklist Item', required=True)
+
+    # Relational fields
+    # checklists_id = fields.Many2one('hr.exit.configure.checklists',)
+    checklists_line_id = fields.Many2one('hr.exit.configure.checklists')
+
