b3da5679d70689f737d69b96445ae174a97d4d46
rabbi <rabbi998@gmail.com>
2018-06-28 11:57:26 +0600

    Renamed

--- product_sales_pricelist/__manifest__.py
+++ product_sales_pricelist/__manifest__.py
@@ -27,7 +27,7 @@
         'report/change_product_price_report.xml',
         'views/ir_cron.xml',
         'views/product_variants_price_history_view.xml',
-        'views/inherit_account_view.xml',
+        'views/inherit_partner_view.xml',
     ],
     'installable': True,
     'application': False,
