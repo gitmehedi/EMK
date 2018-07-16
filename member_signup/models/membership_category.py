099fc4f8fd8dbdcf802df217f2e732da78ff8bfa
gitmehedi <git.mehedi@gmail.com>
2018-07-16 19:47:40 +0600

    [ADD] Add member_signup module for member registration.

---
+++ member_signup/models/membership_category.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api, _
+
+
+class InheritedMembershipCategory(models.Model):
+    _inherit = "membership.membership_category"
+
+    @api.constrains('name')
+    def _check_name(self):
+        name = self.search([('name', '=ilike', self.name)])
+        if len(name) > 1:
+            raise Exception(_('Name should not be duplicate.'))
+
+
+class InheritedProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.constrains('name')
+    def _check_name(self):
+        name = self.search([('name', '=ilike', self.name)])
+        if len(name) > 1:
+            raise Exception(_('Name should not be duplicate.'))
+
+
+class InheritedMembershipWithdrawalReason(models.Model):
+    _inherit = "membership.withdrawal_reason"
+
+    @api.constrains('name')
+    def _check_name(self):
+        name = self.search([('name', '=ilike', self.name)])
+        if len(name) > 1:
+            raise Exception(_('Name should not be duplicate.'))
---
+++ member_signup/models/res_partner.py
@@ -0,0 +1,287 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import random
+import werkzeug
+import logging
+
+from datetime import datetime, timedelta
+from urlparse import urljoin
+from odoo.exceptions import UserError
+
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class SignupError(Exception):
+    pass
+
+
+def random_token():
+    # the token has an entropy of about 120 bits (6 bits/char * 20 chars)
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    return ''.join(random.SystemRandom().choice(chars) for i in xrange(20))
+
+
+def now(**kwargs):
+    dt = datetime.now() + timedelta(**kwargs)
+    return fields.Datetime.to_string(dt)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    signup_token = fields.Char(copy=False)
+    signup_type = fields.Char(string='Signup Token Type', copy=False)
+    signup_expiration = fields.Datetime(copy=False)
+    signup_valid = fields.Boolean(compute='_compute_signup_valid', string='Signup Token is Valid')
+    signup_url = fields.Char(compute='_compute_signup_url', string='Signup URL')
+    birthdate_date = fields.Date("Birthdate")
+
+    last_place_of_study = fields.Char(string='Last or Current Place of Study')
+    field_of_study = fields.Char(string='Field of Study')
+    usa_work_or_study_place = fields.Char(string='Work Place')
+    alumni_institute = fields.Char(string='Alumni Institute')
+    current_employee = fields.Char(string='Current Employee')
+    work_title = fields.Char(string='Work Title')
+    work_phone = fields.Char(string='Work Phone')
+    signature_image = fields.Binary(string='Signature')
+    is_applicant = fields.Boolean(default=False)
+    info_about_emk = fields.Text(string="How did you learn about the EMK Center?")
+
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], default='male', string='Gender')
+    usa_work_or_study = fields.Selection([('yes', 'Yes'), ('no', 'No')], default='no', string="USA Work or Study")
+
+    nationality_id = fields.Many2one("res.country", "Nationality")
+    occupation = fields.Many2one('member.occupation', string='Occupation')
+    subject_of_interest = fields.Many2one('member.subject.interest', string='Subject of Interest')
+    hightest_certification = fields.Many2one('member.certification', string='Highest Certification Achieved')
+
+    state = fields.Selection(
+        [('application', 'Application'), ('invoice', 'Invoiced'), ('paid', 'Paid'),
+         ('member', 'Membership')], default='application')
+
+    @api.onchange('birthdate')
+    def _onchange(self):
+        self.birthdate_date
+
+    @api.constrains('birthdate')
+    def _check_birthdate(self):
+        self.birthdate_date
+
+    @api.multi
+    def _compute_signup_valid(self):
+        dt = now()
+        for partner in self:
+            partner.signup_valid = bool(partner.signup_token) and \
+                                   (not partner.signup_expiration or dt <= partner.signup_expiration)
+
+    @api.multi
+    def _compute_signup_url(self):
+        """ proxy for function field towards actual implementation """
+        result = self._get_signup_url_for_action()
+        for partner in self:
+            partner.signup_url = result.get(partner.id, False)
+
+    @api.multi
+    def _get_signup_url_for_action(self, action=None, view_type=None, menu_id=None, res_id=None, model=None):
+        """ generate a signup url for the given partner ids and action, possibly overriding
+            the url state components (menu_id, id, view_type) """
+
+        res = dict.fromkeys(self.ids, False)
+        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+        for partner in self:
+            # when required, make sure the partner has a valid signup token
+            if self.env.context.get('signup_valid') and not partner.user_ids:
+                partner.signup_prepare()
+
+            route = 'login'
+            # the parameters to encode for the query
+            query = dict(db=self.env.cr.dbname)
+            signup_type = self.env.context.get('signup_force_type_in_url', partner.signup_type or '')
+            if signup_type:
+                route = 'reset_password' if signup_type == 'reset' else signup_type
+
+            if partner.signup_token and signup_type:
+                query['token'] = partner.signup_token
+            elif partner.user_ids:
+                query['login'] = partner.user_ids[0].login
+            else:
+                continue  # no signup token, no user, thus no signup url!
+
+            fragment = dict()
+            base = '/web#'
+            if action == '/mail/view':
+                base = '/mail/view?'
+            elif action:
+                fragment['action'] = action
+            if view_type:
+                fragment['view_type'] = view_type
+            if menu_id:
+                fragment['menu_id'] = menu_id
+            if model:
+                fragment['model'] = model
+            if res_id:
+                fragment['res_id'] = res_id
+
+            if fragment:
+                query['redirect'] = base + werkzeug.url_encode(fragment)
+
+            res[partner.id] = urljoin(base_url, "/web/%s?%s" % (route, werkzeug.url_encode(query)))
+        return res
+
+    @api.multi
+    def action_signup_prepare(self):
+        return self.signup_prepare()
+
+    @api.multi
+    def member_invoiced(self):
+
+        if self.state == 'application':
+            product_id = self.env['product.product'].search([('name', '=', 'Basic Membership')])
+            ins_inv = self.env['account.invoice']
+            journal_id = self.env['account.journal'].search([('code', '=', 'INV')])
+            account_id = self.env['account.account'].search(
+                [('internal_type', '=', 'receivable'), ('deprecated', '=', False)])
+            # for record in self.line_ids:
+            acc_invoice = {
+                'partner_id': self.id,
+                'date_invoice': datetime.now(),
+                'user_id': self.env.user.id,
+                'account_id': account_id.id,
+                'state': 'draft',
+                'invoice_line_ids': [
+                    (0, 0, {
+                        'name': product_id.name,
+                        'product_id': product_id.id,
+                        'price_unit': product_id.list_price,
+                        'account_id': journal_id.default_debit_account_id.id,
+                    })]
+            }
+            inv = ins_inv.create(acc_invoice)
+            inv.action_invoice_open()
+            mail = self.env['mail.compose.message']
+            # inv.action_invoice_sent()
+            # record.write({'invoice_id': inv.id, 'account_move_id': inv.move_id.id})
+
+            if inv:
+                template = self.env.ref('account.email_template_edi_invoice', False)
+                compose_form = self.env.ref('mail.email_compose_message_wizard_form', False)
+                ctx = dict(
+                    default_model='account.invoice',
+                    default_res_id=self.id,
+                    default_use_template=bool(template),
+                    default_template_id=template and template.id or False,
+                    default_composition_mode='comment',
+                    mark_invoice_as_sent=True,
+                    custom_layout="account.mail_template_data_notification_email_account_invoice"
+                )
+                return {
+                    'name': _('Compose Email'),
+                    'type': 'ir.actions.act_window',
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'mail.compose.message',
+                    'views': [(compose_form.id, 'form')],
+                    'view_id': compose_form.id,
+                    'target': 'new',
+                    'context': ctx,
+                }
+                # self.state = 'invoice'
+
+        # self.is_applicant = False
+        # self.free_member = True
+        # invoice_ins = self.env['account.invoice']
+        # data = {
+        #     'user_id': self.id,
+        #
+        # }
+        # invoice_ins.create(data)
+        # self.state = 'invoice'
+
+        # mail_ins = self.env['mail.mail']
+        # email_server = self.env['ir.mail_server'].search([], order='id DESC', limit=1)
+        #
+        # template = {
+        #     'subject': "Test Email",
+        #     'body_html': "Test Email",
+        #     'email_from': email_server.smtp_user,
+        #     'email_to': "git.mehedi@gmail.com"
+        # }
+        # mail = mail_ins.create(template)
+        # # mail.sudo().send()
+        #
+        # self.env['mail.template'].browse(26).sudo().send_mail(mail.id)
+        #
+        # template = False
+        # try:
+        #     template = self.env.ref('member_signup.set_password_email', raise_if_not_found=False)
+        # except ValueError:
+        #     pass
+        # if not template:
+        #     template = self.env.ref('member_signup.reset_password_email')
+        # assert template._name == 'mail.template'
+        #
+        # for user in self:
+        #     if not user.email:
+        #         raise UserError(_("Cannot send email: user %s has no email address.") % user.name)
+        #     template.with_context(lang=user.lang).send_mail(user.id, force_send=True, raise_exception=True)
+        #     _logger.info("Password reset email sent for user <%s> to <%s>", user.login, user.email)
+
+    @api.multi
+    def signup_cancel(self):
+        return self.write({'signup_token': False, 'signup_type': False, 'signup_expiration': False})
+
+    @api.multi
+    def signup_prepare(self, signup_type="signup", expiration=False):
+        """ generate a new token for the partners with the given validity, if necessary
+            :param expiration: the expiration datetime of the token (string, optional)
+        """
+        for partner in self:
+            if expiration or not partner.signup_valid:
+                token = random_token()
+                while self._signup_retrieve_partner(token):
+                    token = random_token()
+                partner.write({'signup_token': token, 'signup_type': signup_type, 'signup_expiration': expiration})
+        return True
+
+    @api.model
+    def _signup_retrieve_partner(self, token, check_validity=False, raise_exception=False):
+        """ find the partner corresponding to a token, and possibly check its validity
+            :param token: the token to resolve
+            :param check_validity: if True, also check validity
+            :param raise_exception: if True, raise exception instead of returning False
+            :return: partner (browse record) or False (if raise_exception is False)
+        """
+        partner = self.search([('signup_token', '=', token)], limit=1)
+        if not partner:
+            if raise_exception:
+                raise SignupError("Signup token '%s' is not valid" % token)
+            return False
+        if check_validity and not partner.signup_valid:
+            if raise_exception:
+                raise SignupError("Signup token '%s' is no longer valid" % token)
+            return False
+        return partner
+
+    @api.model
+    def signup_retrieve_info(self, token):
+        """ retrieve the user info about the token
+            :return: a dictionary with the user information:
+                - 'db': the name of the database
+                - 'token': the token, if token is valid
+                - 'name': the name of the partner, if token is valid
+                - 'login': the user login, if the user already exists
+                - 'email': the partner email, if the user does not exist
+        """
+        partner = self._signup_retrieve_partner(token, raise_exception=True)
+        res = {'db': self.env.cr.dbname}
+        if partner.signup_valid:
+            res['token'] = token
+            res['name'] = partner.name
+        if partner.user_ids:
+            res['login'] = partner.user_ids[0].login
+        else:
+            res['email'] = res['login'] = partner.email or ''
+        return res
