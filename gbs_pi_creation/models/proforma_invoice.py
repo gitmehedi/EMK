f8cf533bfccc41645aa761dafe7976e6fff78a43
rabbi <rabbi998@gmail.com>
2018-07-16 16:58:24 +0600

    PI line onchange, packing type on change, currency onchange done

--- gbs_pi_creation/models/proforma_invoice.py
+++ gbs_pi_creation/models/proforma_invoice.py
@@ -7,36 +7,47 @@ class ProformaInvoice(models.Model):
     _description = 'Proforma Invoice (PI)'
     _inherit = ['mail.thread', 'ir.needaction_mixin']
 
-    _rec_name='name'
+    _order = 'id DESC'
+    _rec_name = 'name'
 
     name = fields.Char(string='Name', index=True, readonly=True, default="/")
-
-    partner_id = fields.Many2one('res.partner', string='Customer', domain=[('customer', '=', True)],required=True,readonly=True,states={'draft': [('readonly', False)]})
+    partner_id = fields.Many2one('res.partner', string='Customer', domain=[('customer', '=', True)], required=True,
+                                 readonly=True, states={'draft': [('readonly', False)]})
     invoice_date = fields.Date('Invoice Date', readonly=True, required=1, states={'draft': [('readonly', False)]})
-    advising_bank_id = fields.Many2one('res.bank', string='Advising Bank', required=True, readonly=True, states={'draft': [('readonly', False)]})
-    currency_id = fields.Many2one('res.currency', string='Currency', readonly=True,required=True, states={'draft': [('readonly', False)]})
+    advising_bank_id = fields.Many2one('res.bank', string='Advising Bank', required=True, readonly=True,
+                                       states={'draft': [('readonly', False)]})
+    currency_id = fields.Many2one('res.currency', string='Currency', readonly=True, required=True,
+                                  states={'draft': [('readonly', False)]})
 
-    beneficiary_id = fields.Many2one('res.company', string='Beneficiary', required=True, readonly=True, states={'draft': [('readonly', False)]})
-    operating_unit_id = fields.Many2one('operating.unit', string='Unit', required=True, readonly=True, states={'draft': [('readonly', False)]})
-    transport_by = fields.Char(string='Transport By',required=True, readonly=True, states={'draft': [('readonly', False)]})
-    terms_condition = fields.Text(string='Terms & Conditions', required=True, readonly=True, states={'draft': [('readonly', False)]})
-    terms_id = fields.Many2one('terms.setup', string='Terms',store = False,readonly=True, states={'draft': [('readonly', False)]})
-
+    beneficiary_id = fields.Many2one('res.company', string='Beneficiary', required=True, readonly=True,
+                                     states={'draft': [('readonly', False)]})
+    operating_unit_id = fields.Many2one('operating.unit', string='Unit', required=True, readonly=True,
+                                        states={'draft': [('readonly', False)]})
+    transport_by = fields.Char(string='Transport By', required=True, readonly=True,
+                               states={'draft': [('readonly', False)]})
+    terms_condition = fields.Text(string='Terms & Conditions', required=True, readonly=True,
+                                  states={'draft': [('readonly', False)]})
+    terms_id = fields.Many2one('terms.setup', string='Terms', store=False, readonly=True,
+                               states={'draft': [('readonly', False)]})
 
     """ Shipping Address"""
-    ship_freight_type = fields.Char(string='Freight Type',readonly=True, states={'draft': [('readonly', False)]})
-    ship_exp_date = fields.Char(string='Exp. Shipping Date',readonly=True,states={'draft': [('readonly', False)]})
-    ship_exp_good_weight = fields.Char(string='Exp. Goods Weight',readonly=True,states={'draft': [('readonly', False)]})
-    ship_exp_cubic_weight = fields.Char(string='Exp. Cubic Weight',readonly=True,states={'draft': [('readonly', False)]})
-    ship_total_pkg = fields.Char(string='Total Package',readonly=True,states={'draft': [('readonly', False)]})
+    ship_freight_type = fields.Char(string='Freight Type', readonly=True, states={'draft': [('readonly', False)]})
+    ship_exp_date = fields.Char(string='Exp. Shipping Date', readonly=True, states={'draft': [('readonly', False)]})
+    ship_exp_good_weight = fields.Char(string='Exp. Goods Weight', readonly=True,
+                                       states={'draft': [('readonly', False)]})
+    ship_exp_cubic_weight = fields.Char(string='Exp. Cubic Weight', readonly=True,
+                                        states={'draft': [('readonly', False)]})
+    ship_total_pkg = fields.Char(string='Total Package', readonly=True, states={'draft': [('readonly', False)]})
 
     """ Customer Address"""
-    customer_add = fields.Text(string='Customer Address',readonly=True, compute='_compute_customer_address')
+    customer_add = fields.Text(string='Customer Address', readonly=True, compute='_compute_customer_address')
 
     """ Ship To"""
-    terms_condition = fields.Text(string='Terms of Condition', required=True, readonly=True, states={'draft': [('readonly', False)]})
+    terms_condition = fields.Text(string='Terms of Condition', required=True, readonly=True,
+                                  states={'draft': [('readonly', False)]})
     packing = fields.Char(string='Packing', required=True, readonly=True, states={'draft': [('readonly', False)]})
-    terms_of_payment = fields.Char(string='Terms Of Payment', required=True, readonly=True, states={'draft': [('readonly', False)]})
+    terms_of_payment = fields.Char(string='Terms Of Payment', required=True, readonly=True,
+                                   states={'draft': [('readonly', False)]})
 
     state = fields.Selection([
         ('draft', "Draft"),
@@ -50,16 +61,65 @@ class ProformaInvoice(models.Model):
     taxable_amount = fields.Float(string='Taxable Amount', readonly=True)
     untaxaed_amount = fields.Float(string='Untaxed Amount', readonly=True)
     taxed_amount = fields.Float(string='Tax', readonly=True)
-    freight_charge = fields.Float(string='Freight Charge',readonly=True, states={'draft': [('readonly', False)]})
+    freight_charge = fields.Float(string='Freight Charge', readonly=True, states={'draft': [('readonly', False)]})
     total = fields.Float(string='Total', readonly=True)
 
-
     """ Relational field"""
-    line_ids = fields.One2many('proforma.invoice.line', 'pi_id', string="Products", readonly=True, states={'draft': [('readonly', False)]})
-    so_ids = fields.Many2many('sale.order', 'so_pi_rel', 'pi_id', 'so_id',
-                              string='Sale Order', required=True,
-                              readonly=True, states={'draft': [('readonly', False)]},
-                              domain="[('pi_id', '=', False), ('state', '=', 'done'), ('credit_sales_or_lc', '=','lc_sales')]")
+    line_ids = fields.One2many('proforma.invoice.line', 'pi_id', string="Products", readonly=True,
+                               states={'draft': [('readonly', False)]})
+
+    # so_ids = fields.Many2many('sale.order', 'so_pi_rel', 'pi_id', 'so_id',
+    #                           string='Sale Order', required=True,
+    #                           readonly=True, states={'draft': [('readonly', False)]},
+    #                           domain="[('pi_id', '=', False), ('state', '=', 'done'), ('credit_sales_or_lc', '=','lc_sales')]")
+
+    def _get_pack_type(self):
+        return self.env['product.packaging.mode'].search([], limit=1)
+
+    """New field"""
+    pack_type = fields.Many2one('product.packaging.mode', string='Packing Mode', default=_get_pack_type,
+                                required=True)
+
+    credit_sales_or_lc = fields.Selection([
+        ('cash', 'Cash'),
+        ('credit_sales', 'Credit'),
+        ('lc_sales', 'L/C'),
+    ], string='Sales Type', track_visibility='onchange', required=True)
+
+    def _get_order_type(self):
+        return self.env['sale.order.type'].search([], limit=1)
+
+    type_id = fields.Many2one(comodel_name='sale.order.type', string='Type', default=_get_order_type, readonly=True,
+                              states={'draft': [('readonly', False)]}, track_visibility='onchange')
+
+    """On Change fucn"""
+
+    @api.onchange('type_id')
+    def onchange_type(self):
+        sale_type_pool = self.env['sale.order.type'].search([('id', '=', self.type_id.id)])
+        if self.type_id:
+            self.credit_sales_or_lc = sale_type_pool.sale_order_type
+            self.currency_id = sale_type_pool.currency_id.id
+
+
+    @api.multi
+    @api.onchange('currency_id')
+    def currency_id_onchange(self):
+        self._get_changed_price()
+
+
+    @api.multi
+    @api.onchange('pack_type')
+    def pack_type_onchange(self):
+        self._get_changed_price()
+
+
+    def _get_changed_price(self):
+        for order in self:
+            for line in order.line_ids:
+                line._set_product_line()
+
+        """On Change fucn ends"""
 
     @api.multi
     def _compute_customer_address(self, context=None):
@@ -80,7 +140,8 @@ class ProformaInvoice(models.Model):
                         ### Need Update
                         total_qty = line.product_uom_qty + prod['quantity']
                         total_price_subtotal = line.price_subtotal + prod['price_subtotal']
-                        avg_unit_price = total_price_subtotal / (total_qty * 1.0)  # to ensure flaot value multily with 1.0
+                        avg_unit_price = total_price_subtotal / (
+                            total_qty * 1.0)  # to ensure flaot value multily with 1.0
 
                         prod.update({
                             'quantity': total_qty,
@@ -100,7 +161,6 @@ class ProformaInvoice(models.Model):
                                  }
                     product_list.append(prod_line)
 
-
         for prod in product_list:
             res.append([0, 0, prod])
 
@@ -118,16 +178,15 @@ class ProformaInvoice(models.Model):
 
         return super(ProformaInvoice, self).create(vals)
 
-
-    @api.multi
-    def write(self, vals, context=None):
-        if self.state == 'draft' and vals.get('so_ids'):
-            self.update_total_info(vals)
-
-        # if self.state == 'confirm':
-        #      vals['so_ids'] = vals['so_ids'].ids
-
-        return super(ProformaInvoice, self).write(vals)
+    # @api.multi
+    # def write(self, vals, context=None):
+    #     if self.state == 'draft' and vals.get('so_ids'):
+    #         self.update_total_info(vals)
+    #
+    #     # if self.state == 'confirm':
+    #     #      vals['so_ids'] = vals['so_ids'].ids
+    #
+    #     return super(ProformaInvoice, self).write(vals)
 
 
     def update_total_info(self, vals):
@@ -149,12 +208,10 @@ class ProformaInvoice(models.Model):
         vals['total'] = total
         vals['untaxed_amount'] = untaxed_amount
 
-
     @api.constrains('freight_charge')
     def check_freight_charge_val(self):
         if self.freight_charge < 0:
             raise UserError('Freight Charge can not be Negative')
-
 
     @api.onchange('so_ids')
     def so_product_line(self):
@@ -192,7 +249,6 @@ class ProformaInvoice(models.Model):
         self.total = total
         self.untaxed_amount = untaxed_amount
 
-
     def getAddressByPartner(self, partner):
 
         address = partner.address_get(['delivery', 'invoice'])
@@ -224,32 +280,29 @@ class ProformaInvoice(models.Model):
     @api.multi
     def action_confirm(self):
 
-        self.update_Pi_to_so_obj()
+        # self.update_Pi_to_so_obj()
         res = {'state': 'confirm'}
         new_seq = self.env['ir.sequence'].next_by_code_new('proforma.invoice', self.invoice_date)
         if new_seq:
             res['name'] = new_seq
         self.write(res)
 
-
     def update_Pi_to_so_obj(self):
-        #Update PI to SO
+        # Update PI to SO
         for so in self.so_ids:
             so.pi_id = self.id
 
-            #update DA
+            # update DA
             da_obj = so.env['delivery.authorization'].search([('sale_order_id', '=', so.id)])
             if da_obj:
                 for da_ in da_obj:
-                    da_.pi_id = self.id # update PI to DA if it is already created
+                    da_.pi_id = self.id  # update PI to DA if it is already created
 
-            #update DO
+            # update DO
             do_obj = so.env['delivery.order'].search([('sale_order_id', '=', so.id)])
             if do_obj:
                 for do_ in do_obj:
                     do_.pi_id = self.id  # update PI to DO if it is already created
-
-
 
     @api.onchange('freight_charge')
     def onchange_freight_charge(self):
@@ -269,54 +322,83 @@ class ProformaInvoice(models.Model):
         return [('state', 'in', ['draft'])]
 
 
-    ## mail notification
-    # @api.multi
-    # def _notify_approvers(self):
-    #     approvers = self.employee_id._get_employee_manager()
-    #     if not approvers:
-    #         return True
-    #     for approver in approvers:
-    #         self.sudo(SUPERUSER_ID).add_follower(approver.id)
-    #         if approver.sudo(SUPERUSER_ID).user_id:
-    #             self.sudo(SUPERUSER_ID)._message_auto_subscribe_notify(
-    #                 [approver.sudo(SUPERUSER_ID).user_id.partner_id.id])
-    #     return True
+        ## mail notification
+        # @api.multi
+        # def _notify_approvers(self):
+        #     approvers = self.employee_id._get_employee_manager()
+        #     if not approvers:
+        #         return True
+        #     for approver in approvers:
+        #         self.sudo(SUPERUSER_ID).add_follower(approver.id)
+        #         if approver.sudo(SUPERUSER_ID).user_id:
+        #             self.sudo(SUPERUSER_ID)._message_auto_subscribe_notify(
+        #                 [approver.sudo(SUPERUSER_ID).user_id.partner_id.id])
+        #     return True
+
+
 
 class ProformaInvoiceLine(models.Model):
     _name = 'proforma.invoice.line'
     _description = 'Proforma Invoice Line (PI Line)'
 
     """ Line values"""
-    product_id = fields.Many2one('product.product', string="Product", ondelete='cascade')
-    uom_id = fields.Many2one('product.uom', string="UoM", ondelete='cascade', readonly=True)
+    product_id = fields.Many2one('product.product', string="Product",required=True)
+    description = fields.Char(string='Description',required=True)
+    uom_id = fields.Many2one('product.uom', string="Unit of Measure", required=True)
     quantity = fields.Float(string="Ordered Qty")
-    price_unit = fields.Float(string="Price Unit", readonly=True)
-    tax = fields.Many2one('account.tax', string='Tax (%)')
-    price_subtotal = fields.Float(string="Price Subtotal", readonly=True)
+    price_unit = fields.Float(string="Unit Price",required=True)
+    tax = fields.Many2one('account.tax', string='Taxes')
+    price_subtotal = fields.Float(string="Subtotal", readonly=True)
 
     """ Relational field"""
     pi_id = fields.Many2one('proforma.invoice', ondelete='cascade')
 
 
+    def _set_product_line(self):
+        if self.product_id:
+            self.price_unit = self._get_product_sales_price(self.product_id)
+
+            self.description = self.product_id.name + ' (' +self.product_id.attribute_value_ids.name+')'
+            self.quantity = 1
+            self.price_subtotal = self.price_unit * self.quantity
+            self.pi_id.sub_total = self.price_subtotal
+            self.pi_id.taxed_amount = self.calculate_tax_amount(self.tax.id, self.price_subtotal)
+            self.pi_id.untaxaed_amount = self.price_subtotal
+            self.pi_id.total = self.pi_id.taxed_amount + self.price_subtotal
+
+
+
     @api.onchange('product_id')
     def onchange_product_id(self):
-        # Fetch Active price for this product
-        active_prod_price_pool = self.env['product.sale.history.line'].search([('product_id','=',self.product_id.id)])
+        self._set_product_line()
 
-        print active_prod_price_pool.currency_id.id
 
-        self.uom_id = active_prod_price_pool.uom_id
-        self.price_unit = active_prod_price_pool.new_price
-        self.quantity = 1
-        self.price_subtotal = self.price_unit * self.quantity
 
-        ## Set Proforma Invoice Table value
-        #self.pi_id.currency_id.id = active_prod_price_pool.currency_id.id
-        self.pi_id.sub_total = self.price_subtotal
-        self.pi_id.taxed_amount = self.calculate_tax_amount(self.tax.id, self.price_subtotal)
-        self.pi_id.untaxaed_amount = self.price_subtotal
-        self.pi_id.total = self.pi_id.taxed_amount + self.price_subtotal
 
+    # @api.multi
+    # @api.onchange('product_id', 'currency_id', 'pack_type')
+    # def product_id_change(self):
+    #
+    #     res = super(InheritedSaleOrderLine, self).product_id_change()
+    #     vals = {}
+    #
+    #     if self.product_id:
+    #         vals['price_unit'] = self._get_product_sales_price(self.product_id)
+    #         self.update(vals)
+    #
+    #     return res
+
+    # @api.onchange('product_uom', 'product_uom_qty')
+    # def product_uom_change(self):
+    #     res = super(InheritedSaleOrderLine, self).product_uom_change()
+    #     self.da_qty = self.product_uom_qty
+    #
+    #     vals = {}
+    #     if self.product_id:
+    #         vals['price_unit'] = self._get_product_sales_price(self.product_id)
+    #         self.update(vals)
+    #
+    #     return res
 
 
     @api.onchange('quantity')
@@ -334,7 +416,47 @@ class ProformaInvoiceLine(models.Model):
         self.pi_id.total = self.pi_id.taxed_amount + self.price_subtotal
 
 
+    @api.onchange('uom_id')
+    def onchange_uomid(self):
+        self._set_product_line()
+
+
     def calculate_tax_amount(self, tax_id, total_price):
         if tax_id:
-            tax_pool = self.env['account.tax'].search([('id','=',tax_id)])
-            return (tax_pool.amount/100) * total_price
+            tax_pool = self.env['account.tax'].search([('id', '=', tax_id)])
+            return (tax_pool.amount / 100) * total_price
+
+
+    def _get_product_sales_price(self, product):
+
+        if product:
+            if not self.uom_id:
+                self.uom_id = self.product_id.uom_id
+
+            price_change_pool = self.env['product.sale.history.line'].search(
+                [('product_id', '=', product.id),
+                 ('currency_id', '=', self.pi_id.currency_id.id),
+                 ('product_package_mode', '=', self.pi_id.pack_type.id),
+                 ('uom_id', '=', self.uom_id.id)])
+
+            if not price_change_pool:
+                price_change_pool = self.env['product.sale.history.line'].search(
+                    [('product_id', '=', product.id),
+                     ('currency_id', '=', self.pi_id.currency_id.id),
+                     ('product_package_mode', '=', self.pi_id.pack_type.id),
+                     ('category_id', '=', self.uom_id.category_id.id)])
+
+                if price_change_pool:
+                    if not price_change_pool.uom_id.uom_type == "reference":
+                        uom_base_price = price_change_pool.new_price / price_change_pool.uom_id.factor_inv
+                    else:
+                        uom_base_price = price_change_pool.new_price
+
+                    if not self.uom_id.uom_type == "reference":
+                        return uom_base_price * self.uom_id.factor_inv
+                    else:
+                        return uom_base_price
+            else:
+                return price_change_pool.new_price
+
+        return 0.00
