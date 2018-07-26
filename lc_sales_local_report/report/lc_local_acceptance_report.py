6e3ef71ec30ef04ab5c1c0f1b007ce87eea3369e
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2018-07-26 14:40:39 +0600

    [IMP]Refactoring

--- lc_sales_local_report/report/lc_local_acceptance_report.py
+++ lc_sales_local_report/report/lc_local_acceptance_report.py
@@ -52,6 +52,41 @@ class LocalSecondAcceptanceReport(models.AbstractModel):
 class AcceptanceReportsUtility(models.TransientModel):
     _name = 'acceptance.report.utility'
 
+    sql_in_tk = '''SELECT DISTINCT pt.id as template_id,
+                                  lc.id as lc_id,
+                                  ps.id as shipment_id,
+                                  pt.name as product_name,
+                                  ps.name as shipment_name,
+                                  lc.unrevisioned_name as lc_name, 
+                                  COALESCE((ps.invoice_value),0) as value,
+                                  rp.name as customer,
+                                  lc.tenure as tenor,
+                                  lc.second_party_bank as customer_bank,
+                                  rb.bic as beneficiary_bank,
+                                  ps.to_sales_date as dispatch_to_sale,
+                                  ps.to_buyer_date as dispatch_to_customer,
+                                  ps.to_buyer_bank_date as buyer_bank_date,
+                                  ps.to_seller_bank_date as seller_bank_date,
+                                  ps.comment as comment,
+                                  rc.symbol as currency,
+                                  (CASE
+                                      WHEN ps.to_buyer_bank_date is not null THEN date(%s) - date(ps.to_buyer_bank_date)
+                                      WHEN ps.to_seller_bank_date is not null THEN date(%s) - date(ps.to_seller_bank_date)
+                                      WHEN ps.to_buyer_date is not null THEN date(%s) - date(ps.to_buyer_date)
+                                      WHEN ps.to_sales_date is not null THEN date(%s) - date(ps.to_sales_date)
+                                    ELSE 0
+                                  END) as aging
+                           FROM product_template pt
+                              JOIN product_product pp on pp.product_tmpl_id = pt.id
+                              JOIN shipment_product_line sp on pp.id = sp.product_id
+                              JOIN purchase_shipment ps on ps.id = sp.shipment_id
+                              JOIN letter_credit lc on lc.id = ps.lc_id
+                              LEFT JOIN res_partner rp on lc.second_party_applicant = rp.id
+                              LEFT JOIN res_bank rb on lc.first_party_bank = rb.id
+                              LEFT JOIN res_currency rc on lc.currency_id = rc.id
+                           WHERE pt.id = %s AND lc.type = 'export' AND lc.region_type = 'local' AND ps.state in %s
+                           ORDER BY pt.id ASC'''
+
     def get_report_data(self, data,state_condition):
         acceptances = []
         current_date =  fields.Date.context_today(self)
@@ -62,66 +97,25 @@ class AcceptanceReportsUtility(models.TransientModel):
             'title': 'TOTAL VALUE',
             'total_val': 0,
         }
-
-        sql_in_tk = '''SELECT DISTINCT pt.id as template_id,
-                              lc.id as lc_id,
-                              ps.id as shipment_id,
-                              pt.name as product_name,
-                              ps.name as shipment_name,
-                              lc.unrevisioned_name as lc_name, 
-                              COALESCE((ps.invoice_value),0) as value,
-                              rp.name as customer,
-                              lc.tenure as tenor,
-                              lc.second_party_bank as customer_bank,
-                              rb.bic as beneficiary_bank,
-                              ps.to_sales_date as dispatch_to_sale,
-                              ps.to_buyer_date as dispatch_to_customer,
-                              ps.to_buyer_bank_date as buyer_bank_date,
-                              ps.to_seller_bank_date as seller_bank_date,
-                              ps.comment as comment,
-                              rc.symbol as currency,
-                              (CASE
-                                  WHEN ps.to_buyer_bank_date is not null THEN date('%s') - date(ps.to_buyer_bank_date)
-                                  WHEN ps.to_seller_bank_date is not null THEN date('%s') - date(ps.to_seller_bank_date)
-                                  WHEN ps.to_buyer_date is not null THEN date('%s') - date(ps.to_buyer_date)
-                                  WHEN ps.to_sales_date is not null THEN date('%s') - date(ps.to_sales_date)
-                                ELSE 0
-                              END) as aging
-                       FROM product_template pt
-                          JOIN product_product pp on pp.product_tmpl_id = pt.id
-                          JOIN shipment_product_line sp on pp.id = sp.product_id
-                          JOIN purchase_shipment ps on ps.id = sp.shipment_id
-                          JOIN letter_credit lc on lc.id = ps.lc_id
-                          LEFT JOIN res_partner rp on lc.second_party_applicant = rp.id
-                          LEFT JOIN res_bank rb on lc.first_party_bank = rb.id
-                          LEFT JOIN res_currency rc on lc.currency_id = rc.id
-                       WHERE pt.id = %s AND lc.type = 'export' AND lc.region_type = 'local' AND ps.state in %s
-                       ORDER BY pt.id ASC
-                    '''% (current_date,current_date,current_date,current_date,product_temp_id,state_condition)
-
-        self.env.cr.execute(sql_in_tk)
+        self.env.cr.execute(self.sql_in_tk,(current_date, current_date, current_date, current_date, product_temp_id, state_condition))
         for vals in self.env.cr.dictfetchall():
-            if vals:
-                sale_person_list = []
-                lc_objs = self.env['letter.credit'].search([('id','=',vals['lc_id'])])
-                for pi_id in lc_objs.pi_ids_temp:
-                    so_ids = self.env['sale.order'].search([('pi_id', '=', pi_id.id)])
-                    for so_id in so_ids:
-                        sale_person_list.append(so_id.user_id.name)
-                        # first_delivery_date = so_id.picking_ids.filtered(lambda o: o.state == 'done')[-1].date_done
-                        # last_delivery_date = so_id.picking_ids.filtered(lambda o: o.state == 'done')[0].date_done
-                sale_persons = ','.join(sale_person_list)
-                vals.update({'sale_persons': sale_persons, })
-                picking_ids = self.env['stock.picking'].search([('lc_id','=',lc_objs.id),('state','=','done')] , order='date_done asc')
-                if picking_ids:
-                    first_delivery_date =picking_ids[0].date_done or False
-                    last_delivery_date =picking_ids[-1].date_done or False
-                    vals.update({'first_delivery_date': first_delivery_date, })
-                    vals.update({'last_delivery_date': last_delivery_date, })
+            sale_person_list = []
+            lc_obj = self.env['letter.credit'].search([('id','=',vals['lc_id'])])
+            so_ids = self.env['sale.order'].search([('lc_id', '=', lc_obj.id)])
+            for so_id in so_ids:
+                sale_person_list.append(so_id.user_id.name)
+            sale_persons = ','.join(sale_person_list)
+            vals.update({'sale_persons': sale_persons, })
+            picking_ids = self.env['stock.picking'].search([('lc_id','=',lc_obj.id),('state','=','done')] , order='date_done asc')
+            if picking_ids:
+                first_delivery_date =picking_ids[0].date_done or False
+                last_delivery_date =picking_ids[-1].date_done or False
+                vals.update({'first_delivery_date': first_delivery_date, })
+                vals.update({'last_delivery_date': last_delivery_date, })
 
-                total_value['total_val'] = total_value['total_val'] + vals['value']
-                vals['value'] = formatLang(self.env, vals['value']) if vals['value'] else None
-                acceptances.append(vals)
+            total_value['total_val'] = total_value['total_val'] + vals['value']
+            vals['value'] = formatLang(self.env, vals['value']) if vals['value'] else None
+            acceptances.append(vals)
 
         total_value['total_val'] = formatLang(self.env, total_value['total_val']) if total_value['total_val'] else None
         return {'acceptances': acceptances,'total': total_value}
