84ec861e23063cf292266270c019099c1c8d31cd
rabbi <rabbi998@gmail.com>
2018-07-26 13:22:21 +0600

    VAT Challan ID added to picking and report

--- delivery_order/models/inherit_stock_picking.py
+++ delivery_order/models/inherit_stock_picking.py
@@ -13,6 +13,13 @@ class InheritStockPicking(models.Model):
                                                  'confirmed': [('readonly', True)], 'waiting': [('readonly', True)],
                                                  'done': [('readonly', True)], 'cancel': [('readonly', True)]})
 
+    vat_challan_id = fields.Char(string='VAT Challan Id', states={'partially_available': [('readonly', True)],
+                                                                  'confirmed': [('readonly', True)],
+                                                                  'waiting': [('readonly', True)],
+                                                                  'done': [('readonly', True)],
+                                                                  'cancel': [('readonly', True)]},
+                                 track_visibility='onchange')
+
     transport_details = fields.Selection([
         ('owned', 'Owned'),
         ('hired', 'Hired'),
@@ -27,7 +34,8 @@ class InheritStockPicking(models.Model):
     vehicle_no = fields.Char(size=100, string='Vehicle #',
                              states={'partially_available': [('readonly', True)],
                                      'confirmed': [('readonly', True)], 'waiting': [('readonly', True)],
-                                     'done': [('readonly', True)], 'cancel': [('readonly', True)]})
+                                     'done': [('readonly', True)], 'cancel': [('readonly', True)]},
+                             track_visibility='onchange')
     driver_no = fields.Char(size=100, string='Driver Name',
                             states={'partially_available': [('readonly', True)],
                                     'confirmed': [('readonly', True)], 'waiting': [('readonly', True)],
@@ -108,7 +116,6 @@ class InheritStockPicking(models.Model):
     def _calculate_lc_id(self):
         for stock_lc in self:
             stock_lc.lc_id = stock_lc.delivery_order_id.sale_order_id.lc_id.id
-
 
     @api.multi
     def do_print_delivery_challan(self):
