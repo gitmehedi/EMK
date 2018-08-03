14ab65873cf8b38b1456863bb7c1715f5060defa
rabbi <rabbi998@gmail.com>
2018-08-03 10:56:42 +0600

    UoM added inside pack mode

--- product_sales_pricelist/models/product_packaging_mode.py
+++ product_sales_pricelist/models/product_packaging_mode.py
@@ -7,4 +7,5 @@ class ProductPackagingMode(models.Model):
 
     packaging_mode = fields.Char(string='Packaging Mode', required=True)
     is_jar_bill_included = fields.Boolean(string='Is Jar Bill Included?')
+    uom_id = fields.Many2one('product.uom',string='UoM', required=True)
 
