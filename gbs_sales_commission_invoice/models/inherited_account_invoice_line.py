5373b63c012a001fafac7b7e4d7d09b2128888a3
rabbi <rabbi998@gmail.com>
2018-08-07 14:24:09 +0600

    update

--- gbs_sales_commission_invoice/models/inherited_account_invoice_line.py
+++ gbs_sales_commission_invoice/models/inherited_account_invoice_line.py
@@ -17,8 +17,10 @@ class InheritedAccountInvoiceLine(models.Model):
             'currency_id': so_obj.currency_id.id,
         }
 
-        self.write(vals)
+        # update this field to ensure same currency on invoice line and invoice obj
+        self.invoice_id.write({'currency_id': so_obj.currency_id.id})
 
+        self.write(vals)
 
     @api.multi
     @api.depends('product_id', 'quantity')
@@ -27,6 +29,8 @@ class InheritedAccountInvoiceLine(models.Model):
             sale_order_pool = inv.env['sale.order'].search([('name', '=', inv.invoice_id.origin)])
 
             inv._update_commission_related_vals(sale_order_pool)
+
+
 
             commission = None
             for sale_line in sale_order_pool.order_line:
@@ -37,12 +41,19 @@ class InheritedAccountInvoiceLine(models.Model):
                     for picking_line in sale_order_pool.picking_ids[0].pack_operation_ids:
                         commission = sale_line.commission_rate * picking_line.qty_done
 
+                        if self.company_id.currency_id != sale_order_pool.currency_id:
+                            commission = commission * sale_order_pool.currency_id.rate
+
                 elif commission_type == 'percentage':
                     commission_percentage_amt = (sale_line.commission_rate * sale_line.price_subtotal) / 100
                     for picking_line in sale_order_pool.picking_ids[0].pack_operation_ids:
                         commission = commission_percentage_amt * picking_line.qty_done
 
+                        if self.company_id.currency_id != sale_order_pool.currency_id:
+                            commission = commission * sale_order_pool.currency_id.rate
+
                 inv.commission_amount = commission
+
 
 
     commission_amount = fields.Float(string='Commission Amount', compute='_calculate_commission_amount',store=True)
