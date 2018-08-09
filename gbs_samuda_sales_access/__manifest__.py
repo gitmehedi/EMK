9d42cce9fe8ed79cec12f1cf11c5afb1fa2dc5d3
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2018-08-09 20:28:44 +0600

    [IMP]Sales Access dependency Issue fix

--- gbs_samuda_sales_access/__manifest__.py
+++ gbs_samuda_sales_access/__manifest__.py
@@ -21,14 +21,12 @@
         'product_harmonized_system',
         'gbs_application_group',
         'stock',
-        'account',
         'gbs_sales_commission',
         'delivery_order',
         'sales_team',
         'gbs_sales_delivery_goods',
         'customer_credit_limit',
         'sale',
-        'gbs_procure_n_commercial_access',
         'sales_team_operating_unit',
         'product_sales_pricelist',
     ],
@@ -38,7 +36,6 @@
     'data': [
         'security/ir.model.access.csv',
         'security/ir_rule.xml',
-        'views/lc_sales_comm_acc.xml',
         'views/menu_items.xml',
 
     ],
