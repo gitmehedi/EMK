51ca1095438e4030d1b4399d5c2162dd86ed500d
ritachowdhury <ritaislam6789@gmail.com>
2018-08-12 21:02:45 +0600

    [IMP] email template for payroll

--- hr_payroll_festival_bonus/models/inherited_hr_payslip.py
+++ hr_payroll_festival_bonus/models/inherited_hr_payslip.py
@@ -12,19 +12,17 @@ class InheritHRPayslipsBatches(models.Model):
 class InheritHRPayslip(models.Model):
     _inherit = 'hr.payslip'
 
-    # def _get_default_type(self):
-    #     active_id = self._context['active_id']
-    #     if active_id:
-    #         payslip_run_pool = self.env['hr.payslip.run']
-    #         payslip_run = payslip_run_pool.search([('id','=',active_id)])
-    #         if payslip_run:
-    #             return payslip_run.type
-    #
-    #     return "0"
+    def _get_default_type(self):
+        if self.payslip_run_id:
+            self.type = self.payslip_run_id
+            data = self.type
+            return data
+
+        return "0"
 
     type = fields.Selection([("0", "Regular Salary"),
                              ("1", "Festival Bonus"),
                              ("2", "Salary With Bonus")], "Type",
-                            required=True)
+                            required=True, default=_get_default_type)
 
-    festival_date = fields.Date('Festival Date',related='payslip_run_id.festival_date',store=True)
+    festival_date = fields.Date('Festival Date', related='payslip_run_id.festival_date', store=True)
