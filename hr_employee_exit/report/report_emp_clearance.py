8d8cc45bca3ec0fe0ae95d82335b11164305e94a
ritachowdhury <ritaislam6789@gmail.com>
2018-08-29 12:51:30 +0600

    [IMP] exit employee clearance report

--- hr_employee_exit/report/report_emp_clearance.py
+++ hr_employee_exit/report/report_emp_clearance.py
@@ -16,7 +16,7 @@ class LoanReport(models.AbstractModel):
         data['job_id'] = exit_obj.job_id.name
         data['emp_code'] = exit_obj.employee_id.device_employee_acc
 
-        sql = '''SELECT p.state as state,
+        sql_dept = '''SELECT p.state as state,
                         item.name as item_name,
                         p.remarks as remarks,
                         dept.name as responsible_dept
@@ -28,17 +28,38 @@ class LoanReport(models.AbstractModel):
                 left join 
                     hr_department as dept 
                 on dept.id = p.responsible_department'''
-        self.env.cr.execute(sql)
+        self.env.cr.execute(sql_dept)
         data_list = self.env.cr.dictfetchall()
-        supplier = {vals['responsible_dept']: {'item_list': [], }
+        item_checklist = {vals['responsible_dept']: {'item_list': [], }
                     for vals in data_list}
         for vals in data_list:
             if vals:
-                supplier[vals['responsible_dept']]['item_list'].append(vals)
+                item_checklist[vals['responsible_dept']]['item_list'].append(vals)
+
+        sql_emp = '''SELECT p.state AS state,
+                        item.name AS item_name,
+                        p.remarks AS remarks,
+                        emp.name_related as responsible_emp
+                    FROM
+                        hr_exit_checklists_line AS p
+                    LEFT JOIN
+                        hr_exit_checklist_item as item
+                    ON item.id = p.checklist_item_id
+                    LEFT JOIN
+                        hr_employee as emp
+                    ON emp.id = p.responsible_emp'''
+        self.env.cr.execute(sql_emp)
+        data_list = self.env.cr.dictfetchall()
+        checklist = {vals['responsible_emp']:{'item':[],}for vals in data_list}
+        for vals in data_list:
+            if vals:
+                checklist[vals['responsible_emp']]['item'].append(vals)
+
 
         docargs = {
             'data': data,
-            'lists': supplier
+            'lists': item_checklist,
+            'checklist': checklist
         }
 
         return self.env['report'].render('hr_employee_exit.report_employee_clearance',docargs)
