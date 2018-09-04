cfe09688fc0cf87f50cb9669b0bd20c54ebde0fc
rabbi <rabbi998@gmail.com>
2018-09-04 13:37:59 +0600

    update

---
+++ gbs_customize_accountant_view/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Customized Accounts View',
+    'version': '10.0.1.0.0',
+    'author': 'Genweb2 Limited',
+    'website': 'http://www.genweb2.com',
+    'category': 'sales',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/partner_inherit_view.xml',
+
+    ],
+    'summary': 'Customized Accounts View',
+    'description':"Customized Accounts View",
+    'application': False,
+    'installable': True,
+}
