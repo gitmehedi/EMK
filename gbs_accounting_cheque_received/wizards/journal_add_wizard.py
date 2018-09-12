544de0125e39d236032d02310de1859bdc54484a
rabbi <rabbi998@gmail.com>
2018-09-12 18:03:29 +0600

    fixed issue on payment journal

--- gbs_accounting_cheque_received/wizards/journal_add_wizard.py
+++ gbs_accounting_cheque_received/wizards/journal_add_wizard.py
@@ -10,4 +10,11 @@ class JournalAddingWizard(models.TransientModel):
     def update_payment_journal_to_cheque_received(self, context=None):
         if context['active_id']:
             cheque_rcv_pool = self.env['accounting.cheque.received'].search([('id', '=', context['active_id'])])
-            cheque_rcv_pool.write({'journal_id': self.journal_id.id})
+
+            if cheque_rcv_pool.journal_id.currency_id:
+                currency = cheque_rcv_pool.journal_id.currency_id
+            else:
+                currency = cheque_rcv_pool.company_id.currency_id
+
+            cheque_rcv_pool.write({'currency_id':currency.id, 'journal_id': self.journal_id.id})
+
