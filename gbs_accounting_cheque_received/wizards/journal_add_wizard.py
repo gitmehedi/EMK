3c937fde0b933a4e6853be10df58d16b27e84d17
rabbi <rabbi998@gmail.com>
2018-09-12 21:58:09 +0600

    update

--- gbs_accounting_cheque_received/wizards/journal_add_wizard.py
+++ gbs_accounting_cheque_received/wizards/journal_add_wizard.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError, Warning
 
 
 class JournalAddingWizard(models.TransientModel):
@@ -10,6 +11,10 @@ class JournalAddingWizard(models.TransientModel):
     def update_payment_journal_to_cheque_received(self, context=None):
         if context['active_id']:
             cheque_rcv_pool = self.env['accounting.cheque.received'].search([('id', '=', context['active_id'])])
+
+            if self.journal_id.currency_id:
+                if self.journal_id.currency_id != cheque_rcv_pool.currency_id:
+                    raise ValidationError('Payment Journal Currency and Cheque Received Currency must be same')
 
             cheque_rcv_pool.write({'journal_id': self.journal_id.id})
 
