c5e10083bcc79c125758fd1d611c2f6fbda7d15a
gitmehedi <git.mehedi@gmail.com>
2018-09-12 11:49:01 +0600

    [ADD] Add new module for giving permission to Point of Sale User who will see All Sessions.

---
+++ pos_access/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Point of Sale Access Right',
+    'version': '1.1.0',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale'],
+    'data': [
+        'security/ir_rule.xml',
+    ],
+    'installable': True
+}
