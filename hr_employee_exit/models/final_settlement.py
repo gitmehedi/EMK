30c4c677a0af8ac3756c7330933a6ec109db076c
ritachowdhury <ritaislam6789@gmail.com>
2018-09-20 15:10:59 +0600

    [IMP] Full and final settlemnt

--- hr_employee_exit/models/final_settlement.py
+++ hr_employee_exit/models/final_settlement.py
@@ -7,18 +7,35 @@ class FinalSettlement(models.Model):
     _description = 'Employee Exit Full & Final Settlement'
 
     employee_code = fields.Char('Employee Code',related='employee_id.employee_number',readonly=True)
-    employee_id = fields.Many2one('hr.employee',string = 'Employee',required=True)
-    emp_designation = fields.Many2one('hr.job', string='Designation', related='employee_id.job_id',readonly=True, required=True)
+    employee_id = fields.Many2one('hr.employee',string = 'Employee',required=True, track_visibility='onchange')
+    emp_designation = fields.Many2one('hr.job', string='Designation', related='employee_id.job_id',readonly=True)
     joining_date = fields.Date(related='employee_id.initial_employment_date', string='Date of Join', readonly=True)
-    leaving_date = fields.Date(string='Date of Leaving',required=True)
+    leaving_date = fields.Date(string='Date of Leaving',readonly=True, store=True)
     emp_payslip_ids = fields.One2many('final.settlement.line','final_settlement_id', string='Salary')
-    payment_ids = fields.One2many('final.settlement.payments','payment_id', string='Salary')
-    deduction_ids = fields.One2many('final.settlement.deduction','deduction_id', string='Salary')
+    payment_ids = fields.One2many('final.settlement.payments','payment_id', string='Salary',track_visibility='onchange')
+    deduction_ids = fields.One2many('final.settlement.deduction','deduction_id', string='Salary',track_visibility='onchange')
+    payment_total_amount = fields.Float(string='Total Payment', readonly=True, compute='_compute_payment', store=True)
+    deduction_total_amount = fields.Float(string='Total Deduction', readonly=True, compute='_compute_payment', store=True)
+    total_amount = fields.Float(string='Total', readonly=True, track_visibility='onchange', compute='_compute_total_amount', store=True)
+
+    @api.multi
+    @api.depends('payment_ids')
+    def _compute_payment(self):
+        self.payment_total_amount = self.deduction_total_amount = 0.0
+        for pay in self.payment_ids:
+            self.payment_total_amount += pay.amount
+        for deduc in self.deduction_ids:
+            self.deduction_total_amount += deduc.amount
+
+    @api.multi
+    @api.depends('payment_ids')
+    def _compute_total_amount(self):
+       self.total_amount = self.payment_total_amount - self.deduction_total_amount
+
     state = fields.Selection([
         ('draft', 'Draft'),
-        ('submit', 'Submit'),
-        ('validate', 'Supervisor Approve'),
-        ('approved', ' HR Approved'),
+        ('validate', 'Validate'),
+        ('approved', ' Approved'),
         ('done', ' Done'),
     ], string='Status', default='draft', track_visibility='onchange')
 
@@ -31,8 +48,26 @@ class FinalSettlement(models.Model):
         else:
             pass
 
+    @api.model
+    def create(self, vals):
+        vals['leaving_date'] = ''
+        emp = self.env['hr.emp.exit.req'].search([('employee_id', '=', vals['employee_id'])], limit=1)
+        if emp:
+            vals['leaving_date'] = emp.last_date
+        res = super(FinalSettlement, self).create(vals)
+        return res
+
     @api.multi
-    def action_submit(self):
+    def write(self, vals):
+        vals['leaving_date'] = ''
+        emp = self.env['hr.emp.exit.req'].search([('employee_id', '=', vals['employee_id'])], limit=1)
+        if emp:
+            vals['leaving_date'] = emp.last_date
+        res = super(FinalSettlement, self).write(vals)
+        return res
+
+    @api.multi
+    def action_confirm(self):
         vals = []
         epm_slip = self.env['hr.payslip'].search([
             ('employee_id', '=', self.employee_id.id),
@@ -45,6 +80,32 @@ class FinalSettlement(models.Model):
                     'total': fac.total,
                 }))
         self.emp_payslip_ids = vals
+        self.state = 'validate'
+        self.payment_ids.write({'state': 'validate'})
+        self.deduction_ids.write({'state': 'validate'})
+        self.emp_payslip_ids.write({'state': 'validate'})
+
+    @api.multi
+    def action_reset(self):
+        self.state = 'draft'
+        self.payment_ids.write({'state': 'draft'})
+        self.deduction_ids.write({'state': 'draft'})
+        self.emp_payslip_ids.write({'state': 'draft'})
+        self.emp_payslip_ids = ''
+
+    @api.multi
+    def action_approved(self):
+        self.state = 'approved'
+        self.payment_ids.write({'state': 'approved'})
+        self.deduction_ids.write({'state': 'approved'})
+        self.emp_payslip_ids.write({'state': 'approved'})\
+
+    @api.multi
+    def action_done(self):
+        self.state = 'done'
+        self.payment_ids.write({'state': 'done'})
+        self.deduction_ids.write({'state': 'done'})
+        self.emp_payslip_ids.write({'state': 'done'})
 
 class FinalSettlementLine(models.Model):
     _name = "final.settlement.line"
@@ -52,7 +113,14 @@ class FinalSettlementLine(models.Model):
     name = fields.Char(string='Name')
     code = fields.Char(string='Code')
     total = fields.Float(string='Total')
-    final_settlement_id = fields.Many2one('final.settlement','Final Settlement')
+    final_settlement_id = fields.Many2one('final.settlement','Final Settlement')\
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('validate', 'Validate'),
+        ('approved', ' Approved'),
+        ('done', ' Done'),
+    ], string='Status', default='draft', track_visibility='onchange')
 
 class Payments(models.Model):
     _name = "final.settlement.payments"
@@ -61,9 +129,23 @@ class Payments(models.Model):
     amount = fields.Float(string='Amount')
     payment_id = fields.Many2one('final.settlement','Final Settlement')
 
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('validate', 'Validate'),
+        ('approved', ' Approved'),
+        ('done', ' Done'),
+    ], string='Status', default='draft', track_visibility='onchange')
+
 class Diductions(models.Model):
     _name = "final.settlement.deduction"
 
     name = fields.Char(string='Discription')
     amount = fields.Float(string='Amount')
     deduction_id = fields.Many2one('final.settlement', 'Final Settlement')
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('validate', 'Validate'),
+        ('approved', ' Approved'),
+        ('done', ' Done'),
+    ], string='Status', default='draft', track_visibility='onchange')
