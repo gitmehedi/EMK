a589901dc50339bc9e58a9c441821d5714bc5b03
gitmehedi <git.mehedi@gmail.com>
2018-09-25 17:14:37 +0600

    [ADD] Add new module 'event_user' which will create and update all types of user whom are related to event management.

---
+++ event_user/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Event Management Users',
+    'description': """ All types of users whom are related to activities of event management""",
+    "author": "Genweb2 Limited",
+    "website": "http://www.genweb2.com",
+    'version': '1.0',
+    'category': 'Event Management',
+    'depends': [
+        'event',
+    ],
+    'data': [
+        'security/users.xml',
+    ],
+    'installable': True,
+}
+# -*- coding: utf-8 -*-
