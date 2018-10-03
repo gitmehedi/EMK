4ee0b8c86c716acff7f8c42b69dd1619d2a1c4df
gitmehedi <git.mehedi@gmail.com>
2018-10-03 17:27:34 +0600

    [FIX] Fix bug of sessions existing module.

---
+++ event_management/models/event_session.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class EventSession(models.Model):
+    _inherit = 'event.session'
+
+    total_seat_available = fields.Integer(string="Total Seat Available", compute='compute_total_seat')
+
+    event_book_ids = fields.One2many('event.room.book', 'event_id', string='Event Rooms')
+    event_task_ids = fields.One2many('event.task.list', 'event_id', string='Event Tasks')
+    event_type_id = fields.Many2one('event.type', string='Category',
+                                    readonly=False, states={'done': [('readonly', True)]}, required=True)
+
+    @api.depends('event_book_ids')
+    def compute_total_seat(self):
+        for record in self:
+            record.total_seat_available = sum([rec.seat_no for rec in record.event_book_ids])
