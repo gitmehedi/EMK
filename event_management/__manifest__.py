d8d3b86560060e1765b41deb410d6b809ea4adbf
gitmehedi <git.mehedi@gmail.com>
2018-10-03 12:53:45 +0600

    [ADD] Add scurity and other restriction for Task Assigned User.

--- event_management/__manifest__.py
+++ event_management/__manifest__.py
@@ -26,6 +26,7 @@
         'views/event_task_type.xml',
         'views/event_event_view.xml',
         'views/event_registration_view.xml',
+        'views/event_task_list_view.xml',
     ],
     'installable': True,
     'application': True,
--- event_management/models/event_task_list.py
+++ event_management/models/event_task_list.py
@@ -8,41 +8,54 @@ from odoo.exceptions import UserError, ValidationError
 class EventTaskList(models.Model):
     _name = 'event.task.list'
     _inherit = ['mail.thread', 'ir.needaction_mixin']
+    _rec_name = 'task_id'
 
-    task_duration = fields.Integer(string='Duration', required=True, track_visibility='onchange')
-    task_description = fields.Text(string='Task Description', required=True, track_visibility='onchange')
-    task_feedback = fields.Text(string='Task Feedback', required=True, track_visibility='onchange')
-    assign_date = fields.Datetime(string="Assign Date", default=fields.Datetime.now, required=True,
-                                  track_visibility='onchange')
-    task_start = fields.Datetime(string='Task Start', track_visibility='onchange')
-    task_stop = fields.Datetime(string='Task Stop', track_visibility='onchange')
+    task_duration = fields.Float(string='Duration', required=True, track_visibility='onchange', readonly=True,
+                                   states={'draft': [('readonly', False)]})
+    task_description = fields.Text(string='Task Description', required=True, track_visibility='onchange', readonly=True,
+                                   states={'draft': [('readonly', False)]})
+    task_feedback = fields.Text(string='Task Feedback',  track_visibility='onchange', readonly=True,
+                                states={'start': [('readonly', False)]})
+    assign_date = fields.Datetime(string="Assign Date", track_visibility='onchange', readonly=True,
+                                  states={'draft': [('readonly', False)]})
+    task_start = fields.Datetime(string='Task Start', track_visibility='onchange', readonly=True,
+                                 states={'assign': [('readonly', False)]})
+    task_stop = fields.Datetime(string='Task Stop', track_visibility='onchange', readonly=True,
+                                states={'start': [('readonly', False)]})
 
-    event_id = fields.Many2one('event.event', string='Event')
-    assign_emp_id = fields.Many2one('res.partner', string='Assign To', required=True, track_visibility='onchange')
-    task_id = fields.Many2one('event.task.type', string='Name', required=True, track_visibility='onchange')
-    state = fields.Selection([('assign', 'Assigned'), ('start', 'Start'), ('finish', 'Finish')], default='assign',
-                             track_visibility='onchange')
+    event_id = fields.Many2one('event.event', string='Event', readonly=True, required=True,
+                               states={'draft': [('readonly', False)]})
+    assign_emp_id = fields.Many2one('res.partner', string='Assign To', required=True, track_visibility='onchange',
+                                    readonly=True, states={'draft': [('readonly', False)]})
+    task_id = fields.Many2one('event.task.type', string='Task Name', required=True, track_visibility='onchange',
+                              domain=[('status','=',True)],
+                              readonly=True, states={'draft': [('readonly', False)]})
+    state = fields.Selection([('draft', 'Draft'), ('assign', 'Assigned'), ('start', 'Start'), ('finish', 'Finish')],
+                             default='draft', track_visibility='onchange')
 
-    # @api.onchange('room_id')
-    # def onchange_room(self):
-    #     if self.room_id:
-    #         data = self.search([('room_id', '=', self.room_id.id), ('event_start', '<=', self.event_id.date_begin),
-    #                             ('event_stop', '>=', self.event_id.date_begin), '|',
-    #                             ('event_start', '<=', self.event_id.date_end),
-    #                             ('event_stop', '>=', self.event_id.date_end)])
-    #         if len(data) > 0:
-    #             raise UserError(
-    #                 _('[{0}] room already book for another event, please choose another.'.format(self.room_id.name)))
-    #
-    #         self.event_start = self.event_id.date_begin
-    #         self.event_stop = self.event_id.date_end
-    #
-    # @api.constrains('room_id', 'event_start', 'event_stop')
-    # def check_room(self):
-    #     data = self.search([('room_id', '=', self.room_id.id), ('event_start', '<=', self.event_id.date_begin),
-    #                         ('event_stop', '>=', self.event_id.date_begin), '|',
-    #                         ('event_start', '<=', self.event_id.date_end),
-    #                         ('event_stop', '>=', self.event_id.date_end)])
-    #     if len(data) > 1:
-    #         raise UserError(
-    #             _('[{0}] room already book for another event, please choose another.'.format(self.room_id.name)))
+    @api.model
+    def _needaction_domain_get(self):
+        return [('state', 'in', ['assign', 'start'])]
+
+    @api.one
+    def act_draft(self):
+        if self.state == 'assign':
+            self.state = 'draft'
+
+    @api.one
+    def act_assign(self):
+        if self.state == 'draft':
+            self.assign_date = fields.Datetime.now()
+            self.state = 'assign'
+
+    @api.one
+    def act_start(self):
+        if self.state == 'assign':
+            self.task_start = fields.Datetime.now()
+            self.state = 'start'
+
+    @api.one
+    def act_finish(self):
+        if self.state == 'start':
+            self.task_stop = fields.Datetime.now()
+            self.state = 'finish'
