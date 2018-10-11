2242c7633c333bd78b581177661b6ce765b737ae
gitmehedi <git.mehedi@gmail.com>
2018-10-11 10:14:21 +0600

    [ADD] Add new module mail_send which is responsible for mail sending.

---
+++ mail_send/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Send Mail ',
+    'description': """ Customize mail send functionality""",
+    "author": "Genweb2 Limited",
+    "website": "http://www.genweb2.com",
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'depends': [],
+    'data': [],
+    'installable': True,
+}
---
+++ mail_send/models/mail_mail.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class MailMail(models.Model):
+    _inherit = 'mail.mail'
+
+    @api.model
+    def mailsend(self, vals):
+        template = False
+        try:
+            template = self.env.ref(vals['template'], raise_if_not_found=False)
+        except ValueError:
+            pass
+
+        assert template._name == 'mail.template'
+
+        template.write({
+            'email_to': vals['email_to'] if 'email_to' in vals else '',
+            'attachment_ids': vals['attachment_ids'] if 'attachment_ids' in vals else [],
+        })
+
+        context = {
+            'base_url': self.env['ir.config_parameter'].get_param('web.base.url'),
+            'lang': self.env.user.lang,
+        }
+
+        for key, val in vals['context'].iteritems():
+            context[key] = val
+
+        template.with_context(context).send_mail(self.env.user.id, force_send=True, raise_exception=True)
+        _logger.info("Email sending status of user.")
+
+    @api.model
+    def groupmail(self, val):
+        groups = self.env['res.groups'].search(
+            [('name', 'in', val['group']), ('category_id.name', '=', val['category'])])
+        emails = [str(rec.email) for rec in groups.users if len(rec.create_uid) > 0]
+        return ", ".join(emails)
