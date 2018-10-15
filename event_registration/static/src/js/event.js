b74e48eb257f8e4afb20fcfb4f6cfb8a2c1e169c
gitmehedi <git.mehedi@gmail.com>
2018-10-15 18:51:14 +0600

    [ADD] Change registration design.

--- event_registration/static/src/js/event.js
+++ event_registration/static/src/js/event.js
@@ -1,42 +1,38 @@
-odoo.define('event_registration.eventdfd', function (require) {
-"use strict";
+odoo.define('event_registration.event', function (require) {
+    "use strict";
 
     var ajax = require('web.ajax');
     var Widget = require('web.Widget');
     var web_editor_base = require('web_editor.base')
 
-    var event= require('website_event.website_event');
-    var websiteEvent= event.EventRegistrationForm;
+    var event = require('website_event.website_event');
+    var websiteEvent = event.EventRegistrationForm;
 
-    websiteEvent.include({
-        start: function() {
-        var self = this;
-        var res = this._super.apply(this,arguments).then(function() {
-            $('#registration_form .a-submit')
-                .click(function (ev) {
+    event.EventRegistrationForm.include({
+        start: function () {
+            var self = this;
+            $('#registration_form .a-submit').off('click').click(function (ev) {
                     self.on_click(ev);
                 });
-        });
-        return res
-    },
-    on_click: function(ev) {
-        ev.preventDefault();
-        ev.stopPropagation();
-        debugger;
-        var $form = $(ev.currentTarget).closest('form');
-        var post = {};
-        $("#registration_form select").each(function() {
-            post[$(this).attr('name')] = 1;
-        });
-        return ajax.jsonRpc($form.attr('action'), 'call', post).then(function (modal) {
-            var $modal = $(modal);
-            // $modal.find('.modal-body > div').removeClass('container'); // retrocompatibility - REMOVE ME in master / saas-19
-            $modal.after($form).modal();
-            $modal.on('click', '.js_goto_event', function () {
-                $modal.modal('hide');
+            return self;
+        },
+        on_click: function (ev) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            var $form = $(ev.currentTarget).closest('form');
+            var post = {};
+            $("#registration_form select").each(function () {
+                post[$(this).attr('name')] = 1;
             });
-        });
-    },
+            return ajax.jsonRpc($form.attr('action'), 'call', post).then(function (modal) {
+                var $modal = $(modal);
+                $modal.find('.modal-body > div').removeClass('container'); // retrocompatibility - REMOVE ME in master / saas-19
+                $modal.modal('show');
+                $modal.on('click', '.js_goto_event', function () {
+                    $modal.modal('hide');
+                });
+            });
+        },
 
     });
 
