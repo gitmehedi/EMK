a444db50cc7504ebe725f1328e22c6e095cd0c9c
gitmehedi <git.mehedi@gmail.com>
2018-10-16 18:38:36 +0600

    [ADD] Fix design of event_reservation and event.

--- event_management/models/event_reservation.py
+++ event_management/models/event_reservation.py
@@ -14,14 +14,14 @@ class EventReservation(models.Model):
                                    readonly=True, states={'draft': [('readonly', False)]})
     event_type_id = fields.Many2one('event.type', string='Event Type', required=True, track_visibility='onchange',
                                     readonly=True, states={'draft': [('readonly', False)]})
-    payment_mode = fields.Selection([('cash', 'Cash'), ('cheque', 'cheque')], string="Mode of Payment", required=True,
-                                    track_visibility='onchange',
-                                    readonly=True, states={'draft': [('readonly', False)]})
-    org_type_id = fields.Many2one('event.organization.type',string="Organization Type", required=True, track_visibility='onchange',
-                           readonly=True, states={'draft': [('readonly', False)]})
+    org_type_id = fields.Many2one('event.organization.type', string="Organization Type", required=True,
+                                  track_visibility='onchange',
+                                  readonly=True, states={'draft': [('readonly', False)]})
     contract_number = fields.Char(string="Contract Number", readonly=True, related='organizer_id.mobile')
     work_email = fields.Char(string="Email", readonly=True, related='organizer_id.email')
-    attendee_number = fields.Char('Estimated Number of Attendees', required=True, track_visibility='onchange',
+    attendee_number = fields.Integer('No. of Attendees', required=True, track_visibility='onchange',
+                                  readonly=True, states={'draft': [('readonly', False)]})
+    total_session = fields.Integer('No. of Sessions', required=True, track_visibility='onchange',
                                   readonly=True, states={'draft': [('readonly', False)]})
     start_date = fields.Datetime(string='Start Date', required=True, track_visibility='onchange',
                                  readonly=True, states={'draft': [('readonly', False)]})
@@ -29,17 +29,32 @@ class EventReservation(models.Model):
                                readonly=True, states={'draft': [('readonly', False)]})
     request_date = fields.Datetime(string='Requested Date', required=True, track_visibility='onchange',
                                    readonly=True, states={'draft': [('readonly', False)]})
-    payment_date = fields.Datetime(string='Expected Date for Payment', required=True, track_visibility='onchange',
+    description = fields.Html('Description', track_visibility='onchange', required=True,
+                              readonly=True, states={'draft': [('readonly', False)]})
+    rules_regulation = fields.Text('Rules & regulation', track_visibility='onchange', required=True,
                                    readonly=True, states={'draft': [('readonly', False)]})
-    event_description = fields.Text('Description', track_visibility='onchange', required=True,
+
+
+    payment_type = fields.Selection([('free', 'Free'), ('paid', 'Paid')], required=True, default='free', string='Type',
                                     readonly=True, states={'draft': [('readonly', False)]})
-    rules = fields.Text('Rules & regulation', track_visibility='onchange', required=True,
+    mode_of_payment = fields.Selection([('cash', 'Cash'), ('bank', 'Bank')], required=True, default='cash',
+                                       string='Mode Of Payment', track_visibility='onchange',
+                                       readonly=True, states={'draft': [('readonly', False)]})
+    paid_amount = fields.Float(string='Paid Amount', digits=(12, 2), track_visibility='onchange',
+                               readonly=True, states={'draft': [('readonly', False)]})
+    refundable_amount = fields.Float(string='Refundable Amount', digits=(12, 2), track_visibility='onchange',
+                                     readonly=True, states={'draft': [('readonly', False)]})
+    rules_regulation = fields.Html(string='Rules and Regulation', track_visibility='onchange',
+                                   readonly=True, states={'draft': [('readonly', False)]})
+    date_of_payment = fields.Date(string="Expected Date for Payment", track_visibility='onchange',
+                                  readonly=True, states={'draft': [('readonly', False)]})
+
+    notes = fields.Html(string="Comments/Notes", track_visibility='onchange',
                         readonly=True, states={'draft': [('readonly', False)]})
-    comment = fields.Text('Comments', track_visibility='onchange', required=True,
-                          readonly=True, states={'draft': [('readonly', False)]})
+
     state = fields.Selection(
-        [('draft', 'Draft'), ('on_process', 'On Process'), ('confirm', 'Confirmed'), ('done', 'Close'),
-         ('cancel', 'Canceled')], string="State", default="draft", track_visibility='onchange')
+        [('draft', 'Draft'), ('on_process', 'On Process'), ('confirm', 'Confirmed'), ('done', 'Done'),
+         ('cancel', 'Cancelled')], string="State", default="draft", track_visibility='onchange')
 
     @api.one
     def act_draft(self):
@@ -60,7 +75,7 @@ class EventReservation(models.Model):
     @api.one
     def act_done(self):
         if self.state == 'confirm':
-            self.state = 'draft'
+            self.state = 'done'
 
     @api.one
     def act_cancel(self):
