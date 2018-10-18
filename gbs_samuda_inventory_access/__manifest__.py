47c85c8cea4b5291e42f213eed7026758569c43a
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2018-10-18 09:55:28 +0600

    [ADD]New module added for unused menu hiding for user in inventory

---
+++ gbs_samuda_inventory_access/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "GBS Samuda Inventory Access",
+
+    'summary': """
+        This module will install for Samuda Inventory Access addons.""",
+
+    'description': """
+        This module will install for Samuda Inventory Access.Its contain-
+        --To hide unused menu
+        --To override record rules
+        --To override access rules  
+    """,
+
+    'author': "Genweb2 Limited",
+    'website': "http://www.genweb2.com",
+    'category': 'Tools',
+    'version': '10.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'stock',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'security/ir_rule.xml',
+        'views/menu_items.xml',
+    ],
+}
