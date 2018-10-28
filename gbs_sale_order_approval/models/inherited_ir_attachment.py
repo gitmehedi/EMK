be4dfc41eed0d8a2e27bf096baee8869d6778dc1
rabbi <rabbi998@gmail.com>
2018-10-28 17:15:52 +0600

    MSG str updated

--- gbs_sale_order_approval/models/inherited_ir_attachment.py
+++ gbs_sale_order_approval/models/inherited_ir_attachment.py
@@ -10,7 +10,7 @@ class InheritedIrAttachment(models.Model):
         if self.res_model == 'sale.order':
             so_obj = self.env['sale.order'].search([('id','=',self.res_id)])
             if so_obj.state == 'done':
-                raise ValidationError(_('You can not delete attachments when Sale Order is in done stage'))
+                raise ValidationError(_('You can not delete attachments when Sale Order is in Approved stage'))
 
         return super(InheritedIrAttachment, self).unlink()
 
--- product_sales_pricelist/models/inherited_ir_attachment.py
+++ product_sales_pricelist/models/inherited_ir_attachment.py
@@ -10,7 +10,7 @@ class InheritedIrAttachment(models.Model):
         if self.res_model == 'product.sales.pricelist':
             so_obj = self.env['product.sales.pricelist'].search([('id','=',self.res_id)])
             if so_obj.state == 'validate':
-                raise ValidationError(_('You can not delete attachment (s) when Price List is in done stage'))
+                raise ValidationError(_('You can not delete attachment (s) when Price List is in Approved stage'))
 
         return super(InheritedIrAttachment, self).unlink()
 
