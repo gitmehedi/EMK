38cd3c2fb83c73ed28418e5b4de1839709b30c20
rabbi <rabbi998@gmail.com>
2018-10-28 17:13:07 +0600

    On pricelist and sales order, now attachments can be deleted only when it is not in done state

---
+++ gbs_sale_order_approval/models/inherited_ir_attachment.py
@@ -0,0 +1,17 @@
+from odoo import api,models, _
+from odoo.exceptions import ValidationError
+
+
+class InheritedIrAttachment(models.Model):
+    _inherit = 'ir.attachment'
+
+    @api.multi
+    def unlink(self):
+        if self.res_model == 'sale.order':
+            so_obj = self.env['sale.order'].search([('id','=',self.res_id)])
+            if so_obj.state == 'done':
+                raise ValidationError(_('You can not delete attachments when Sale Order is in done stage'))
+
+        return super(InheritedIrAttachment, self).unlink()
+
+
---
+++ product_sales_pricelist/models/inherited_ir_attachment.py
@@ -0,0 +1,17 @@
+from odoo import api,models, _
+from odoo.exceptions import ValidationError
+
+
+class InheritedIrAttachment(models.Model):
+    _inherit = 'ir.attachment'
+
+    @api.multi
+    def unlink(self):
+        if self.res_model == 'product.sales.pricelist':
+            so_obj = self.env['product.sales.pricelist'].search([('id','=',self.res_id)])
+            if so_obj.state == 'validate':
+                raise ValidationError(_('You can not delete attachment (s) when Price List is in done stage'))
+
+        return super(InheritedIrAttachment, self).unlink()
+
+
