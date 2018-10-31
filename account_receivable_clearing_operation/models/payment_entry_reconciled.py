574988088f76c846ff4bb6929d053cfef54b7409
rabbi <rabbi998@gmail.com>
2018-10-31 16:51:52 +0600

    update

--- account_receivable_clearing_operation/models/payment_entry_reconciled.py
+++ account_receivable_clearing_operation/models/payment_entry_reconciled.py
@@ -18,16 +18,28 @@ class PaymentEntryReconciled(models.TransientModel):
     line_ids = fields.One2many('payment.entry.reconciled.line', 'parent_id', string="Unreconciled Lines",
                                ondelete='cascade')
 
+    line_ids_visibility = fields.Boolean()
+    select_all_line_vals = fields.Boolean(string='Select All')
+
     """ Methods go below """
+
+    @api.onchange('select_all_line_vals')
+    def onchange_select_all_line_vals(self):
+        for s in self:
+            if s.select_all_line_vals:
+                for line in s.line_ids:
+                    line.clear_acc_receivable = True
+            elif not s.select_all_line_vals:
+                for line in s.line_ids:
+                    line.clear_acc_receivable = False
 
 
     def action_clear_accounts(self):
         for rec in self:
 
-            #Proper Validation msg
+            # Proper Validation msg
             if len(rec.line_ids) == 0:
-                raise ValidationError('There is no line values to clear')
-
+                raise ValidationError('There is no line value for Account Clearing')
 
             for line in rec.line_ids:
                 if line.clear_acc_receivable is True:
@@ -39,6 +51,60 @@ class PaymentEntryReconciled(models.TransientModel):
 
                     if line.payment_id:
                         line.payment_id.write({'is_entry_receivable_cleared_payments': True})
+
+
+
+    def action_search(self):
+
+        for pay_en in self:
+            pay_en.line_ids.unlink()
+            pay_en.select_all_line_vals = False
+
+
+            if not pay_en.partner_id.id:
+                payment_pool = pay_en.env['account.payment'].search(
+                    [('is_entry_receivable_cleared_payments', '=', False), ('payment_date', '<=', pay_en.date),
+                     ('state', '=', 'posted')])
+
+                cheque_rcv_pool = pay_en.env['accounting.cheque.received'].search(
+                    [('is_entry_receivable_cleared', '=', False), ('date_on_cheque', '<=', pay_en.date),
+                     ('state', '=', 'honoured')])
+
+            elif pay_en.partner_id:
+                payment_pool = pay_en.env['account.payment'].search(
+                    [('is_entry_receivable_cleared_payments', '=', False), ('payment_date', '<=', pay_en.date),
+                     ('partner_id', '=', pay_en.partner_id.id),
+                     ('state', '=', 'posted')])
+
+                cheque_rcv_pool = pay_en.env['accounting.cheque.received'].search(
+                    [('is_entry_receivable_cleared', '=', False), ('date_on_cheque', '<=', pay_en.date),
+                     ('partner_id', '=', pay_en.partner_id.id),
+                     ('state', '=', 'honoured')])
+
+            vals = []
+
+            for payments in payment_pool:
+                vals.append((0, 0, {'amount': payments.amount,
+                                    'currency_id': payments.currency_id.id,
+                                    'partner_id': payments.partner_id.id,
+                                    'company_id': payments.company_id.id,
+                                    'payment_id': payments.id,
+                                    'date': payments.payment_date,
+                                    }))
+
+            for cheq in cheque_rcv_pool:
+                vals.append((0, 0, {'amount': cheq.cheque_amount,
+                                    'currency_id': cheq.currency_id.id,
+                                    'partner_id': cheq.partner_id.id,
+                                    'company_id': cheq.company_id.id,
+                                    'cheque_received_id': cheq.id,
+                                    'date': cheq.date_on_cheque,
+                                    }))
+
+            if len(vals) > 0:
+                pay_en.line_ids_visibility = True
+
+            pay_en.line_ids = vals
 
 
     def clear_receivable_accounts(self, amt, company_id, partner_id):
@@ -91,71 +157,17 @@ class PaymentEntryReconciled(models.TransientModel):
         move = self.env['account.move'].create(move_dict)
         move.post()
 
-
-    def action_search(self):
-
-        for pay_en in self:
-            pay_en.line_ids.unlink()
-
-            if not pay_en.partner_id.id:
-                payment_pool = pay_en.env['account.payment'].search(
-                    [('is_entry_receivable_cleared_payments', '=', False), ('payment_date', '<=', pay_en.date),
-                     ('state', '=', 'posted')])
-
-                cheque_rcv_pool = pay_en.env['accounting.cheque.received'].search(
-                    [('is_entry_receivable_cleared', '=', False), ('date_on_cheque', '<=', pay_en.date),
-                     ('state', '=', 'honoured')])
-
-            elif pay_en.partner_id:
-                payment_pool = pay_en.env['account.payment'].search(
-                    [('is_entry_receivable_cleared_payments', '=', False), ('payment_date', '<=', pay_en.date),
-                     ('partner_id', '=', pay_en.partner_id.id),
-                     ('state', '=', 'posted')])
-
-                cheque_rcv_pool = pay_en.env['accounting.cheque.received'].search(
-                    [('is_entry_receivable_cleared', '=', False), ('date_on_cheque', '<=', pay_en.date),
-                     ('partner_id', '=', pay_en.partner_id.id),
-                     ('state', '=', 'honoured')])
-
-            vals = []
-
-            for payments in payment_pool:
-                vals.append((0, 0, {'amount': payments.amount,
-                                    'currency_id': payments.currency_id.id,
-                                    'partner_id': payments.partner_id.id,
-                                    'company_id': payments.company_id.id,
-                                    'payment_id': payments.id,
-                                    'date': payments.payment_date,
-                                    }))
-
-
-
-            for cheq in cheque_rcv_pool:
-                vals.append((0, 0, {'amount': cheq.cheque_amount,
-                                    'currency_id': cheq.currency_id.id,
-                                    'partner_id': cheq.partner_id.id,
-                                    'company_id': cheq.company_id.id,
-                                    'cheque_received_id': cheq.id,
-                                    'date':cheq.date_on_cheque,
-                                    }))
-
-            # show the updated list
-            pay_en.line_ids = vals
-
-
-
 class PaymentEntryReconciledLine(models.TransientModel):
     _name = 'payment.entry.reconciled.line'
 
     amount = fields.Float(string='Amount', readonly=True)
-    clear_acc_receivable = fields.Boolean(string='r')
+    clear_acc_receivable = fields.Boolean(string='Select')
     currency_id = fields.Many2one('res.currency', string='Currency', readonly=True)
     partner_id = fields.Many2one('res.partner', string='Customer', domain=[('customer', '=', True)], readonly=True)
     company_id = fields.Many2one('res.company', string='Company')
     cheque_received_id = fields.Many2one('accounting.cheque.received', string='Cheque Received ID')
     payment_id = fields.Many2one('account.payment', string='Customer Payment ID')
-    date = fields.Date(string='Date', required=True)
+    date = fields.Date(string='Date', readonly=True)
 
     """ Relational Fields"""
     parent_id = fields.Many2one('payment.entry.reconciled', ondelete='cascade')
-
