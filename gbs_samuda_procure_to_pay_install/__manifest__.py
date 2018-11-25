abe42b52c99da7e0ab6a8ec8a53ad385aaa19060
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2018-11-25 15:36:52 +0600

    [IMP]        'ir_sequence_operating_unit',

--- gbs_samuda_procure_to_pay_install/__manifest__.py
+++ gbs_samuda_procure_to_pay_install/__manifest__.py
@@ -5,6 +5,7 @@
     'website': 'http://www.genweb2.com',
     'category': 'Sales',
     'depends': [
+        'base_suspend_security',
         'stock_indent',
         'ir_sequence_operating_unit',
         'indent_type',
