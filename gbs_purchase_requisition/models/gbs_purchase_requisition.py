20a75d77aef43128713d88533d26542180318f96
rabbi <rabbi998@gmail.com>
2018-12-02 17:43:24 +0600

    [ADD] Added a indent calculation button into pr

--- gbs_purchase_requisition/models/gbs_purchase_requisition.py
+++ gbs_purchase_requisition/models/gbs_purchase_requisition.py
@@ -109,6 +109,12 @@ class PurchaseRequisition(models.Model):
         # self.write({'state': 'approve_head_procurement'})
 
     @api.multi
+    def action_get_indent(self):
+        action = self.env.ref('stock_indent.action_stock_indent').read([])[0]
+        action['domain'] = [('id', '=', self.indent_ids.ids)]
+        return action
+
+    @api.multi
     def action_approve(self):
         res = self.env.ref('gbs_purchase_requisition.purchase_requisition_type_wizard')
         result = {
