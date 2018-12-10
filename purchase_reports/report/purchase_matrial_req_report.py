74f3a0333ce4c64e43c03b999f859f1f331eea32
Rita <rabbi998@gmail.com>
2018-12-10 14:08:52 +0600

    [ADD]reports

---
+++ purchase_reports/report/purchase_matrial_req_report.py
@@ -0,0 +1,47 @@
+from odoo import api,models,fields
+
+class PurchaseReport(models.AbstractModel):
+    _name = "report.purchase_reports.report_purchase_material_requisition"
+
+    sql_str = """ """
+
+    @api.multi
+    def render_html(self,docids,data=None):
+
+        report_title = ''
+
+        record_list = []
+        data['operating_unit_id'] = data['operating_unit_id']
+        data['dept_location_id'] = data['dept_location_id']
+        data['date_from'] = data['date_from']
+        data['date_to'] = data['date_to']
+
+        if data['report_type'] == 'local':
+
+            report_title = 'Purchase Material Requisition(Local)'
+
+        else:
+            report_title = 'Purchase Material Requisition(Foreign)'
+
+        # self._cr.execute(self.sql_str)
+        # data_list = self._cr.fetchall()
+
+        #for row in data_list:
+        list_obj = {}
+        list_obj['ref'] = 'MRR NO-4648'
+        list_obj['particulars'] = '39.033.002-Air Condition Splite Type-02(220 V,2400 BTU): For Lab'
+        list_obj['qty'] = '10,000'
+        list_obj['unit'] = 'Pair'
+        list_obj['rate'] = '66000'
+        list_obj['discount'] = '5'
+        list_obj['amount'] = '66000'
+        list_obj['remarks'] = 'This is a Lab Product'
+        record_list.append(list_obj)
+
+        docargs = {
+            'data': data,
+            'report_title':report_title,
+            'lists': record_list,
+
+        }
+        return self.env['report'].render('purchase_reports.report_purchase_material_requisition', docargs)
