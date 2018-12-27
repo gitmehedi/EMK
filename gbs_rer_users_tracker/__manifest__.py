a62591162d8c60ace728c942c5bc8d2f05bf87d4
Rita <ritaislam6789@gmail.com>
2018-12-27 14:47:03 +0600

    [ADD] thousand separetor on commercial reports

---
+++ gbs_rer_users_tracker/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Users tracker',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'base',
+    'version':'10.0.1',
+    'depends': [
+        'base',
+        'mail',
+    ],
+    'data': [
+        'views/res_user_inherited_views.xml'
+    ],
+
+    'description':
+    "This module is tracking all users activity",
+    'installable': True,
+    'application': True,
+}
