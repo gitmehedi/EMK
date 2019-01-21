5abc3f7d53c656523b20eb4bbb7a9fbd94af89bb
Rita <ritaislam6789@gmail.com>
2019-01-21 19:18:37 +0600

    [ADD] Added ou_currency module

---
+++ ou_currency/models/inherit_operating_unit.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models,_
+
+class OperatingUnitCurrency(models.Model):
+    _inherit = 'operating.unit'
+
+    currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.user.company_id.currency_id)
