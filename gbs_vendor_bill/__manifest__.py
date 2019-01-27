6f4ff9876fb996ad3142a2e5cfb9db7193d78a39
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2019-01-27 20:06:09 +0600

    [IMP]Task list updated:MTB-186,189,190,391,255

---
+++ gbs_vendor_bill/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'GBS Vendor Bill',
+    'description': """ Extended module to manage Vendor Bill.""",
+    "author": "Genweb2 Limited",
+    "website": "http://www.genweb2.com",
+    'version': '10.0.0.1',
+    'category': 'Accounting',
+    'depends': [
+        'account_operating_unit',
+        'sub_operating_unit',
+    ],
+    'data': [
+        'views/account_invoice_view.xml',
+
+    ],
+    'installable': True,
+    'application': True,
+}
