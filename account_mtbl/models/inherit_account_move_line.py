a0234a5b7744a6002a996507c3fc4f17fb8934b1
Rita <ritaislam6789@gmail.com>
2019-03-03 19:41:48 +0600

    [FIX] master data remove from journal entry and add to journal item

--- account_mtbl/models/inherit_account_move_line.py
+++ account_mtbl/models/inherit_account_move_line.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    sub_operating_unit_id = fields.Many2one('sub.operating.unit', string="Sub Operating Unit",readonly=True)
-    segment_id = fields.Many2one('segment', string="Segment",readonly=True)
-    acquiring_channel_id = fields.Many2one('acquiring.channel', string="Acquiring Channel",readonly=True)
-    servicing_channel_id = fields.Many2one('servicing.channel', string="Servicing Channel",readonly=True)
+    sub_operating_unit_id = fields.Many2one('sub.operating.unit', string="Sub Operating Unit")
+    segment_id = fields.Many2one('segment', string="Segment")
+    acquiring_channel_id = fields.Many2one('acquiring.channel', string="Acquiring Channel")
+    servicing_channel_id = fields.Many2one('servicing.channel', string="Servicing Channel")
