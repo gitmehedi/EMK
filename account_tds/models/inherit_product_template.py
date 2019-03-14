7e2307de0eaccf72e7c3452bcfcd2fe753d5fe7c
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2019-03-14 20:17:50 +0600

    [IMP]Update access

--- account_tds/models/inherit_product_template.py
+++ account_tds/models/inherit_product_template.py
@@ -5,10 +5,10 @@ class InheritedProductTemplate(models.Model):
     _inherit = 'product.template'
 
     account_tds_id = fields.Many2one('tds.rule', string="TDS Rule", required=False,related='product_variant_ids.account_tds_id',
-                                     domain="[('active', '=', True),('state', '=','confirm' )]")
+                                     domain="[('active', '=', True),('state', '=','confirm' )]",track_visibility='onchange')
 
 class InheritedProductProduct(models.Model):
     _inherit = 'product.product'
 
-    account_tds_id = fields.Many2one('tds.rule', string="TDS Rule", required=False,
+    account_tds_id = fields.Many2one('tds.rule', string="TDS Rule", required=False,track_visibility='onchange',
                                      domain="[('active', '=', True),('state', '=','confirm' )]")
