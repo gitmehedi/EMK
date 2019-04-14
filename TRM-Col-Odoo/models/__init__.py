79028eeaad2d331ac6a72f929254e70f219e9592
gitmehedi <git.mehedi@gmail.com>
2019-04-15 00:38:19 +0600

    [RM] Remove modules which is not necessary not right now.

--- TRM-Col-Odoo/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-# OpenERP, Open Source Management Solution
-# Copyright 2016 Christian Camilo Camargo
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from . import account_exchange_rate
