5de1d809df03df3e4a5a5d6ffcb9934576a2ce2c
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2019-05-12 10:34:08 +0600

    [FIX] Currency ordering fixed

--- account_mtbl/models/inherit_res_currency.py
+++ account_mtbl/models/inherit_res_currency.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api, _
 
 class Currency(models.Model):
     _name = 'res.currency'
-    _order = 'name desc'
+    _order = 'name asc'
     _inherit = ['res.currency', 'mail.thread']
 
     name = fields.Char(string='Currency', size=3, required=True, help="Currency Code (ISO 4217)",
