088ca6271a04563ae16e792adc9cd87aed8fb181
Rita <ritaislam6789@gmail.com>
2019-05-13 12:44:49 +0600

    [ADD]Added Macker and checker,state,edit button

---
+++ account_mtbl/models/branch.py
@@ -0,0 +1,110 @@
+from odoo import api, fields, models, _
+from psycopg2 import IntegrityError
+from odoo.exceptions import ValidationError
+
+
+class Branch(models.Model):
+    _name = 'operating.unit'
+    _order = 'name desc'
+    _inherit = ['operating.unit', 'mail.thread']
+    _description = 'Branch'
+
+
+    name = fields.Char('Name', required=True, size=50, track_visibility='onchange', readonly=True,
+                       states={'draft': [('readonly', False)]})
+    code = fields.Char('Code', required=True, size=3, track_visibility='onchange', readonly=True,
+                       states={'draft': [('readonly', False)]})
+    pending = fields.Boolean(string='Pending', default=True, track_visibility='onchange', readonly=True,
+                             states={'draft': [('readonly', False)]})
+    active = fields.Boolean(string='Active', default=False, track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    state = fields.Selection([('draft', 'Draft'), ('approve', 'Approve'), ('reject', 'Reject')], default='draft',
+                             track_visibility='onchange')
+    company_id = fields.Many2one(
+        'res.company', 'Company', required=True, track_visibility='onchange', default=lambda self:
+        self.env['res.company']._company_default_get('account.account'),readonly=True,
+                            states={'draft': [('readonly', False)]})
+    partner_id = fields.Many2one('res.partner', 'Partner', required=True, default=lambda self:
+    self.env['res.company']._company_default_get('account.account'),readonly=True,
+                            states={'draft': [('readonly', False)]})
+    branch_type = fields.Selection([('metro', 'Metro'), ('urban', 'Urban'), ('rural', 'Rural')],
+                                   string='Location of Branch', track_visibility='onchange', required=True,readonly=True,
+                            states={'draft': [('readonly', False)]})
+    line_ids = fields.One2many('history.operating.unit', 'line_id', string='Lines', readonly=True,
+                               states={'draft': [('readonly', False)]})
+
+
+    @api.one
+    def act_approve(self):
+        if self.state == 'draft':
+            self.active = True
+            self.pending = False
+            self.state = 'approve'
+
+    @api.one
+    def act_reject(self):
+        if self.state == 'draft':
+            self.state = 'reject'
+            self.pending = False
+
+    @api.one
+    def act_approve_pending(self):
+        if self.pending == True:
+            requested = self.line_ids.search([('state', '=', 'pending'), ('line_id', '=', self.id)], order='id desc',
+                                             limit=1)
+            if requested:
+                self.name = requested.change_name
+                self.active = requested.status
+                self.pending = False
+                requested.state = 'approve'
+                requested.change_date = fields.Datetime.now()
+
+    @api.one
+    def act_reject_pending(self):
+        if self.pending == True:
+            requested = self.line_ids.search([('state', '=', 'pending'), ('line_id', '=', self.id)], order='id desc',
+                                             limit=1)
+            if requested:
+                self.pending = False
+                requested.state = 'reject'
+                requested.change_date = fields.Datetime.now()
+
+
+    @api.one
+    def name_get(self):
+        name = self.name
+        if self.name and self.code:
+            name = '[%s] %s' % (self.code, self.name)
+        return (self.id, name)
+
+    @api.onchange("name", "code")
+    def onchange_strips(self):
+        if self.name:
+            self.name = self.name.strip()
+        if self.code:
+            self.code = str(self.code.strip()).upper()
+
+    @api.multi
+    def unlink(self):
+        for rec in self:
+            if rec.state in ('approve', 'reject'):
+                raise ValidationError(_('[Warning] Approves and Rejected record cannot be deleted.'))
+
+            try:
+                return super(Branch, rec).unlink()
+            except IntegrityError:
+                raise ValidationError(_("The operation cannot be completed, probably due to the following:\n"
+                                        "- deletion: you may be trying to delete a record while other records still reference it"))
+
+
+class HistoryBranch(models.Model):
+    _name = 'history.operating.unit'
+    _description = 'History Branch'
+    _order = 'id desc'
+
+    change_name = fields.Char('Proposed Name', size=50, readonly=True, states={'draft': [('readonly', False)]})
+    status = fields.Boolean('Active', default=True, track_visibility='onchange')
+    change_date = fields.Datetime(string='Date')
+    line_id = fields.Many2one('operating.unit', ondelete='restrict')
+    state = fields.Selection([('pending', 'Pending'), ('approve', 'Approve'), ('reject', 'Reject')],
+                             default='pending')
