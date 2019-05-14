f37a5e31c00449a0fdf5d2a64b50a5024a5835aa
Rita <ritaislam6789@gmail.com>
2019-05-14 12:41:31 +0600

    [ADD]Added Macker and checker,state,edit button for journal type

--- account_mtbl/models/inherit_account_journal.py
+++ account_mtbl/models/inherit_account_journal.py
@@ -6,19 +6,77 @@ from odoo.exceptions import ValidationError
 class AccountJournal(models.Model):
     _name = 'account.journal'
     _inherit = ['account.journal', 'mail.thread']
+    _order = 'name desc'
+    _description = 'Journal Type'
 
-    name = fields.Char(size=50, track_visibility='onchange')
-    code = fields.Char(size=10, track_visibility='onchange')
-    type = fields.Selection(track_visibility='onchange')
-    currency_id = fields.Many2one(track_visibility='onchange')
-    company_id = fields.Many2one(track_visibility='onchange')
-    operating_unit_id = fields.Many2one(track_visibility='onchange', string='Branch')
-    default_credit_account_id = fields.Many2one(track_visibility='onchange')
-    default_debit_account_id = fields.Many2one(track_visibility='onchange')
-    refund_sequence = fields.Boolean(track_visibility='onchange')
-    update_posted = fields.Boolean(track_visibility='onchange')
-    group_invoice_lines = fields.Boolean(track_visibility='onchange')
-    show_on_dashboard = fields.Boolean(track_visibility='onchange')
+
+    name = fields.Char('Name', required=True, size=50, track_visibility='onchange', readonly=True,
+                       states={'draft': [('readonly', False)]})
+    code = fields.Char('Code', required=True, size=10, track_visibility='onchange', readonly=True,
+                       states={'draft': [('readonly', False)]})
+    pending = fields.Boolean(string='Pending', default=True, track_visibility='onchange', readonly=True,
+                             states={'draft': [('readonly', False)]})
+    active = fields.Boolean(string='Active', default=False, track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    state = fields.Selection([('draft', 'Draft'), ('approve', 'Approve'), ('reject', 'Reject')], default='draft',
+                             track_visibility='onchange')
+    type = fields.Selection(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    currency_id = fields.Many2one(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    company_id = fields.Many2one(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    operating_unit_id = fields.Many2one(string='Branch',track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    default_credit_account_id = fields.Many2one(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    default_debit_account_id = fields.Many2one(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    refund_sequence = fields.Boolean(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    update_posted = fields.Boolean(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    group_invoice_lines = fields.Boolean(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    show_on_dashboard = fields.Boolean(track_visibility='onchange', readonly=True,
+                            states={'draft': [('readonly', False)]})
+    line_ids = fields.One2many('history.account.journal', 'line_id', string='Lines', readonly=True,
+                               states={'draft': [('readonly', False)]})
+
+    @api.one
+    def act_approve(self):
+        if self.state == 'draft':
+            self.active = True
+            self.pending = False
+            self.state = 'approve'
+
+    @api.one
+    def act_reject(self):
+        if self.state == 'draft':
+            self.state = 'reject'
+            self.pending = False
+
+    @api.one
+    def act_approve_pending(self):
+        if self.pending == True:
+            requested = self.line_ids.search([('state', '=', 'pending'), ('line_id', '=', self.id)], order='id desc',
+                                             limit=1)
+            if requested:
+                self.name = requested.change_name
+                self.active = requested.status
+                self.pending = False
+                requested.state = 'approve'
+                requested.change_date = fields.Datetime.now()
+
+    @api.one
+    def act_reject_pending(self):
+        if self.pending == True:
+            requested = self.line_ids.search([('state', '=', 'pending'), ('line_id', '=', self.id)], order='id desc',
+                                             limit=1)
+            if requested:
+                self.pending = False
+                requested.state = 'reject'
+                requested.change_date = fields.Datetime.now()
 
     @api.one
     def name_get(self):
@@ -33,10 +91,27 @@ class AccountJournal(models.Model):
         if self.code:
             self.code = str(self.code.strip()).upper()
 
+
     @api.multi
     def unlink(self):
-        try:
-            return super(AccountJournal, self).unlink()
-        except IntegrityError:
-            raise ValidationError(_("The operation cannot be completed, probably due to the following:\n"
-                                    "- deletion: you may be trying to delete a record while other records still reference it"))
+        for rec in self:
+            if rec.state in ('approve', 'reject'):
+                raise ValidationError(_('[Warning] Approves and Rejected record cannot be deleted.'))
+
+            try:
+                return super(AccountJournal, rec).unlink()
+            except IntegrityError:
+                raise ValidationError(_("The operation cannot be completed, probably due to the following:\n"
+                                        "- deletion: you may be trying to delete a record while other records still reference it"))
+
+class HistoryBranch(models.Model):
+    _name = 'history.account.journal'
+    _description = 'History Branch'
+    _order = 'id desc'
+
+    change_name = fields.Char('Proposed Name', size=50, readonly=True, states={'draft': [('readonly', False)]})
+    status = fields.Boolean('Active', default=True, track_visibility='onchange')
+    change_date = fields.Datetime(string='Date')
+    line_id = fields.Many2one('account.journal', ondelete='restrict')
+    state = fields.Selection([('pending', 'Pending'), ('approve', 'Approve'), ('reject', 'Reject')],
+                             default='pending')
---
+++ account_mtbl/wizard/account_journal_wizard.py
@@ -0,0 +1,45 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning
+
+
+class AccountJournalWizard(models.TransientModel):
+    _name = 'account.journal.wizard'
+
+    @api.model
+    def default_name(self):
+        context = self._context
+        return self.env[context['active_model']].search([('id', '=', context['active_id'])]).name
+
+    @api.model
+    def default_status(self):
+        context = self._context
+        return self.env[context['active_model']].search([('id', '=', context['active_id'])]).active
+
+    status = fields.Boolean(string='Active', default=default_status)
+    name = fields.Char(string='Requested Name')
+
+    @api.constrains('name')
+    def _check_unique_constrain(self):
+        if self.name:
+            name = self.env['account.journal'].search(
+                [('name', '=ilike', self.name.strip()), '|', ('active', '=', True), ('active', '=', False)])
+            if len(name) > 1:
+                raise Warning('[Unique Error] Name must be unique!')
+
+    @api.multi
+    def act_change_name(self):
+        id = self._context['active_id']
+
+        name = self.env['account.journal'].search([('name', '=ilike', self.name)])
+        if len(name) > 0:
+            raise Warning('[Unique Error] Name must be unique!')
+
+        pending = self.env['history.account.journal'].search([('state', '=', 'pending'), ('line_id', '=', id)])
+        if len(pending) > 0:
+            raise Warning('[Warning] You already have a pending request!')
+
+        self.env['history.account.journal'].create({'change_name': self.name, 'line_id': id, 'status': self.status})
+        record = self.env['account.journal'].search([('id', '=', id)])
+        if record:
+            record.pending = True
+            record.pending = True
