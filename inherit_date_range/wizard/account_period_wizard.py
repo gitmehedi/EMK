f03b2c9e62efdc3899aae84642244829c89fcc74
Rita <ritaislam6789@gmail.com>
2019-05-14 15:54:45 +0600

    [ADD]Added Macker and checker,state,edit button for Account Period Type

--- inherit_date_range/wizard/account_period_wizard.py
+++ inherit_date_range/wizard/account_period_wizard.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api, _
 from odoo.exceptions import Warning
 
 
-class BranchWizard(models.TransientModel):
+class AccountPeriodWizard(models.TransientModel):
     _name = 'account.period.wizard'
 
     @api.model
@@ -40,6 +40,50 @@ class BranchWizard(models.TransientModel):
 
         self.env['history.date.range'].create({'change_name': self.name, 'line_id': id, 'status': self.status})
         record = self.env['date.range'].search([('id', '=', id)])
+        if record:
+            record.pending = True
+            record.pending = True
+
+
+
+class AccountPeriodTypeWizard(models.TransientModel):
+    _name = 'account.period.type.wizard'
+
+    @api.model
+    def default_name(self):
+        context = self._context
+        return self.env[context['active_model']].search([('id', '=', context['active_id'])]).name
+
+    @api.model
+    def default_status(self):
+        context = self._context
+        return self.env[context['active_model']].search([('id', '=', context['active_id'])]).active
+
+    status = fields.Boolean(string='Active', default=default_status)
+    name = fields.Char(string='Requested Name')
+
+    @api.constrains('name')
+    def _check_unique_constrain(self):
+        if self.name:
+            name = self.env['date.range.type'].search(
+                [('name', '=ilike', self.name.strip()), '|', ('active', '=', True), ('active', '=', False)])
+            if len(name) > 1:
+                raise Warning('[Unique Error] Name must be unique!')
+
+    @api.multi
+    def act_change_name(self):
+        id = self._context['active_id']
+
+        name = self.env['date.range.type'].search([('name', '=ilike', self.name)])
+        if len(name) > 0:
+            raise Warning('[Unique Error] Name must be unique!')
+
+        pending = self.env['history.date.range.type'].search([('state', '=', 'pending'), ('line_id', '=', id)])
+        if len(pending) > 0:
+            raise Warning('[Warning] You already have a pending request!')
+
+        self.env['history.date.range.type'].create({'change_name': self.name, 'line_id': id, 'status': self.status})
+        record = self.env['date.range.type'].search([('id', '=', id)])
         if record:
             record.pending = True
             record.pending = True
