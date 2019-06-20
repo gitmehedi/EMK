a10198ba488e4f8c33a3d106ffc7a432c8ab2ed9
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2019-06-20 15:20:38 +0600

    [IMP]TDS/Vendor Bill updated

--- tds_vat_challan/wizards/challan_ou_selection_wizard.py
+++ tds_vat_challan/wizards/challan_ou_selection_wizard.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 from odoo import models, fields, api,_
 
 
@@ -12,50 +13,61 @@ class TDSChallaOUSelectionWizard(models.TransientModel):
     @api.multi
     def generate_challan(self):
         # active_ids = self.env.context.get('active_ids')
-        account_move_line_objs = self.env['account.move.line'].search([('id', 'in', self.env.context.get('active_ids'))], order='partner_id asc')
+        account_move_line_objs = self.env['account.move.line'].search([('id', 'in', self.env.context.get('active_ids'))],
+                                                                      order='partner_id asc,operating_unit_id asc')
         challan_obj = self.env['tds.vat.challan']
         challan_line_obj = self.env['tds.vat.challan.line']
-        challan_id = False
-        pre_supplier_list = []
-        pre_op_unit_list = []
+        challan_objs = []
         tds_challan_list = []
 
         if self.operating_unit_id:
             account_move_line_objs = account_move_line_objs.filtered(lambda r: r.operating_unit_id.id == self.operating_unit_id.id)
 
         for account_move_line_obj in account_move_line_objs:
-
-            if account_move_line_obj.partner_id.id in pre_supplier_list and account_move_line_obj.operating_unit_id.id in pre_op_unit_list:
-                pass
-            else:
+            is_create = False
+            for challan_obj in challan_objs:
+                if account_move_line_obj.partner_id.id == challan_obj.supplier_id.id and \
+                    account_move_line_obj.operating_unit_id.id == challan_obj.operating_unit_id.id:
+                    challan_obj.write({'acc_move_line_ids':[(4, account_move_line_obj.id)]})
+                    is_create = True
+                    break
+            if not is_create:
                 challan = {
                     'supplier_id': account_move_line_obj.partner_id.id,
                     'operating_unit_id': account_move_line_obj.operating_unit_id.id,
                     'date': fields.Date.today(),
                     'state': 'draft',
+                    'acc_move_line_ids':[(4, account_move_line_obj.id)],
                 }
-
                 res_challan_obj = challan_obj.create(challan)
                 if res_challan_obj:
-                    challan_id = res_challan_obj.id
+                    challan_objs.append(res_challan_obj)
                     tds_challan_list.append(res_challan_obj.id)
-                    pre_supplier_list.append(res_challan_obj.supplier_id.id)
-                    pre_op_unit_list.append(res_challan_obj.operating_unit_id.id)
-
-            line = {
-                'supplier_id': account_move_line_obj.partner_id.id,
-                'challan_provided': 0.0,
-                'total_bill': account_move_line_obj.credit,
-                'undistributed_bill': account_move_line_obj.credit,
-                'parent_id': challan_id,
-                'acc_move_line_id': account_move_line_obj.id,
-                'state': 'draft',
-                'type_name': account_move_line_obj.name,
-
-            }
-            challan_line_obj.create(line)
 
             account_move_line_obj.write({'is_deposit': True})
+
+        for challan in challan_objs:
+            if len(challan.acc_move_line_ids) == 1:
+                id_param = "(" + str(challan.acc_move_line_ids.id) + ")"
+            else:
+                id_param = str(tuple(challan.acc_move_line_ids.ids))
+            self.env.cr.execute("""
+                                SELECT SUM(credit) as amount,name
+                                FROM account_move_line
+                                WHERE id in %s
+                                group by name"""%(id_param))
+            results = self.env.cr.fetchall()
+            for amount,name in results:
+                line = {
+                    'supplier_id': challan.supplier_id.id,
+                    'challan_provided': 0.0,
+                    'total_bill': amount,
+                    'undistributed_bill': amount,
+                    'parent_id': challan.id,
+                    'state': 'draft',
+                    'type_name': name,
+                }
+                challan_line_obj.create(line)
 
         vals = [('id', 'in', tds_challan_list)]
         return {
@@ -69,4 +81,3 @@ class TDSChallaOUSelectionWizard(models.TransientModel):
             'target': 'current',
             'domain': vals,
         }
-
