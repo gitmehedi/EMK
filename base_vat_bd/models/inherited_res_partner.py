0f469e06d085cac55c2e024569f64a9ecc95a479
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2019-06-30 19:37:17 +0600

    [IMP]Vendor default payable account added

--- base_vat_bd/models/inherited_res_partner.py
+++ base_vat_bd/models/inherited_res_partner.py
@@ -7,7 +7,7 @@ class InheritedResPartner(models.Model):
     @api.model
     def default_payable_acc(self):
         return self.env['account.account'].search([
-            ('id', '=', 'payable'),
+            ('internal_type', '=', 'payable'),
             ('deprecated', '=', False)],
             order='id DESC', limit=1)
 
@@ -15,4 +15,4 @@ class InheritedResPartner(models.Model):
     vat = fields.Char(string='VAT Registration')
     bin = fields.Char(string='BIN Number')
     tin = fields.Char(string='TIN Number')
-    property_account_payable_id = fields.Many2one(default = default_payable_acc)
+    property_account_payable_id = fields.Many2one(default = default_payable_acc, company_dependent=False)
