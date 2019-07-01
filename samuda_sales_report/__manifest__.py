4b562fcc1d13e7527ce6c0dbd2936b8644d92cc6
aburayhan157 <aburayhan157@gmail.com>
2019-07-01 13:01:43 +0600

    Implement sector and product wise sales report

--- samuda_sales_report/__manifest__.py
+++ samuda_sales_report/__manifest__.py
@@ -11,9 +11,13 @@
     'data': [
         'reports/executive_sales_report.xml',
         'reports/customer_sales_report.xml',
+        'reports/sector_sales_report.xml',
+        'reports/product_sales_report.xml',
         'wizard/customer_aging_statement_wizard.xml',
         'wizard/executive_sales_wizard.xml',
         'wizard/customer_sales_wizard.xml',
+        'wizard/sector_sales_wizard.xml',
+        'wizard/product_sales_wizard.xml',
         'reports/customer_aging_statement.xml',
         'reports/inherited_views.xml',
         'view/menu_items.xml',
