535a6d97464911171196dbf6ec49f75625b13aa4
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2019-08-28 14:15:37 +0600

    [IMP] Custom Amount Conversion Rate Updated

--- custom_amount_conversion/models/inherit_account_invoice.py
+++ custom_amount_conversion/models/inherit_account_invoice.py
@@ -19,6 +19,18 @@ class InheritAccountInvoice(models.Model):
 
     conversion_rate = fields.Float(string='Conversion Rate')
 
+
+    @api.onchange('currency_id')
+    def _onchange_currency_id(self):
+        res = super(InheritAccountInvoice, self)._onchange_currency_id()
+        if self.currency_id:
+            to_currency = self.company_id.currency_id
+            from_currency = self.currency_id.with_context(
+                date=self._get_currency_rate_date() or fields.Date.context_today(self))
+            self.conversion_rate = to_currency.rate / from_currency.rate
+        return res
+
+
     @api.depends('currency_id')
     def _get_fields_invisible(self):
         if self.currency_id.id != self.company_id.currency_id.id:
