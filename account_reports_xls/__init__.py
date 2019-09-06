074f608cd425990e51e1dfdb934a29b25468cd7e
gitmehedi <git.mehedi@gmail.com>
2019-09-06 19:52:13 +0600

    [ADD] Generate XLSX report for traial balance.

--- account_reports_xls/__init__.py
+++ account_reports_xls/__init__.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-# Author: Damien Crier
-# Copyright 2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-# from . import models
-from . import wizard
-
-# Don't break installations that don't have this module installed
 have_report_xlsx = False
 try:
     from odoo.addons.report_xlsx.report.report_xlsx import ReportXlsx
