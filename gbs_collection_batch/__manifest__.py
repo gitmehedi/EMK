f56436e95a0aa2e5b34e4fd1e0e626564f94244e
aburayhan157 <aburayhan157@gmail.com>
2019-09-11 12:27:27 +0600

    Implemented new gbs_collection_batch module

---
+++ gbs_collection_batch/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'GBS Collection Batch',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': '',
+    'version':'1.0.0',
+    'depends': [
+        'account',
+        'delivery_order',
+    ],
+
+    'data': [
+        'views/account_payment_batch_view.xml',
+        'views/menu_items_view.xml',
+    ],
+
+    'summary': 'GBS Collection Batch',
+    'description': """""",
+    'installable': True,
+    'application': False,
+}
