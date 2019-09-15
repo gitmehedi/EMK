4d75946f4cfbca84ebf6826ef638483f30d77d10
gitmehedi <gitmehedi@gmail.com>
2019-09-15 13:07:17 +0600

    [FIX] Change operating unit to Branch in trial balance wizards.

--- account_reports_xls/wizard/trial_balance_wizard.py
+++ account_reports_xls/wizard/trial_balance_wizard.py
@@ -6,7 +6,9 @@ from odoo import fields, models, api
 class AccountBalanceReport(models.TransientModel):
     _inherit = 'account.balance.report'
 
+    operating_unit_ids = fields.Many2many(string='Branch')
+
     @api.multi
-    def button_export_xlsx(self,data):
+    def button_export_xlsx(self, data):
         self.ensure_one()
         return self.env['report'].get_action(self, report_name='account_reports_xls.payroll_xls')
