a503812769b7576e549721ba700fb504df253ce2
aburayhan157 <aburayhan157@gmail.com>
2019-09-23 15:07:37 +0600

    Implemented log module for account journal

---
+++ gbs_account_journal_log/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Account Journal Log',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'accounts',
+    'version': '10.1.1.1',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/inherit_account_move_view.xml',
+        'views/inherit_account_move_line_view.xml',
+    ],
+
+    'summary': 'Account Journal Log',
+    'description':
+    """Creating log for journal item""",
+    'installable': True,
+    'application': False,
+}
