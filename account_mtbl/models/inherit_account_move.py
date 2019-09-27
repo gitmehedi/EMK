509a912e9fc618b4276248ea819ba5c829de523b
gitmehedi <gitmehedi@gmail.com>
2019-09-27 17:56:26 +0600

    [FIX] Fix issue on adding branch in account.invoice.line

--- account_mtbl/models/inherit_account_move.py
+++ account_mtbl/models/inherit_account_move.py
@@ -10,7 +10,7 @@ class AccountMove(models.Model):
     ref = fields.Char(states={'posted': [('readonly', True)]}, track_visibility='onchange')
     state = fields.Selection(track_visibility='onchange')
     narration = fields.Text(states={'posted': [('readonly', True)]}, track_visibility='onchange')
-    operating_unit_id = fields.Many2one(string='Branch', required=True, states={'posted': [('readonly', True)]},
+    operating_unit_id = fields.Many2one(string='Branch',  states={'posted': [('readonly', True)]},
                                         track_visibility='onchange')
     is_cbs = fields.Boolean(default=False, help='CBS data always sync with OGL using GLIF.')
     is_sync = fields.Boolean(default=False, help='OGL continuously send data to CBS for journal sync.')
@@ -26,4 +26,4 @@ class AccountMoveLine(models.Model):
     segment_id = fields.Many2one('segment', string="Segment")
     acquiring_channel_id = fields.Many2one('acquiring.channel', string="AC")
     servicing_channel_id = fields.Many2one('servicing.channel', string="SC")
-    operating_unit_id = fields.Many2one(string='Branch', required=True)
+    operating_unit_id = fields.Many2one(string='Branch')
