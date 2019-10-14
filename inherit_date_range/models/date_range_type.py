e998ba4bd028b51b141f4f803b7021a58bd9745c
ShahAlamSumonGenweb2 <sacsesumon@yahoo.com>
2019-10-14 16:28:38 +0600

    [IMP] Maker,checker validation impose in date range

--- inherit_date_range/models/date_range_type.py
+++ inherit_date_range/models/date_range_type.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, SUPERUSER_ID
 from odoo.tools.translate import _
 from odoo.exceptions import ValidationError
 
@@ -33,6 +33,8 @@ class DateRangeType(models.Model):
                                      states={'draft': [('readonly', False)]})
     line_ids = fields.One2many('history.date.range.type', 'line_id', string='Lines', readonly=True,
                                states={'draft': [('readonly', False)]})
+    maker_id = fields.Many2one('res.users', 'Maker', default=lambda self: self.env.user.id, track_visibility='onchange')
+    approver_id = fields.Many2one('res.users', 'Checker', track_visibility='onchange')
 
     @api.constrains('name')
     def _check_unique_constrain(self):
@@ -58,11 +60,14 @@ class DateRangeType(models.Model):
 
     @api.one
     def act_approve(self):
+        if self.env.user.id == self.maker_id.id and self.env.user.id != SUPERUSER_ID:
+            raise ValidationError(_("[Validation Error] Maker and Approver can't be same person!"))
         if self.state == 'draft':
             self.write({
                 'state': 'approve',
                 'pending': False,
                 'active': True,
+                'approver_id': self.env.user.id,
             })
 
     @api.one
@@ -76,6 +81,8 @@ class DateRangeType(models.Model):
 
     @api.one
     def act_approve_pending(self):
+        if self.env.user.id == self.maker_id.id and self.env.user.id != SUPERUSER_ID:
+            raise ValidationError(_("[Validation Error] Editor and Approver can't be same person!"))
         if self.pending == True:
             requested = self.line_ids.search([('state', '=', 'pending'), ('line_id', '=', self.id)], order='id desc',
                                              limit=1)
@@ -84,6 +91,7 @@ class DateRangeType(models.Model):
                     'name': self.name if not requested.change_name else requested.change_name,
                     'pending': False,
                     'active': requested.status,
+                    'approver_id': self.env.user.id,
                 })
                 requested.write({
                     'state': 'approve',
