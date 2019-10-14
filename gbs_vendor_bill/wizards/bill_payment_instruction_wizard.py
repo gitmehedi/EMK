8ef85db87cdb7d6174be65f0f4488c059322c20c
gitmehedi <gitmehedi@gmail.com>
2019-10-14 19:20:48 +0600

    [RF] Refactor following Payment Instruction functionality - Vendor Bill Payment Instruction - Asset Vendor Bill Payment Instruction - Agreement Payment Instructions

--- gbs_vendor_bill/wizards/bill_payment_instruction_wizard.py
+++ gbs_vendor_bill/wizards/bill_payment_instruction_wizard.py
@@ -9,17 +9,19 @@ class BillPaymentInstructionWizard(models.TransientModel):
                                  string="Invoice", copy=False, readonly=True)
     currency_id = fields.Many2one('res.currency', string='Currency', required=True,
                                   default=lambda self: self.env.context.get('currency_id'))
-    amount = fields.Float(string='Amount', required=True,
-                          default=lambda self: self.env.context.get('invoice_amount'))
-    instruction_date = fields.Date(string='Date', default=fields.Date.context_today,
-                                   required=True, copy=False)
-    credit_operating_unit_id = fields.Many2one('operating.unit', string='Credit Branch', required=True)
+    amount = fields.Float(string='Amount', required=True, default=lambda self: self.env.context.get('invoice_amount'))
+    instruction_date = fields.Date(string='Date', default=fields.Date.context_today, required=True, copy=False)
+
     debit_operating_unit_id = fields.Many2one('operating.unit', string='Debit Branch', required=True,
-                                        default=lambda self: self.env.context.get('op_unit'))
-    credit_sub_operating_unit_id = fields.Many2one('sub.operating.unit', string='Credit SOU')
+                                              default=lambda self: self.env.context.get('op_unit'))
     debit_sub_operating_unit_id = fields.Many2one('sub.operating.unit', string='Debit SOU')
-    credit_account_id = fields.Many2one('account.account', string='Credit Account',
-                                        required=True)
+    partner_id = fields.Many2one('res.partner', string='Vendor',
+                                 default=lambda self: self.env.context.get('partner_id'))
+    vendor_bank_acc = fields.Char(related='partner_id.vendor_bank_acc', string='Vendor Bank Account')
+    type = fields.Selection([('casa', 'CASA'), ('credit', 'Credit Account')], default='casa', string='Payment To')
+    credit_account_id = fields.Many2one('account.account', string='Credit Account')
+    credit_operating_unit_id = fields.Many2one('operating.unit', string='Credit Branch')
+    credit_sub_operating_unit_id = fields.Many2one('sub.operating.unit', string='Credit SOU')
 
     @api.constrains('amount')
     def _check_amount(self):
@@ -29,55 +31,46 @@ class BillPaymentInstructionWizard(models.TransientModel):
                 raise ValidationError(_("Sorry! This amount is bigger then remaining balance. "
                                         "Remaining balance is %s") % (rem_amount))
 
-    # @api.onchange('invoice_id')
-    # def _onchange_invoice_id(self):
-    #     if self.invoice_id:
-    #         operating_unit_ids = [i.operating_unit_id.id for i in self.invoice_id.invoice_line_ids]
-    #         sub_operating_unit_ids = [i.sub_operating_unit_id.id for i in self.invoice_id.invoice_line_ids]
-    #         return {'domain': {
-    #             'operating_unit_id': [('id', 'in', operating_unit_ids)],
-    #             'sub_operating_unit_id': [('id', 'in', sub_operating_unit_ids)]
-    #         }}
-
     @api.onchange('credit_operating_unit_id')
     def _onchange_operating_unit_id(self):
         if self.credit_operating_unit_id:
             self.credit_sub_operating_unit_id = []
             credit_sub_operating_unit_ids = self.env['sub.operating.unit'].search([
-                ('operating_unit_id','=',self.credit_operating_unit_id.id)])
+                ('operating_unit_id', '=', self.credit_operating_unit_id.id)])
             return {'domain': {
                 'credit_sub_operating_unit_id': [('id', 'in', credit_sub_operating_unit_ids.ids)]
             }}
 
-
     @api.multi
     def action_validate(self):
-        # for line in self.invoice_id.suspend_security().move_id.line_ids:
-        #     if line.account_id.internal_type in ('receivable', 'payable'):
-        #         if line.amount_residual < 0:
-        #             val = -1
-        #         else:
-        #             val = 1
-        #         line.write({'amount_residual': ((line.amount_residual) * val) - self.amount})
+        debit_acc = self.invoice_id.partner_id.property_account_payable_id.id
+        debit_branch = self.debit_operating_unit_id.id or None
+        debit_sou = self.debit_sub_operating_unit_id.id if self.debit_sub_operating_unit_id else None
+        partner_id = self.invoice_id.partner_id.id
 
-        credit_acc = False
-        account_config_pool = self.env.user.company_id
-        debit_acc_id = self.invoice_id.partner_id.property_account_payable_id.id
-        if self.invoice_id.partner_id.vendor_bank_acc:
-            credit_acc = self.invoice_id.partner_id.vendor_bank_acc
+        if self.type == 'casa':
+            vendor_bank_acc = self.invoice_id.partner_id.vendor_bank_acc
+            credit_acc = False
+            credit_branch = False
+            credit_sou = False
+        if self.type == 'credit':
+            vendor_bank_acc = False
+            credit_acc = self.credit_account_id.id
+            credit_branch = self.credit_operating_unit_id.id
+            credit_sou = self.credit_sub_operating_unit_id.id if self.credit_sub_operating_unit_id else None
 
         self.env['payment.instruction'].create({
             'invoice_id': self.invoice_id.id,
-            'partner_id': self.invoice_id.partner_id.id,
-            'currency_id': self.invoice_id.currency_id.id,
-            'default_debit_account_id': debit_acc_id,
-            'default_credit_account_id': self.credit_account_id.id,
-            'vendor_bank_acc': credit_acc,
             'instruction_date': self.instruction_date,
             'amount': self.amount,
-            'credit_operating_unit_id': self.credit_operating_unit_id.id or None,
-            'debit_operating_unit_id': self.debit_operating_unit_id.id or None,
-            'credit_sub_operating_unit_id': self.credit_sub_operating_unit_id.id if self.credit_sub_operating_unit_id else None,
-            'debit_sub_operating_unit_id': self.debit_sub_operating_unit_id.id if self.debit_sub_operating_unit_id else None,
+            'currency_id': self.invoice_id.currency_id.id,
+            'default_debit_account_id': debit_acc,
+            'debit_operating_unit_id': debit_branch,
+            'debit_sub_operating_unit_id': debit_sou,
+            'partner_id': partner_id,
+            'vendor_bank_acc': vendor_bank_acc,
+            'default_credit_account_id': credit_acc,
+            'credit_operating_unit_id': credit_branch,
+            'credit_sub_operating_unit_id': credit_sou,
         })
         return {'type': 'ir.actions.act_window_close'}
