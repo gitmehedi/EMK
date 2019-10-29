63988691e548e7019c032e18d4fa7bf52bb38bcd
aburayhan157 <aburayhan157@gmail.com>
2019-10-29 18:26:53 +0600

    Updated

--- gbs_vendor_bill/models/account_invoice.py
+++ gbs_vendor_bill/models/account_invoice.py
@@ -6,7 +6,7 @@ class AccountInvoice(models.Model):
     _name = 'account.invoice'
     _inherit = ['account.invoice', 'ir.needaction_mixin']
 
-    operating_unit_id = fields.Many2one('operating.unit', 'Branch',
+    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
                                         default=lambda self:
                                         self.env['res.users'].
                                         operating_unit_default_get(self._uid),
