3b92ec40db628351157fb2aec9161849742ad358
gitmehedi <gitmehedi@gmail.com>
2020-01-22 17:06:57 +0600

    [ADD] Check constrains on Date Start and Date End in Trial Balance.

--- account_reports_xls/wizard/trial_balance_wizard.py
+++ account_reports_xls/wizard/trial_balance_wizard.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime as dt
 
-from odoo import fields, models, api
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class AccountBalanceReport(models.TransientModel):
@@ -10,6 +12,25 @@ class AccountBalanceReport(models.TransientModel):
     ex_operating_unit_ids = fields.Many2many('operating.unit', 'account_balance_report_ex_operating_unit_rel',
                                              'account_id', 'operating_unit_id', string='Branch (Exclude)',
                                              required=False, default=[])
+
+    @api.constrains('date_from', 'date_to')
+    def check_date(self):
+        if self.date_from:
+            if self.date_to:
+                start = dt.strptime(self.date_from, "%Y-%m-%d")
+                end = dt.strptime(self.date_to, "%Y-%m-%d")
+                start_year = self.date_from[:4]
+                end_year = self.date_to[:4]
+                msg = ''
+                if start > end:
+                    msg += "Start Date [{0}] shouldn't be greater than End Date [{1}]\n".format(self.date_from,
+                                                                                                self.date_to)
+                if start_year != end_year:
+                    msg += "Start Date [{0}] and End Date [{1}] should be in same year.".format(self.date_from,
+                                                                                                self.date_to)
+
+                if msg:
+                    raise ValidationError(_(msg))
 
     def _build_contexts(self, data):
         result = super(AccountBalanceReport, self)._build_contexts(data)
