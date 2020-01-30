709759c1be41623dfe0e5a03befe2ad1f10012e7
gitmehedi <gitmehedi@gmail.com>
2020-01-30 14:44:16 +0600

    [MOD] Change on setting field presentation.

--- tds_vat_challan/models/account_config.py
+++ tds_vat_challan/models/account_config.py
@@ -1,12 +1,13 @@
 from odoo import api, fields, models, _
 
+
 class AccountConfigSettings(models.TransientModel):
     _inherit = 'account.config.settings'
 
     def _get_default_journal_id(self):
         return self.env.user.company_id.tds_vat_transfer_journal_id
 
-    tds_vat_transfer_journal_id = fields.Many2one('account.journal',string='Transfer Journal',
+    tds_vat_transfer_journal_id = fields.Many2one('account.journal', string='Transfer Journal', required=True,
                                                   default=lambda self: self._get_default_journal_id(),
                                                   help="Account Journal that used when TDS/VAT challan deposited")
 
@@ -16,7 +17,6 @@ class AccountConfigSettings(models.TransientModel):
             self.company_id.write({'tds_vat_transfer_journal_id': self.tds_vat_transfer_journal_id.id})
         return self.env['ir.values'].sudo().set_default(
             'account.config.settings', 'tds_vat_transfer_journal_id', self.tds_vat_transfer_journal_id.id)
-
 
 
 class ResCompany(models.Model):
