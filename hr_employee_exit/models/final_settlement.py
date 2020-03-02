ed763c31427c52aa751f32611196208850a365e8
root <matiar.rahman@gmail.com>
2020-03-02 19:08:36 +0600

    ..

--- hr_employee_exit/models/final_settlement.py
+++ hr_employee_exit/models/final_settlement.py
@@ -7,7 +7,7 @@ class FinalSettlement(models.Model):
     _description = 'Employee Exit Full & Final Settlement'
 
     employee_code = fields.Char('Employee Code',related='employee_id.identification_id',readonly=True)
-    employee_id = fields.Many2one('hr.employee',string = 'Employee',required=True, track_visibility='onchange',domain = [('state', '=', 'relieved')])
+    employee_id = fields.Many2one('hr.employee',string = 'Employee',required=True, track_visibility='onchange')
     emp_designation = fields.Many2one('hr.job', string='Designation', related='employee_id.job_id',readonly=True)
     joining_date = fields.Date(related='employee_id.initial_employment_date', string='Date of Join', readonly=True)
     leaving_date = fields.Date(string='Date of Leaving',readonly=True)
