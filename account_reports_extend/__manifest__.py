5dd265618ab332386fc26828f9312056d7786e40
aburayhan157 <aburayhan157@gmail.com>
2020-03-09 17:14:29 +0600

    [FIX] Resolved the issue of Balance Sheet and Purchase Requisition report.

--- account_reports_extend/__manifest__.py
+++ account_reports_extend/__manifest__.py
@@ -10,7 +10,7 @@ Accounting Reports
     "website": "http://www.genweb2.com",
     'version': '10.0.0.1',
     'category': 'Accounting',
-    'depends': ['account_reports'],
+    'depends': ['base', 'account_reports'],
     'data': [
         'data/inherit_account_financial_report_data.xml',
     ],
