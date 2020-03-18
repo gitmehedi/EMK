739e011abe2025e13643f9e12a6793d61f8406e9
aburayhan157 <aburayhan157@gmail.com>
2020-03-18 16:58:01 +0600

    [FIX] Resolved sale order attachment deleting issue.

--- gbs_sale_order_approval/models/inherited_ir_attachment.py
+++ gbs_sale_order_approval/models/inherited_ir_attachment.py
@@ -1,5 +1,8 @@
 from odoo import api,models, _
 from odoo.exceptions import ValidationError
+from datetime import datetime
+
+VALID_DAYS = 185
 
 
 class InheritedIrAttachment(models.Model):
@@ -7,10 +10,16 @@ class InheritedIrAttachment(models.Model):
 
     @api.multi
     def unlink(self):
+        date_now = datetime.now()
+
         for i in self:
             if i.res_model == 'sale.order':
-                so_obj = i.env['sale.order'].search([('id','=',i.res_id)])
-                if so_obj.state == 'done':
+                so_obj = i.env['sale.order'].search([('id', '=', i.res_id)])
+
+                date_create = datetime.strptime(i.create_date, '%Y-%m-%d %H:%M:%S')
+                days = (date_now - date_create).days
+
+                if so_obj.state == 'done' and days <= VALID_DAYS:
                     raise ValidationError(_('You can not delete attachments when Sale Order is in Approved stage'))
 
         return super(InheritedIrAttachment, self).unlink()
