470bae06f33c3b610213c1492ebef8f0a90f4e0d
mehedi <mehedi1344@gmail.com>
2020-03-24 14:44:18 +0600

    Project task filter Open Stage added

---
+++ project_task_filter/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project Task Filter",
+    'version': '1.0',
+    'summary': """
+        Create a new filter that project task stage is not closed""",
+    'description': """
+        Create a new filter that project task stage is not closed
+    """,
+    'license': 'AGPL-3',
+    'author': "Genweb2 Limited",
+    'website': "",
+    'category': 'project',
+    'version': '0.1',
+    'depends': ['project'],
+    'data': [
+        'views/project_task_filter_view.xml'
+    ],
+}
