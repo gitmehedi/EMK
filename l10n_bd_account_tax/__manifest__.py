90a9b690dd19bf87ab3b0e8ac7421c66b6724a75
mehedi1344 <mehedi1344@gmail.com>
2020-04-07 03:57:01 +0600

    [MOD]Modify reverse account tax calculation feature

---
+++ l10n_bd_account_tax/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Tax Reverse",
+    'description': """Account Tax Reverse""",
+    'author': "Genweb2 Limited",
+    'website': "http://www.genweb2.com",
+    'version': '10.0.0.1',
+    'category': 'Accounting & Finance',
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/inherit_account_tax_view.xml'
+    ],
+    'installable': True,
+}
