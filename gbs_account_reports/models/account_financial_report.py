62e8a30a54813a74e8ca75040af9b73d79f00eb3
aburayhan157 <aburayhan157@gmail.com>
2020-04-12 17:56:37 +0600

    [RF] Resolved issue of Cash Flow Statement report

--- gbs_account_reports/models/account_financial_report.py
+++ gbs_account_reports/models/account_financial_report.py
@@ -6,6 +6,53 @@ from odoo.addons.account_reports.models.account_financial_report import FormulaL
 class AccountFinancialReportLine(models.Model):
     _inherit = "account.financial.html.report.line"
 
+    def _get_with_statement(self, financial_report):
+        sql = ''
+        params = []
+
+        if self.code not in ('CASHSTART', 'CASHEND') \
+          and (financial_report == self.env.ref('account_reports.account_financial_report_cashsummary0') or self.env.context.get('cash_basis')):
+            user_types = self.env['account.account.type'].search([('type', 'in', ('receivable', 'payable'))])
+            if not user_types:
+                return sql, params
+            #we use query_get() to filter out unrelevant journal items to have a shadowed table as small as possible
+            tables, where_clause, where_params = self.env['account.move.line']._query_get()
+            sql = """WITH account_move_line AS (
+              SELECT \"account_move_line\".id, \"account_move_line\".date, \"account_move_line\".name, \"account_move_line\".debit_cash_basis, \"account_move_line\".credit_cash_basis, \"account_move_line\".move_id, \"account_move_line\".account_id, \"account_move_line\".journal_id, \"account_move_line\".balance_cash_basis, \"account_move_line\".amount_residual, \"account_move_line\".partner_id, \"account_move_line\".reconciled, \"account_move_line\".company_id, \"account_move_line\".company_currency_id, \"account_move_line\".amount_currency, \"account_move_line\".balance, \"account_move_line\".user_type_id, \"account_move_line\".tax_line_id, \"account_move_line\".invoice_id, \"account_move_line\".credit, \"account_move_line\".tax_exigible, \"account_move_line\".debit, \"account_move_line\".analytic_account_id, \"account_move_line\".operating_unit_id
+               FROM """ + tables + """
+               WHERE (\"account_move_line\".journal_id IN (SELECT id FROM account_journal WHERE type in ('cash', 'bank'))
+                 OR \"account_move_line\".move_id NOT IN (SELECT DISTINCT move_id FROM account_move_line WHERE user_type_id IN %s))
+                 AND """ + where_clause + """
+              UNION ALL
+              (
+               WITH payment_table AS (
+                 SELECT aml.move_id, \"account_move_line\".date, CASE WHEN aml.balance = 0 THEN 0 ELSE part.amount / ABS(am.amount) END as matched_percentage
+                   FROM account_partial_reconcile part LEFT JOIN account_move_line aml ON aml.id = part.debit_move_id LEFT JOIN account_move am ON aml.move_id = am.id, """ + tables + """
+                   WHERE part.credit_move_id = "account_move_line".id
+                    AND "account_move_line".user_type_id IN %s
+                    AND """ + where_clause + """
+                 UNION ALL
+                 SELECT aml.move_id, \"account_move_line\".date, CASE WHEN aml.balance = 0 THEN 0 ELSE part.amount / ABS(am.amount) END as matched_percentage
+                   FROM account_partial_reconcile part LEFT JOIN account_move_line aml ON aml.id = part.credit_move_id LEFT JOIN account_move am ON aml.move_id = am.id, """ + tables + """
+                   WHERE part.debit_move_id = "account_move_line".id
+                    AND "account_move_line".user_type_id IN %s
+                    AND """ + where_clause + """
+               )
+               SELECT aml.id, ref.date, aml.name,
+                 CASE WHEN aml.debit > 0 THEN ref.matched_percentage * aml.debit ELSE 0 END AS debit_cash_basis,
+                 CASE WHEN aml.credit > 0 THEN ref.matched_percentage * aml.credit ELSE 0 END AS credit_cash_basis,
+                 aml.move_id, aml.account_id, aml.journal_id,
+                 ref.matched_percentage * aml.balance AS balance_cash_basis,
+                 aml.amount_residual, aml.partner_id, aml.reconciled, aml.company_id, aml.company_currency_id, aml.amount_currency, aml.balance, aml.user_type_id, aml.tax_line_id, aml.invoice_id, aml.credit, aml.tax_exigible, aml.debit, aml.analytic_account_id, aml.operating_unit_id
+                FROM account_move_line aml
+                RIGHT JOIN payment_table ref ON aml.move_id = ref.move_id
+                WHERE journal_id NOT IN (SELECT id FROM account_journal WHERE type in ('cash', 'bank'))
+                  AND aml.move_id IN (SELECT DISTINCT move_id FROM account_move_line WHERE user_type_id IN %s)
+              )
+            ) """
+            params = [tuple(user_types.ids)] + where_params + [tuple(user_types.ids)] + where_params + [tuple(user_types.ids)] + where_params + [tuple(user_types.ids)]
+        return sql, params
+
     def _eval_formula(self, financial_report, debit_credit, context, currency_table, linesDict):
         debit_credit = debit_credit and financial_report.debit_credit
         formulas = self._split_formulas()
