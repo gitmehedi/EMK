318e94ccfd38401d572b4f34c1a0684320c11f94
gitmehedi <git.mehedi@gmail.com>
2020-06-17 20:14:31 +0600

    [ADD] Add following features - Remove module asset_vendor_bill. - Move asset_verdor_bills_view.xml to account_fam module. - Move payment_instrauction_view.xml to gbs_payment_instructions.

--- account_fam/models/account_invoice.py
+++ account_fam/models/account_invoice.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
-
-import random
-
 from odoo import api, fields, models, _
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    is_asset = fields.Boolean(default=False)
+
+    @api.multi
+    def do_merge(self, keep_references=True, date_invoice=False):
+        res = super(AccountInvoice, self).do_merge(keep_references=keep_references, date_invoice=date_invoice)
+        if res:
+            if True in [i.is_asset for i in self]:
+                self.browse(res).write({'is_asset': True})
+        return res
 
 
 class AccountInvoiceLine(models.Model):
