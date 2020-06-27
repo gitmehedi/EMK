fcf32e0ccdcbfd0d7c7e2c0f530c080c1b3fc5e2
gitmehedi <git.mehedi@gmail.com>
2020-06-27 22:51:25 +0600

    [ADD] Add Location for category summary.

--- category_stock_summary_reports/report/product_category_summary_report.py
+++ category_stock_summary_reports/report/product_category_summary_report.py
@@ -6,6 +6,12 @@ class ProductCategorySummaryReport(models.AbstractModel):
 
     sql_str = """SELECT pp.default_code,
                        pp.name_template,
+                       pb.name,
+                       (SELECT STRING_AGG(Name, ', ') FROM product_attribute_value_product_product_rel pavpp
+                        LEFT JOIN product_attribute_value pav
+                         ON (pav.id=pavpp.att_id)
+                        WHERE pp.id = pavpp.prod_id
+                        ) AS attribute,
                        pt.categ_id,
                        pt.list_price AS sale_price,
                        COALESCE((SELECT ph.cost 
@@ -15,14 +21,8 @@ class ProductCategorySummaryReport(models.AbstractModel):
                                  LIMIT  1), 0)          AS cost_price, 
                        COALESCE(SUM(stock.rec_qty),0) AS rec_qty,
                        COALESCE(SUM(stock.sale_qty),0) AS sale_qty,
-                       COALESCE(SUM(stock.move_qty),0) AS move_qty,
                        COALESCE(SUM(stock.rec_qty) - (SUM(stock.sale_qty) + SUM(stock.move_qty)),0) AS stock_qty,
-                       pb.name,
-                       (SELECT STRING_AGG(Name, ', ') FROM product_attribute_value_product_product_rel pavpp
-                        LEFT JOIN product_attribute_value pav
-                         ON (pav.id=pavpp.att_id)
-                        WHERE pp.id = pavpp.prod_id
-                        ) AS attribute
+                       COALESCE(SUM(stock.move_qty),0) AS move_qty
                 FROM product_template pt
                 LEFT JOIN product_product pp ON (pp.product_tmpl_id=pt.id)
                 LEFT JOIN product_brand pb ON (pb.id = pt.product_brand_id)
@@ -74,6 +74,63 @@ class ProductCategorySummaryReport(models.AbstractModel):
                     ORDER BY pp.default_code ASC
     """
 
+
+    sql_all = """SELECT pp.default_code,
+                           pp.name_template,
+                           pb.name,
+                           (SELECT STRING_AGG(Name, ', ') FROM product_attribute_value_product_product_rel pavpp
+                            LEFT JOIN product_attribute_value pav
+                             ON (pav.id=pavpp.att_id)
+                            WHERE pp.id = pavpp.prod_id
+                            ) AS attribute,
+                           pt.categ_id,
+                           pt.list_price AS sale_price,
+                           COALESCE((SELECT ph.cost 
+                                     FROM   product_price_history ph 
+                                     WHERE  pt.id = ph.product_template_id 
+                                     ORDER  BY ph.datetime DESC 
+                                     LIMIT  1), 0)          AS cost_price, 
+                           COALESCE(SUM(stock.rec_qty),0) AS rec_qty,
+                           COALESCE(SUM(stock.sale_qty),0) AS sale_qty,
+                           COALESCE(SUM(stock.rec_qty) - SUM(stock.sale_qty),0) AS stock_qty
+                    FROM product_template pt
+                    LEFT JOIN product_product pp ON (pp.product_tmpl_id=pt.id)
+                    LEFT JOIN product_brand pb ON (pb.id = pt.product_brand_id)
+                    LEFT JOIN
+                      (SELECT rec.product_id,
+                              COALESCE(rec.rec_qty,0) AS rec_qty,
+                              COALESCE(sale.sale_qty,0) AS sale_qty
+                       FROM
+                         (SELECT sm.product_id,
+                                 COALESCE(SUM(sm.product_qty),0) AS rec_qty
+                          FROM stock_move sm
+                          LEFT JOIN stock_location sl 
+                              ON (sl.id = sm.location_id)
+                          WHERE sm.location_id IN (SELECT id FROM stock_location WHERE usage ='supplier') 
+                          GROUP BY sm.product_id
+                          ORDER BY product_id ASC) rec
+                       LEFT JOIN
+                         (SELECT sm.product_id,
+                                 COALESCE(SUM(sm.product_qty),0) AS sale_qty
+                          FROM stock_move sm
+                          LEFT JOIN stock_location sl 
+                             ON (sl.id=sm.location_id)
+                          WHERE sm.location_dest_id IN (SELECT id FROM stock_location WHERE usage='customer')
+                          GROUP BY sm.product_id
+                          ORDER BY sm.product_id ASC) sale 
+                         ON (rec.product_id = sale.product_id)) stock 
+                            ON (stock.product_id = pp.id)
+                        WHERE pt.categ_id=%s
+                        GROUP BY pp.default_code,
+                                 pp.name_template,
+                                 pt.categ_id,
+                                 pt.list_price,
+                                 pt.id,
+                                 pp.id,
+                                 pb.name
+                        ORDER BY pp.default_code ASC
+        """
+
     @api.multi
     def render_html(self, data=None):
         report_data = self.get_data(data)
@@ -86,21 +143,26 @@ class ProductCategorySummaryReport(models.AbstractModel):
     def get_data(self, data):
         report_data = []
 
-        self._cr.execute(self.sql_str, (data['location_id'], data['location_id'],data['location_id'],data['location_id'], data['category_id']))
+        if data['location_id']:
+            self._cr.execute(self.sql_str, (
+                data['location_id'], data['location_id'], data['location_id'], data['location_id'],
+                data['category_id']))
+        else:
+            self._cr.execute(self.sql_all, (data['category_id'],))
 
         for val in self._cr.fetchall():
             temp_dict = {}
             temp_dict['code'] = val[0]
             temp_dict['name'] = val[1]
-            temp_dict['brand'] = val[9]
-            temp_dict['attributes'] = val[10]
-            temp_dict['received_qty'] = val[5]
-            temp_dict['sale_qty'] = val[6]
-            temp_dict['move_qty'] = val[7]
-            temp_dict['on_hand'] = val[8]
-            temp_dict['sale_price'] = val[3]
-            temp_dict['cost_price'] = val[4]
-
+            temp_dict['brand'] = val[2]
+            temp_dict['attributes'] = val[3]
+            temp_dict['sale_price'] = val[5]
+            temp_dict['cost_price'] = val[6]
+            temp_dict['received_qty'] = val[7]
+            temp_dict['sale_qty'] = val[8]
+            temp_dict['on_hand'] = val[9]
+            if data['location_id']:
+                temp_dict['move_qty'] = val[10]
             report_data.append(temp_dict)
 
         return report_data
--- category_stock_summary_reports/wizard/product_category_summary.py
+++ category_stock_summary_reports/wizard/product_category_summary.py
@@ -4,17 +4,16 @@ from openerp import api, exceptions, fields, models
 class ProductCategorySummaryWizard(models.TransientModel):
     _name = 'product.category.summary.wizard'
 
-    category_id = fields.Many2one('product.category', string='Category', required=True)
-    location_id = fields.Many2one('stock.location', string='Location', domain="[('usage','=','internal')]",
-                                  required=True)
+    category_id = fields.Many2one('product.category', string='Category Name', required=True)
+    location_id = fields.Many2one('stock.location', string='Shop Name', domain="[('usage','=','internal')]")
 
     @api.multi
     def report_print(self):
         data = {}
         data['category_name'] = self.category_id.name
         data['category_id'] = self.category_id.id
-        data['location_name'] = self.location_id.name
-        data['location_id'] = self.location_id.id
+        data['location_name'] = self.location_id.name if self.location_id else ''
+        data['location_id'] = self.location_id.id if self.location_id else False
 
         return self.env['report'].get_action(self,
                                              'category_stock_summary_reports.report_product_category_summary_qweb',
