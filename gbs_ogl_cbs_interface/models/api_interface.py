5d8c6d5a89ec5c9be6a373de5d80f3fd99a7dcee
gitmehedi <git.mehedi@gmail.com>
2020-07-06 18:31:35 +0600

    [FIX] Fix bug on api_interface.

--- gbs_ogl_cbs_interface/models/api_interface.py
+++ gbs_ogl_cbs_interface/models/api_interface.py
@@ -788,7 +788,7 @@ class APIInterface(models.Model):
                                                          ('move_id.is_sync', '=', False),
                                                          ('move_id.is_opening', '=', False),
                                                          ('move_id.is_cbs', '=', False),
-                                                         ('state', '=', 'posted')])
+                                                         ('move_id.state', '=', 'posted')])
         for mv in mv_lines:
             response = self.call_gl_enquiry_payment(mv)
             if 'error_code' in response:
