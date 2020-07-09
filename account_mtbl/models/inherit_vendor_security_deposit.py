fb6c612caca835988ac2eef442776c57dfb50a58
mehedi <mehedi1344@gmail.com>
2020-07-09 19:30:48 +0600

    [FIX]fix mandatory field for vendor security deposit creation

--- account_mtbl/models/inherit_vendor_security_deposit.py
+++ account_mtbl/models/inherit_vendor_security_deposit.py
@@ -4,5 +4,5 @@ from odoo import api, fields, models, _
 class VendorSecurityDeposit(models.Model):
     _inherit = 'vendor.security.deposit'
 
-    sub_operating_unit_id = fields.Many2one('sub.operating.unit', string='Sequence', readonly=True, required=True,
+    sub_operating_unit_id = fields.Many2one('sub.operating.unit', string='Sequence', readonly=True,
                                             track_visibility='onchange', states={'draft': [('readonly', False)]})
