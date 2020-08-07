e0413b7b702fb350887866bfc84e5e55701c4554
aburayhan157 <aburayhan157@gmail.com>
2020-08-07 19:45:23 +0600

    [MOD] generating reconcile ref code for payment instruction of Vendor Bill, Vendor Advance and Vendor Security Returns

--- gbs_payment_instruction/wizard/payment_instruction_wizard.py
+++ gbs_payment_instruction/wizard/payment_instruction_wizard.py
@@ -90,8 +90,10 @@ class BillPaymentInstructionWizard(models.TransientModel):
             credit_branch = self.credit_operating_unit_id.id
             credit_sou = self.credit_sub_operating_unit_id.id if self.credit_sub_operating_unit_id else None
 
+        # vendor bill or vendor advance or vendor security returns name
+        ref_code = self.invoice_id.name or self.advance_id.name or self.security_return_id.name
         # Generate reconcile ref code for debit account
-        reconcile_ref = self.advance_id.get_reconcile_ref(self.debit_account_id.id, self.advance_id.name)
+        reconcile_ref = self.advance_id.get_reconcile_ref(self.debit_account_id.id, ref_code)
 
         self.env['payment.instruction'].create({
             'invoice_id': self.invoice_id.id or False,
