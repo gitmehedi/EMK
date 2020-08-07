f06bf261672cf878b5944544dbc8acf5e6099bf9
mehedi <mehedi1344@gmail.com>
2020-08-07 15:34:15 +0600

    [ADD]add reconcile reference in journal entries for vendor security return

--- account_mtbl/models/inherit_vendor_security_return.py
+++ account_mtbl/models/inherit_vendor_security_return.py
@@ -7,10 +7,21 @@ class VendorSecurityReturn(models.Model):
     def get_deposit_debit_item(self, deposit_line):
         res = super(VendorSecurityReturn, self).get_deposit_debit_item(deposit_line)
         res['sub_operating_unit_id'] = deposit_line.vsd_id.sub_operating_unit_id.id or False
+        res['reconcile_ref'] = deposit_line.vsd_id.reconcile_ref
         return res
 
     def get_supplier_credit_item(self, journal_id):
         res = super(VendorSecurityReturn, self).get_supplier_credit_item(journal_id)
         res['sub_operating_unit_id'] = self.partner_id.property_account_payable_sou_id.id or False
+        res['reconcile_ref'] = self.get_reconcile_ref(res['account_id'], res['ref'])
         return res
+
+    def get_reconcile_ref(self, account_id, ref):
+        # Generate reconcile ref code
+        reconcile_ref = None
+        account_obj = self.env['account.account'].search([('id', '=', account_id)])
+        if account_obj.reconcile:
+            reconcile_ref = account_obj.code + ref.replace('/', '')
+
+        return reconcile_ref
 
