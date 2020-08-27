856e176ee950dde812d729588af99f240aa6ac5d
aburayhan157 <aburayhan157@gmail.com>
2020-08-27 11:26:29 +0600

    [IMP] Implement Data Migration feature of Vendor Advance and Rent.

--- gbs_data_migration/models/__init__.py
+++ gbs_data_migration/models/__init__.py
@@ -3,3 +3,5 @@
 from . import gbs_data_migration_cob
 from . import gbs_fam_data_migration
 from . import gbs_coa_data_migration
+from . import gbs_va_data_migration
+from . import gbs_rent_data_migration
--- gbs_data_migration/wizards/__init__.py
+++ gbs_data_migration/wizards/__init__.py
@@ -1,3 +1,5 @@
 from . import gbs_data_migration_wizard
 from . import gbs_fam_data_migration_wizard
 from . import gbs_coa_data_migration_wizard
+from . import gbs_va_data_migration_wizard
+from . import gbs_rent_data_migration_wizard
