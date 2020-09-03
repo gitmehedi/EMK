e76f1e79f550927f574bea755a0d8efaee8616f0
aburayhan157 <aburayhan157@gmail.com>
2020-09-03 19:15:41 +0600

    [FIX] Fix duplicate vendor issue and add validation for vendor bank acc.

--- account_mtbl/models/inherit_res_partner.py
+++ account_mtbl/models/inherit_res_partner.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import Warning
 
 
 class ResPartner(models.Model):
@@ -6,6 +7,24 @@ class ResPartner(models.Model):
 
     property_account_payable_sou_id = fields.Many2one('sub.operating.unit', string='Sequence', copy=False,
                                                       track_visibility='onchange', required=True)
+
+    @api.constrains('name')
+    def _constrains_name(self):
+        # Check for Unique Name
+        if self.name:
+            vendor_id = self.search([('name', '=ilike', self.name.strip()), ('supplier', '=', True)])
+            if len(vendor_id.ids) > 0:
+                raise Warning('[Unique Error] Name must be unique!')
+
+    @api.constrains('vendor_bank_acc')
+    def _constrains_vendor_bank_acc(self):
+        if self.vendor_bank_acc and len(self.vendor_bank_acc) != 14:
+            raise Warning('Vendor Bank Account must be 14 digit.')
+
+    @api.onchange('vendor_bank_acc')
+    def _onchange_vendor_bank_acc(self):
+        if self.vendor_bank_acc and not self.vendor_bank_acc.isdigit():
+            raise Warning('Vendor Bank Account must be numeric!')
 
     @api.onchange('property_account_payable_id')
     def _onchange_property_account_payable_id(self):
