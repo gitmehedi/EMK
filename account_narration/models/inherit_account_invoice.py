2043174356ad809aceaf10051a379a3a192dbae0
aburayhan157 <aburayhan157@gmail.com>
2020-09-07 19:03:04 +0600

    [FIX] Fix 'if condition' of finalize_invoice_move_lines func and add a func in inherit_account_invoice for narration.

--- account_narration/models/inherit_account_invoice.py
+++ account_narration/models/inherit_account_invoice.py
@@ -8,3 +8,14 @@ class AccountInvoice(models.Model):
         res = super(AccountInvoice, self)._prepare_tax_line_vals(line, tax)
         res['name'] = line.name
         return res
+
+    # @api.multi
+    # def finalize_invoice_move_lines(self, move_lines):
+    #     aml = super(AccountInvoice, self).finalize_invoice_move_lines(move_lines)
+    #     for line in aml:
+    #         if line[2]['account_id'] == self.partner_id.property_account_payable_id.id:
+    #             # here, set the narration
+    #             line[2]['name'] = 'Custom'
+    #             break
+    #
+    #     return aml
