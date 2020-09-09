e6967c8c2d633a598e07e1608c2d25acd6483b08
gitmehedi <git.mehedi@gmail.com>
2020-09-09 17:51:53 +0600

    [FIX] Fix narration issue where VAT, TDS and Sundry Account narration will be first products narration.

--- account_narration/models/inherit_account_invoice.py
+++ account_narration/models/inherit_account_invoice.py
@@ -4,18 +4,14 @@ from odoo import models, fields, api, _
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    def _prepare_tax_line_vals(self, line, tax):
-        res = super(AccountInvoice, self)._prepare_tax_line_vals(line, tax)
-        res['name'] = line.name
-        return res
+    @api.multi
+    def finalize_invoice_move_lines(self, move_lines):
+        aml = super(AccountInvoice, self).finalize_invoice_move_lines(move_lines)
+        narration = None
+        for line in aml:
+            if not narration:
+                narration = line[2]['name']
+            if not line[2]['product_id']:
+                line[2]['name'] = narration
 
-    # @api.multi
-    # def finalize_invoice_move_lines(self, move_lines):
-    #     aml = super(AccountInvoice, self).finalize_invoice_move_lines(move_lines)
-    #     for line in aml:
-    #         if line[2]['account_id'] == self.partner_id.property_account_payable_id.id:
-    #             # here, set the narration
-    #             line[2]['name'] = 'Custom'
-    #             break
-    #
-    #     return aml
+        return aml
