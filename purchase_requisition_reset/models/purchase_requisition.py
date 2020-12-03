127d448e964bf75b9f692738a9dfd2e62feef8dd
aburayhan157 <aburayhan157@gmail.com>
2020-12-03 15:56:16 +0600

    [FIX] purchase_requisition_reset: Add new condition and change the position and color of Reset to Confirm button.
    Condition: If a RFQ is created using the PR, system will give a warning message to the user that you can not change the state.

--- purchase_requisition_reset/models/purchase_requisition.py
+++ purchase_requisition_reset/models/purchase_requisition.py
@@ -14,16 +14,30 @@ class PurchaseRequisition(models.Model):
             raise Warning(_('You can not reset to confirm state due to have quotation using this PR.'))
 
         sql_str = """SELECT
-                        o.id AS purchase_order_id
+                        r.id
                     FROM 
-                        purchase_order o
-                        JOIN purchase_rfq r ON r.id = o.rfq_id
+                        purchase_rfq r
                         JOIN purchase_requisition_purchase_rfq_rel l ON l.purchase_rfq_id = r.id
                     WHERE
-                        l.purchase_requisition_id=%s"""
+                        l.purchase_requisition_id=%s
+        """
+
         self.env.cr.execute(sql_str, (self.id,))
-        purchase_order_ids = self.env.cr.fetchall()
-        if len(purchase_order_ids) > 0:
-            raise Warning(_('You can not reset to confirm state due to have quotation using this PR through RFQ.'))
+        purchase_rfq_ids = self.env.cr.fetchall()
+        if len(purchase_rfq_ids) > 0:
+            raise Warning(_('You can not reset to confirm state due to have RFQ using this PR.'))
+
+        # sql_str = """SELECT
+        #                 o.id AS purchase_order_id
+        #             FROM
+        #                 purchase_order o
+        #                 JOIN purchase_rfq r ON r.id = o.rfq_id
+        #                 JOIN purchase_requisition_purchase_rfq_rel l ON l.purchase_rfq_id = r.id
+        #             WHERE
+        #                 l.purchase_requisition_id=%s"""
+        # self.env.cr.execute(sql_str, (self.id,))
+        # purchase_order_ids = self.env.cr.fetchall()
+        # if len(purchase_order_ids) > 0:
+        #     raise Warning(_('You can not reset to confirm state due to have quotation using this PR through RFQ.'))
 
         self.write({'state': 'in_progress'})
