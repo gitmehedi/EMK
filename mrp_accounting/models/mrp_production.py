c97d46c33a7cdd37ff2cf68f110962216e3d9b5a
aburayhan157 <aburayhan157@gmail.com>
2020-12-17 19:56:13 +0600

    [FIX] mrp_accounting: Add validation functionality

--- mrp_accounting/models/mrp_production.py
+++ mrp_accounting/models/mrp_production.py
@@ -1,5 +1,6 @@
 # imports of odoo
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
 
 
 class MrpProduction(models.Model):
@@ -9,12 +10,46 @@ class MrpProduction(models.Model):
     def button_mark_done(self):
         self.ensure_one()
         # Check Precondition
+        self.validate()
 
         res = super(MrpProduction, self).button_mark_done()
 
         self.do_accounting()
 
         return res
+
+    @api.multi
+    def validate(self):
+        message = ''
+        categ_names_of_inventory_valuation = set()
+        categ_names_of_stock_valuation_account = set()
+        categ_name_of_stock_journal = None
+
+        for move in self.move_raw_ids:
+            if move.product_id.product_tmpl_id.categ_id.property_valuation != 'manual_periodic':
+                categ_names_of_inventory_valuation.add(str(move.product_id.product_tmpl_id.categ_id.name))
+            if not move.product_id.product_tmpl_id.categ_id.property_stock_valuation_account_id.id:
+                categ_names_of_stock_valuation_account.add(str(move.product_id.product_tmpl_id.categ_id.name))
+
+        for move in self.move_finished_ids:
+            if move.product_id.product_tmpl_id.categ_id.property_valuation != 'manual_periodic':
+                categ_names_of_inventory_valuation.add(str(move.product_id.product_tmpl_id.categ_id.name))
+            if not move.product_id.product_tmpl_id.categ_id.property_stock_valuation_account_id.id:
+                categ_names_of_stock_valuation_account.add(str(move.product_id.product_tmpl_id.categ_id.name))
+
+        if not self.move_finished_ids[0].product_id.categ_id.property_stock_journal.id:
+            categ_name_of_stock_journal = str(self.move_finished_ids[0].product_id.product_tmpl_id.categ_id.name)
+
+        if categ_names_of_inventory_valuation:
+            message += _('- Inventory Valuation must be "Periodic" for %s category.\n') % str(tuple(categ_names_of_inventory_valuation))
+        if categ_names_of_stock_valuation_account:
+            message += _('- Stock Valuation Account is missing for %s category.\n') % str(tuple(categ_names_of_stock_valuation_account))
+        if categ_name_of_stock_journal:
+            message += _('- Stock Journal is missing for "%s" category.\n') % categ_name_of_stock_journal
+
+        if len(message) > 0:
+            message += _('Contact with Account group')
+            raise ValidationError(message)
 
     @api.multi
     def do_accounting(self):
