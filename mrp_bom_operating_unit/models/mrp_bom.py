ea69149e0878ac1601be007f0c4a9a5e6f24f8ff
aburayhan157 <aburayhan157@gmail.com>
2020-12-20 18:54:10 +0600

    [FIX] mrp_bom_operating_unit: Add validating condition.

--- mrp_bom_operating_unit/models/mrp_bom.py
+++ mrp_bom_operating_unit/models/mrp_bom.py
@@ -1,5 +1,6 @@
 # imports of odoo
 from odoo import models, fields, api, _
+from odoo.exceptions import Warning
 
 
 class MrpBom(models.Model):
@@ -9,6 +10,14 @@ class MrpBom(models.Model):
 
     @api.multi
     def action_confirm(self):
+        bom = self.env['mrp.bom'].search([('product_id', '=', self.product_id.id),
+                                          ('operating_unit_id', '=', self.operating_unit_id.id),
+                                          ('active', '=', True)])
+        if bom.ids:
+            raise Warning(_('Already BOM exists for "%s" product variant of "%s" operating unit.\n'
+                            'You are only allowed to create new version of "%s" product variant.')
+                          % (self.product_id.name, self.operating_unit_id.name, self.product_id.name))
+
         new_name = self.env['ir.sequence'].next_by_code_new('mrp.bom', None, self.operating_unit_id)
         self.write({
             'name': new_name,
