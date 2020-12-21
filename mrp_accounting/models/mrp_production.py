56950b8986af0624ecf9b71350214d27ff1e532d
mrbappy <mahfuz.bappy@gmail.com>
2020-12-21 16:41:15 +0600

    Update

--- mrp_accounting/models/mrp_production.py
+++ mrp_accounting/models/mrp_production.py
@@ -83,8 +83,9 @@ class MrpProduction(models.Model):
         for order in self:
             moves_raw_ids = order.move_raw_ids.filtered(lambda x: x.state in ('done'))
             moves_finish_ids = order.move_finished_ids.filtered(lambda x: x.state in ('done'))
-            raw_meterial_acc = self._get_account_id_value(moves_raw_ids)
-            finish_goods_acc = self._get_account_id_value(moves_finish_ids)
+            tatal_raw_value, raw_meterial_acc = self._get_account_id_value_for_raw_goods(moves_raw_ids)
+
+            finish_goods_acc = self._get_account_id_value_for_finish_goods(moves_finish_ids,tatal_raw_value)
 
             journal_id = moves_finish_ids[0].product_id.categ_id.property_stock_journal
 
@@ -92,20 +93,34 @@ class MrpProduction(models.Model):
 
             moves_finish_ids.product_price_update_()
 
-    def _get_account_id_value(self, moves_ids):
+    def _get_account_id_value_for_raw_goods(self, moves_ids):
         raw_meterial_acc_id = {}
-
+        tatal_raw_value  = 0
         for move in moves_ids:
             acc_id = move.product_id.product_tmpl_id.categ_id.property_stock_valuation_account_id.id
 
             total_price = move.product_uom_qty * move.price_unit
+            tatal_raw_value += total_price
 
             if acc_id in raw_meterial_acc_id.keys():
                 raw_meterial_acc_id[acc_id] = raw_meterial_acc_id[acc_id] + total_price
             else:
                 raw_meterial_acc_id[acc_id] = total_price
 
-        return raw_meterial_acc_id
+        return tatal_raw_value, raw_meterial_acc_id
+
+    def _get_account_id_value_for_finish_goods(self, moves_ids, tatal_raw_value):
+
+        finish_acc_id = {}
+        for move in moves_ids:
+            acc_id = move.product_id.product_tmpl_id.categ_id.property_stock_valuation_account_id.id
+
+            if acc_id in finish_acc_id.keys():
+                finish_acc_id[acc_id] = finish_acc_id[acc_id] + tatal_raw_value
+            else:
+                finish_acc_id[acc_id] = tatal_raw_value
+
+        return finish_acc_id
 
     def create_account_move_line(self, raw_meterial_acc, finish_goods_acc, journal_id, operating_unit_id):
 
