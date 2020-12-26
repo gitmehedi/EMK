b57c56e6643dd4f1f1bc77accdc0e634b5087153
mrbappy <mahfuzur.bappy@gmail.com>
2020-12-27 03:17:51 +0600

    Add MRO Access Modules

---
+++ mrp_gbs_access/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'GBS Samuda Manufacturing Access',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Manufacturing',
+    'version': '10.0.1.0.0',
+    'summary': """
+        This module will install for Samuda Manufacturing Access addons.""",
+
+    'description': """
+        This module will install for Samuda Manufacturing Access 
+    """,
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'mrp',
+        'mrp_operating_unit',
+        'mrp_extend',
+        'mrp_accounting',
+        'mrp_bom_extend',
+        'mrp_bom_version',
+        'mrp_bom_operating_unit'
+    ],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'security/ir_rule.xml',
+        'views/menu_items.xml',
+    ],
+}
