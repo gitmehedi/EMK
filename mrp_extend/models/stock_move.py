63d0f762d17fc112efbdd77e0db0d740f8fc72cc
aburayhan157 <aburayhan157@gmail.com>
2020-12-29 12:39:35 +0600

    [FIX] mrp_extend: Remove wizard

--- mrp_extend/models/stock_move.py
+++ mrp_extend/models/stock_move.py
@@ -6,7 +6,3 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     standard_qty = fields.Float(string='Standard Quantity', required=True, readonly=True)
-
-    @api.onchange('product_uom_qty')
-    def _onchange_product_uom_qty(self):
-        self.quantity_done = self.product_uom_qty
