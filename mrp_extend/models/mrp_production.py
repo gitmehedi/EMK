da1acfe2194fca966a1cf33fd1fc78f69ff1596a
aburayhan157 <aburayhan157@gmail.com>
2021-01-04 16:29:23 +0600

    [FIX] mrp_extend: Remove Standard Quantity field from MO line.

--- mrp_extend/models/mrp_production.py
+++ mrp_extend/models/mrp_production.py
@@ -18,33 +18,6 @@ class MrpProduction(models.Model):
 
         return res
 
-    def _generate_raw_move(self, bom_line, line_data):
-        move = super(MrpProduction, self)._generate_raw_move(bom_line, line_data)
-        product_uom_qty = move.product_uom_qty
-        move.write({'standard_qty': product_uom_qty})
-
-        return move
-
-    @api.multi
-    def _update_raw_move(self, bom_line, line_data):
-        # overwrite the base model function
-        quantity = line_data['qty']
-        self.ensure_one()
-        move = self.move_raw_ids.filtered(
-            lambda x: x.bom_line_id.id == bom_line.id and x.state not in ('done', 'cancel'))
-        if move:
-            if quantity > 0:
-                move[0].write({'standard_qty': quantity, 'product_uom_qty': quantity})
-            else:
-                if move[0].quantity_done > 0:
-                    raise UserError(_(
-                        'Lines need to be deleted, but can not as you still have some quantities to consume in them. '))
-                move[0].action_cancel()
-                move[0].unlink()
-            return move
-        else:
-            self._generate_raw_move(bom_line, line_data)
-
     @api.model
     def create(self, vals):
         if 'product_id' in vals:
--- mrp_extend/models/stock_move.py
+++
@@ -1,8 +0,0 @@
-# imports of odoo
-from odoo import models, fields, api, _
-
-
-class StockMove(models.Model):
-    _inherit = 'stock.move'
-
-    standard_qty = fields.Float(string='Standard Quantity', required=True, readonly=True)
