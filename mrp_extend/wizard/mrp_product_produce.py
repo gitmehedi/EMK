6f249f96191fa431485603894ddf86edf924c915
aburayhan157 <aburayhan157@gmail.com>
2021-01-13 13:16:24 +0600

    [FIX] mrp_extend: Fix quantity rounding issue.

--- mrp_extend/wizard/mrp_product_produce.py
+++ mrp_extend/wizard/mrp_product_produce.py
@@ -1,4 +1,10 @@
+# imports of python lib
+from datetime import datetime
+
+# imports of odoo
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.tools import float_compare, float_round
 
 
 class MrpProductProduce(models.TransientModel):
@@ -6,8 +12,29 @@ class MrpProductProduce(models.TransientModel):
 
     @api.multi
     def do_produce(self):
-        # execute the parent function
-        res = super(MrpProductProduce, self).do_produce()
+        """Override do_produce to remove rounding"""
+        # Nothing to do for lots since values are created using default data (stock.move.lots)
+        moves = self.production_id.move_raw_ids
+        quantity = self.product_qty
+        if float_compare(quantity, 0, precision_rounding=self.product_uom_id.rounding) <= 0:
+            raise UserError(_('You should at least produce some quantity'))
+        for move in moves.filtered(lambda x: x.product_id.tracking == 'none' and x.state not in ('done', 'cancel')):
+            if move.unit_factor:
+                move.quantity_done_store += quantity * move.unit_factor
+        moves = self.production_id.move_finished_ids.filtered(
+            lambda x: x.product_id.tracking == 'none' and x.state not in ('done', 'cancel'))
+        for move in moves:
+            if move.product_id.id == self.production_id.product_id.id:
+                move.quantity_done_store += quantity
+            elif move.unit_factor:
+                # byproducts handling
+                move.quantity_done_store += quantity * move.unit_factor
+        self.check_finished_move_lots()
+        if self.production_id.state == 'confirmed':
+            self.production_id.write({
+                'state': 'progress',
+                'date_start': datetime.now(),
+            })
 
         # check continue production
         done_moves = self.production_id.move_finished_ids.filtered(lambda x: x.state != 'cancel' and x.product_id.id == self.production_id.product_id.id)
@@ -30,4 +57,4 @@ class MrpProductProduce(models.TransientModel):
                 'context': self.env.context,
             }
 
-        return res
+        return {'type': 'ir.actions.act_window_close'}
