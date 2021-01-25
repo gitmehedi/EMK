eafa8eb353ea243f74c661f40b0e978aa9631e84
mrbappy <mahfuz.bappy@gmail.com>
2021-01-25 20:53:46 +0600

    Update

---
+++ mrp_bom_product_user/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MRP BOM Product User',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Manufacturing',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'mrp'
+    ],
+    'data': [
+        'views/product_users_views.xml',
+    ],
+    'summary': 'Relational Module Of Product and User for BOM/MO',
+    'description': """
+        By this moduel user account will mapped by Product. So that user only access Product BOM/MO who has mapped by user account. 
+    """,
+    'installable': True,
+    'application': False,
+
+
+}
---
+++ mrp_bom_product_user/models/product_users.py
@@ -0,0 +1,7 @@
+from odoo import fields, models, api,_
+from odoo.exceptions import ValidationError
+
+class InheritUsers(models.Model):
+    _inherit = "res.users"
+
+    product_ids = fields.Many2many('product.template', 'product_users_rel', 'user_id', 'product_id', 'Allow Products')
