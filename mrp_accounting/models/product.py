59a686e87e097dd42e2b01fa18e669b1078786f9
aburayhan157 <aburayhan157@gmail.com>
2021-01-26 17:41:56 +0600

    [REM] mrp_accounting: Remove 'manufacture_ok' field from 'product.template' model.

--- mrp_accounting/models/product.py
+++ mrp_accounting/models/product.py
@@ -5,6 +5,4 @@ from odoo import models, fields, api, _
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    manufacture_ok = fields.Boolean(string="Can be Manufactured", default=False,
-                                    help="This is used for Allowing Products in User Account")
     cogs_account_id = fields.Many2one('account.account', string="COGS Account")
