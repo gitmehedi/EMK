37c98abfa39f0847fbeb45df0de0e9a165428ba1
aburayhan157 <aburayhan157@gmail.com>
2021-02-09 12:47:50 +0600

    [MOD] mrp_bom_extend: Hide Unit Price, Sub total, Total field.

--- mrp_bom_extend/models/mrp_bom.py
+++ mrp_bom_extend/models/mrp_bom.py
@@ -11,12 +11,12 @@ class MrpBom(models.Model):
         ('confirmed', 'Confirmed'),
         ('historical', 'Historical')
     ], string='State', readonly=True, default='draft', track_visibility='onchange')
-    amount_total = fields.Float(string='Total', store=True, readonly=True, compute='_compute_amount')
+    # amount_total = fields.Float(string='Total', store=True, readonly=True, compute='_compute_amount')
 
-    @api.depends('bom_line_ids.price_subtotal')
-    def _compute_amount(self):
-        for rec in self:
-            rec.amount_total = sum(line.price_subtotal for line in rec.bom_line_ids)
+    # @api.depends('bom_line_ids.price_subtotal')
+    # def _compute_amount(self):
+    #     for rec in self:
+    #         rec.amount_total = sum(line.price_subtotal for line in rec.bom_line_ids)
 
     @api.constrains('product_id', 'product_tmpl_id', 'bom_line_ids')
     def _check_product_recursion(self):
@@ -32,15 +32,15 @@ class MrpBom(models.Model):
         if len(self.bom_line_ids.ids) <= 0:
             raise ValidationError(_("can not create Bill of Materials without Product line !!!"))
 
-        if self.bom_line_ids.ids:
-            product_names = set()
-            for line in self.bom_line_ids:
-                if line.price_unit <= 0:
-                    product_names.add(str(line.product_id.product_tmpl_id.name))
-
-            if len(product_names) > 0:
-                raise ValidationError(_('In Product line, the value of Unit Price must be greater than 0 '
-                                        'for the following product(s):\n => %s') % str(tuple(product_names))[1:-1])
+        # if self.bom_line_ids.ids:
+        #     product_names = set()
+        #     for line in self.bom_line_ids:
+        #         if line.price_unit <= 0:
+        #             product_names.add(str(line.product_id.product_tmpl_id.name))
+        #
+        #     if len(product_names) > 0:
+        #         raise ValidationError(_('In Product line, the value of Unit Price must be greater than 0 '
+        #                                 'for the following product(s):\n => %s') % str(tuple(product_names))[1:-1])
 
     @api.model
     def _bom_find(self, product_tmpl=None, product=None, picking_type=None, company_id=False):
@@ -65,24 +65,24 @@ class MrpBom(models.Model):
 class MrpBomLine(models.Model):
     _inherit = 'mrp.bom.line'
 
-    price_unit = fields.Float(string='Unit Price', store=True, readonly=True, compute='_compute_price_unit')
-    price_subtotal = fields.Float(string='Amount', store=True, readonly=True, compute='_compute_price')
+    # price_unit = fields.Float(string='Unit Price', store=True, readonly=True, compute='_compute_price_unit')
+    # price_subtotal = fields.Float(string='Amount', store=True, readonly=True, compute='_compute_price')
     categ_id = fields.Many2one('product.category', 'Internal Category', store=False, compute='_compute_categ_id')
 
     _sql_constraints = [
         ('bom_qty_zero', 'CHECK (product_qty>0)', 'All product quantities must be greater than 0.'),
     ]
 
-    @api.depends('product_id', 'product_uom_id')
-    def _compute_price_unit(self):
-        for rec in self:
-            if rec.product_id:
-                rec.price_unit = rec.product_id.uom_id._compute_price(rec.product_id.standard_price, rec.product_uom_id)
-
-    @api.depends('product_qty', 'price_unit')
-    def _compute_price(self):
-        for rec in self:
-            rec.price_subtotal = rec.price_unit * rec.product_qty
+    # @api.depends('product_id', 'product_uom_id')
+    # def _compute_price_unit(self):
+    #     for rec in self:
+    #         if rec.product_id:
+    #             rec.price_unit = rec.product_id.uom_id._compute_price(rec.product_id.standard_price, rec.product_uom_id)
+    #
+    # @api.depends('product_qty', 'price_unit')
+    # def _compute_price(self):
+    #     for rec in self:
+    #         rec.price_subtotal = rec.price_unit * rec.product_qty
 
     @api.depends('product_id')
     def _compute_categ_id(self):
