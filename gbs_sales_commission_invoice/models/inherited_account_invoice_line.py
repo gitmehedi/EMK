9cb87723a1621046bb94cf3a5696e069f5dd2e32
aburayhan157 <aburayhan157@gmail.com>
2021-02-28 16:44:54 +0600

    [FIX] gbs_sales_commission_invoice: Fix Vendor Bill KeyError.

--- gbs_sales_commission_invoice/models/inherited_account_invoice_line.py
+++ gbs_sales_commission_invoice/models/inherited_account_invoice_line.py
@@ -9,7 +9,6 @@ class InheritedAccountInvoiceLine(models.Model):
 
     is_commission_paid = fields.Boolean(string="Is Commission Paid?", default=False)
 
-
     def _update_commission_related_vals(self, so_obj):
 
         vals = {
@@ -26,10 +25,13 @@ class InheritedAccountInvoiceLine(models.Model):
     @api.depends('product_id', 'quantity')
     def _calculate_commission_amount(self):
         for inv in self:
+            # check invoice type
+            if inv.invoice_id.type == 'in_invoice':
+                break
+
             sale_order_pool = inv.env['sale.order'].search([('name', '=', inv.invoice_id.origin)])
 
             inv._update_commission_related_vals(sale_order_pool)
-
 
             commission = None
             for sale_line in sale_order_pool.order_line:
@@ -52,8 +54,6 @@ class InheritedAccountInvoiceLine(models.Model):
                             commission = commission * sale_order_pool.currency_id.rate
 
                 inv.commission_amount = commission
-
-
 
     commission_amount = fields.Float(string='Commission Amount', compute='_calculate_commission_amount',store=True)
 
