18424b81681da92f8e5e7e9143d375147555ed5c
aburayhan157 <aburayhan157@gmail.com>
2021-03-10 21:00:24 +0600

    [IMP] gbs_product: Add 'receive_over_ok' new field in product.template model.

--- gbs_product/models/inherit_product_template.py
+++ gbs_product/models/inherit_product_template.py
@@ -6,6 +6,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     cost_center_id = fields.Many2one('account.cost.center', string='Cost Center')
+    receive_over_ok = fields.Boolean(string='Can be Received Over', default=False, track_visibility='onchange')
 
     @api.constrains('default_code')
     def _check_default_code(self):
