fa43e8ec6e5617a87e0dc702f88e642ff9c17024
aburayhan157 <aburayhan157@gmail.com>
2021-06-21 19:46:27 +0600

    [IMP] gbs_account_journal_log: Generate log for 'Cancel Entry' operation of Journal Entries.

--- gbs_account_journal_log/models/inherit_account_move.py
+++ gbs_account_journal_log/models/inherit_account_move.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
 
 
 class InheritAccountMove(models.Model):
@@ -15,3 +16,18 @@ class InheritAccountMove(models.Model):
                        default=fields.Date.context_today, track_visibility='onchange')
     journal_id = fields.Many2one('account.journal', string='Journal', required=True,
                                  states={'posted': [('readonly', True)]}, default=_get_default_journal, track_visibility='onchange')
+
+    @api.multi
+    def button_cancel(self):
+        for move in self:
+            if not move.journal_id.update_posted:
+                raise UserError(_(
+                    'You cannot modify a posted entry of this journal.\nFirst you should set the journal to allow cancelling entries.'))
+        if self.ids:
+            self.check_access_rights('write')
+            self.check_access_rule('write')
+            self._check_lock_date()
+            self.write({'state': 'draft'})
+            self.invalidate_cache()
+        self._check_lock_date()
+        return True
