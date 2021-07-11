ccfc52a298442402b9fa9510b16c7048b20233db
aburayhan157 <aburayhan157@gmail.com>
2021-07-11 18:07:02 +0600

    [FIX] gbs_samuda_sales_return: Fix the issue which are mentioned in email.

--- gbs_samuda_sales_return/wizard/stock_picking_return.py
+++ gbs_samuda_sales_return/wizard/stock_picking_return.py
@@ -7,17 +7,19 @@ from odoo.exceptions import UserError, ValidationError
 class ReturnPicking(models.TransientModel):
     _inherit = 'stock.return.picking'
 
-    price_unit = fields.Float(string='Unit Price', digits=dp.get_precision('Product Price'))
+    def _get_default_price_unit(self):
+        picking = self.env['stock.picking'].browse(self.env.context['active_id'])
+        amount = sum(line.credit for line in picking.cogs_move_id.line_ids)
+        price_unit = amount / picking.pack_operation_product_ids[0].qty_done
+
+        return price_unit
+
+    price_unit = fields.Float(string='COGS Unit Price', digits=dp.get_precision('Product Price'), default=_get_default_price_unit)
 
     @api.constrains('product_return_moves')
     def _check_quantity(self):
         if any(move.quantity < 0 for move in self.product_return_moves):
             raise ValidationError(_("Quantity cannot be negative"))
-
-    @api.constrains('price_unit')
-    def _check_price_unit(self):
-        if self.price_unit <= 0:
-            raise ValidationError(_("Unit price must be greater than zero"))
 
     @api.model
     def default_get(self, fields):
@@ -44,12 +46,19 @@ class ReturnPicking(models.TransientModel):
         new_picking = self.env['stock.picking'].browse(new_picking_id)
 
         if new_picking.picking_type_id.code == 'outgoing_return':
+
+            for pack in new_picking.pack_operation_ids:
+                if pack.product_qty > 0:
+                    pack.write({'qty_done': pack.product_qty})
+
+            new_picking.write({'date_done': picking.date_done})
+
             new_picking.do_transfer()
             new_picking.button_ac_approve()
             new_picking.button_approve()
 
             # reverse cogs entry
-            move = self.env['account.move'].search([('ref', 'ilike', picking.name)])
+            move = self.env['account.move'].search([('id', '=', picking.cogs_move_id.id)])
             if move:
                 self.do_cogs_reverse_accounting(new_picking, move)
 
@@ -58,10 +67,10 @@ class ReturnPicking(models.TransientModel):
                                                          ('picking_type_id', '=', picking.picking_type_id.id),
                                                          ('state', '=', 'confirmed')])
             if existing_dc:
-                existing_dc.move_lines[0].product_uom_qty += new_picking.pack_operation_product_ids[0].product_qty
+                existing_dc.move_lines[0].product_uom_qty += new_picking.pack_operation_product_ids[0].qty_done
             else:
                 new_dc = picking.copy()
-                new_dc.move_lines[0].product_uom_qty = new_picking.pack_operation_product_ids[0].product_qty
+                new_dc.move_lines[0].product_uom_qty = new_picking.pack_operation_product_ids[0].qty_done
                 new_dc.action_confirm()
 
         return new_picking_id, picking_type_id
@@ -71,8 +80,8 @@ class ReturnPicking(models.TransientModel):
         stock_pack_operation = picking.pack_operation_product_ids[0]
 
         ref = "reversal of: " + move.name
-        amount = self.price_unit * stock_pack_operation.product_qty
-        name = product.display_name + "; Rate: " + str(self.price_unit) + "; Qty: " + str(stock_pack_operation.product_qty)
+        amount = self.price_unit * stock_pack_operation.qty_done
+        name = product.display_name + "; Rate: " + str(self.price_unit) + "; Qty: " + str(stock_pack_operation.qty_done)
 
         move_of_reverse_cogs = move.copy(default={'ref': ref})
         for aml in move_of_reverse_cogs.line_ids:
