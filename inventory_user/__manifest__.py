a352968d8818ae0fdaf585b14f4de1316dca3536
shakibmist <shakibmist@gmail.com>
2021-09-08 13:13:44 +0600

    [ADD] Define all users and category for inventory module.

---
+++ inventory_user/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Inventory Management Users',
+    'description': """ All types of users whom are related to activities of inventory management""",
+    "author": "Genweb2 Limited",
+    "website": "http://www.genweb2.com",
+    'version': '1.0',
+    'category': 'Inventory Management',
+    'depends': [
+
+    ],
+    'data': [
+        'security/users.xml',
+    ],
+    'installable': True,
+}
+
