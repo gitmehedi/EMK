6bf1b0157c3d6ca89eab6dd6fc17d1b55a5dbf5a
gitmehedi <git.mehedi@gmail.com>
2021-09-09 11:30:59 +0600

    [ADD] Add new module for menu representation.

---
+++ emk_menu/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'EMK Menu',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'EMK Menu',
+    'version': '10.0.1.0.0',
+    'category': 'Menu',
+    'summary': '',
+    'description': '',
+    'depends': [
+        'hr',
+        'hr_payroll',
+        'hr_holidays',
+        'hr_recruitment',
+        'hr_attendance',
+        'stock',
+        'calendar',
+        'mail',
+    ],
+    'data': [
+        'views/menu_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
