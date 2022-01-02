80b107b1a5fcb050926941fea24e0c0917cd2bd3
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2022-01-02 18:19:57 +0600

    Unit Cost, Total Cost added on mrp reports

--- mrp_reports/reports/production_report_xlsx.py
+++ mrp_reports/reports/production_report_xlsx.py
@@ -26,6 +26,7 @@ class ProductionReportXLSX(ReportXlsx):
         sheet = workbook.add_worksheet(report_name)
         sheet.set_column(0, 1, 14)
         sheet.set_column(2, 2, 46)
+        sheet.set_column(5, 5, 24)
         sheet.set_column(3, 4, 14)
         # FORMAT
         bold = workbook.add_format({'bold': True, 'size': 10})
@@ -43,17 +44,33 @@ class ProductionReportXLSX(ReportXlsx):
         total_format = workbook.add_format({'num_format': '#,###0.00', 'bold': True, 'size': 7, 'border': 1})
 
         # SHEET HEADER
-        sheet.merge_range(0, 0, 0, 4, self.env.user.company_id.name, name_format)
-        sheet.merge_range(1, 0, 1, 4, self.env.user.company_id.street, address_format)
-        sheet.merge_range(2, 0, 2, 4, self.env.user.company_id.street2, address_format)
-        sheet.merge_range(3, 0, 3, 4, self.env.user.company_id.city + '-' + self.env.user.company_id.zip,
-                          address_format)
-        sheet.merge_range(4, 0, 4, 4, "Production Report", name_format)
+        if self.env.user.has_group('account.group_account_user'):
+            sheet.merge_range(0, 0, 0, 6, self.env.user.company_id.name, name_format)
+            sheet.merge_range(1, 0, 1, 6, self.env.user.company_id.street, address_format)
+            sheet.merge_range(2, 0, 2, 6, self.env.user.company_id.street2, address_format)
+            sheet.merge_range(3, 0, 3, 6, self.env.user.company_id.city + '-' + self.env.user.company_id.zip,
+                              address_format)
+        else:
+            sheet.merge_range(0, 0, 0, 4, self.env.user.company_id.name, name_format)
+            sheet.merge_range(1, 0, 1, 4, self.env.user.company_id.street, address_format)
+            sheet.merge_range(2, 0, 2, 4, self.env.user.company_id.street2, address_format)
+            sheet.merge_range(3, 0, 3, 4, self.env.user.company_id.city + '-' + self.env.user.company_id.zip,
+                              address_format)
+        if self.env.user.has_group('account.group_account_user'):
+            sheet.merge_range(4, 0, 4, 6, "Production Report", name_format)
+        else:
+            sheet.merge_range(4, 0, 4, 4, "Production Report", name_format)
         product_name = obj.product_id.name_get()[0][1]
 
         sheet.merge_range(5, 0, 5, 2, "Product : " + product_name, name_format_left)
-        sheet.merge_range(5, 3, 5, 4, "Date : " + ReportUtility.get_date_from_string(
-            obj.date_from) + ' to ' + ReportUtility.get_date_from_string(obj.date_to), name_format_left)
+        if self.env.user.has_group('account.group_account_user'):
+            sheet.merge_range(5, 3, 5, 6, "Date : " + ReportUtility.get_date_from_string(
+                obj.date_from) + ' to ' + ReportUtility.get_date_from_string(obj.date_to), name_format_left)
+        else:
+
+            sheet.merge_range(5, 3, 5, 4, "Date : " + ReportUtility.get_date_from_string(
+                obj.date_from) + ' to ' + ReportUtility.get_date_from_string(obj.date_to), name_format_left)
+
         sheet.merge_range(6, 0, 6, 2, "Operating Unit : " + obj.operating_unit_id.name, name_format_left)
 
         sheet.write(8, 0, "Total Production (MT)", header_format_left)
@@ -62,6 +79,10 @@ class ProductionReportXLSX(ReportXlsx):
         sheet.write(8, 2, "Raw / Packing Materials", header_format_left)
         sheet.write(8, 3, "Quantity", header_format_left)
         sheet.write(8, 4, "Unit", header_format_left)
+
+        if self.env.user.has_group('account.group_account_user'):
+            sheet.write(8, 5, "Unit Cost", header_format_left)
+            sheet.write(8, 6, "Total Cost", header_format_left)
 
         sql = '''
         
@@ -72,6 +93,7 @@ class ProductionReportXLSX(ReportXlsx):
                t1.product_name,
                t1.product_uom,
                t1.name,
+               t1.cost,
                COALESCE(COALESCE(t1.total_qty, 0) - COALESCE(t2.total_qty, 0), 0) as after_total_qty,
                COALESCE(t1.total_qty, 0) as production_qty,
                COALESCE(t2.total_qty, 0) as unbuild_qty 
@@ -88,7 +110,13 @@ class ProductionReportXLSX(ReportXlsx):
                         ELSE
                            'consume' 
                      END
-                     AS type, sm.product_uom, uom.name, SUM(sm.quantity_done_store) AS total_qty 
+                     AS type, sm.product_uom, uom.name, SUM(sm.quantity_done_store) AS total_qty,
+                      Coalesce((SELECT ph.cost
+                                             FROM   product_price_history ph
+                                             WHERE  ph.datetime + interval'6h' <= DATE('%s')+TIME '00:00:01'
+                                                    AND  sm.product_id = ph.product_id
+                                             ORDER  BY ph.datetime DESC,ph.id DESC
+                                             LIMIT  1), 0) as cost  
                   FROM
                      stock_move sm 
                      JOIN
@@ -106,6 +134,7 @@ class ProductionReportXLSX(ReportXlsx):
                         ON pt.id = pp.product_tmpl_id 
                   WHERE
                      mp.product_id = %s 
+                     AND mp.state = 'done'
                      AND mp.date_planned_start + interval'6h' BETWEEN DATE('%s')+TIME '00:00:01' AND DATE('%s')+TIME '23:59:59' 
                      AND sm.state = 'done' 
                      AND mp.operating_unit_id = %s
@@ -128,7 +157,15 @@ class ProductionReportXLSX(ReportXlsx):
                            ELSE
                               'consume' 
                         END
-                        AS type, sm.product_uom, uom.name, SUM(sm.quantity_done_store) AS total_qty 
+                        AS type, sm.product_uom, uom.name, SUM(sm.quantity_done_store) AS total_qty,
+                          Coalesce((SELECT ph.cost
+                                             FROM   product_price_history ph
+                                             WHERE  ph.datetime + interval'6h' <= DATE('%s')+TIME '00:00:01'
+                                                    AND  sm.product_id = ph.product_id
+                                             ORDER  BY ph.datetime DESC,ph.id DESC
+                                             LIMIT  1), 0)  as cost
+                  
+                         
                      FROM
                         stock_move sm 
                         JOIN
@@ -146,6 +183,7 @@ class ProductionReportXLSX(ReportXlsx):
                            ON pt.id = pp.product_tmpl_id 
                      WHERE
                         sm.state = 'done' 
+                        AND mu.state = 'done'
                         AND mu.date_unbuild BETWEEN '%s' AND '%s'
                         and mu.product_id = %s 
                         AND mu.operating_unit_id = %s
@@ -156,19 +194,26 @@ class ProductionReportXLSX(ReportXlsx):
                   )
                   t2 
                   ON (t1.product_id = t2.product_id);
-        ''' % (obj.product_id.id, obj.date_from, obj.date_to, obj.operating_unit_id.id, obj.date_from, obj.date_to,
+        ''' % (obj.date_from, obj.product_id.id, obj.date_from, obj.date_to, obj.operating_unit_id.id, obj.date_from,
+               obj.date_from, obj.date_to,
                obj.product_id.id, obj.operating_unit_id.id)
 
         self.env.cr.execute(sql)
         row_no = 9
         initial_row = row_no
         production_total_qty = 0
+        cost_sum = 0
+
         for vals in self.env.cr.dictfetchall():
             if not vals['production_type'] == 'produce':
                 sheet.write(row_no, 2, vals['product_name'], normal_format_left)
                 sheet.write(row_no, 3, vals['after_total_qty'], normal_format_left_comma_separator)
                 sheet.write(row_no, 4, vals['name'], normal_format_left)
+                if self.env.user.has_group('account.group_account_user'):
+                    sheet.write(row_no, 5, vals['cost'], normal_format_left_comma_separator)
+                    sheet.write(row_no, 6, vals['after_total_qty'] * vals['cost'], normal_format_left_comma_separator)
                 row_no = row_no + 1
+                cost_sum = cost_sum + vals['after_total_qty'] * vals['cost']
             if vals['production_type'] == 'produce':
                 production_total_qty = vals['after_total_qty']
         if initial_row == row_no:
@@ -235,6 +280,19 @@ class ProductionReportXLSX(ReportXlsx):
         else:
             sheet.merge_range(initial_row, 1, row_no - 1, 1, delivery_quantity, merged_format_center)
 
+        if self.env.user.has_group('account.group_account_user'):
+            sheet.write(row_no + 1, 5, 'Total Cost of Raw Materials', normal_format_left)
+            sheet.write(row_no + 1, 6, cost_sum, normal_format_left_comma_separator)
+
+            sheet.write(row_no + 2, 5, 'Cost for ' + str(production_total_qty) + ' MT', normal_format_left_comma_separator)
+            sheet.write(row_no + 2, 6, cost_sum, normal_format_left_comma_separator)
+
+            sheet.write(row_no + 3, 5, 'Unit Cost',
+                        normal_format_left_comma_separator)
+            if production_total_qty > 0:
+                sheet.write(row_no + 3, 6, cost_sum/production_total_qty, normal_format_left_comma_separator)
+            else:
+                sheet.write(row_no + 3, 6, '', normal_format_left_comma_separator)
         data['name'] = 'Production Report'
 
 
