d91c9979cc3a4abe355423e6a4f52d6999b8e470
Aynul Islam <aynulislam>
2022-01-30 17:44:50 +0600

    generic module create for excel upload

---
+++ gbs_read_excel_utility/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Excel Read Utility",
+    "summary": "Use For Excel Read Utility Function ",
+    "version": "10.0.0.1",
+    'author': 'Genweb2 Limited',
+    'website': 'http://www.genweb2.com',
+    "category": "Generic",
+    "depends": [
+    ],
+    "data": [
+
+],
+    'installable': True,
+    'application': False,
+}
