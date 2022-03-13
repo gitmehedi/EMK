3387f78f496bb73352bc3a295816b76c4709e2ce
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2022-03-13 17:06:42 +0600

    product menu access on inventory

---
+++ gbs_samuda_inventory_product/models/inherited_product_template.py
@@ -0,0 +1,33 @@
+from odoo import fields, models, api
+from lxml import etree
+
+
+class InheritedProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(InheritedProductTemplate, self).fields_view_get(view_id=view_id, view_type=view_type,
+                                                                    toolbar=toolbar,
+                                                                    submenu=submenu)
+
+        doc = etree.XML(res['arch'])
+        no_create_edit_button = self.env.context.get('no_create_edit_button')
+
+        if no_create_edit_button:
+            if view_type == 'form' or view_type == 'kanban' or view_type == 'tree':
+                for node_form in doc.xpath("//kanban"):
+                    node_form.set("create", 'false')
+                    node_form.set("edit", 'false')
+                for node_form in doc.xpath("//tree"):
+                    node_form.set("create", 'false')
+                    node_form.set("edit", 'false')
+                for node_form in doc.xpath("//form"):
+                    node_form.set("create", 'false')
+                    node_form.set("edit", 'false')
+
+        res['arch'] = etree.tostring(doc)
+        return res
+
+
+
