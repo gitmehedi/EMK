96d647ea82f318e47301724ff279541d2453cfb9
Kamrul Hasan <kamrul.devcs@gmail.com>
2022-03-16 12:24:13 +0600

    check status before approve action on LC collection

---
+++ hr_payroll_ot/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'HR Payroll Overtime',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'HR Payroll',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'hr_payroll',
+        ],
+    'data': [
+    ],
+
+    'summary': 'This Module Generate Payroll overtime Summary',
+
+    'installable': True,
+    'application': False,
+}
---
+++ hr_payroll_ot/models/inherited_hr_payslip.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+class InheritedPaySlipsBatch(models.Model):
+    _inherit = 'hr.payslip.run'
+
+    type = fields.Selection(selection_add=[('2', 'OT')])
+
+
+class InheritHRPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+    type = fields.Selection(selection_add=[('2', 'OT')])
