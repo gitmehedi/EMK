17a64a16a72eaf83f1887366638c486f78e1f841
aynulislam <itamxeed7@gmail.com>
2022-03-30 18:32:02 +0600

    employee work address and home address view custom created

--- hr_employee_additional_info/__manifest__.py
+++ hr_employee_additional_info/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    "name": "Employee Blood Group",
+    "name": "Employee Additional Information",
+    'description': 'Additional Information for employee profile',
     "author": "Genweb2 Ltd.",
     "website": "http://www.genweb2.com",
     "category": "Human Resources",
-    "depends": ["hr"],
+    "depends": ["hr", "web"],
     "data": [
+        "views/address_partner_view.xml",
         "views/hr_emplyee_blood_group_view.xml",
     ],
 
