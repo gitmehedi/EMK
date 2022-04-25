5fe20780e836c26ecc84c4201879a99eeab062ba
aynulislam <itamxeed7@gmail.com>
2022-04-25 12:35:51 +0600

    other deduction ot line and arrear ot line are move to line files

--- hr_other_deduction_ot/models/__init__.py
+++ hr_other_deduction_ot/models/__init__.py
@@ -1,2 +1,3 @@
 from . import hr_other_deduction_ot
+from . import hr_other_deduction_ot_line
 from . import inherited_hr_payslip
--- hr_payroll_arrear_ot/models/__init__.py
+++ hr_payroll_arrear_ot/models/__init__.py
@@ -1,2 +1,3 @@
 from . import hr_payroll_arrear_ot
+from . import hr_payroll_arrear_ot_line
 from . import inherited_hr_payslip
