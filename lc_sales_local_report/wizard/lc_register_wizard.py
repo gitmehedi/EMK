9cda1c4a34154dbda3b603e66c7071c7ea736663
Kamrul Hasan <kamrul.devcs@gmail.com>
2022-05-10 16:33:57 +0600

    added new selection on type dropdown

--- lc_sales_local_report/wizard/lc_register_wizard.py
+++ lc_sales_local_report/wizard/lc_register_wizard.py
@@ -3,7 +3,10 @@ from odoo import fields, models, api
 class LcRegisterWizard(models.Model):
     _name = 'lc.register.wizard'
 
-    filter_by = fields.Selection([('goods_delivered_doc_not_prepared', 'Goods Delivered but doc. not prepared'), ('first_acceptance', '1\'st Acceptance'), ('second_acceptance', '2nd Acceptance'), ('maturated_but_payment_not_done', 'Maturated but payment not done')])
+    filter_by = fields.Selection([('goods_delivered_doc_not_prepared', 'Goods Delivered but doc. not prepared'),
+                                  ('first_acceptance', '1\'st Acceptance'), ('second_acceptance', '2nd Acceptance'),
+                                  ('maturated_but_amount_not_collect', 'Matured but Amount not collected'),
+                                  ('goods_delivered_but_lc_not_received', 'Goods Delivered but LC not received')])
     acceptance_default_value = fields.Char(string='Default')
     type = fields.Selection([('all', 'All'), ('local', 'Local'), ('foreign', 'Foreign')], default='all')
     is_type_hide = fields.Boolean(string='is type hide', default=False)
