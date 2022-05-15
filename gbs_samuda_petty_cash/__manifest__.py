4e36ee92af2200dda00f5c3bbcb09144210177f6
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2022-05-15 18:17:30 +0600

    petty cash added

--- gbs_samuda_petty_cash/__manifest__.py
+++ gbs_samuda_petty_cash/__manifest__.py
@@ -1,14 +1,17 @@
 {
-    'name': 'Excel Report For Petty Cash',
+    'name': 'Samuda Petty Cash',
     'author': 'Genweb2 Limited',
     'website': 'www.genweb2.com',
     'category': 'Gbs Samuda Petty Cash',
     'version': '10.0.1.0.0',
     'depends': [
-        'account','base', 'report_xlsx', 'report'
-               ],
+        'account', 'base', 'report_xlsx', 'report', 'gbs_application_group'
+    ],
 
     'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/account.xml',
         'views/inherited_acc_bank_statement_button_add_view.xml',
         'report/petty_cash_report_xlsx_view.xml'
 
--- gbs_samuda_petty_cash/report/petty_cash_report_xlsx.py
+++ gbs_samuda_petty_cash/report/petty_cash_report_xlsx.py
@@ -1,11 +1,5 @@
 from odoo.report import report_sxw
 from odoo.addons.report_xlsx.report.report_xlsx import ReportXlsx
-from odoo.tools.misc import formatLang
-from odoo.tools import float_compare, float_round
-from collections import OrderedDict
-import operator, math, locale
-from odoo.exceptions import UserError, ValidationError
-from odoo import api, fields, models, _
 
 
 class PettyCashReportXLSX(ReportXlsx):
@@ -15,10 +9,11 @@ class PettyCashReportXLSX(ReportXlsx):
         report_name = 'Petty Cash Report'
         sheet = workbook.add_worksheet(report_name)
         header_bold = workbook.add_format(
-            {'font_size': 12, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#FFE0E3', 'bold': True, 'border': 1, 'size':10})
-        no_format = workbook.add_format({'font_size': 12,'border': 1,'num_format': '#,###0.00' })
-        normal = workbook.add_format({'font_size': 12,'border': 1 })
-        no_format_bold = workbook.add_format({'font_size': 12,'border': 1, 'bold': True, 'num_format': '#,###0.00' })
+            {'font_size': 12, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#FFE0E3', 'bold': True, 'border': 1,
+             'size': 10})
+        no_format = workbook.add_format({'font_size': 12, 'border': 1, 'num_format': '#,###0.00'})
+        normal = workbook.add_format({'font_size': 12, 'border': 1})
+        no_format_bold = workbook.add_format({'font_size': 12, 'border': 1, 'bold': True, 'num_format': '#,###0.00'})
 
         # bg_normal_bordered = workbook.add_format({'font_size': 8, 'bg_color': '#78B0DE', 'border': 1})
         # no_format = workbook.add_format({'num_format': '#,###0.00', 'font_size': 8})
@@ -65,7 +60,6 @@ class PettyCashReportXLSX(ReportXlsx):
         col = 3
         sheet.write(row, col, "Total", no_format_bold)
         sheet.write(row, col + 1, obj.balance_end, no_format_bold)
-
 
 
 PettyCashReportXLSX('report.gbs_samuda_petty_cash.petty_cash_report_xlsx',
