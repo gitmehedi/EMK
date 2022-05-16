3721281af3e67c707c00ef98bbec09547a35f882
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2022-05-16 15:28:01 +0600

    quantity load from po line to bill line

--- gbs_samuda_analytic_vendor_bills/models/inherited_account_invoice.py
+++ gbs_samuda_analytic_vendor_bills/models/inherited_account_invoice.py
@@ -8,6 +8,10 @@ class InheritedAccountInvoice(models.Model):
     def _prepare_invoice_line_from_po_line(self, line):
         """ Override parent's method to add lc analytic account on invoice line"""
         invoice_line = super(InheritedAccountInvoice, self)._prepare_invoice_line_from_po_line(line)
+
+        if line.product_qty:
+            invoice_line.update({'quantity': line.product_qty})
+
         if self.type == 'in_invoice' and self.purchase_id:
             if self.purchase_id.region_type == 'foreign' or self.purchase_id.is_service_order:
                 if self.purchase_id.lc_ids:
@@ -28,29 +32,3 @@ class InheritedAccountInvoice(models.Model):
                 invoice_line.update({'account_id': account_conf_pool.lc_pad_account.id})  # update the dictionary
 
         return invoice_line
-
-    # @api.model
-    # def create(self, values):
-    #     res = super(InheritedAccountInvoice, self).create(values)
-    #     if 'invoice_line_ids' in values:
-    #         for line in res.invoice_line_ids:
-    #             print(line)
-    #             if not line.account_analytic_id:
-    #                 if line.account_id.analytic_account_required:
-    #                     raise UserError(
-    #                         _("Analytic Account not set for Account : %s" % line.account_id.name))
-    #
-    #     return res
-    #
-    # def write(self, values):
-    #     res = super(InheritedAccountInvoice, self).write(values)
-    #     if 'invoice_line_ids' in values:
-    #         for line in values['invoice_line_ids']:
-    #             print(line)
-    #             if not line.account_analytic_id:
-    #                 if line.account_id.analytic_account_required:
-    #                     raise UserError(
-    #                         _("Analytic Account not set for Account : %s" % line.account_id.name))
-    #
-    #     return res
-
