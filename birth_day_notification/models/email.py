d98f31c9c6a4be2172fefef24d80322f06f75f43
gitmehedi <git.mehedi@gmail.com>
2022-05-26 13:12:52 +0600

    [FIX] Fix e-mail issue on Date of Anniversary in EMK Live.

--- birth_day_notification/models/email.py
+++ birth_day_notification/models/email.py
@@ -10,7 +10,7 @@ class ReminderVisa(models.Model):
     anniversary = fields.Date('Date of Anniversary')
 
     @api.model
-    def mail_reminder(self):
+    def birthday_mail_reminder(self):
         today = datetime.now()
         employees = self.env['hr.employee'].search([])
         for i in employees:
@@ -22,24 +22,8 @@ class ReminderVisa(models.Model):
 
     @api.model
     def send_birthday_wish(self, emp_id):
-        su_id = self.env['res.partner'].browse(SUPERUSER_ID)
-        template_id = self.env['ir.model.data'].get_object_reference('birth_day_notification',
-                                                                     'birthday_notification')[1]
-        template_browse = self.env['mail.template'].browse(template_id)
-        email_to = self.env['hr.employee'].browse(emp_id).work_email
-        if template_browse:
-            values = template_browse.generate_email(emp_id, fields=None)
-            values['email_to'] = email_to
-            values['email_from'] = su_id.email
-            values['res_id'] = False
-            if not values['email_to'] and not values['email_from']:
-                pass
-            mail_mail_obj = self.env['mail.mail']
-            msg_id = mail_mail_obj.create(values)
-            if msg_id:
-                mail_mail_obj.send(msg_id)
-            return True
-
+        tmpl_id = 'birth_day_notification.birthday_notification'
+        return self.send_wish_email(emp_id, tmpl_id)
 
     @api.model
     def anniversary_mail_reminder(self):
@@ -47,27 +31,25 @@ class ReminderVisa(models.Model):
         employees = self.env['hr.employee'].search([])
         for i in employees:
             if i.anniversary:
-                daymonth = datetime.strptime(i.birthday, "%Y-%m-%d")
+                daymonth = datetime.strptime(i.anniversary, "%Y-%m-%d")
                 if today.day == daymonth.day and today.month == daymonth.month:
                     self.send_anniversary_wish(i.id)
         return
 
     @api.model
     def send_anniversary_wish(self, emp_id):
+        tmpl_id = 'birth_day_notification.anniversary_wish_notify'
+
+        return self.send_wish_email(emp_id,tmpl_id)
+
+    def send_wish_email(self,emp_id,tmpl_id):
         su_id = self.env['res.partner'].browse(SUPERUSER_ID)
-        template_id = self.env['ir.model.data'].get_object_reference('birth_day_notification',
-                                                                     'anniversary_notification')[1]
-        template_browse = self.env['mail.template'].browse(template_id)
-        email_to = self.env['hr.employee'].browse(emp_id).work_email
-        if template_browse:
-            values = template_browse.generate_email(emp_id, fields=None)
-            values['email_to'] = email_to
-            values['email_from'] = su_id.email
-            values['res_id'] = False
-            if not values['email_to'] and not values['email_from']:
-                pass
-            mail_mail_obj = self.env['mail.mail']
-            msg_id = mail_mail_obj.create(values)
-            if msg_id:
-                mail_mail_obj.send(msg_id)
-            return True
+        template = self.env.ref(tmpl_id, raise_if_not_found=False)
+        employee = self.env['hr.employee'].browse(emp_id)
+        context = {
+            'res_id': False,
+            'lang': self.env.user.lang,
+        }
+
+        template.with_context(context).send_mail(emp_id, force_send=True, raise_exception=True)
+
