8ab31373d5fd9e6d5e6d347e7ea0de8dc9f59af8
aynulislam <itamxeed7@gmail.com>
2022-06-06 15:35:18 +0600

    item transfer challan report modified and prepare by added

--- item_transfer_process/report/item_transfer_challan_report.py
+++ item_transfer_process/report/item_transfer_challan_report.py
@@ -13,24 +13,29 @@ class TransferChallanReport(models.AbstractModel):
         lending_run_pool = self.env['item.loan.lending']
         docs = lending_run_pool.browse(data.get('active_id'))
         ReportUtility = self.env['report.utility']
+        employee_information = self.env['employee.information.from.user'].get_employee_information(
+        docs.issuer_id.id)
+        employee_name = (employee_information[0])
         item_lists = []
 
         data = {}
-        data['request_date'] = ReportUtility.getERPDateFormat(ReportUtility.getDateTimeFromStr(docs.request_date))
         data['name'] = docs.name
         data['operating_unit_id'] = docs.operating_unit_id.name
         data['receiving_operating_unit_id'] = docs.receiving_operating_unit_id.name
         data['company_id'] = docs.company_id.name
-        data['transfer_date'] = ReportUtility.getERPDateFormat(ReportUtility.getDateTimeFromStr( docs.picking_id.date_done))
+        data['employee_name'] = employee_name
         picking_id = docs.picking_id
         move_ids = self.env['stock.move'].search([('picking_id', '=', picking_id.id)])
 
         for move in move_ids:
+            data['transfer_date'] = ReportUtility.getERPDateFormat(ReportUtility.getDateTimeFromStr(move.date))
             list_obj = {}
-            list_obj['product_id']= move.product_id.name
-            list_obj['product_qty']= move.product_qty
-            list_obj['product_uom'] = move.product_uom.name
-            list_obj['product_code'] = move.product_id.code
+            if move.product_id:
+                pro_name = move.product_id.name_get()[0][1]
+                list_obj['product_id']= pro_name
+                list_obj['product_qty']= move.product_qty
+                list_obj['product_uom'] = move.product_uom.name
+                list_obj['product_code'] = move.product_id.code
 
             item_lists.append(list_obj)
 
