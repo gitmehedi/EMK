aa3186137fc26b86bb48153c201ba92a56551a82
gitmehedi <git.mehedi@gmail.com>
2022-06-15 16:08:15 +0600

    [ADD] Add new module ted cafe features

---
+++ hr_employee_ted_cafe_bills/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Employee Ted Cafe Bills',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'Employee Ted Cafe',
+    'summary': 'Calculates Employees Mobile Bills',
+    'description': """This module calculates the ted cafe bills 
+                    of the employee
+                    based on the condition'""",
+    'version': '1.0',
+    'depends': ['hr',
+                'hr_payroll',
+                'l10n_in_hr_payroll',
+                'report_layout',
+                'point_of_sale'
+                ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'security/ir_rule.xml',
+        'views/hr_ted_cafe_bill_view.xml',
+        # 'views/hr_emp_mb_bill_view.xml',
+        # 'views/hr_emp_mb_bill_limit_view.xml',
+        # 'report/gbs_hr_mobile_report.xml',
+        # 'report/gbs_hr_mobile_report_templates.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+}
---
+++ hr_employee_ted_cafe_bills/models/__init__.py
@@ -0,0 +1,2 @@
+from . import hr_ted_cafe_bill
+from . import inherited_hr_payslip
