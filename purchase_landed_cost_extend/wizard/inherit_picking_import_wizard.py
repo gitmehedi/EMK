19c03032917079566d402ab222e368fa5bc364a9
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2022-07-03 19:56:05 +0600

    Added Validation for sound stock and MRR

--- purchase_landed_cost_extend/wizard/inherit_picking_import_wizard.py
+++ purchase_landed_cost_extend/wizard/inherit_picking_import_wizard.py
@@ -11,10 +11,25 @@ class InheritedPickingImportWizard(models.TransientModel):
     def onchange_lc_id(self):
         for rec in self:
             if rec.lc_id:
+                #
+
                 purchase_cost_dist_obj = self.env['purchase.cost.distribution'].sudo().search(
                     [('lc_id', '=', rec.lc_id.id)])
                 purchase_cost_distribution_obj = self.env['purchase.cost.distribution'].browse(
                     self.env.context['active_id'])
+                if not purchase_cost_distribution_obj.operating_unit_id:
+                    raise UserError(
+                        "Select Operating Unit First!")
+
+                pickings = self.env['stock.picking'].search([('origin', '=', rec.lc_id.name), ('name', '=', 'Stock'),
+                                                             ('operating_unit_id', '=',
+                                                              purchase_cost_distribution_obj.operating_unit_id.id)])
+
+                for picking in pickings:
+                    if not picking.mrr_no:
+                        raise UserError(
+                            "%s - Sound Stock MRR not approved and printed!" % picking.name)
+
                 if purchase_cost_dist_obj:
                     for cost in purchase_cost_dist_obj:
                         if cost.id != purchase_cost_distribution_obj.id:
@@ -27,7 +42,7 @@ class InheritedPickingImportWizard(models.TransientModel):
     @api.depends('lc_id')
     def compute_lc_name(self):
         for rec in self:
-            if rec.lc_id :
+            if rec.lc_id:
                 rec.lc_name = rec.lc_id.name
 
     lc_name = fields.Char(compute='compute_lc_name')
@@ -58,5 +73,3 @@ class InheritedPickingImportWizard(models.TransientModel):
     #     if purchase_cost_distribution_obj:
     #         purchase_cost_distribution_obj.write({'lc_id': self.lc_id.id})
     #     super(InheritedPickingImportWizard, self).action_import_picking()
-
-
