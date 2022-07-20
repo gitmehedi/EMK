4683d55dbb3d09bcf6e848ca16919e3f8ba4e4dd
Kamrul Hasan <kamrul.devcs@gmail.com>
2022-07-20 19:45:28 +0600

    uncomment code

--- lc_sales_product_local/wizard/sales_invoice_wizard.py
+++ lc_sales_product_local/wizard/sales_invoice_wizard.py
@@ -43,18 +43,7 @@ class InvoiceExportWizard(models.TransientModel):
             })
             return {'type': 'ir.actions.act_window_close'}
 
-    # @api.onchange('shipment_id')
-    # def _onchange_shipment_id(self):
-    #     invoice_ids = self.env['account.invoice'].search(
-    #         ['&', '|', ('partner_id', '=', self.shipment_id.lc_id.second_party_applicant.id),
-    #          ('partner_id', 'in', self.shipment_id.lc_id.second_party_applicant.child_ids.ids), '&',
-    #          ('sale_type_id.sale_order_type', 'in', ['lc_sales', 'tt_sales', 'contract_sales']),
-    #          ('state', 'in', ['open', 'paid'])])
-    #
-    #     used_invoice_ids = self.env['purchase.shipment'].search([('invoice_id', 'in', invoice_ids.ids)]).mapped('invoice_id')
-    #     domain_id_list = list(set(invoice_ids.ids) - set(used_invoice_ids.ids))
-    #
-    #     return {'domain': {'invoice_id': [('id', 'in', domain_id_list)]}}
+
 
     @api.onchange('invoice_ids')
     @api.multi
