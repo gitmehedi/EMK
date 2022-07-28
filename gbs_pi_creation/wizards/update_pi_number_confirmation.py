0abc226f256a4e5befc781b403ec3872142c2c8d
Kamrul Hasan <khasancsit@gmail.com>
2022-07-28 15:02:02 +0600

    add pi must be unique

--- gbs_pi_creation/wizards/update_pi_number_confirmation.py
+++ gbs_pi_creation/wizards/update_pi_number_confirmation.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models, _
-
+from odoo.exceptions import UserError
 
 class UpdatePINumberConfirmation(models.TransientModel):
     _name = 'update.pi.number.confirmation'
@@ -9,5 +9,9 @@ class UpdatePINumberConfirmation(models.TransientModel):
 
     def action_update_pi_number(self):
         pi_id = self._context['pi_id']
+        check_proforma_obj = self.env['proforma.invoice'].search([('name', '=', self.pi_number)])
+        if check_proforma_obj:
+            raise UserError(_("PI number must be unique"))
+
         proforma_invoice_obj = self.env['proforma.invoice'].browse(pi_id)
         proforma_invoice_obj.write({'name': self.pi_number})
