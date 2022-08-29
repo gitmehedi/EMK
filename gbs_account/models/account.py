30cbede2819f205235cfeffc73dae1e7b5ad29b7
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2022-08-29 14:06:26 +0600

    Cannot change account type to view type if journal entry exists

--- gbs_account/models/account.py
+++ gbs_account/models/account.py
@@ -2,19 +2,23 @@
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError, ValidationError
 
+
 class AccountAccount(models.Model):
     _inherit = "account.account"
 
     open_journal_entry = fields.Boolean(string='Allow Open Journal Entry', default=True)
 
-    @api.onchange('user_type_id')
-    def onchange_user_type_id(self):
-        for rec in self:
-            if self._origin.id:
-                acc_move_line = self.env['account.move.line'].search([('account_id', '=', self._origin.id)])
-                view_type_account = self.env['account.account.type'].search([('type', '=', 'view')], limit=1)
+    @api.multi
+    def write(self, values):
+        if 'user_type_id' in values:
+            account_type = self.env['account.account.type'].browse(values['user_type_id'])
+            # if wants to convert to view type
+            if account_type.type == 'view':
+                # if move lines exist
+                acc_move_line = self.env['account.move.line'].search([('account_id', '=', self.id)])
+                if len(acc_move_line) > 0:
+                    raise UserError(_('''You cannot change account type to 'View Type' because this account has journal entries!'''))
 
-                if rec.user_type_id.id == view_type_account.id and acc_move_line:
-                    raise UserError(_("You cannot change account type because this account has journal entries!"))
-
+        res = super(AccountAccount, self).write(values)
+        return res
 
