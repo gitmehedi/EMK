ae2a4aa1d1ac861e650a5359622aae51af704ba6
gitmehedi <git.mehedi@gmail.com>
2022-08-30 18:52:39 +0600

    [ADD] Add new module for compensatory leave.

---
+++ hr_compensatory_leave/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'HR Compensatory Leave',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'HR Attendance',
+    'summary': 'This modules calculates compensatory leave depending on OT Requests',
+    'version': '1.0',
+    'depends': [
+        'hr_overtime_requisition',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        # 'security/ir_rule.xml',
+        'views/hr_compensatory_leave_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
