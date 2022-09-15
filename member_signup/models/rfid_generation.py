faa89367259af55bf70ffa32570c72d47c7ba5b4
gitmehedi <git.mehedi@gmail.com>
2022-09-15 16:59:16 +0600

    [FIX] Fix following issues on pos and member management - Fix permission issues on membership management - Add new menu on Point of Sale for push money and take money - Change permission on event management

--- member_signup/models/rfid_generation.py
+++ member_signup/models/rfid_generation.py
@@ -11,24 +11,42 @@ class RFIDGeneration(models.Model):
     _rec_name = 'membership_id'
     _order = 'date desc'
 
-    ref = fields.Text(string='Reference', readonly=True,track_visibility="onchange", states={'draft': [('readonly', False)]})
-    date = fields.Date(default=fields.Datetime.now(), string='Date', readonly=True,track_visibility="onchange",
+    ref = fields.Text(string='Reference', readonly=True, track_visibility="onchange",
+                      states={'draft': [('readonly', False)]})
+    date = fields.Date(default=fields.Datetime.now(), string='Date', readonly=True, track_visibility="onchange",
                        states={'draft': [('readonly', False)]})
-    membership_id = fields.Many2one('res.partner', string='Applicant/Member', required=True,track_visibility="onchange",
+    membership_id = fields.Many2one('res.partner', string='Applicant/Member', required=True,
+                                    track_visibility="onchange",
                                     domain=['&', ('is_applicant', '=', True), ('credit', '>', 0)],
                                     readonly=True, states={'draft': [('readonly', False)]})
-    state = fields.Selection([('draft', 'Draft'), ('generate', 'Generated')],track_visibility="onchange", default='draft', string='State')
+    state = fields.Selection([('draft', 'Draft'),
+                              ('issue', 'Issued'),
+                              ('generate', 'Generated'),
+                              ('handover', 'Hand Over')],
+                             track_visibility="onchange", default='draft', string='State')
 
     @api.multi
     def rfid_print(self):
-        self.ensure_one()
-        report = self.env['report'].get_action(self, 'member_signup.rfid_gen_tmpl')
-        return report
+        if self.state == 'issue':
+            self.ensure_one()
+            report = self.env['report'].get_action(self, 'member_signup.rfid_gen_tmpl')
+            return report
+
+    @api.multi
+    def act_issue(self):
+        if self.state == 'draft':
+            self.state = 'issue'
+
+    @api.multi
+    def act_generate(self):
+        if self.state == 'issue':
+            self.state = 'generate'
+
+    @api.multi
+    def act_handover(self):
+        if self.state == 'generate':
+            self.state = 'handover'
 
     @api.model
     def _needaction_domain_get(self):
         return [('state', '=', 'draft')]
-
-    @api.multi
-    def act_generate(self):
-        self.state = 'generate'
