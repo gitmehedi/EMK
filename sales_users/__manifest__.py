f6a56a8c9931404fac0f976ae16d5bc1821877d2
gitmehedi <git.mehedi@gmail.com>
2022-09-19 16:39:07 +0600

    [ADD] Add sales management access implementaion and menu permission

---
+++ sales_users/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Management Users',
+    'description': """ All types of users whom are related to activities of Sales Management""",
+    "author": "Genweb2 Limited",
+    "website": "http://www.genweb2.com",
+    'version': '1.0',
+    'category': 'Sales',
+    'depends': [
+        'sales_users',
+    ],
+    'data': [
+        'security/users.xml',
+    ],
+    'installable': True,
+}
+
