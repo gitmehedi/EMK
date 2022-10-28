24d226d132704994ca68627266b74d7f4a9cd8bf
gitmehedi <git.mehedi@gmail.com>
2022-10-28 17:55:16 +0600

    [FIX] Add new feature booking

---
+++ appointments/models/booking_timeslot.py
@@ -0,0 +1,72 @@
+from psycopg2 import IntegrityError
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.addons.opa_utility.helper.utility import Utility,Message
+
+
+class BookingTimeslot(models.Model):
+    _name = 'booking.timeslot'
+    _description = "Booking Timeslot"
+    _inherit = ['mail.thread', 'ir.needaction_mixin']
+    _order = "id desc"
+
+    name = fields.Char(string='Title', required=True, track_visibility="onchange")
+    contact_ids = fields.Many2many('appointment.contact','contact_topics_relation','topics_id','contact_id' ,string='Appointment Person', required=True)
+    active = fields.Boolean(string='Active', default=False, track_visibility='onchange')
+    pending = fields.Boolean(string='Pending', default=True, track_visibility='onchange')
+    state = fields.Selection([('draft', 'Draft'), ('approve', 'Approved'), ('reject', 'Rejected')], default='draft',
+                             string='Status', track_visibility='onchange', )
+
+    @api.constrains('name')
+    def _check_name(self):
+        name = self.search(
+            [('name', '=ilike', self.name.strip()), ('state', '!=', 'reject'), '|', ('active', '=', True),
+             ('active', '=', False)])
+        if len(name) > 1:
+            raise ValidationError(_(Message.UNIQUE_WARNING))
+
+    @api.onchange("name")
+    def onchange_strips(self):
+        if self.name:
+            self.name = self.name.strip()
+
+    @api.model
+    def _needaction_domain_get(self):
+        return [('state', 'in', ('draft', 'approve', 'reject'))]
+
+    @api.one
+    def act_draft(self):
+        if self.state == 'reject':
+            self.write({
+                'state': 'draft',
+                'pending': True,
+                'active': False,
+            })
+
+    @api.one
+    def act_approve(self):
+        if self.state == 'draft':
+            self.write({
+                'state': 'approve',
+                'pending': False,
+                'active': True,
+            })
+
+    @api.one
+    def act_reject(self):
+        if self.state == 'draft':
+            self.write({
+                'state': 'reject',
+                'pending': False,
+                'active': False,
+            })
+
+    @api.multi
+    def unlink(self):
+        for rec in self:
+            if rec.state in ('approve', 'reject'):
+                raise ValidationError(_(Message.UNLINK_WARNING))
+            try:
+                return super(BookingTimeslot, rec).unlink()
+            except IntegrityError:
+                raise ValidationError(_(Message.UNLINK_INT_WARNING))
