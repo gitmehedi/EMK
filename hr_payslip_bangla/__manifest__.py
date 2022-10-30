1bacf37c2d8501d86a9c21729e879c3ccac43030
aynulislam <itamxeed7@gmail.com>
2022-10-30 13:39:16 +0600

    [ADD] hr bangla payslip done

---
+++ hr_payslip_bangla/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Bangla Payslip',
+    'author': 'Genweb2 Limited',
+    'website': 'www.genweb2.com',
+    'category': 'HR Payroll',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'hr_payroll',
+        'report_xlsx',
+        ],
+    'data': [
+        'views/inherited_hr_payslip_view.xml',
+        'reports/payslip_bangla_report.xml',
+
+    ],
+
+
+    'installable': True,
+    'application': False,
+}
