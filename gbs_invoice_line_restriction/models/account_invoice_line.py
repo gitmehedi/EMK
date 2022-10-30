7f6c3725b5347a073d685fae31aef65939d473f9
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2022-10-30 19:19:16 +0600

    Revert "[FIX] module name renaming causing group not found"
    This reverts commit cb8ee3fa8c45081285f4982bef68bda5f749b172.

--- gbs_invoice_line_restriction/models/account_invoice_line.py
+++ gbs_invoice_line_restriction/models/account_invoice_line.py
@@ -11,10 +11,10 @@ class AccountInvoiceLine(models.Model):
             if rec.invoice_id.type == 'in_invoice':
                 if rec.invoice_id.from_po_form:
                     if self.env.user.has_group(
-                            'gbs_invoice_line_restriction.group_vendor_invoice_editor'):
+                            'gbs_invoices_using_picking_qty.group_vendor_invoice_editor'):
                         rec.can_edit_bill_line = True
                 else:
-                    if len(rec.purchase_id) <= 0 or self.env.user.has_group('gbs_invoice_line_restriction.group_vendor_invoice_editor'):
+                    if len(rec.purchase_id) <= 0 or self.env.user.has_group('gbs_invoices_using_picking_qty.group_vendor_invoice_editor'):
                         rec.can_edit_bill_line = True
             # if rec.invoice_id.type == 'in_invoice':
             #     rec.can_edit_bill_line = True
@@ -30,7 +30,7 @@ class AccountInvoiceLine(models.Model):
         for rec in self:
             rec.can_edit_invoice_line = False
             if rec.invoice_id.type == 'out_invoice':
-                if self.env.user.has_group('gbs_invoice_line_restriction.group_customer_invoice_editor'):
+                if self.env.user.has_group('gbs_invoices_using_picking_qty.group_customer_invoice_editor'):
                     rec.can_edit_invoice_line = True
             elif rec.invoice_id.type == 'out_refund':
                 rec.can_edit_invoice_line = True
