074f9527abf83dfcedcc0707c2f63553edd7e346
Kamrul Hasan <khasancsit@gmail.com>
2022-11-08 11:48:21 +0600

    fix Partial delivery then SO cancel validation

--- gbs_sale_order_approval/wizards/sale_order_cancel_confirmation.py
+++ gbs_sale_order_approval/wizards/sale_order_cancel_confirmation.py
@@ -10,15 +10,12 @@ class SaleOrderCancelConfirmation(models.TransientModel):
     def action_yes(self):
         sale_order_id = self._context['sale_order_id']
         sale_order_obj = self.env['sale.order'].browse(sale_order_id)
-        dc_objs = self.env['stock.picking'].search([('origin', '=', sale_order_obj.name)])
-        do_objs = self.env['delivery.order'].search([('sale_order_id', '=', sale_order_obj.id)])
-        stock_move_objs = self.env['stock.move'].search([('picking_id', '=', dc_objs.id)])
 
         #************************* Check pre condition
+        dc_objs = self.env['stock.picking'].search([('origin', '=', sale_order_obj.name)])
         for dc_obj in dc_objs:
             if dc_obj.state == 'done':
                 raise UserError('You cannot cancel sale orders whose goods are already delivered!')
-
 
         # *************************  Execution
         for dc_obj in dc_objs:
@@ -28,9 +25,11 @@ class SaleOrderCancelConfirmation(models.TransientModel):
         for da_obj in da_objs:
             da_obj.write({'state': 'refused'})
 
+        do_objs = self.env['delivery.order'].search([('sale_order_id', '=', sale_order_obj.id)])
         for do_obj in do_objs:
             do_obj.write({'state': 'refused'})
 
+        stock_move_objs = self.env['stock.move'].search([('picking_id', '=', dc_objs.id)])
         for sm_obj in stock_move_objs:
             sm_obj.write({'state': 'cancel'})
 
