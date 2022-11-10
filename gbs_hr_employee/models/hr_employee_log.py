6383f3f64da5a5cf1e849183f4efbc7cd90c8d39
aynulislam <itamxeed7@gmail.com>
2022-11-10 15:02:06 +0600

    [ADD] Job position must be unique

--- gbs_hr_employee/models/hr_employee_log.py
+++ gbs_hr_employee/models/hr_employee_log.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.exceptions import UserError, ValidationError
 
 
 class HrEmployee(models.Model):
@@ -12,6 +13,12 @@ class HrJob(models.Model):
     _inherit = "hr.job"
 
     name = fields.Char(string='Job Title', required=True, index=True, translate=True, track_visibility='onchange')
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        name = self.env['hr.job'].search([('name', '=', self.name)])
+        if len(name) > 1:
+            raise ValidationError("Job Title must be unique!")
 
 
 class HrDepartment(models.Model):
