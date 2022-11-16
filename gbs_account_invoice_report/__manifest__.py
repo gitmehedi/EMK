1ddb24201471ead9ebb300152f4e7d96d41b512d
Kamrul Hasan <khasancsit@gmail.com>
2022-11-16 18:18:18 +0600

    add cron job for sales analysis report

--- gbs_account_invoice_report/__manifest__.py
+++ gbs_account_invoice_report/__manifest__.py
@@ -4,16 +4,17 @@
     'summary': 'Custom Report of Account Invoice',
     'sequence': 300,
     'description': """
-Invoicing & Payments
-====================
-The specific and easy-to-view report of account invoice.This is custom analytic report 
-    """,
+        Invoicing & Payments
+        ====================
+        The specific and easy-to-view report of account invoice.This is custom analytic report 
+            """,
     'category': 'Accounting',
     'website': 'www.genweb2.com',
     'author': 'Genweb2 Limited',
     'depends' : ['account','sale', 'gbs_product', 'gbs_samuda_sales_access', 'gbs_procure_n_commercial_access'],
     'data': [
         'security/ir.model.access.csv',
+        'security/ir_cron.xml',
         'report/account_invoice_report_view.xml',
     ],
     'installable': True,
