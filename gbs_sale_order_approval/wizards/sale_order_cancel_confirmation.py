58b427809e07fae663244a72e8b1152fd1183a51
Kamrul Hasan <khasancsit@gmail.com>
2023-01-02 17:26:39 +0600

    fix cancel dc to unreserve

--- gbs_sale_order_approval/wizards/sale_order_cancel_confirmation.py
+++ gbs_sale_order_approval/wizards/sale_order_cancel_confirmation.py
@@ -12,27 +12,29 @@ class SaleOrderCancelConfirmation(models.TransientModel):
         sale_order_obj = self.env['sale.order'].browse(sale_order_id)
 
         #************************* Check pre condition
-        dc_objs = self.env['stock.picking'].search([('origin', '=', sale_order_obj.name)])
-        for dc_obj in dc_objs:
-            if dc_obj.state == 'done':
-                raise UserError('You cannot cancel sale orders whose goods are already delivered!')
+        if sale_order_obj:
+            dc_objs = self.env['stock.picking'].search([('origin', '=', sale_order_obj.name)])
+            for dc_obj in dc_objs:
+                if dc_obj.state == 'done':
+                    raise UserError('You cannot cancel sale orders whose goods are already delivered!')
 
-        # *************************  Execution
-        for dc_obj in dc_objs:
-            dc_obj.write({'state': 'cancel'})
+            # *************************  Execution
+            for dc_obj in dc_objs:
+                dc_obj.do_unreserve()
+                dc_obj.write({'state': 'cancel'})
 
-        da_objs = self.env['delivery.authorization'].search([('sale_order_id', '=', sale_order_obj.id)])
-        for da_obj in da_objs:
-            da_obj.write({'state': 'refused'})
+                stock_move_objs = self.env['stock.move'].search([('picking_id', '=', dc_obj.id)])
+                for sm_obj in stock_move_objs:
+                    sm_obj.write({'state': 'cancel'})
 
-        do_objs = self.env['delivery.order'].search([('sale_order_id', '=', sale_order_obj.id)])
-        for do_obj in do_objs:
-            do_obj.write({'state': 'refused'})
 
-        if dc_objs:
-            stock_move_objs = self.env['stock.move'].search([('picking_id', '=', dc_objs.id)])
-            for sm_obj in stock_move_objs:
-                sm_obj.write({'state': 'cancel'})
+            da_objs = self.env['delivery.authorization'].search([('sale_order_id', '=', sale_order_obj.id)])
+            for da_obj in da_objs:
+                da_obj.write({'state': 'refused'})
+
+            do_objs = self.env['delivery.order'].search([('sale_order_id', '=', sale_order_obj.id)])
+            for do_obj in do_objs:
+                do_obj.write({'state': 'refused'})
 
         return sale_order_obj.write({'state': 'cancel'})
 
