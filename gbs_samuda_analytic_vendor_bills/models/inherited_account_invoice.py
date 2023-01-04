1ee4f5e67c0f4fbff584f0cd2c470aaf221442b4
IamShoaibAhmed <shoaib.cse.27@gmail.com>
2023-01-05 03:01:10 +0600

    [FIX] vendor bill purchase order not saving

--- gbs_samuda_analytic_vendor_bills/models/inherited_account_invoice.py
+++ gbs_samuda_analytic_vendor_bills/models/inherited_account_invoice.py
@@ -20,7 +20,8 @@ class InheritedAccountInvoice(models.Model):
                         total_tds = total_tds + line.amount
                 rec.tds_value = total_tds
 
-    tds_value = fields.Monetary(string='TDS & Other Adjustment', store=False, readonly=True, compute='calculate_tds_value')
+    tds_value = fields.Monetary(string='TDS & Other Adjustment', store=False, readonly=True,
+                                compute='calculate_tds_value')
 
     @api.depends('tax_line_ids')
     def calculate_vds_value(self):
@@ -39,15 +40,7 @@ class InheritedAccountInvoice(models.Model):
         """ Override parent's method to add lc analytic account on invoice line"""
         invoice_line = super(InheritedAccountInvoice, self)._prepare_invoice_line_from_po_line(line)
 
-        # if line.product_qty:
-        #     invoice_line.update({'quantity': line.product_qty})
-
         if self.type == 'in_invoice' and self.purchase_id:
-            # # vendor reference
-            # if self.purchase_id.cnf_quotation and self.purchase_id.shipment_id and self.purchase_id.shipment_id.cnf_id:
-            #     vendor_ref = self.purchase_id.shipment_id.cnf_id.name
-            #     self.update({'reference': vendor_ref})
-
             if self.purchase_id.region_type == 'foreign' or self.purchase_id.is_service_order:
                 if self.purchase_id.lc_ids:
                     analytic_account_id = self.purchase_id.lc_ids[0].analytic_account_id.id
@@ -61,9 +54,10 @@ class InheritedAccountInvoice(models.Model):
             if self.purchase_id.region_type == 'foreign':
                 account_conf_pool = self.env.user.company_id
                 if not account_conf_pool.lc_pad_account:
+
                     raise UserError(
                         _("LC Goods In Transit Account not set. Please contact your system administrator for "
                           "assistance."))
                 invoice_line.update({'account_id': account_conf_pool.lc_pad_account.id})  # update the dictionary
-
+       
         return invoice_line
