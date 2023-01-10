3821ebc25824b214564c52dfe891c1caec553bde
Kamrul Hasan <khasancsit@gmail.com>
2023-01-10 19:44:56 +0600

    [Modify] sales order volumn report

--- samuda_sales_report/reports/sale_under_over_approved_price_xlsx.py
+++ samuda_sales_report/reports/sale_under_over_approved_price_xlsx.py
@@ -6,16 +6,6 @@ from odoo.addons.report_xlsx.report.report_xlsx import ReportXlsx
 
 class SaleUnderOverApprovedPriceReportXLSX(ReportXlsx):
 
-    def _get_conversion_rate(self):
-        conversion_rate_dict = {}
-        currencies = self.env['res.currency'].search([('active', '=', True)])
-        for cr in currencies:
-            to_currency = self.env.user.company_id.currency_id
-            from_currency = cr.with_context(date=datetime.datetime.now())
-            conversion_rate_dict[cr.name] = to_currency.round(to_currency.rate / from_currency.rate)
-
-        return conversion_rate_dict
-
     def _get_query_where_clause(self, obj):
         where_str = """where so.state='done' and so.operating_unit_id=%s and so.date_order between '%s' and '%s'""" % (obj.operating_unit_id.id, obj.date_from, obj.date_to)
         #and so.partner_id=7030  and pp.product_tmpl_id=4 and sol.product_id=26'
@@ -63,12 +53,6 @@ class SaleUnderOverApprovedPriceReportXLSX(ReportXlsx):
                 sale_over_under_dict[row['product_id']]['over_count'] = 0
                 sale_over_under_dict[row['product_id']]['under_count'] = 0
 
-            # product_id = sale_over_under_dict.get('product_id')
-            # if row['sale_price'] < row['approved_price']:
-            #     sale_over_under_dict[row['product_id']]['under_count'] = int(product_id.get('under_count')) + 1
-            # else:
-            #     sale_over_under_dict[row['product_id']]['over_count'] = int(product_id.get('over_count')) + 1
-
         return sale_over_under_dict
 
     def generate_xlsx_report(self, workbook, data, obj):
@@ -107,7 +91,7 @@ class SaleUnderOverApprovedPriceReportXLSX(ReportXlsx):
         sheet.set_column(0, 0, 25)
         sheet.set_column(1, 1, 20)
         sheet.set_column(2, 2, 16)
-        sheet.set_column(3, 3, 16)
+        sheet.set_column(3, 3, 30)
         sheet.set_column(4, 4, 17)
         sheet.set_column(5, 5, 17)
 
@@ -118,7 +102,7 @@ class SaleUnderOverApprovedPriceReportXLSX(ReportXlsx):
         sheet.merge_range(1, 0, 1, last_col, self.env.user.company_id.street, address_format)
         sheet.merge_range(2, 0, 2, last_col, self.env.user.company_id.street2, address_format)
         sheet.merge_range(3, 0, 3, last_col, self.env.user.company_id.city + '-' + self.env.user.company_id.zip, address_format)
-        sheet.merge_range(4, 0, 4, last_col, "Delivery Report", name_format)
+        sheet.merge_range(4, 0, 4, last_col, "Sales Volume Report (under/over price)", name_format)
 
         row = 5
 
@@ -157,7 +141,8 @@ class SaleUnderOverApprovedPriceReportXLSX(ReportXlsx):
                 else:
                     over += 1
             sheet.merge_range(row, col, row, 2, value['product_name'], td_cell_left_bold)
-            sheet.merge_range(row, 3, row, 5, "-" + str(under) + "/" + str(over), td_cell_left_bold)
+            sheet.write(row, 3, "-" + str(under) + "/" + str(over), td_cell_center)
+            sheet.merge_range(row, 4, row, 5, "", td_cell_center)
             row += 1
             sub_total_sold_qty = 0
             for rec in value['order_lines']:
