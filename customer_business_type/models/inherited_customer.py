180afcaa345b303fa02c170612c951b0050a6653
aynulislam <itamxeed7@gmail.com>
2023-01-12 16:44:29 +0600

    [FIX] customer business type should not be deleted when customer business type set in others model

--- customer_business_type/models/inherited_customer.py
+++ customer_business_type/models/inherited_customer.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models, _
 class ResPartnerInherited(models.Model):
     _inherit = 'res.partner'
 
-    business_type = fields.Many2one('res.customer.type', string="Business Type")
+    business_type = fields.Many2one('res.customer.type', string="Business Type",ondelete='restrict')
